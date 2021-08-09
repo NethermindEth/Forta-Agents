fc9c4534ed5afec43d44a1fddec1aa26cca90e20
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-08-09 14:54:22 -0400

    test: Create for handleTransaction

---
+++ multi-gas-threshold-agent/src/index.spec.ts
@@ -0,0 +1,80 @@
+import { BlockEvent, EventType, Finding, FindingSeverity, FindingType, HandleBlock, Network, HandleTransaction, TransactionEvent } from "forta-agent"
+import agent, { MEDIUM_GAS_THRESHOLD, HIGH_GAS_THRESHOLD } from "."
+
+
+const createTxEvent  = ({ gasUsed, addresses, logs, blockNumber } : any) : TransactionEvent => {
+  const tx = { } as any;
+  const receipt = { gasUsed, logs } as any;
+  const block = { number: blockNumber } as any;
+  const eventAddresses = { ...addresses } as any;
+  return new TransactionEvent(EventType.BLOCK, Network.MAINNET, tx, receipt, [], eventAddresses, block);
+}
+
+
+describe("multi gas threshold agent", () => {
+  let handleTransaction: HandleTransaction;
+
+  beforeAll(() => {
+    handleTransaction = agent.handleTransaction;
+  })
+
+  describe("handleTransaction", () => {
+
+    it("Returns empty findings if gas used is below lowest threshold", async () => {
+      const txEvent: TransactionEvent = createTxEvent({ gasUsed: "500000" });
+      const findings: Finding[] = await handleTransaction(txEvent);
+      expect(findings).toStrictEqual([])
+    })
+
+    it("Returns finding with severity Medium if gas used is between 1000000 and 3000000", async () => {
+      const txEvent: TransactionEvent = createTxEvent({ gasUsed: MEDIUM_GAS_THRESHOLD });
+      const findings: Finding[] = await handleTransaction(txEvent);
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "High Gas Used",
+          description: `Gas Used: ${MEDIUM_GAS_THRESHOLD}`,
+          alertId: "NEDMFORTA-1",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Suspicious,
+        })
+      ]);
+    })
+
+    it("Returns finding with severity High if gas used is greater than or equal to 3000000", async () => {
+      const txEvent: TransactionEvent = createTxEvent({ gasUsed: HIGH_GAS_THRESHOLD});
+      const findings: Finding[] = await handleTransaction(txEvent);
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "High Gas Used",
+          description: `Gas Used: ${HIGH_GAS_THRESHOLD}`,
+          alertId: "NEDMFORTA-1",
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+        })
+      ]);
+    })
+
+    // it("returns a finding if balance is below threshold", async () => {
+    //   const balance = "400000000000000000"
+    //   mockWeb3.eth.getBalance.mockReset()
+    //   mockWeb3.eth.getBalance.mockReturnValueOnce(balance)
+
+    //   const findings = await handleBlock(blockEvent)
+
+    //   expect(mockWeb3.eth.getBalance).toHaveBeenCalledTimes(1)
+    //   expect(mockWeb3.eth.getBalance).toHaveBeenCalledWith(ACCOUNT)
+    //   expect(findings).toStrictEqual([
+    //     Finding.fromObject({
+    //       name: "Minimum Account Balance",
+    //       description: `Account balance (${balance}) below threshold (${MIN_BALANCE})`,
+    //       alertId: "FORTA-6",
+    //       severity: FindingSeverity.Info,
+    //       type: FindingType.Suspicious,
+    //       metadata: {
+    //         balance
+    //       }
+    //     })
+    //   ])
+    // })
+  })
+})
