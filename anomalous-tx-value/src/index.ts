5dcfec99f993e497765ded347ca62d9f3d72c946
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-08-10 21:04:56 +0300

    initialize agent

---
+++ anomalous-tx-value/src/index.ts
@@ -0,0 +1,46 @@
+import BigNumber from 'bignumber.js'
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType
+} from 'forta-agent'
+
+const DECIMALS = 10 ** 18
+const THRESHHOLD_TX_VALUE = 10 * 10 ** 18
+
+const handleTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = []
+
+  // create finding if gas used is higher than threshold
+  const gasUsed = new BigNumber(txEvent.gasUsed)
+  if (gasUsed.isGreaterThan('1000000')) {
+    findings.push(
+      Finding.fromObject({
+        name: 'High Gas Used',
+        description: `Gas Used: ${gasUsed}`,
+        alertId: 'FORTA-1',
+        severity: FindingSeverity.Medium,
+        type: FindingType.Suspicious
+      })
+    )
+  }
+
+  return findings
+}
+
+// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some block condition
+//   return findings;
+// }
+
+export default {
+  handleTransaction
+  // handleBlock
+}
