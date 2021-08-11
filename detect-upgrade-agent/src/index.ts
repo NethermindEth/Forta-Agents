6f64b605483c764e13fbfd923a38ce2d9fef638d
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-08-11 16:32:28 +0300

    remove unused codes

--- detect-upgrade-agent/src/index.ts
+++ detect-upgrade-agent/src/index.ts
@@ -12,28 +12,9 @@ import {
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = []
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-  }
-
   return findings
 }
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
-
 export default {
   handleTransaction,
-  // handleBlock
 }
