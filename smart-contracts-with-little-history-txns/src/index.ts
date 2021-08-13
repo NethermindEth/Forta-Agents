5ea0f9e128056ee354ab570b96eb2d62e810e431
Lázaro Raúl <riglesiasvera@gmail.com>
2021-08-13 12:11:32 -0400

    Initial Agent implementation using etherscan API

--- smart-contracts-with-little-history-txns/src/index.ts
+++ smart-contracts-with-little-history-txns/src/index.ts
@@ -1,39 +1,53 @@
-import BigNumber from 'bignumber.js'
 import { 
-  BlockEvent, 
   Finding, 
-  HandleBlock, 
   HandleTransaction, 
   TransactionEvent, 
   FindingSeverity, 
   FindingType 
-} from 'forta-agent'
+} from 'forta-agent';
+import axios from 'axios';
+
+const ETHERSCAN_API_TOKEN: string = "YourApiKeyToken";
+const ETHERSCAN_API_ENDPOINT: string = "https://api.etherscan.io/api?module=account&action=txlistinternal&sort=asc";
+const HISTORY_THRESHOLD: number = 10000;
+const TIMESTAMP_THRESHOLD: number = 5 * 30 * 24 * 60 * 60; // 5 months aprox
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+  if(txEvent.to === null) return [];
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
+  const address: string = txEvent.to;
+  const url: string = `${ETHERSCAN_API_ENDPOINT}&address=${address}&apiKey=${ETHERSCAN_API_TOKEN}`;
+  const { data } = await axios.get(url);
+  
+  if(data.result.length === 0) return [];
+  if(data.result[0].contractAddress !== address) return [];
+
+  const findings: Finding[] = [];
+  
+  if(data.result.length < HISTORY_THRESHOLD){
     findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
+      name: "Transaction to an smart contract with little history",
+      description: `Contract (${address}) has only ${data.result.length} internal transactions`,
+      alertId: "NETHERMIND-AGENTS-02",
+      type: FindingType.Suspicious,
       severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
+    }));
+  }
+  if(data.result[0].timestamp + TIMESTAMP_THRESHOLD >= txEvent.timestamp){
+    const date: Date = new Date(data.result[0].timestamp * 1000);
+
+    findings.push(Finding.fromObject({
+      name: "Transaction to an smart contract recently created",
+      description: `Contract created on ${date.toUTCString()}`,
+      alertId: "NETHERMIND-AGENTS-02",
+      type: FindingType.Suspicious,
+      severity: FindingSeverity.High,
+    }));
   }
 
-  return findings
-}
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+  return findings;
+};
 
 export default {
   handleTransaction,
-  // handleBlock
-}
+};
