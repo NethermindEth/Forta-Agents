52ca8f4e197d3f3a2581418ef47c1bf4d5cec09a
Lázaro Raúl <riglesiasvera@gmail.com>
2021-08-16 17:24:27 -0400

    Agent handler

--- contracts-developed-by-contracts/src/index.ts
+++ contracts-developed-by-contracts/src/index.ts
@@ -1,30 +1,43 @@
-import BigNumber from 'bignumber.js'
 import { 
   Finding, 
   HandleTransaction, 
   TransactionEvent, 
   FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+  FindingType, 
+  getJsonRpcUrl
+} from 'forta-agent';
+import { Trace } from 'forta-agent/dist/sdk/trace';
+import Web3 from 'web3';
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const provideHandleTransaction = (web3: any): HandleTransaction => {
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
+    for(let i = 0; i < txEvent.traces.length; ++i){
+      const trace: Trace = txEvent.traces[i];
+      if(trace.type === 'create'){
+        const from: string = trace.action.from;
+
+        const code: string = await web3.eth.getCode(from);
+        if(code !== '0x'){
+          const addr: string = trace.result.address;
+          findings.push(
+            Finding.fromObject({
+              name: "Contract developed by a contract",
+              description: `Contract (${from}) develop the new contract (${addr})`,
+              alertId: "NETHFORTA-07",
+              type: FindingType.Suspicious,
+              severity: FindingSeverity.High,
+            })
+          )
+        }
+      }
+    };
+
+    return findings;
   }
-
-  return findings
-}
+};
 
 export default {
-  handleTransaction,
-}
+  handleTransaction: provideHandleTransaction(new Web3(getJsonRpcUrl())),
+};
