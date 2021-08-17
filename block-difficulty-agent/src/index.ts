47f69e98140c4fb922e6f5bd4600da5a7fb648e2
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-08-17 13:30:35 -0400

    feat: Create agent

--- block-difficulty-agent/src/index.ts
+++ block-difficulty-agent/src/index.ts
@@ -1,39 +1,105 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import BigNumber from "bignumber.js";
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  getJsonRpcUrl,
+} from "forta-agent";
+import BlockDifficultyGetter from "./blockDifficultyGetter";
+import Web3 from "web3";
+import { mean, std, abs } from "mathjs";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const NUMBER_OF_BLOCKS_TO_CHECK = 50;
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
+const web3: Web3 = new Web3(getJsonRpcUrl());
+const blockDifficultyGetter: BlockDifficultyGetter = new BlockDifficultyGetter(
+  web3
+);
+
+const getDifficultyChangeForBlock = async (
+  blockNumber: number,
+  blockDifficultyGetter: BlockDifficultyGetter
+): Promise<number> => {
+  const prevBlockDifficulty: number = await blockDifficultyGetter.getDifficulty(
+    blockNumber - 1
+  );
+  const blockDifficulty: number = await blockDifficultyGetter.getDifficulty(
+    blockNumber
+  );
+  return blockDifficulty - prevBlockDifficulty;
+};
+
+const getDifficultyChangesFromBlockRange = async (
+  startBlock: number,
+  endBlock: number,
+  blockDifficultyGetter: BlockDifficultyGetter
+): Promise<number[]> => {
+  let currentBlock: number = startBlock;
+  const difficultyChanges: number[] = [];
+  while (currentBlock <= endBlock) {
+    difficultyChanges.push(
+      await getDifficultyChangeForBlock(currentBlock, blockDifficultyGetter)
+    );
+    currentBlock++;
   }
+  return difficultyChanges;
+};
 
-  return findings
+const isRelevant = (
+  difficultyChange: number,
+  difficultyChangeHistory: number[]
+): Boolean => {
+  const meanChange: number = mean(difficultyChangeHistory);
+  const stDeviationChange: number = std(difficultyChangeHistory);
+  const deviation: number = abs(meanChange - difficultyChange);
+  return deviation > stDeviationChange;
+};
+
+const getSeverity = (difficultyChange: number): FindingSeverity => {
+  if (difficultyChange >= 0) {
+    return FindingSeverity.Info;
+  }
+  return FindingSeverity.High;
 }
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+const provideHandleBlock = (
+  web3: Web3,
+  blockDifficultyGetter: BlockDifficultyGetter,
+  numberOfBlocksToCheck: number
+): HandleBlock => {
+  return async (blockEvent: BlockEvent) => {
+    const findings: Finding[] = [];
+
+    const currentBlock = blockEvent.blockNumber;
+    const startBlock = currentBlock - numberOfBlocksToCheck;
+    const difficultyChangesHistory = await getDifficultyChangesFromBlockRange(
+      startBlock,
+      currentBlock - 1,
+      blockDifficultyGetter
+    );
+
+    const actualBlockChange = await getDifficultyChangeForBlock(
+      currentBlock,
+      blockDifficultyGetter
+    );
+
+    if (isRelevant(actualBlockChange, difficultyChangesHistory)) {
+      findings.push(Finding.fromObject({
+        name: "Unusual Difficulty Change",
+        description: `Block number ${currentBlock} made an unusual difficulty change`,
+        alertId: "NETHFORTA-8",
+        severity: getSeverity(actualBlockChange),
+        type: FindingType.Suspicious
+      }));
+    }
+    return findings;
+  };
+};
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleBlock: provideHandleBlock(web3, blockDifficultyGetter, NUMBER_OF_BLOCKS_TO_CHECK),
+};
