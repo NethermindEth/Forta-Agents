9293f96f77b4badf431ee4d6920360f55d0ef8ed
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-08-23 17:17:21 +0300

    update addresses

--- MEV-Tracker/src/index.spec.ts
+++ MEV-Tracker/src/index.spec.ts
@@ -1,75 +1,73 @@
-import { 
-  Finding, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
   FindingType,
   EventType,
   Network,
-} from 'forta-agent';
-import axios from 'axios';
-import agent, { getAPIUrl } from './index';
-import { Transaction } from 'forta-agent/dist/sdk/transaction';
-import { Receipt } from 'forta-agent/dist/sdk/receipt';
-import { Block } from 'forta-agent/dist/sdk/block';
+} from 'forta-agent'
+import axios from 'axios'
+import agent, { getAPIUrl } from './index'
+import { Transaction } from 'forta-agent/dist/sdk/transaction'
+import { Receipt } from 'forta-agent/dist/sdk/receipt'
+import { Block } from 'forta-agent/dist/sdk/block'
 
-interface Addresses{
-  [key: string]: boolean,
+interface Addresses {
+  [key: string]: boolean
 }
 
 interface TxEventInfo {
-  hash: string,
-  to: string,
-  number: number,
-};
+  hash: string
+  to: string
+  number: number
+}
 
 // transaction in a bundle
 const txnInBundle = {
-  hash: "0x14e3ef169d0bb84683d0a9ba75f45d38526f3a199e97f94e6c2fe4287e260d2d",
-  to: "0xcC3938DCc005EacF98Fa53AD05BF861ca2fD485e",
-  bundle_type: "flashbots",
+  hash: '0x14e3ef169d0bb84683d0a9ba75f45d38526f3a199e97f94e6c2fe4287e260d2d',
+  to: '0xcC3938DCc005EacF98Fa53AD05BF861ca2fD485e',
+  bundle_type: 'flashbots',
   number: 13055778,
-};
+}
 
-const PROTOCOLS: string[] = ["0x1", "0x2", "0x3"];
+const PROTOCOLS: string[] = ['0x1', '0x2', '0x3']
 
-const createTxEvent  = (
-  { hash, to, number }: TxEventInfo, 
+const createTxEvent = (
+  { hash, to, number }: TxEventInfo,
   extra_addresses: Addresses = {}
-) : TransactionEvent => {
-  const tx: Transaction = { hash, to } as Transaction;
-  const receipt: Receipt = { } as Receipt;
-  const block: Block = { number } as Block;
+): TransactionEvent => {
+  const tx: Transaction = { hash, to } as Transaction
+  const receipt: Receipt = {} as Receipt
+  const block: Block = { number } as Block
   const addresses: Addresses = {
     [to]: true,
     ...extra_addresses,
   }
   return new TransactionEvent(
-    EventType.BLOCK, 
-    Network.MAINNET, 
-    tx, 
-    receipt, 
-    [], 
-    addresses, 
+    EventType.BLOCK,
+    Network.MAINNET,
+    tx,
+    receipt,
+    [],
+    addresses,
     block
-  );
-};
+  )
+}
 
 describe('MEV-tracker agent test suit', () => {
   describe('handleTransaction', () => {
     it('Should query the API and found the transaction', async () => {
-      const handleTransaction:HandleTransaction = agent.provideHandleTransaction(
-        axios.get,
-        [txnInBundle.to],
-      );
+      const handleTransaction: HandleTransaction =
+        agent.provideHandleTransaction(axios.get, [txnInBundle.to])
 
-      const txn: TransactionEvent = createTxEvent(txnInBundle);
-      const findings: Finding[] = await handleTransaction(txn);
+      const txn: TransactionEvent = createTxEvent(txnInBundle)
+      const findings: Finding[] = await handleTransaction(txn)
       expect(findings).toStrictEqual([
         Finding.fromObject({
-          name: "Protocol interaction inside a MEV bundle",
+          name: 'Protocol interaction inside a MEV bundle',
           description: `Protocol used (${txnInBundle.to})`,
-          alertId: "NETHERMIND-AGENTS-10",
+          alertId: 'NETHERMIND-AGENTS-10',
           type: FindingType.Suspicious,
           severity: FindingSeverity.Info,
           metadata: {
@@ -77,117 +75,122 @@ describe('MEV-tracker agent test suit', () => {
             hash: txnInBundle.hash,
           },
         }),
-      ]);
-    });
+      ])
+    })
 
     describe('Mocking the API', () => {
-      const mockGet = jest.fn();
-      const handleTransaction: HandleTransaction = agent.provideHandleTransaction(
-        mockGet,
-        PROTOCOLS,
-      );
+      const mockGet = jest.fn()
+      const handleTransaction: HandleTransaction =
+        agent.provideHandleTransaction(mockGet, PROTOCOLS)
 
       beforeEach(() => {
-        mockGet.mockClear();
-      });
+        mockGet.mockClear()
+      })
 
       it('Should report 0 findings if the block has no bundle', async () => {
         mockGet.mockReturnValueOnce({
-          data:{
+          data: {
             blocks: [],
-          }
-        });
+          },
+        })
         const txn: TransactionEvent = createTxEvent({
-          hash: "0xH1",
+          hash: '0xH1',
           number: 1,
-          to: "0x1",
-        });
-        const findings: Finding[] = await handleTransaction(txn);
-        expect(findings).toStrictEqual([]);
-        expect(mockGet).toHaveBeenCalledTimes(1);
-        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(1));
-      });
+          to: '0x1',
+        })
+        const findings: Finding[] = await handleTransaction(txn)
+        expect(findings).toStrictEqual([])
+        expect(mockGet).toHaveBeenCalledTimes(1)
+        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(1))
+      })
 
       it('Should report 0 findings if the transaction is not inside the bundle', async () => {
         mockGet.mockReturnValueOnce({
-          data:{
-            blocks: [{
-              transactions: [{
-                transaction_hash: "0xH1",
-              }],
-            }],
-          }
-        });
+          data: {
+            blocks: [
+              {
+                transactions: [
+                  {
+                    transaction_hash: '0xH1',
+                  },
+                ],
+              },
+            ],
+          },
+        })
         const txn: TransactionEvent = createTxEvent({
-          hash: "0xH2",
+          hash: '0xH2',
           number: 2,
-          to: "0x2",
-        });
-        const findings: Finding[] = await handleTransaction(txn);
-        expect(findings).toStrictEqual([]);
-        expect(mockGet).toHaveBeenCalledTimes(1);
-        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(2));
-      });
+          to: '0x2',
+        })
+        const findings: Finding[] = await handleTransaction(txn)
+        expect(findings).toStrictEqual([])
+        expect(mockGet).toHaveBeenCalledTimes(1)
+        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(2))
+      })
 
       it('Should ignore transaction without protocols of interest', async () => {
         const txn: TransactionEvent = createTxEvent({
-          hash: "0xH20",
+          hash: '0xH20',
           number: 20,
-          to: "0x20",
-        });
-        const findings: Finding[] = await handleTransaction(txn);
-        expect(findings).toStrictEqual([]);
-        expect(mockGet).toHaveBeenCalledTimes(0);
-      });
+          to: '0x20',
+        })
+        const findings: Finding[] = await handleTransaction(txn)
+        expect(findings).toStrictEqual([])
+        expect(mockGet).toHaveBeenCalledTimes(0)
+      })
 
       it('Should report all the protocols used inside the bundled transaction', async () => {
         mockGet.mockReturnValueOnce({
-          data:{
-            blocks: [{
-              transactions: [{
-                transaction_hash: "0xH3",
-                bundle_type: "nethermind-flasbots",
-              }],
-            }],
-          }
-        });
+          data: {
+            blocks: [
+              {
+                transactions: [
+                  {
+                    transaction_hash: '0xH3',
+                    bundle_type: 'nethermind-flasbots',
+                  },
+                ],
+              },
+            ],
+          },
+        })
         const txn: TransactionEvent = createTxEvent(
           {
-            hash: "0xH3",
+            hash: '0xH3',
             number: 3,
-            to: "0x3",
-          }, 
-          { "0x1": true },
-        );
-        const findings: Finding[] = await handleTransaction(txn);
+            to: '0x3',
+          },
+          { '0x1': true }
+        )
+        const findings: Finding[] = await handleTransaction(txn)
         expect(findings).toStrictEqual([
           Finding.fromObject({
-            name: "Protocol interaction inside a MEV bundle",
+            name: 'Protocol interaction inside a MEV bundle',
             description: `Protocol used (0x1)`,
-            alertId: "NETHERMIND-AGENTS-10",
+            alertId: 'NETHERMIND-AGENTS-10',
             type: FindingType.Suspicious,
             severity: FindingSeverity.Info,
             metadata: {
-              bundle_type: "nethermind-flasbots",
-              hash: "0xH3",
+              bundle_type: 'nethermind-flasbots',
+              hash: '0xH3',
             },
           }),
           Finding.fromObject({
-            name: "Protocol interaction inside a MEV bundle",
+            name: 'Protocol interaction inside a MEV bundle',
             description: `Protocol used (0x3)`,
-            alertId: "NETHERMIND-AGENTS-10",
+            alertId: 'NETHERMIND-AGENTS-10',
             type: FindingType.Suspicious,
             severity: FindingSeverity.Info,
             metadata: {
-              bundle_type: "nethermind-flasbots",
-              hash: "0xH3",
+              bundle_type: 'nethermind-flasbots',
+              hash: '0xH3',
             },
-          })
-        ]);
-        expect(mockGet).toHaveBeenCalledTimes(1);
-        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(3));
-      });
-    });
-  });
-});
-
+          }),
+        ])
+        expect(mockGet).toHaveBeenCalledTimes(1)
+        expect(mockGet).toHaveBeenCalledWith(getAPIUrl(3))
+      })
+    })
+  })
+})
--- MEV-Tracker/src/index.ts
+++ MEV-Tracker/src/index.ts
@@ -1,66 +1,68 @@
-import { 
-  Finding, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent';
-import axios from 'axios';
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from 'forta-agent'
+import axios from 'axios'
 
-const INTERESTING_PROTOCOLS: string[] = [];
-const API_ENDPOINT: string = "ttps://blocks.flashbots.net/v1/blocks?block_number=";
+const INTERESTING_PROTOCOLS: string[] = [
+  '0x11111112542d85b3ef69ae05771c2dccff4faa26'.toLowerCase(), // 1inch V3
+  '0x7a250d5630b4cf539739df2c5dacb4c659f2488d'.toLowerCase(), // Uniswap V2
+  '0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f'.toLowerCase(), // SushiSwap Router
+]
+const API_ENDPOINT: string =
+  'https://blocks.flashbots.net/v1/blocks?block_number='
 
-export const getAPIUrl = (block: number) => `${API_ENDPOINT}${block}`;
+export const getAPIUrl = (block: number) => `${API_ENDPOINT}${block}`
 
 const provideHandleTransaction = (
-  getter: any, 
+  getter: any,
   protocols: string[]
 ): HandleTransaction => {
   return async (txEvent: TransactionEvent) => {
-    const findings: Finding[] = [];
+    const findings: Finding[] = []
 
     const protocolsInUse: string[] = protocols.filter(
       (p: string) => txEvent.addresses[p]
-    );
+    )
 
-    if(protocolsInUse.length === 0) return findings;
+    if (protocolsInUse.length === 0) return findings
 
-    const { data } = await getter(getAPIUrl(txEvent.blockNumber));
+    const { data } = await getter(getAPIUrl(txEvent.blockNumber))
 
     // check if the block has a bundle
-    if(data.blocks.length === 0) return findings;
-
+    if (data.blocks.length === 0) return findings
     // check if the transaction is inside the bundle
     const currentTxn = data.blocks[0].transactions.filter(
       (txn: any) => txn.transaction_hash === txEvent.hash
-    );
-    if(currentTxn.length === 0) return findings;
-    const txn = currentTxn[0];
+    )
+    if (currentTxn.length === 0) return findings
+    const txn = currentTxn[0]
 
     // report findings
-    protocolsInUse.forEach(
-      (p: string) => {
-        findings.push(
-          Finding.fromObject({
-            name: "Protocol interaction inside a MEV bundle",
-            description: `Protocol used (${p})`,
-            alertId: "NETHERMIND-AGENTS-10",
-            type: FindingType.Suspicious,
-            severity: FindingSeverity.Info,
-            metadata: {
-              bundle_type: txn.bundle_type,
-              hash: txEvent.hash,
-            },
-          })
-        );
-      }
-    )
-  
-    return findings;
+    protocolsInUse.forEach((p: string) => {
+      findings.push(
+        Finding.fromObject({
+          name: 'Protocol interaction inside a MEV bundle',
+          description: `Protocol used (${p})`,
+          alertId: 'NETHERMIND-AGENTS-10',
+          type: FindingType.Suspicious,
+          severity: FindingSeverity.Info,
+          metadata: {
+            bundle_type: txn.bundle_type,
+            hash: txEvent.hash,
+          },
+        })
+      )
+    })
+
+    return findings
   }
 }
 
 export default {
   provideHandleTransaction,
   handleTransaction: provideHandleTransaction(axios.get, INTERESTING_PROTOCOLS),
-};
+}
