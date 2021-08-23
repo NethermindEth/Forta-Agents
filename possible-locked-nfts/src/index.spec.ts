c6edd2ae48988179733c1ce90c93096bcd0ce5ca
Lázaro Raúl <riglesiasvera@gmail.com>
2021-08-23 15:41:57 -0400

    Initial Tests

--- possible-locked-nfts/src/index.spec.ts
+++ possible-locked-nfts/src/index.spec.ts
@@ -1,17 +1,121 @@
 import {
   TransactionEvent,
-  FindingType,
-  FindingSeverity,
   Finding,
   HandleTransaction,
-  EventType,
-  Network
+  Trace,
 } from "forta-agent";
-import agent from ".";
+import agent, {
+  createFinding,
+  SIGHASH,
+} from ".";
+import { utils } from 'ethers';
 
-describe("high gas agent", () => {
+const EMPTY_METHOD: string = "0x00000000";
+const protocols: string[] = [
+  "0x1", "0x2", "0x3",
+];
+const addresses: string[] = [
+  "0x0f51bb10119727a7e5ea3538074fb341f56b09ad",
+  "0x00010000219ab540356cbb839cbe05303d7705fa",
+  "0x00000000219ab540356cbb839cbe05303d7705fb",
+];
+
+interface TraceInfo {
+  from: string,
+  to: string,
+  tokenId: number,
+  contract: string,
+  method: string,
+}
+
+const encodeUint256 = (uint256: number) => 
+  utils.defaultAbiCoder.encode(["uint256"], [uint256])[0];
+
+const encodeTransferFromInput = (trace:TraceInfo): string =>
+  trace.method + utils.defaultAbiCoder.encode(
+    ["address", "address", "uint256"], 
+    [trace.from, trace.to, trace.tokenId],
+  ).slice(2);
+
+const createTxEvent  = (data: TraceInfo[]) : TransactionEvent => {
+  const traces: Trace[] = data.map((traceInfo: TraceInfo) => {
+    return {
+      action: {
+        to: traceInfo.contract,
+        input: encodeTransferFromInput(traceInfo),
+      },
+    } as Trace;
+  });
+  const txn: TransactionEvent = { traces } as TransactionEvent;
+  return txn;
+};
+
+describe("Possible locked nfts agent test suit", () => {
+  const mockGetCode = jest.fn();
+  let handleTransaction: HandleTransaction = agent.provideHandleTransaction(
+    mockGetCode,
+    protocols,
+  );
+
+  beforeEach(() => {
+    mockGetCode.mockClear();
+  });
 
   describe("handleTransaction", () => {
-    
+    it("Should ignore txns to non-interesting protocols", async () => {
+      const txn: TransactionEvent = createTxEvent([{
+        contract: addresses[0], // non interesting protocol
+        from: addresses[1],
+        to: addresses[2],
+        tokenId: 0,
+        method: SIGHASH,
+      }]);
+      const findings: Finding[] = await handleTransaction(txn);
+      expect(findings).toStrictEqual([]);
+      expect(mockGetCode).toBeCalledTimes(0);
+    });
+
+    it("Should ignore txns not calling trasferFrom", async () => {
+      const txn: TransactionEvent = createTxEvent([{
+        contract: "0x1",
+        from: addresses[1],
+        to: addresses[2],
+        tokenId: 0,
+        method: EMPTY_METHOD, // non transferFrom Sighash
+      }]);
+      const findings: Finding[] = await handleTransaction(txn);
+      expect(findings).toStrictEqual([]);
+      expect(mockGetCode).toBeCalledTimes(0);
+    });
+
+    it("Should detect insecure calls to transferForm", async () => {
+      const txn: TransactionEvent = createTxEvent([
+        {
+          contract: "0x1",
+          from: addresses[1],
+          to: addresses[2],
+          tokenId: 10,
+          method: SIGHASH,
+        },
+        {
+          contract: "0x2",
+          from: addresses[2],
+          to: addresses[1],
+          tokenId: 5,
+          method: SIGHASH,
+        },
+      ]);
+      mockGetCode
+        .mockReturnValueOnce("0xCode1")
+        .mockReturnValueOnce("0xCode2");
+      const findings: Finding[] = await handleTransaction(txn);
+      expect(findings).toStrictEqual([
+        createFinding("0x1", addresses[2], 10),
+        createFinding("0x2", addresses[1], 5),
+      ]);
+      expect(mockGetCode).toBeCalledTimes(2);
+      expect(mockGetCode).nthCalledWith(1, addresses[2]);
+      expect(mockGetCode).nthCalledWith(2, addresses[1]);
+    });
   });
 });
--- possible-locked-nfts/src/index.ts
+++ possible-locked-nfts/src/index.ts
@@ -13,20 +13,21 @@ import { utils } from 'ethers';
 import Web3 from 'web3';
 
 const SIGNATURE: string = 'transferFrom(address,address,uint256)';
-const SIGHASH: string = utils.id(SIGNATURE).slice(0, 10);
+export const SIGHASH: string = utils.id(SIGNATURE).slice(0, 10);
 
 const INTERESTING_PROTOCOLS: string[] = [];
 
-const decodeInput = (input: string): any => {
-  return utils.defaultAbiCoder.decode(
-    ["address", "address", "uint256"], 
+const decodeInput = (input: string): any => 
+  utils.defaultAbiCoder.decode(
+    ["address from", "address to", "uint256 tokenId"], 
     addHexPrefix(input.slice(10))
   );
-};
 
-const getToFromInput = (input: string): any => decodeInput(input)[1];
-
-const createFinding = (protocol:string, to:string): Finding =>
+export const createFinding = (
+  protocol:string, 
+  to:string, 
+  tokenId:number
+): Finding =>
   Finding.fromObject({
     name: "Insecure transferFrom call",
     description: `transferFrom execution detected in Contract (${protocol})`,
@@ -35,6 +36,7 @@ const createFinding = (protocol:string, to:string): Finding =>
     severity: FindingSeverity.Info,
     metadata: {
       receiver: to,
+      tokenId: tokenId.toString(),
     },
   });
 
@@ -57,13 +59,14 @@ const provideHandleTransaction = (getCode: any, protocols: string[]): HandleTran
       if(action.input.slice(0, 10) !== SIGHASH) continue;
 
       // Check if the `to` parameter is a contract
-      const transferTarget: string = getToFromInput(action.input);
-      const code: string = await getCode(transferTarget);
+      const input: any = decodeInput(action.input);
+      const code: string = await getCode(input.to.toLowerCase());
       if(code !== "0x"){
         findings.push(
           createFinding(
-            action.to,
-            transferTarget,
+            action.to.toLowerCase(),
+            input.to.toLowerCase(),
+            input.tokenId,
           ),
         );
       }
