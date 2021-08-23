53757d0d599c506b59cb753185514d830ba07ee2
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-08-23 16:21:42 -0400

    feature: Add agent logic

--- detect-blacklisted-addresses/src/index.ts
+++ detect-blacklisted-addresses/src/index.ts
@@ -1,38 +1,48 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
-
+import BigNumber from "bignumber.js";
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from "forta-agent";
 
 const BLACKLISTED_ADDRESSES: string[] = [];
 
+export const createFinding = (addressesDetected: string[]): Finding => {
+  return Finding.fromObject({
+    name: "Chainkeeper",
+    description: "Detect transactions involving blacklisted addresses",
+    alertId: "NETHFORTA-18",
+    severity: FindingSeverity.Critical,
+    type: FindingType.Suspicious,
+    metadata: {
+      Addresses: JSON.stringify(addressesDetected),
+    },
+  });
+};
 
-const provideHandleTransaction = (blacklistedAddresses : string[]): HandleTransaction => {
+export const provideHandleTransaction = (
+  blacklistedAddresses: string[]
+): HandleTransaction => {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
-    const findings: Finding[] = []
+    const findings: Finding[] = [];
 
-    // create finding if gas used is higher than threshold
-    const gasUsed = new BigNumber(txEvent.gasUsed)
-    if (gasUsed.isGreaterThan("1000000")) {
-      findings.push(Finding.fromObject({
-        name: "High Gas Used",
-        description: `Gas Used: ${gasUsed}`,
-        alertId: "FORTA-1",
-        severity: FindingSeverity.Medium,
-        type: FindingType.Suspicious
-      }))
+    const blacklistedAddressesInvolved = blacklistedAddresses.filter(
+      (addr: string) => txEvent.addresses[addr]
+    );
+
+    if (blacklistedAddressesInvolved.length === 0) {
+      return findings;
     }
 
-    return findings
+    findings.push(createFinding(blacklistedAddressesInvolved));
+    return findings;
   };
-}
+};
 
 export default {
   handleTransaction: provideHandleTransaction(BLACKLISTED_ADDRESSES),
-}
+};
