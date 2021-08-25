8a25b8c130496a7c5692585aff2607d6e65cdec9
Lázaro Raúl <riglesiasvera@gmail.com>
2021-08-25 13:04:50 -0400

    Add missing types in tests

--- gnosis-safe-admin-changes/src/index.spec.ts
+++ gnosis-safe-admin-changes/src/index.spec.ts
@@ -6,6 +6,8 @@ import {
   TransactionEvent,
   Log,
   Receipt,
+  Transaction,
+  Block,
 } from "forta-agent";
 import agent, { EVENTS } from ".";
 import { Event } from "./event.data";
@@ -29,27 +31,23 @@ const toHex = (param: Param): string =>
   utils.defaultAbiCoder.encode([getType(param)], [param]);
 
 const buildLogForGnosisSafeEvent = (signature:string, param: Param) : Log => {
-    const topics : string[] = [
-        utils.id(signature),
-    ];
     return {
-        topics: topics,
+        topics: [utils.id(signature)],
         data: toHex(param),
     } as Log;
 };
 
 const createTxEvent = (logs : Log[]): TransactionEvent => {
-  const tx = { } as any;
-  const receipt = { logs } as Receipt;
-  const block = { } as any;
-  const eventAddresses = { } as any;
+  const tx: Transaction = { } as Transaction;
+  const receipt: Receipt = { logs } as Receipt;
+  const block: Block = { } as Block;
   return new TransactionEvent(
     EventType.BLOCK,
     Network.MAINNET,
     tx,
     receipt,
     [],
-    eventAddresses,
+    {},
     block
   );
 };
