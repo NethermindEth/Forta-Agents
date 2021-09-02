46ab08d5e8bd572fb6843051e307af4adda856d9
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-02 11:58:45 -0400

    feat: Add agent logic

--- yfinance-too-much-time-without-harvest/src/index.ts
+++ yfinance-too-much-time-without-harvest/src/index.ts
@@ -1,13 +1,21 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import BigNumber from "bignumber.js";
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  FindingSeverity,
+  FindingType,
+  getJsonRpcUrl,
+  Block,
+} from "forta-agent";
+import Web3 from "web3";
+import {
+  decodeReturnStrategies,
+  createTxDataStrategiesCall,
+  createTxDataVaultCall,
+  createTxDataMaxReportDelayCall,
+  StrategyParams,
+} from "./abi.utils";
 
 const web3: Web3 = new Web3(getJsonRpcUrl());
 
@@ -15,40 +23,96 @@ export const createFinding = (strategyToReport: string): Finding => {
   return Finding.fromObject({
     name: "Yearn Finance no harvested strategies",
     alertId: "NETHFORTA-22",
-    description: "A yearn finance strategy have been too much time without trigerring harvest",
+    description:
+      "A yearn finance strategy have been too much time without trigerring harvest",
     severity: FindingSeverity.Info,
     type: FindingType.Suspicious,
     metadata: {
-      Strategy: strategyToReport
-    }
-  })
-}
+      Strategy: strategyToReport,
+    },
+  });
+};
 
-const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-  const findings: Finding[] = []
+const getVaultAddress = async (
+  strategyAddress: string,
+  web3: Web3
+): Promise<string> => {
+  const returnValueVaultCall: string = await web3.eth.call({
+    to: strategyAddress,
+    data: createTxDataVaultCall(),
+  });
+  return web3.eth.abi.decodeParameter("address", returnValueVaultCall) as any;
+};
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
+const getStrategyParams = async (
+  vaultAddress: string,
+  strategyAddress: string,
+  web3: Web3
+): Promise<StrategyParams> => {
+  const returnValueStrategies: string = await web3.eth.call({
+    to: vaultAddress,
+    data: createTxDataStrategiesCall(strategyAddress),
+  });
+  return decodeReturnStrategies(returnValueStrategies);
+};
+
+const getMaxReportDelay = async (
+  strategyAddress: string,
+  web3: Web3
+): Promise<bigint> => {
+  const returnValueMaxReportDelay = await web3.eth.call({
+    to: strategyAddress,
+    data: createTxDataMaxReportDelayCall(),
+  });
+  return BigInt(web3.eth.abi.decodeParameter(
+    "uint256",
+    returnValueMaxReportDelay
+  ) as any);
+};
+
+const checkStrategyInBlock = async (
+  strategyAddress: string,
+  block: Block,
+  web3: Web3
+): Promise<Finding[]> => {
+  const findings: Finding[] = [];
+
+  const vaultAddress: string = await getVaultAddress(strategyAddress, web3);
+  const strategyParams: StrategyParams = await getStrategyParams(
+    vaultAddress,
+    strategyAddress,
+    web3
+  );
+
+  const lastReport: bigint = BigInt(strategyParams.lastReport);
+  const maxReportDelay: bigint = await getMaxReportDelay(strategyAddress, web3);
+  const actualTimestamp: bigint = BigInt(block.timestamp);
+
+  if (actualTimestamp - lastReport > maxReportDelay) {
+    findings.push(createFinding(strategyAddress));
   }
 
-  return findings
-}
+  return findings;
+};
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+export const provideHandleBlock = (
+  web3: Web3,
+  strategiesToCheck: string[]
+): HandleBlock => {
+  return async (blockEvent: BlockEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+    for (let i = 0; i < strategiesToCheck.length; i++) {
+      const newFindings: Finding[] = await checkStrategyInBlock(
+        strategiesToCheck[i],
+        blockEvent.block,
+        web3
+      );
+      findings.concat(newFindings);
+    }
+    return findings;
+  };
+};
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleBlock: provideHandleBlock(web3, []),
+};
