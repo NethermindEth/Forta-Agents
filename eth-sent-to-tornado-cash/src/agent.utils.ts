b680c5c14f2ce160ffbbefe83d11a3edc21a8496
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-03 12:27:21 -0400

    feat: Create agent.utils

---
+++ eth-sent-to-tornado-cash/src/agent.utils.ts
@@ -0,0 +1,15 @@
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
+
+
+export const createFinding = (address: string): Finding => {
+    return Finding.fromObject({
+        alertId: "NETHFORTA-22",
+        description: "Too much eth sent into Tornado from the same address in a short time interval",
+        name: "Tornado Cash 1",
+        severity: FindingSeverity.High,
+        type: FindingType.Suspicious,
+        metadata: {
+            Address: address
+        }
+    });
+}
