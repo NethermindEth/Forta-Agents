9a7d177be7a0e9233c137a5fbde5bdb1ef765f39
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-03 13:23:10 -0400

    feat: Add tornado addresses to agent

--- eth-sent-to-tornado-cash/src/index.ts
+++ eth-sent-to-tornado-cash/src/index.ts
@@ -1,13 +1,12 @@
-import {
-  Finding,
-  HandleTransaction,
-  TransactionEvent,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
 import { createFinding, isInArray } from "./agent.utils";
 import AccountCashRecord, { createCashIn } from "./account.cash.record";
 
 const TORNADO_ADDRESSES: string[] = [
-
+  "0x12d66f87a04a9e220743712ce6d9bb1b5616b8fc",
+  "0x47ce0c6ed5b0ce3d3a51fdb1c52dc66a7c3c2936",
+  "0x910cbd523d972eb0a6f4cae4618ad62622b39dbf",
+  "0xa160cdab225685da1d56aa342ad8841c3b53f291",
 ];
 const VALUE_THRESHOLD: bigint = BigInt("100000000000000000000"); // 100 ETH
 const TIME_LIMIT: bigint = BigInt("86400"); // 1 day
@@ -17,8 +16,9 @@ export const provideHandleTransaction = (
   valueThreshold: bigint,
   timeLimit: bigint
 ): HandleTransaction => {
-  const createAccountCashRecord = (): AccountCashRecord => new AccountCashRecord(timeLimit);
-  const accountCashRecords: { [key: string]: AccountCashRecord } = { };
+  const createAccountCashRecord = (): AccountCashRecord =>
+    new AccountCashRecord(timeLimit);
+  const accountCashRecords: { [key: string]: AccountCashRecord } = {};
 
   return async (txEvent: TransactionEvent) => {
     const findings: Finding[] = [];
@@ -42,13 +42,15 @@ export const provideHandleTransaction = (
     if (totalValueOfSender > valueThreshold) {
       findings.push(createFinding(sender));
     }
-    
+
     return findings;
   };
 };
 
-
-
 export default {
-  handleTransaction: provideHandleTransaction(TORNADO_ADDRESSES, VALUE_THRESHOLD, TIME_LIMIT),
+  handleTransaction: provideHandleTransaction(
+    TORNADO_ADDRESSES,
+    VALUE_THRESHOLD,
+    TIME_LIMIT
+  ),
 };
