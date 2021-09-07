01c12bc4e3816e7bb91c45672b7e01182218b026
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-07 16:09:36 -0400

    feat: Add logic for strategy revoked agent

---
+++ yfinance-suite-module/src/strategy.revoked.ts
@@ -0,0 +1,30 @@
+import { Finding, FindingSeverity, FindingType, HandleTransaction, TransactionEvent } from "forta-agent";
+
+export const EVENT_SIGNATURE: string = "StrategyRevoked(address)";
+
+
+export const createFinding = (alertId: string, yearnVaultAddress: string): Finding => {
+    return Finding.fromObject({
+        name: "Yearn Finance Strategy Revoked",
+        description: "Detects Strategy Revoked event on the watched Yearn Vault",
+        alertId: alertId,
+        type: FindingType.Suspicious,
+        severity: FindingSeverity.Medium,
+        metadata: {
+            YearnVault: yearnVaultAddress
+        },
+    })
+}
+
+
+export default function provideEmergencyShutdownAgent(yearnVaultAddress: string, alertId: string): HandleTransaction {
+    return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+        const findings: Finding[] = [];
+
+        if (txEvent.filterEvent(EVENT_SIGNATURE, yearnVaultAddress).length > 0) {
+            findings.push(createFinding(alertId, yearnVaultAddress));
+        }
+
+        return findings;
+    }
+};
