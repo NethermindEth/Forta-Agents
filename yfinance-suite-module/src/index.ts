37b97b2b6770416273e4962d83e3cacbf938620a
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-07 16:15:33 -0400

    feat: Create index file

---
+++ yfinance-suite-module/src/index.ts
@@ -0,0 +1,13 @@
+import provideEmergencyShutdownAgent from "./emergency.shutdown";
+import provideStrategyMigratedAgent from "./strategy.migrated";
+import provideStrategyRevokedAgent from "./strategy.revoked";
+import provideUpdatedGovernanceAgent from "./updated.governance";
+import provideUpdatedGuardianAgent from "./updated.guardian";
+
+export default {
+    provideEmergencyShutdownAgent,
+    provideStrategyMigratedAgent,
+    provideStrategyRevokedAgent,
+    provideUpdatedGovernanceAgent,
+    provideUpdatedGuardianAgent,
+}
