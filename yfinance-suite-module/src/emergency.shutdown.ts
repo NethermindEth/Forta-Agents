257703e4c7192768139ad172d3060bd455ddd004
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-07 16:08:13 -0400

    feat: Add logic for emergency shutdown agent

---
+++ yfinance-suite-module/src/emergency.shutdown.ts
@@ -0,0 +1,30 @@
+import { Finding, FindingSeverity, FindingType, HandleTransaction, TransactionEvent } from "forta-agent";
+
+export const EVENT_SIGNATURE: string = "EmergencyShutdown(bool)";
+
+
+export const createFinding = (alertId: string, yearnVaultAddress: string): Finding => {
+    return Finding.fromObject({
+        name: "Yearn Finance Emergency Shutdown",
+        description: "Detects Emergency Shutdown event on the watched Yearn Vault",
+        alertId: alertId,
+        type: FindingType.Suspicious,
+        severity: FindingSeverity.Medium,
+        metadata: {
+            YearnVault: yearnVaultAddress
+        },
+    })
+}
+
+
+export default function provideEmergencyShutdownAgent(yearnVaultAddress: string, alertId: string): HandleTransaction {
+    return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+        const findings: Finding[] = [];
+
+        if (txEvent.filterEvent(EVENT_SIGNATURE, yearnVaultAddress).length > 0) {
+            findings.push(createFinding(alertId, yearnVaultAddress));
+        }
+
+        return findings;
+    }
+};
