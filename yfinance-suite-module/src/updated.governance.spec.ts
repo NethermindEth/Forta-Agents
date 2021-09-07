30aa36855bc75b179f3c4a1ab6b9267c725fe094
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-07 15:51:50 -0400

    tests: Create tests for updated governance agent

---
+++ yfinance-suite-module/src/updated.governance.spec.ts
@@ -0,0 +1,54 @@
+import { FindingType, FindingSeverity, Finding, HandleTransaction, TransactionEvent } from "forta-agent";
+import provideUpdatedGovernanceAgent from "./updated.governance";
+import { createTxEventWithEventLogged } from "./test.utils";
+
+
+const YEARN_VAULT_ADDRESS = "0x121212";
+const ALERT_ID = "testID";
+const EVENT_SIGNATURE = "UpdateGorvenance(address)";
+
+
+const createFinding = (): Finding => {
+    return Finding.fromObject({
+        name: "Yearn Finance Updated Governance",
+        description: "Detects Updated Governance event on the watched Yearn Vault",
+        alertId: ALERT_ID,
+        type: FindingType.Suspicious,
+        severity: FindingSeverity.Medium,
+        metadata: {
+            YearnVault: YEARN_VAULT_ADDRESS 
+        },
+    })
+};
+
+describe("Yearn Finance Updated Governance Tests", () => {
+  let handleTransaction: HandleTransaction;  
+
+  beforeAll(() => {
+    handleTransaction = provideUpdatedGovernanceAgent(YEARN_VAULT_ADDRESS, ALERT_ID);
+  })
+
+  it("should return empty findings if the expected event wasn't called", async () => {
+    const txEvent: TransactionEvent = createTxEventWithEventLogged("badEvent", "0x121212");
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("should return empty findings if the event is not related with the specified vault", async () => {
+    const txEvent: TransactionEvent = createTxEventWithEventLogged(EVENT_SIGNATURE, "0x0");
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("should return findings when the specified vault emit the expected event", async () => {
+    const txEvent: TransactionEvent = createTxEventWithEventLogged(EVENT_SIGNATURE, "0x121212");
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([createFinding()]);
+  });
+});
