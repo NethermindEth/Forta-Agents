2ce9eca14b2c29184c251315134e297f68abb779
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-12 21:46:29 -0400

    Initial agent tests

--- reentrancy-counter/src/agent.spec.ts
+++ reentrancy-counter/src/agent.spec.ts
@@ -1,48 +1,113 @@
 import {
-  FindingType,
-  FindingSeverity,
   Finding,
   HandleTransaction,
-  createTransactionEvent
-} from "forta-agent"
-import agent from "./agent"
+  createTransactionEvent,
+  Trace,
+  Transaction,
+  Receipt,
+  Block,
+  TransactionEvent,
+  TraceAction,
+} from "forta-agent";
+import agent from "./agent";
+import { createFinding, reentracyLevel } from "./agent.utils";
 
-describe("high gas agent", () => {
-  let handleTransaction: HandleTransaction
+const createTrace = (to: string, stack: number[]): Trace => {
+  return {
+    traceAddress: stack,
+    action: {
+      to: to,
+    } as TraceAction,
+  } as Trace;
+};
 
-  const createTxEventWithGasUsed = (gasUsed: string) => createTransactionEvent({
-    transaction: {} as any,
-    receipt: { gasUsed } as any,
-    block: {} as any,
-  })
+const createTxEvent = (traces: Trace[]) =>
+  createTransactionEvent({
+    transaction: {} as Transaction,
+    receipt: {} as Receipt,
+    block: {} as Block,
+    traces: traces,
+  });
 
-  beforeAll(() => {
-    handleTransaction = agent.handleTransaction
-  })
+
+describe("Reentrancy counter agent tests suit", () => {
+  const handleTransaction: HandleTransaction = agent.handleTransaction;
 
   describe("handleTransaction", () => {
-    it("returns empty findings if gas used is below threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1")
+    it("Should return empty findinds if no traces provided", async () => {
+      const tx: TransactionEvent = createTxEvent([]);
+      const findings: Finding[] = await handleTransaction(tx);
+      expect(findings).toStrictEqual([]);
+    });
 
-      const findings = await handleTransaction(txEvent)
+    it("Should return empty findinds if no repetition detected", async () => {
+      const tx: TransactionEvent = createTxEvent([
+        createTrace("0x0", []),
+        createTrace("0x1", [0]),
+        createTrace("0x2", [0, 0]),
+        createTrace("0x3", [0, 0, 0]),
+      ]);
+      const findings: Finding[] = await handleTransaction(tx);
+      expect(findings).toStrictEqual([]);
+    });
 
-      expect(findings).toStrictEqual([])
-    })
+    it("Should ignore non reentrant calls", async () => {
+      const tx: TransactionEvent = createTxEvent([
+        createTrace("0x0", []),
+        createTrace("0x1", [0]),
+        createTrace("0x1", [1]),
+        createTrace("0x2", [1, 0]),
+        createTrace("0x2", [1, 1]),
+        createTrace("0x2", [1, 2]),
+        createTrace("0x2", [1, 3]),
+        createTrace("0x1", [2]),
+        createTrace("0x1", [3]),
+        createTrace("0x1", [4]),
+        createTrace("0x1", [5]),
+        createTrace("0x1", [6]),
+        createTrace("0x1", [7]),
+      ]);
+      const findings: Finding[] = await handleTransaction(tx);
+      expect(findings).toStrictEqual([]);
+    });
 
-    it("returns a finding if gas used is above threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1000001")
+    it("Should detect different levels of reentrancy", async () => {
+      // 0x0, 0x1, 0x3, 0x5, 0x6 called less than 3 times
+      // 0x2 called 3 times
+      // 0x4 called 5 times
+      const tx: TransactionEvent = createTxEvent([
+        createTrace("0x0", []),
+        createTrace("0x1", [0]),
+        createTrace("0x2", [0, 0]),
+        createTrace("0x3", [0, 0, 0]),
+        createTrace("0x2", [0, 0, 0, 0]),
+        createTrace("0x3", [0, 0, 0, 0, 0]),
+        createTrace("0x2", [0, 0, 0, 0, 0, 0]),
+        createTrace("0x4", [0, 1]),
+        createTrace("0x5", [0, 1, 0]),
+        createTrace("0x6", [0, 1, 0, 0]),
+        createTrace("0x4", [0, 1, 0, 0, 0]),
+        createTrace("0x5", [0, 1, 0, 0, 0, 0]),
+        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0]),
+        createTrace("0x6", [0, 1, 0, 0, 0, 0, 0, 0]),
+        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0, 0, 0]),
+        createTrace("0x1", [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),        
+      ]);
+      const [report0x1, severity0x1] = reentracyLevel(1);
+      const [report0x2, severity0x2] = reentracyLevel(3);
+      const [report0x4, severity0x4] = reentracyLevel(5);
+      const expected: Finding[] = [];
+      if(report0x1) 
+        expected.push(createFinding("0x1", 1, severity0x1))
+      if(report0x2) 
+        expected.push(createFinding("0x2", 3, severity0x2))
+      if(report0x4) 
+        expected.push(createFinding("0x4", 5, severity0x4))
 
-      const findings = await handleTransaction(txEvent)
-
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Gas Used",
-          description: `Gas Used: ${txEvent.gasUsed}`,
-          alertId: "FORTA-1",
-          type: FindingType.Suspicious,
-          severity: FindingSeverity.Medium
-        }),
-      ])
-    })
-  })
-})
+      const findings: Finding[] = await handleTransaction(tx);
+      expect(findings).toEqual(expect.arrayContaining(expected));
+      expect(findings.length).toEqual(expected.length);
+    });
+  });
+});
