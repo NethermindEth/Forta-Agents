4e53db825cd202ca579d490d1aaa14fac775b950
rachit2501 <rac.sri25@gmail.com>
2021-09-15 00:44:49 +0530

    curve dao

--- Curve-finance-suite/src/CurveDao-CreateLockEvent.ts
+++ Curve-finance-suite/src/CurveDao-CreateLockEvent.ts
@@ -1,1 +1,58 @@
-    
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from "forta-agent";
+
+import Web3 from "web3";
+import abi from "./curve-dao-abi";
+
+// @ts-ignore
+import abiDecoder from "abi-decoder";
+abiDecoder.addABI(abi);
+
+export const web3 = new Web3();
+
+export const createLock = {
+  name: "create_lock",
+  outputs: [],
+  inputs: [
+    { type: "uint256", name: "_value" },
+    { type: "uint256", name: "_unlock_time" },
+  ],
+  stateMutability: "nonpayable",
+  type: "function",
+  gas: 74281465,
+};
+
+const createFinding = (alertId: string): Finding => {
+  return Finding.fromObject({
+    name: "Create Lock Event called",
+    description: "Create Lock Event funciton called on pool",
+    alertId: alertId,
+    severity: FindingSeverity.Low,
+    type: FindingType.Suspicious,
+  });
+};
+
+export default function providecreateLockAgent(
+  alertID: string,
+  address: string
+): HandleTransaction {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+
+    if (txEvent.addresses[address] == false) return findings;
+
+    const data = abiDecoder.decodeMethod(txEvent.transaction.data);
+    if (!data) return findings;
+
+    if (data.name === "create_lock") {
+      findings.push(createFinding(alertID));
+    }
+
+    return findings;
+  };
+}
