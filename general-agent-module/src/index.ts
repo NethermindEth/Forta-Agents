800cc2ea701e254044ece1457933e0f28516e3d9
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-14 14:07:30 -0400

    feat: Add FindingGenerator to exported features

--- general-agent-module/src/index.ts
+++ general-agent-module/src/index.ts
@@ -2,10 +2,12 @@ import provideERC20TransferAgent from "./erc20.transfers";
 import provideETHTransferAgent from "./eth.transfers";
 import provideFunctionCallsDetectorAgent from "./function.calls";
 import provideEventCheckerHandler from "./events.checker";
+import { FindingGenerator } from "./utils";
 
 export {
   provideERC20TransferAgent,
   provideETHTransferAgent,
   provideFunctionCallsDetectorAgent,
   provideEventCheckerHandler,
+  FindingGenerator,
 };
