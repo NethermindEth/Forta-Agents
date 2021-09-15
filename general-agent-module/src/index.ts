2d96af3bdb5c5b2e00e9ddcfce48f470eb28ec79
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-15 11:00:19 -0400

    feat: Add utils function and class to module export

--- general-agent-module/src/index.ts
+++ general-agent-module/src/index.ts
@@ -3,11 +3,14 @@ import provideETHTransferAgent from "./eth.transfers";
 import provideFunctionCallsDetectorAgent from "./function.calls";
 import provideEventCheckerHandler from "./events.checker";
 import { FindingGenerator } from "./utils";
+import { TestTransactionEvent, createAddress } from "./tests.utils";
 
 export {
   provideERC20TransferAgent,
   provideETHTransferAgent,
   provideFunctionCallsDetectorAgent,
   provideEventCheckerHandler,
+  createAddress,
   FindingGenerator,
+  TestTransactionEvent,
 };
