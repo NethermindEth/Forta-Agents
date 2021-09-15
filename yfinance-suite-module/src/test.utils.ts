9c0bb48c772ce45028fefd7885e5bcfc65d0604b
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-15 14:59:32 -0400

    feat: Create some static functions for web3 utilities

--- yfinance-suite-module/src/test.utils.ts
+++ yfinance-suite-module/src/test.utils.ts
@@ -1,5 +1,7 @@
 import { TransactionEvent, Log, Network, EventType } from "forta-agent";
 import { createTransactionEvent, keccak256 } from "forta-agent/dist/sdk/utils";
+import Web3 from "web3";
+
 
 export const createTxEventWithEventLogged = (eventSignature: string, address: string): TransactionEvent => {
   const log: Log = {
@@ -17,3 +19,9 @@ export const createTxEventWithEventLogged = (eventSignature: string, address: st
     } as any,
   });
 };
+
+export const toWei = Web3.utils.toWei;
+
+export const encodeParameter = (type: string, value: string): string => {
+  return new Web3().eth.abi.encodeParameter(type, value);
+}
