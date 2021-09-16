de03d2a3c1570ec5855ed9168eb83cd9ac3d35f5
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-16 14:18:57 -0400

    feat: YFI governance changes agent handler

--- YFI-governance-changes/src/agent.ts
+++ YFI-governance-changes/src/agent.ts
@@ -7,11 +7,38 @@ import {
 } from 'forta-agent';
 import {
   provideFunctionCallsDetectorAgent,
+  FindingGenerator,
 } from 'general-agents-module';
+import Web3 from 'web3';
+import { AbiCoder } from 'web3-eth-abi';
+
+export const YFI_GOVERNANCE_SIGNATURE: string = "setGovernance(address)";
+export const YFI_ADDRESS: string = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e";
+const abi: AbiCoder = new Web3().eth.abi;
+
+const createFinding: FindingGenerator = (metadata: { [key: string]: any } | undefined): Finding => {
+  const input: string = metadata?.input;
+  const addr: string = abi.decodeParameter(['address'], input.slice(10))[0];
+  return Finding.fromObject({
+    name: "YFI Governance address changed",
+    description: `New owner is (${addr})`,
+    alertId: "NETHFORTA-27",
+    severity: FindingSeverity.High,
+    type: FindingType.Suspicious,
+    metadata: {
+      previousOwner: metadata?.from,
+      newOwner: addr,
+    },
+  });
+};
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = [];
-
+  const txHandler = provideFunctionCallsDetectorAgent(
+    createFinding,
+    YFI_GOVERNANCE_SIGNATURE,
+    { to: YFI_ADDRESS },
+  );
+  const findings = await txHandler(txEvent);
   return findings;
 }
 
