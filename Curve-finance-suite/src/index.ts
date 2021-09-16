af14cb39dd165c87457830e8f562b3b105e8e30a
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-16 12:17:31 +0300

    concat agents

--- Curve-finance-suite/src/index.ts
+++ Curve-finance-suite/src/index.ts
@@ -4,12 +4,22 @@ import provideRemoveLiquidityImbalanceAgent from "./agents/removeImbalanceLiquid
 import provideUnkillAgent from "./agents/unkill";
 import provideAddPoolAgent from "./agents/addPool";
 import provideRemovePoolAgent from "./agents/removePool";
+import provideMetaPoolDeployment from "./agents/deployMetaPool";
+import providecreateLockAgent from "./agents/CurveDao-CreateLockEvent";
+import providesetKilledAgent from "./agents/Curve-dao-KillingGauge";
+import providesetRewardsAgent from "./agents/Curve-Gauge-SetRewards";
+import provideCommitNewAdminEvent from "./agents/registry_Ownership_Transfer";
 
 export {
   provideCrossAssetSwap,
   provideKillMeAgent,
+  providesetKilledAgent,
   provideRemoveLiquidityImbalanceAgent,
   provideUnkillAgent,
   provideAddPoolAgent,
   provideRemovePoolAgent,
+  provideMetaPoolDeployment,
+  providecreateLockAgent,
+  providesetRewardsAgent,
+  provideCommitNewAdminEvent,
 };
