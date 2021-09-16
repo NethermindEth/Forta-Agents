ad018ad72ab0e2e36cfade51473b73bc8ab65d1f
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-16 14:23:08 +0300

    fix: rename agents

--- Curve-finance-suite/src/index.ts
+++ Curve-finance-suite/src/index.ts
@@ -2,13 +2,13 @@ import provideCrossAssetSwap from "./agents/crossAssetSwap";
 import provideKillMeAgent from "./agents/killme";
 import provideRemoveLiquidityImbalanceAgent from "./agents/removeImbalanceLiquidity";
 import provideUnkillAgent from "./agents/unkill";
-import provideAddPoolAgent from "./agents/addPool";
-import provideRemovePoolAgent from "./agents/removePool";
+import provideAddPoolAgent from "./agents/registry_addPool";
+import provideRemovePoolAgent from "./agents/registry_RemovePool";
 import provideMetaPoolDeployment from "./agents/deployMetaPool";
 import providecreateLockAgent from "./agents/CurveDao-CreateLockEvent";
 import providesetKilledAgent from "./agents/Curve-dao-KillingGauge";
 import providesetRewardsAgent from "./agents/Curve-Gauge-SetRewards";
-import provideCommitNewAdminEvent from "./agents/registry_Ownership_Transfer";
+import provideCommitNewAdminEvent from "./agents/addressProvider_Ownership_Transfer";
 
 export {
   provideCrossAssetSwap,
