f2beef57edd392f2e7c4befe6175c770e58350a5
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-21 14:23:24 +0300

    feat: initialize agent

---
+++ Maker-Agents-Suit-Module/src/ESM_Join_Event_Agent.ts
@@ -0,0 +1,53 @@
+import {
+  FindingGenerator,
+  provideEventCheckerHandler,
+} from '@nethermindeth/general-agents-module';
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  Log,
+} from 'forta-agent';
+
+const MAKER_ESM_JOIN_EVENT_SIGNATURE = 'Join(address,uint256)';
+const MKR_DECIMALS = 18;
+
+const filterLog = (log: Log): boolean => {
+  const value = Number(BigInt(log.data)) / 10 ** MKR_DECIMALS;
+
+  if (value > 2) return true;
+  return false;
+};
+
+const createFindingGenerator = (alertID: string): FindingGenerator => {
+  return (metadata: { [key: string]: any } | undefined): Finding => {
+    return Finding.fromObject({
+      name: 'Maker ESM Contract Join Detect Agent',
+      description: 'Greater than 2 MKR is sent to ESM contract.',
+      alertId: alertID,
+      severity: FindingSeverity.Medium,
+      type: FindingType.Unknown,
+      metadata,
+    });
+  };
+};
+
+const provideESMJoinEventAgent = (
+  _alertID: string,
+  _contractAddress: string,
+): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const agentHandler = provideEventCheckerHandler(
+      createFindingGenerator(_alertID),
+      MAKER_ESM_JOIN_EVENT_SIGNATURE,
+      _contractAddress,
+    );
+
+    const findings: Finding[] = await agentHandler(txEvent);
+    return findings;
+  };
+};
+
+export default provideESMJoinEventAgent;
