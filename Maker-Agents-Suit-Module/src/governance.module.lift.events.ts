c59cf87c33f2ecf557f2434c4fffa61c39db8e26
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-21 07:50:14 -0400

    feat: Lift events listener provider

--- Maker-Agents-Suit-Module/src/governance.module.lift.events.ts
+++ Maker-Agents-Suit-Module/src/governance.module.lift.events.ts
@@ -3,16 +3,48 @@ import {
   HandleTransaction, 
   TransactionEvent, 
   FindingSeverity, 
-  FindingType 
+  FindingType, 
+  Log,
 } from 'forta-agent';
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = [];
+const LIFT_EVENT: string = "0x3c278bd500000000000000000000000000000000000000000000000000000000";
 
-
-  return findings;
+export interface Set{
+  [key: string]: boolean,
 };
 
-export default {
-  handleTransaction,
-};
+export const createFinding = (alertId: string, unknown: string, topic: number): Finding =>
+  Finding.fromObject({
+    name: "MakerDAO MCDM_ADM lift event detected",
+    description: `Topic #${topic} is an unknown address`,
+    alertId: alertId,
+    type: FindingType.Suspicious,
+    severity: FindingSeverity.High,
+    metadata: {
+      address: unknown,
+    },
+  });
+
+export const provideLiftEventsListener = (
+  alertId: string, 
+  targetAddress: string,
+  knownAddresses: Set,
+  topic: string = LIFT_EVENT,
+): HandleTransaction =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    if(!txEvent.addresses[targetAddress.toLowerCase()])
+      return findings;
+
+    txEvent.logs.forEach((log: Log) => {
+      if(log.topics[0] === topic){
+        if(!knownAddresses[log.topics[1]])
+          findings.push(createFinding(alertId, log.topics[1], 1));
+        if(!knownAddresses[log.topics[2]])
+        findings.push(createFinding(alertId, log.topics[2], 2));
+      }
+    })
+
+    return findings;
+  };
