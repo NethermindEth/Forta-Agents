c4d0cd726e5aff58f09f7b45cd2f1fd62380b51f
rachit2501 <rac.sri25@gmail.com>
2021-09-22 17:07:13 +0530

    internal tx support

--- Curve-finance-suite/src/tests/Curve-Dao-ClaimMany.spec.ts
+++ Curve-finance-suite/src/tests/Curve-Dao-ClaimMany.spec.ts
@@ -2,11 +2,19 @@ import {
   Finding,
   HandleTransaction,
   createTransactionEvent,
+  Trace,
+  TransactionEvent,
 } from "forta-agent";
 import provideclaimManyAgent, {
   web3,
   claimMany,
 } from "../agents/Curve-Dao-ClaimMany";
+
+interface TraceInfo {
+  from: string;
+  to: string;
+  input: string;
+}
 
 const ADDRESS = "0x1111";
 const PAYOUTADDRESS = "0x5C34E725CcA657F02C1D81fb16142F6F0067689b";
@@ -19,13 +27,32 @@ describe("Claim Many Agent", () => {
     handleTransaction = provideclaimManyAgent(ALERTID, ADDRESS);
   });
 
-  const createTxEvent = (signature: string, protocol: boolean) =>
-    createTransactionEvent({
+  const createTxEvent = (
+    data: TraceInfo[],
+    signature: string,
+    protocol: boolean
+  ): TransactionEvent => {
+    const traces: Trace[] = data.map((traceInfo: TraceInfo) => {
+      return {
+        action: {
+          from: traceInfo.from,
+          input: traceInfo.input,
+          to: traceInfo.to,
+        },
+      } as Trace;
+    });
+
+    const txn: TransactionEvent = {
       transaction: { data: signature } as any,
       addresses: { "0x1111": protocol },
       receipt: {} as any,
       block: {} as any,
-    });
+      traces,
+    } as any;
+
+    console.log(txn);
+    return txn;
+  };
 
   it("create event but from different protocol", async () => {
     const signature = web3.eth.abi.encodeFunctionCall(claimMany as any, [
@@ -33,7 +60,7 @@ describe("Claim Many Agent", () => {
         (_, i) => "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
       ) as any,
     ]);
-    const tx = createTxEvent(signature, false);
+    const tx = createTxEvent([], signature, false);
     const findings = await handleTransaction(tx);
     expect(findings).toStrictEqual([]);
   });
@@ -44,7 +71,7 @@ describe("Claim Many Agent", () => {
         (_, i) => "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
       ) as any,
     ]);
-    const tx = createTxEvent(signature, true);
+    const tx = createTxEvent([], signature, true);
     const findings = await handleTransaction(tx);
     expect(findings).toStrictEqual([
       Finding.fromObject({
