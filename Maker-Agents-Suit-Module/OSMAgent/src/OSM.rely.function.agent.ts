6319578c86b986e6c3da363b73db9f1118d05b90
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-23 13:08:17 +0300

    feat: create finding generator

--- Maker-Agents-Suit-Module/OSMAgent/src/OSM.rely.function.agent.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/OSM.rely.function.agent.ts
@@ -10,3 +10,16 @@ import {
   provideFunctionCallsDetectorAgent,
   FindingGenerator,
 } from '@nethermindeth/general-agents-module';
+
+export const RELY_FUNCTION_SIG = 'rely(address)';
+
+const createFindingGenerator = (alertID: string): FindingGenerator => {
+  return () =>
+    Finding.fromObject({
+      name: 'Maker OSM Contract RELY Function Agent',
+      description: 'RELY Function is called',
+      alertId: alertID,
+      severity: FindingSeverity.Medium,
+      type: FindingType.Unknown,
+    });
+};
