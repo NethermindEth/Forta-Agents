56f6bce220959264e6da3339ab5e75b5c00611b4
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-23 20:24:00 +0300

    feat: add agent.ts for ESM

---
+++ Maker-Agents-Suit-Module/ESMAgent/src/agent.ts
@@ -0,0 +1,4 @@
+import provideESMJoinEventAgent from './ESM_Join_Event_Agent';
+import provideESMFireEventAgent from './ESM_fire_event_agent';
+
+export { provideESMFireEventAgent, provideESMJoinEventAgent };
