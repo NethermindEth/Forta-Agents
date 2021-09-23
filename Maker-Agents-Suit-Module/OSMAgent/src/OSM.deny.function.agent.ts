39e4325f89b8f820ed162941d9f4745f00c6cb66
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-23 14:25:23 +0300

    feat: create finding generator

--- Maker-Agents-Suit-Module/OSMAgent/src/OSM.deny.function.agent.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/OSM.deny.function.agent.ts
@@ -13,3 +13,17 @@ import {
 import { OSM_CONTRACTS } from './utils';
 
 export const DENY_FUNCTION_SIG = 'deny(address)';
+
+const createFindingGenerator = (alertID: string): FindingGenerator => {
+  return (metadata: { [key: string]: any } | undefined) =>
+    Finding.fromObject({
+      name: 'Maker OSM DENY Function Agent',
+      description: 'DENY Function is called',
+      alertId: alertID,
+      severity: FindingSeverity.Medium,
+      type: FindingType.Unknown,
+      metadata: {
+        contract: metadata ? metadata.to : null,
+      },
+    });
+};
