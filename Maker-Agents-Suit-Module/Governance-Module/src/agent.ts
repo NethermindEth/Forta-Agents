adac010d17f550ff01fcc9eacbdd3f5b7b9e694d
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-24 11:47:10 -0400

    feat: Update the governance module agent

--- Maker-Agents-Suit-Module/Governance-Module/src/agent.ts
+++ Maker-Agents-Suit-Module/Governance-Module/src/agent.ts
@@ -4,16 +4,17 @@ import {
   HandleTransaction, 
 } from 'forta-agent'
 import Web3 from "web3";
-import { argsToSet, Set } from "./utils";  
+import { AddressVerifier } from "./utils";  
 import { provideHatChecker } from "./new.hat";
 import { provideLiftEventsListener } from "./lift.events";
 
 const MCD_ADM: string = "0x0a3f6849f78076aefaDf113F5BED87720274dDC0";
-const knownAddress: Set = argsToSet(); //TODO: Add the knowAddress
 const web3Call = new Web3(getJsonRpcUrl()).eth.call;
 
-const handleBlock: HandleBlock = provideHatChecker(web3Call, "MakerDAO-GM-1", MCD_ADM, knownAddress);
-const handleTransaction: HandleTransaction = provideLiftEventsListener("MakerDAO-GM-2", MCD_ADM, knownAddress);
+const isAddressKnown: AddressVerifier = async (address: string) => false; //TODO: Needs to be implemented after MakerDAO decides how to provide the addresses 
+
+const handleBlock: HandleBlock = provideHatChecker(web3Call, "MakerDAO-GM-1", MCD_ADM, isAddressKnown);
+const handleTransaction: HandleTransaction = provideLiftEventsListener("MakerDAO-GM-2", MCD_ADM, isAddressKnown);
 
 export default {
   handleTransaction,
