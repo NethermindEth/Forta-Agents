6a65a8d2f71104caff77f78f9a88f29e7d6cebf1
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-27 15:25:40 -0400

    refac: update tests findings

--- contracts-deployed-by-contracts/src/index.ts
+++ contracts-deployed-by-contracts/src/index.ts
@@ -9,6 +9,19 @@ import {
 import { Trace } from 'forta-agent/dist/sdk/trace';
 import Web3 from 'web3';
 
+export const createFinding = (from: string, addr: string): Finding => 
+  Finding.fromObject({
+    name: "Contract deployed by a contract",
+    description: `Contract (${from}) deployed the new contract (${addr})`,
+    alertId: "NETHFORTA-9",
+    type: FindingType.Suspicious,
+    severity: FindingSeverity.Info,
+    metadata:{
+      deployer: from,
+      contract: addr,
+    }
+  });
+
 const provideHandleTransaction = (getCode: any): HandleTransaction => {
   return async (txEvent: TransactionEvent) => {
     const findings: Finding[] = [];
@@ -22,15 +35,7 @@ const provideHandleTransaction = (getCode: any): HandleTransaction => {
         const code: string = await getCode(from);
         if(code !== '0x'){
           const addr: string = trace.result.address;
-          findings.push(
-            Finding.fromObject({
-              name: "Contract deployed by a contract",
-              description: `Contract (${from}) deployed the new contract (${addr})`,
-              alertId: "NETHFORTA-9",
-              type: FindingType.Suspicious,
-              severity: FindingSeverity.Info,
-            })
-          )
+          findings.push(createFinding(from, addr));
         }
       }
     };
