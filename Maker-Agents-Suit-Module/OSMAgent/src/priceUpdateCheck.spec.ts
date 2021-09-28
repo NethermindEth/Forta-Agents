f3a6160e021c0c8dd8bba34c18cd3e42582da251
rachit2501 <rac.sri25@gmail.com>
2021-09-28 22:59:25 +0530

    stage fixes

--- Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.spec.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.spec.ts
@@ -1,25 +1,61 @@
-import {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  HandleTransaction,
-  createTransactionEvent,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
 import agent from "./priceUpdateCheck";
+
+import { TestTransactionEvent } from "@nethermindeth/general-agents-module";
 
 describe("high gas agent", () => {
   let handleTransaction: HandleTransaction;
-
-  const createTxEventWithGasUsed = (gasUsed: string) =>
-    createTransactionEvent({
-      transaction: {} as any,
-      receipt: { gasUsed } as any,
-      block: {} as any,
-    });
 
   beforeAll(() => {
     handleTransaction = agent.handleTransaction;
   });
 
-  describe("handleTransaction", () => {});
+  it("No response if different protocol", async () => {
+    const txEvent: TransactionEvent = {
+      addresses: { "0x0000000000000000000000000000000000000000": true },
+      traces: [],
+    } as any;
+
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("Time < 10min, nothing returned, first time function call: sets status to true", async () => {
+    const txEvent = new TestTransactionEvent().addInvolvedAddress(
+      "0x2417c2762ec12f2696f62cfa5492953b9467dc81"
+    );
+    txEvent.block.timestamp = 1469021981; // 9 minutes hour - 19
+
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("getStatus after time has lasped > 10 min and the status is true", async () => {
+    const txEvent = new TestTransactionEvent().addInvolvedAddress(
+      "0x2417c2762ec12f2696f62cfa5492953b9467dc81"
+    );
+    txEvent.block.timestamp = 1469022581; // 19 minutes hour - 19
+
+    const findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("getStatus after time has lasped > 10 min and the status is false due to hour change, should return a critical shoutout", async () => {
+    const txEvent = new TestTransactionEvent().addInvolvedAddress(
+      "0x2417c2762ec12f2696f62cfa5492953b9467dc81"
+    );
+    txEvent.block.timestamp = 1469332581; // hour - 9 minutes - 26
+
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        alertId: "NETHFORTA-24",
+        description: "Poke() function not called within 10 minutes of the hour",
+        name: "Method not called within the first 10 minutes",
+        severity: 5,
+        type: 0,
+      }),
+    ]);
+  });
 });
--- Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
@@ -11,20 +11,17 @@ import TimeTracking from "./TimeTracking";
 import {
   provideFunctionCallsDetectorAgent,
   FindingGenerator,
-} from "general-agents-module";
-
-import Web3 from "web3";
-const abi = new Web3().eth.abi;
+} from "@nethermindeth/general-agents-module";
 
 const time = new TimeTracking();
 const address = "0x2417c2762ec12f2696f62cfa5492953b9467dc81";
-const functionSignature = "function poke()";
+export const functionSignature = "function poke()";
 
 const createFindingGenerator = (alertId: string): FindingGenerator => {
   return (metadata: { [key: string]: any } | undefined): Finding => {
     return Finding.fromObject({
       name: "Method not called within the first 10 minutes",
-      description: "Poke() functioon not called within 10 minutes of the hour",
+      description: "Poke() function not called within 10 minutes of the hour",
       alertId: "NETHFORTA-24",
       severity: FindingSeverity.Critical,
       type: FindingType.Unknown,
@@ -39,6 +36,8 @@ const handleTransaction: HandleTransaction = async (
 
   if (!txEvent.addresses[address]) return findings;
 
+  const timestamp = txEvent.block.timestamp;
+
   const agentHandler = provideFunctionCallsDetectorAgent(
     createFindingGenerator("Nethforta-24"),
     functionSignature
@@ -46,20 +45,21 @@ const handleTransaction: HandleTransaction = async (
 
   findings = [...findings, ...(await agentHandler(txEvent))];
 
-  time.initialUpdate();
+  time.initialUpdate(timestamp);
 
   // if time is less than 10 min when the tx is submitted.
-  if (time.getTime()) {
+  if (time.getTime(timestamp)) {
     time.setStatus(true);
     return findings;
   } else {
     // time > 10min
+
     if (!time.getStatus()) {
       findings.push(
         Finding.fromObject({
           name: "Method not called within the first 10 minutes",
           description:
-            "Poke() functioon not called within 10 minutes of the hour",
+            "Poke() function not called within 10 minutes of the hour",
           alertId: "NETHFORTA-24",
           severity: FindingSeverity.Critical,
           type: FindingType.Unknown,
