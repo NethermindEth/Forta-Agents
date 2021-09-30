e9a6c38bb93f53042586a2066e8f9d2dee533a66
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-29 23:41:32 -0400

    refac: Wrapper TransactionHandler inside a provider

--- Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
@@ -6,14 +6,11 @@ import {
   FindingType,
 } from "forta-agent";
 import { provideFunctionCallsDetectorAgent } from "@nethermindeth/general-agents-module";
-import TimeTracker from "./TimeTracker";
+import TimeTracker from "./timeTracker";
 
-const timeTracker = new TimeTracker();
 const address = "0x2417c2762ec12f2696f62cfa5492953b9467dc81";
-export const functionSignature = "poke()";
+const functionSignature = "poke()";
 
-let functionWasCalled: boolean = false;
-let findingReported: boolean = false;
 const functionCallDetector = provideFunctionCallsDetectorAgent(
   () => {
     return {} as Finding;
@@ -22,44 +19,49 @@ const functionCallDetector = provideFunctionCallsDetectorAgent(
   { to: address }
 );
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  let findings: Finding[] = [];
-  const timestamp = txEvent.block.timestamp;
+export const provideHandleTransaction = (): HandleTransaction => {
+  const timeTracker = new TimeTracker();
+  let functionWasCalled: boolean = false;
+  let findingReported: boolean = false;
 
-  if (timeTracker.isDifferentHour(timestamp)) {
-    functionWasCalled = false;
-    findingReported = false;
-  }
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    let findings: Finding[] = [];
+    const timestamp = txEvent.block.timestamp;
 
-  if (
-    (await functionCallDetector(txEvent)) !== [] &&
-    timeTracker.isInFirstTenMins(timestamp)
-  ) {
-    functionWasCalled = true;
-  }
+    if (timeTracker.isDifferentHour(timestamp)) {
+      functionWasCalled = false;
+      findingReported = false;
+    }
 
-  if (
-    !timeTracker.isInFirstTenMins(timestamp) &&
-    !functionWasCalled &&
-    !findingReported
-  ) {
-    findings.push(
-      Finding.fromObject({
-        name: "Method not called within the first 10 minutes",
-        description: "Poke() function not called within 10 minutes of the hour",
-        alertId: "MakerDAO-OSM-4",
-        severity: FindingSeverity.Critical,
-        type: FindingType.Unknown,
-      })
-    );
-  }
+    if (
+      (await functionCallDetector(txEvent)).length !== 0 &&
+      timeTracker.isInFirstTenMins(timestamp)
+    ) {
+      functionWasCalled = true;
+    }
 
-  timeTracker.updateHour(timestamp);
-  return findings;
+    if (
+      !timeTracker.isInFirstTenMins(timestamp) &&
+      !functionWasCalled &&
+      !findingReported
+    ) {
+      findings.push(
+        Finding.fromObject({
+          name: "Method not called within the first 10 minutes",
+          description:
+            "Poke() function not called within 10 minutes of the hour",
+          alertId: "MakerDAO-OSM-4",
+          severity: FindingSeverity.Critical,
+          type: FindingType.Unknown,
+        })
+      );
+    }
+
+    timeTracker.updateHour(timestamp);
+    return findings;
+  };
 };
 
 export default {
-  handleTransaction,
+  handleTransaction: provideHandleTransaction(),
 };
