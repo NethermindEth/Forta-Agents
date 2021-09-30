8d270c95939f084a2bbc7d325c9e7e2c84e93357
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-29 22:39:58 -0400

    refac: Refact priceUpdateCheck handler

--- Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/priceUpdateCheck.ts
@@ -1,4 +1,3 @@
-import BigNumber from "bignumber.js";
 import {
   Finding,
   HandleTransaction,
@@ -6,72 +5,58 @@ import {
   FindingSeverity,
   FindingType,
 } from "forta-agent";
+import { provideFunctionCallsDetectorAgent } from "@nethermindeth/general-agents-module";
+import TimeTracker from "./TimeTracker";
 
-import TimeTracking from "./TimeTracking";
-import {
-  provideFunctionCallsDetectorAgent,
-  FindingGenerator,
-} from "@nethermindeth/general-agents-module";
-
-const time = new TimeTracking();
+const timeTracker = new TimeTracker();
 const address = "0x2417c2762ec12f2696f62cfa5492953b9467dc81";
 export const functionSignature = "poke()";
 
-export const finding = Finding.fromObject({
-  name: "Method not called within the first 10 minutes",
-  description: "Poke() function not called within 10 minutes of the hour",
-  alertId: "MakerDAO-OSM-4",
-  severity: FindingSeverity.Critical,
-  type: FindingType.Unknown,
-});
-
-const createFindingGenerator = (alertId: string): FindingGenerator => {
-  return (metadata: { [key: string]: any } | undefined): Finding => {
-    return finding;
-  };
-};
+let functionWasCalled: boolean = false;
+let findingReported: boolean = false;
+const functionCallDetector = provideFunctionCallsDetectorAgent(
+  () => {
+    return {} as Finding;
+  },
+  functionSignature,
+  { to: address }
+);
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   let findings: Finding[] = [];
-
   const timestamp = txEvent.block.timestamp;
 
-  const newHour = time.isNewHour(timestamp);
-
-  if (newHour) {
-    time.hour = time.getHour(timestamp);
-    // when the  previous hour never got called
-    if (time.functionWasCalled === false) {
-      findings.push(finding as any);
-    } else {
-      time.functionWasCalled = false;
-    }
+  if (timeTracker.isDifferentHour(timestamp)) {
+    functionWasCalled = false;
+    findingReported = false;
   }
 
-  if (findings.length !== 0) return findings;
-
-  const agentHandler = provideFunctionCallsDetectorAgent(
-    createFindingGenerator("MakerDAO-OSM-4"),
-    functionSignature,
-    { to: address }
-  );
-
-  if ((await agentHandler(txEvent)) === [] && !txEvent.addresses[address])
-    return findings;
-
-  // if time is less than 10 min when the tx is submitted.
-  if (time.isInFirstTenMins(timestamp)) {
-    time.setFunctionCalledStatus(true);
-    return [];
-  } else {
-    // time > 10min and if function is already called do nothing, else raise a warning
-    if (!time.getFunctionCalledStatus()) {
-      findings.push(finding);
-    }
+  if (
+    (await functionCallDetector(txEvent)) !== [] &&
+    timeTracker.isInFirstTenMins(timestamp)
+  ) {
+    functionWasCalled = true;
   }
 
+  if (
+    !timeTracker.isInFirstTenMins(timestamp) &&
+    !functionWasCalled &&
+    !findingReported
+  ) {
+    findings.push(
+      Finding.fromObject({
+        name: "Method not called within the first 10 minutes",
+        description: "Poke() function not called within 10 minutes of the hour",
+        alertId: "MakerDAO-OSM-4",
+        severity: FindingSeverity.Critical,
+        type: FindingType.Unknown,
+      })
+    );
+  }
+
+  timeTracker.updateHour(timestamp);
   return findings;
 };
 
