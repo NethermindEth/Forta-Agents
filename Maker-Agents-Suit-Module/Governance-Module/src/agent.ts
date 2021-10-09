aa26e83ab83fba28d75da8f54563fa6ba455f29b
Lázaro Raúl <riglesiasvera@gmail.com>
2021-10-08 21:07:36 -0400

    feat: Update GM AddressVerifier function

--- Maker-Agents-Suit-Module/Governance-Module/src/agent.ts
+++ Maker-Agents-Suit-Module/Governance-Module/src/agent.ts
@@ -1,27 +1,39 @@
 import { 
+  BlockEvent,
+  Finding,
   getJsonRpcUrl,
   HandleBlock, 
   HandleTransaction, 
 } from 'forta-agent'
 import Web3 from "web3";
-import { AddressVerifier, generateAddressVerifier } from "./utils";  
+import { AddressVerifier } from "./utils";  
 import provideHatChecker from "./new.hat";
 import provideLiftEventsListener from "./lift.events";
-import knownAddresses from './known.addresses';
+import DeployedAddressManager from './deployed.address.manager';
 
+export const SPELL_DEPLOYER: string = "0xda0c0de01d90a5933692edf03c7ce946c7c50445";
 export const CHIEF_CONTRACT: string = "0x0a3f6849f78076aefaDf113F5BED87720274dDC0";
-const web3Call = new Web3(getJsonRpcUrl()).eth.call;
+const _web3 = new Web3(getJsonRpcUrl());
 
-//TODO: Needs to be implemented after MakerDAO decides how to provide the addresses 
-const isAddressKnown: AddressVerifier = generateAddressVerifier(...knownAddresses); // Beta Implementation
+const addressManager: DeployedAddressManager = 
+  new DeployedAddressManager(SPELL_DEPLOYER, _web3.eth.getTransactionCount);
+
+const isAddressKnown: AddressVerifier = 
+  async (addr: string): Promise<boolean> => 
+    addressManager.isDeployedAddress(addr);
 
 export const handleTransaction: HandleTransaction =
   provideLiftEventsListener("MakerDAO-GM-2", CHIEF_CONTRACT, isAddressKnown);
 
-export const handleBlock: HandleBlock =
-  provideHatChecker(web3Call, "MakerDAO-GM-1", CHIEF_CONTRACT, isAddressKnown);
+export function provideHandleBlock(): HandleBlock {
+  const handler: HandleBlock = provideHatChecker(_web3.eth.call, "MakerDAO-GM-1", CHIEF_CONTRACT, isAddressKnown);
+  return async (blockEvent: BlockEvent): Promise<Finding[]> => {
+    await addressManager.update(blockEvent.blockNumber);
+    return handler(blockEvent);
+  };
+};
 
 export default {
   handleTransaction,
-  handleBlock,
+  handleBlock: provideHandleBlock(),
 }
