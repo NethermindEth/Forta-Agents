90698e7f8edb5220dbb0932156d4a8f7bd500e90
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-10-18 11:42:24 +0300

    feat: clear agent files

--- Vesper-Agents-Suite/src/agent.ts
+++ Vesper-Agents-Suite/src/agent.ts
@@ -1,31 +1,21 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import BigNumber from 'bignumber.js';
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from 'forta-agent';
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const handleTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-  }
-
-  return findings
-}
+  return findings;
+};
 
 // const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
 //   const findings: Finding[] = [];
@@ -36,4 +26,4 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
 export default {
   handleTransaction,
   // handleBlock
-}
+};
