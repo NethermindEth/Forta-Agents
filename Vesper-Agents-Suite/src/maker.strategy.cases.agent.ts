318fa0e0a0e7202950de8a9e20954a27bf2d13e8
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-10-18 13:26:32 +0300

    feat: fetch vesper API

--- Vesper-Agents-Suite/src/maker.strategy.cases.agent.ts
+++ Vesper-Agents-Suite/src/maker.strategy.cases.agent.ts
@@ -5,13 +5,41 @@ import {
   FindingSeverity,
   FindingType
 } from "forta-agent";
-import fetch from "node-fetch";
+//@ts-ignore
+import axios from "axios";
+
+interface Strategy {
+  address: string;
+  tokens: [];
+  info: string;
+  weight: number;
+}
+
+interface Pools {
+  name: string;
+  contract: Object;
+  strategies: Array<Strategy>;
+  strategy: Object;
+  poolRewards: Object;
+  status: string;
+  stage: string;
+}
 
 export const provideMakerStrategyHandler = (): HandleBlock => {
   return async (blockEvent: BlockEvent) => {
     const findings: Finding[] = [];
+    const MakerStrategies: Strategy[] = [];
 
-    const strategies = await fetch("https://api.vesper.finance/dashboard");
+    const res = await axios("https://api.vesper.finance/dashboard");
+    const pools: Pools[] = res.data as Pools[];
+
+    pools.forEach((pool) => {
+      pool.strategies.map((strategy) => {
+        if (strategy.info.includes("Maker")) {
+          MakerStrategies.push(strategy);
+        }
+      });
+    });
 
     return findings;
   };
