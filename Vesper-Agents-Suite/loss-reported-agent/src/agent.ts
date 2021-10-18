93b9b7bae4e1b2a5ec79e5decdf7fbec55522c90
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-18 16:26:19 -0400

    feat: Add high level implementation of handleTransaction

--- Vesper-Agents-Suite/loss-reported-agent/src/agent.ts
+++ Vesper-Agents-Suite/loss-reported-agent/src/agent.ts
@@ -10,6 +10,27 @@ import {
 } from 'forta-agent'
 import { provideFunctionCallsDetectorHandler, FindingGenerator } from 'forta-agent-tools';
 
+  
+
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  
+  const v3Strategies: string[] = await getV3Strategies();
+  const reportLossHandlers: HandleTransaction[] = v3Strategies.map((strategy) => provideFunctionCallsDetectorHandler());
+  const reportEarningsHandlers: HandleTransaction[] = v3Strategies.map((strategy) => provideFunctionCallsDetectorHandler());
+
+  let findings: Finding[] = []
+
+  for (let reportLossHandler of reportLossHandlers) {
+    findings = findings.concat(await reportLossHandler(txEvent));
+  }
+
+  for (let reportEarningsHandler of reportEarningsHandlers ) {
+    findings = findings.concat(await reportEarningsHandler(txEvent));
+  }
+  
+  return findings
+}
 
 
 export default {
