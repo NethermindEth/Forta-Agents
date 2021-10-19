45657b51d0c43036fe3a5ddb0428bcd30c54f0c2
rachit2501 <rac.sri25@gmail.com>
2021-10-20 02:25:36 +0530

    under researc

--- Vesper-Agents-Suite/Fund/src/agent.ts
+++ Vesper-Agents-Suite/Fund/src/agent.ts
@@ -1,39 +1,41 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import BigNumber from "bignumber.js";
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  getJsonRpcUrl,
+} from "forta-agent";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+import Web3 from "web3";
+const web3 = new Web3(getJsonRpcUrl());
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-  }
+import Pool from "./pool";
+const defaultList: Array<string> = [];
 
-  return findings
+// pool.tokenHere() - pool.totalValue()*(MAX_BPS-totalDebtRatio) > 10% of pool.totalValue()
+const protocolAddress = "0x8d0b8e2b5584cE1487317f81Da7d97397eF3e899";
+
+function provideHandleFunction(
+  web3: any,
+  pools: Array<string>
+): HandleTransaction {
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    if (!txEvent.addresses[protocolAddress]) return findings;
+
+    const totalValue = await contract.methods.totalValue().call();
+    const tokenHere = await contract.methods.tokenHere().call();
+    const MAX_BPS = 
+    const totalDebtRatio = await contract.methods.
+
+    return findings;
+  };
 }
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleTransaction: provideHandleFunction(web3, defaultList),
+  provideHandleFunction,
+};
