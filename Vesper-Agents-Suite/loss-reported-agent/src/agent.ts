cddac6202b35115964f4cf7e63728523ace09998
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-19 09:53:35 -0400

    refac: Create handler transaction for receiving web3 argument

--- Vesper-Agents-Suite/loss-reported-agent/src/agent.ts
+++ Vesper-Agents-Suite/loss-reported-agent/src/agent.ts
@@ -16,26 +16,30 @@ import Web3 from "web3";
 
 const web3: Web3 = new Web3(getJsonRpcUrl());
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const poolAccountant: string[] = await getPoolAccountants();
-  const reportLossHandlers: HandleTransaction[] = poolAccountant.map(
-    (poolAccountant) =>
-      provideFunctionCallsDetectorHandler(createFinding, reportLossSignature, {
-        to: poolAccountant,
-      })
-  );
+const providerHandleTransaction = (web3: Web3): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const poolAccountant: string[] = await getPoolAccountants();
+    const reportLossHandlers: HandleTransaction[] = poolAccountant.map(
+      (poolAccountant) =>
+        provideFunctionCallsDetectorHandler(
+          createFinding,
+          reportLossSignature,
+          {
+            to: poolAccountant,
+          }
+        )
+    );
 
-  let findings: Finding[] = [];
+    let findings: Finding[] = [];
 
-  for (let reportLossHandler of reportLossHandlers) {
-    findings = findings.concat(await reportLossHandler(txEvent));
-  }
+    for (let reportLossHandler of reportLossHandlers) {
+      findings = findings.concat(await reportLossHandler(txEvent));
+    }
 
-  return findings;
+    return findings;
+  };
 };
 
 export default {
-  handleTransaction,
+  handleTransaction: providerHandleTransaction(web3),
 };
