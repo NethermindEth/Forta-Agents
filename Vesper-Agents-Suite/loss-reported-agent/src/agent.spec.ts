382237aa84290024af3f345588f23b78dea63112
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-18 21:54:29 -0400

    tests: Create initial tests

--- Vesper-Agents-Suite/loss-reported-agent/src/agent.spec.ts
+++ Vesper-Agents-Suite/loss-reported-agent/src/agent.spec.ts
@@ -3,46 +3,104 @@ import {
   FindingSeverity,
   Finding,
   HandleTransaction,
-  createTransactionEvent
-} from "forta-agent"
-import agent from "./agent"
+  TransactionEvent,
+} from "forta-agent";
+import agent from "./agent";
+import {
+  TestTransactionEvent,
+  createAddress,
+  encodeFunctionCall,
+} from "forta-agent-tools";
 
-describe("high gas agent", () => {
-  let handleTransaction: HandleTransaction
+const strategyAddresses = [createAddress("0x0"), createAddress("0x1")];
 
-  const createTxEventWithGasUsed = (gasUsed: string) => createTransactionEvent({
-    transaction: {} as any,
-    receipt: { gasUsed } as any,
-    block: {} as any,
-  })
+const reportLossABI = {
+  name: "reportLoss",
+  type: "function",
+  inputs: [
+    {
+      type: "address",
+      name: "",
+    },
+    {
+      type: "uint256",
+      name: "",
+    },
+  ],
+};
 
-  beforeAll(() => {
-    handleTransaction = agent.handleTransaction
-  })
+const createFinding = (strategyAddress: string, lossValue: string): Finding => {
+  return Finding.fromObject({
+    name: "",
+    description: "",
+    alertId: "",
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      strategyAddress: strategyAddress,
+      lossValue: lossValue,
+    },
+  });
+};
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if gas used is below threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1")
+describe("Reported Loss Agent", () => {
+  let handleTransaction: HandleTransaction;
 
-      const findings = await handleTransaction(txEvent)
+  it("should return empty findings if not reportLoss is called", async () => {
+    handleTransaction = agent.handleTransaction;
 
-      expect(findings).toStrictEqual([])
-    })
+    let findings: Finding[] = [];
 
-    it("returns a finding if gas used is above threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1000001")
+    const txEvent: TransactionEvent = new TestTransactionEvent();
 
-      const findings = await handleTransaction(txEvent)
+    findings = findings.concat(await handleTransaction(txEvent));
 
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Gas Used",
-          description: `Gas Used: ${txEvent.gasUsed}`,
-          alertId: "FORTA-1",
-          type: FindingType.Suspicious,
-          severity: FindingSeverity.Medium
-        }),
-      ])
-    })
-  })
-})
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("should returns finding if reportLoss was called", async () => {
+    handleTransaction = agent.handleTransaction;
+
+    let findings: Finding[] = [];
+
+    const txEvent: TransactionEvent = new TestTransactionEvent().addTraces({
+      input: encodeFunctionCall(reportLossABI as any, [
+        strategyAddresses[0],
+        "100",
+      ]),
+    });
+
+    findings = findings.concat(await handleTransaction(txEvent));
+
+    expect(findings).toStrictEqual([
+      createFinding(strategyAddresses[0], "100"),
+    ]);
+  });
+
+  it("should returns multiple findings if reportLoss was called multiple times", async () => {
+    handleTransaction = agent.handleTransaction;
+
+    let findings: Finding[] = [];
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addTraces({
+        input: encodeFunctionCall(reportLossABI as any, [
+          strategyAddresses[0],
+          "100",
+        ]),
+      })
+      .addTraces({
+        input: encodeFunctionCall(reportLossABI as any, [
+          strategyAddresses[1],
+          "150",
+        ]),
+      });
+
+    findings = findings.concat(await handleTransaction(txEvent));
+
+    expect(findings).toStrictEqual([
+      createFinding(strategyAddresses[0], "100"),
+      createFinding(strategyAddresses[1], "150"),
+    ]);
+  });
+});
