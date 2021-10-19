84d9772d83f9052a41201db1fe21f605a2fb63d7
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-19 11:41:06 -0400

    feat: Add getPools method for getting a list of pools

--- Vesper-Agents-Suite/loss-reported-agent/src/utils.ts
+++ Vesper-Agents-Suite/loss-reported-agent/src/utils.ts
@@ -4,26 +4,16 @@ import {
   decodeFunctionCallParameters,
 } from "forta-agent-tools";
 import { AbiItem } from "web3-utils";
+import Web3 from "web3";
+import { ControllerABI, AddressListABI } from "./abi";
 
-export const reportLossABI = {
-  name: "reportLoss",
-  type: "function",
-  inputs: [
-    {
-      type: "address",
-      name: "",
-    },
-    {
-      type: "uint256",
-      name: "",
-    },
-  ],
-} as AbiItem;
+const controllerAddresss = "0xa4F1671d3Aee73C05b552d57f2d16d3cfcBd0217";
+
 
 export const createFinding: FindingGenerator = (callInfo) => {
   const { 0: strategyAddress, 1: lossValue } = decodeFunctionCallParameters(
     ["address", "uint256"],
-    callInfo.input
+    callInfo?.input
   );
 
   return Finding.fromObject({
@@ -39,6 +29,21 @@ export const createFinding: FindingGenerator = (callInfo) => {
   });
 };
 
-export const getPoolAccountants = async (): Promise<string[]> => {
-  return [];
+const getPools = async (web3: Web3): Promise<string[]> => {
+  const pools: string[] = [];
+
+  const controllerContract = new web3.eth.Contract(ControllerABI, controllerAddresss);
+  const addressListAddress: string = await controllerContract.methods.pools().call();
+
+  const addressListContract = new web3.eth.Contract(AddressListABI, addressListAddress);
+  const poolsLength: number = Number(await addressListContract.methods.length().call());
+
+  for (let i = 0; i < poolsLength; i++){
+    const poolAddress = await addressListContract.methods.at(i).call();
+    pools.push(poolAddress);
+  }
+  return pools;
 };
+
+// export const getPoolAccountants = async (web3: Web3): Promise<string[]> => {
+// };
