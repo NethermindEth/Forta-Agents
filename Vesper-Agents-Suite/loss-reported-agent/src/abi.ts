32433cacc771f0d5a6a806c77ae8cf13ece914d6
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-19 11:39:56 -0400

    refact: Create abi file for getting all abi descriptions

---
+++ Vesper-Agents-Suite/loss-reported-agent/src/abi.ts
@@ -0,0 +1,61 @@
+import { AbiItem } from "web3-utils";
+
+
+export const reportLossABI = {
+  name: "reportLoss",
+  type: "function",
+  inputs: [
+    {
+      type: "address",
+      name: "",
+    },
+    {
+      type: "uint256",
+      name: "",
+    },
+  ],
+} as AbiItem;
+
+export const ControllerABI = [
+  {
+    name: "pools",
+    type: "function",
+    inputs: [],
+    outputs: [
+      {
+        type: "address",
+        name: ""
+      }
+    ]
+  }
+] as AbiItem[];
+
+export const AddressListABI = [
+  {
+    name: "length",
+    type: "function",
+    inputs: [],
+    outputs: [
+      {
+        type: "uint256",
+        name: "",
+      }
+    ]
+  },
+  {
+    name: "at",
+    type: "function",
+    inputs: [
+      {
+        type: "uint256",
+        name: "index",
+      }
+    ],
+    outputs: [
+      {
+        type: "address",
+        name: "",
+      }
+    ]
+  }
+] as AbiItem[];
