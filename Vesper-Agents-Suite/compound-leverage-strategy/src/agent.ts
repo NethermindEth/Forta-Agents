86b9148dac31299668b1d46b1c5d55c748883e00
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-21 11:41:14 -0400

    Init agent

---
+++ Vesper-Agents-Suite/compound-leverage-strategy/src/agent.ts
@@ -0,0 +1,25 @@
+import BigNumber from 'bignumber.js'
+import { 
+  BlockEvent, 
+  Finding, 
+  HandleBlock, 
+  HandleTransaction, 
+  TransactionEvent, 
+  FindingSeverity, 
+  FindingType 
+} from 'forta-agent'
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = []
+
+
+const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+  const findings: Finding[] = [];
+  // detect some block condition
+  return findings;
+}
+
+export default {
+  handleTransaction,
+  // handleBlock
+}
