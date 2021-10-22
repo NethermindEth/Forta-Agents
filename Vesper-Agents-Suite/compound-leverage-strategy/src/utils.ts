182fb42f2d68420a0ce74de4e8841693d2aefa4a
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-22 14:14:29 -0400

    feat: Add utils functions for getting Compound-Leverage strategies

--- Vesper-Agents-Suite/compound-leverage-strategy/src/utils.ts
+++ Vesper-Agents-Suite/compound-leverage-strategy/src/utils.ts
@@ -66,3 +66,101 @@ export const getCollateralFactorMantissa = async (
     .call(blockNumber);
   return BigInt(collateralFactorMantissa);
 };
+
+const getVesperPools = async (
+  web3: Web3,
+  blockNumber: string | number
+): Promise<string[]> => {
+  const pools: string[] = [];
+
+  const controllerContract = new web3.eth.Contract(
+    vesperControllerABI,
+    vesperControllerAddress
+  );
+  const addressListAddress: string = await controllerContract.methods
+    .pools()
+    .call({}, blockNumber);
+
+  const addressListContract = new web3.eth.Contract(
+    addressListABI,
+    addressListAddress
+  );
+  const poolsLength: number = Number(
+    await addressListContract.methods.length().call({}, blockNumber)
+  );
+
+  for (let i = 0; i < poolsLength; i++) {
+    const poolAddress = await addressListContract.methods
+      .at(i)
+      .call({}, blockNumber);
+    pools.push(poolAddress);
+  }
+  return pools;
+};
+
+const getStrategiesAsV2 = async (
+  web3: Web3,
+  blockNumber: string | number,
+  pool: string
+): Promise<string[]> => {
+  const vesperControllerContract = new web3.eth.Contract(
+    vesperControllerABI,
+    vesperControllerAddress
+  );
+  const strategy = await vesperControllerContract.methods
+    .strategy(pool)
+    .call(blockNumber);
+  return strategy === zeroAddress ? [] : [strategy];
+};
+
+const getStrategiesAsV3 = async (
+  web3: Web3,
+  blockNumber: string | number,
+  pool: string
+): Promise<string[]> => {
+  const vesperPoolContract = new web3.eth.Contract(poolABI, pool);
+  try {
+    const strategies = await vesperPoolContract.methods
+      .getStrategies()
+      .call(blockNumber);
+    return strategies;
+  } catch {
+    return [];
+  }
+};
+
+const getStrategies = async (
+  web3: Web3,
+  blockNumber: string | number
+): Promise<string[]> => {
+  let strategies: string[] = [];
+  const pools = await getVesperPools(web3, blockNumber);
+  for (let pool of pools) {
+    strategies = strategies.concat(
+      await getStrategiesAsV2(web3, blockNumber, pool)
+    );
+    strategies = strategies.concat(
+      await getStrategiesAsV3(web3, blockNumber, pool)
+    );
+  }
+  return strategies;
+};
+
+export const getCompoundLeverageStrategies = async (
+  web3: Web3,
+  blockNumber: string | number
+): Promise<string[]> => {
+  const compoundLeverageStrategies: string[] = [];
+  const strategies = await getStrategies(web3, blockNumber);
+
+  for (let strategy of strategies) {
+    const strategyContract = new web3.eth.Contract(strategyABI, strategy);
+    const strategyName: string = await strategyContract.methods
+      .NAME()
+      .call(blockNumber);
+    if (strategyName.includes("Compound-Leverage")) {
+      compoundLeverageStrategies.push(strategy);
+    }
+  }
+  return compoundLeverageStrategies;
+};
