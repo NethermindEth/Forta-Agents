743adab3c3f9c6fff0311bc92aabbb1ac2b79163
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-10-22 14:15:51 -0400

    feat: Add main agent logic

--- Vesper-Agents-Suite/compound-leverage-strategy/src/agent.ts
+++ Vesper-Agents-Suite/compound-leverage-strategy/src/agent.ts
@@ -1,25 +1,98 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import BigNumber from "bignumber.js";
+import { BlockEvent, Finding, HandleBlock, getJsonRpcUrl } from "forta-agent";
+import {
+  getCurrentBorrowRatio,
+  getMaxBorrowRatio,
+  getRelatedCToken,
+  getCollateralFactorMantissa,
+  getScaledCurrentBorrowRatio,
+  getCompoundLeverageStrategies,
+  createFindingForLiquidationWarning,
+  createFindingForHighCurrentBorrowRatio,
+} from "./utils";
+import Web3 from "web3";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const web3 = new Web3(getJsonRpcUrl());
 
-
-const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+const currentRatioTooHigh = async (
+  web3: Web3,
+  contractAddress: string,
+  blockEvent: BlockEvent
+): Promise<Finding[]> => {
   const findings: Finding[] = [];
-  // detect some block condition
+  const currentRatio = await getCurrentBorrowRatio(
+    web3,
+    contractAddress,
+    blockEvent.blockNumber
+  );
+  const maxRatio = await getMaxBorrowRatio(
+    web3,
+    contractAddress,
+    blockEvent.blockNumber
+  );
+
+  if (currentRatio > maxRatio) {
+    findings.push(createFindingForHighCurrentBorrowRatio(contractAddress));
+  }
+
   return findings;
-}
+};
+
+const closeToLiquidation = async (
+  web3: Web3,
+  contractAddress: string,
+  blockEvent: BlockEvent
+): Promise<Finding[]> => {
+  const findings: Finding[] = [];
+  const cToken = await getRelatedCToken(
+    web3,
+    contractAddress,
+    blockEvent.blockNumber
+  );
+  const collateralFactor = await getCollateralFactorMantissa(
+    web3,
+    cToken,
+    blockEvent.blockNumber
+  );
+  const scaledBorrowRation = await getScaledCurrentBorrowRatio(
+    web3,
+    contractAddress,
+    blockEvent.blockNumber
+  );
+
+  if (
+    collateralFactor - scaledBorrowRation > 10e18 ||
+    scaledBorrowRation > collateralFactor
+  ) {
+    findings.push(createFindingForLiquidationWarning(contractAddress));
+  }
+
+  return findings;
+};
+
+const provideHandleBlock = (web3: Web3): HandleBlock => {
+  return async (blockEvent: BlockEvent) => {
+    let findings: Finding[] = [];
+
+    const compoundLeverageStrategies: string[] =
+      await getCompoundLeverageStrategies(web3, blockEvent.blockNumber);
+
+    for (let strategy of compoundLeverageStrategies) {
+      findings = findings.concat(
+        await currentRatioTooHigh(web3, strategy, blockEvent)
+      );
+    }
+
+    for (let strategy of compoundLeverageStrategies) {
+      findings = findings.concat(
+        await closeToLiquidation(web3, strategy, blockEvent)
+      );
+    }
+
+    return findings;
+  };
+};
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleBlock: provideHandleBlock(web3),
+};
