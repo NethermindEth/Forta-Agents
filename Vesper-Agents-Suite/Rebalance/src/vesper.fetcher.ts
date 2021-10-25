2771dfbcbb9775106d2344c83ba0260e87054e37
Lázaro Raúl <riglesiasvera@gmail.com>
2021-10-25 19:33:25 -0400

    chore: Add missing types in VesperFetcher

--- Vesper-Agents-Suite/Rebalance/src/vesper.fetcher.ts
+++ Vesper-Agents-Suite/Rebalance/src/vesper.fetcher.ts
@@ -5,6 +5,11 @@ import abi from "./abi";
 
 type BlockId = string | number;
 
+interface StrategiesData {
+  V2: Set<string>,
+  V3: Set<string>,
+};
+
 export default class VesperFetcher{
   private web3Call: any;
   private controller: string;
@@ -30,7 +35,7 @@ export default class VesperFetcher{
     return pools;
   }
 
-  private async _getStrategies(block: BlockId, fetchV3: boolean = true){
+  private async _getStrategies(block: BlockId, fetchV3: boolean = true): Promise<StrategiesData> {
     const pools = await this.getPools(block);
 
     const V2: Set<string> = new Set<string>();
@@ -49,21 +54,21 @@ export default class VesperFetcher{
     return { V2, V3 };
   }
 
-  public async getStrategiesV2(block: BlockId = "latest"){
+  public async getStrategiesV2(block: BlockId = "latest"): Promise<string[]> {
     const { V2 } = await this._getStrategies(block, false);
     return Array.from(V2);
   }
 
-  public async getStrategiesV3(block: BlockId = "latest"){
+  public async getStrategiesV3(block: BlockId = "latest"): Promise<string[]> {
     const { V3 } = await this._getStrategies(block);
     return Array.from(V3);
   }
 
-  public async getAllStrategies(block: BlockId = "latest"){
+  public async getAllStrategies(block: BlockId = "latest"): Promise<string[]> {
     if(block !== "latest" && this.cache.get(block) !== undefined)
       return this.cache.get(block);
     const { V2, V3 } = await this._getStrategies(block);
-    V3.forEach(strat => V2.add(strat));
+    V3.forEach((strat:string) => V2.add(strat));
 
     const strategies: string[] = Array.from(V2);
     this.cache.set(block, strategies);
