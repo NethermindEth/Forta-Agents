257a30599942bb72ce9ee01c0da46d1bbf1755a9
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-11-01 16:07:52 -0400

    feat: Create agent template

--- Yearn-agents/ManagmentChanges/src/agent.ts
+++ Yearn-agents/ManagmentChanges/src/agent.ts
@@ -1,31 +1,61 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import {
+    getJsonRpcUrl,
+  HandleTransaction,
+  TransactionEvent,
+} from "forta-agent";
+import { provideEventCheckerHandler } from "forta-agent-tools";
+import {
+  updateManagementSignature,
+  updatePerformanceFeeSignature,
+  updateManagementFeeSignature,
+  createUpdateManagementFindingGenerator,
+  createUpdateManagementFeeFindingGenerator,
+  createUpdatePerformanceFeeFindingGenerator,
+  getYearnVaults,
+} from "./utils";
+import Web3 from "web3";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const web3 = new Web3(getJsonRpcUrl());
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
+
+
+export const provideHandleTransaction = (web3: Web3): HandleTransaction => {
+  return async (txEvent) => {
+  const yearnVaults: string[] = getYearnVaults(web3, txEvent.blockNumber);
+
+  const updateManagementHandlers = yearnVaults.map((yearnVault: string) =>
+    provideEventCheckerHandler(
+      createUpdateManagementFindingGenerator(yearnVault),
+      updateManagementSignature,
+      yearnVault
+    )
+  );
+
+  const updateManagementFeeHandlers = yearnVaults.map((yearnVault: string) =>
+    provideEventCheckerHandler(
+      createUpdateManagementFeeFindingGenerator(yearnVault),
+      updateManagementFeeSignature,
+      yearnVault
+    )
+  );
+
+  const updatePerformanceFeeHandlers = yearnVaults.map((yearnVault: string) =>
+    provideEventCheckerHandler(
+      createUpdatePerformanceFeeFindingGenerator(yearnVault),
+      updatePerformanceFeeSignature,
+      yearnVault
+    )
+  );
+
+  const handlers = updateManagementHandlers.concat(updateManagementFeeHandlers, updatePerformanceFeeHandlers);
+
+  const findings = await Promise.all(handlers.map((handler) => handler(txEvent)));
+
+  return findings.flat();
   }
-
-  return findings
 }
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+};
 
 // const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
 //   const findings: Finding[] = [];
@@ -36,4 +66,4 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
 export default {
   handleTransaction,
   // handleBlock
-}
+};
