ed63fd576c77a27bc24303e9eabf944f9e74ed11
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-11-03 17:11:17 -0400

    refac: Change metadata to more readable format allowed by ethers

--- Yearn-agents/ManagmentChanges/src/yearn.metadata.ts
+++ Yearn-agents/ManagmentChanges/src/yearn.metadata.ts
@@ -2,47 +2,16 @@ import { AbiItem } from "web3-utils";
 
 export const yearnDataProvider = "0x437758D475F70249e03EDa6bE23684aD1FC375F0";
 
-export const helperAbi = [
-  "function assetAddresses() view"
-]
-// export const helperAbi = [
-//   {
-//     type: "function",
-//     name: "assetsAddresses",
-//     inputs: [],
-//     outputs: [
-//       {
-//         name: "vaults",
-//         type: "address[]",
-//       }
-//     ]
-//   }
-// ] as AbiItem[];
+export const helperAbi = ["function assetsAddresses() view returns (address[])"];
 
 export const vaultAbi = [
   "function setManagement(address vault)",
-  "function governance() view returns(address)"
-]
-// export const vaultAbi = [
-//   {
-//     type: "function",
-//     name: "setManagement",
-//     inputs: [
-//       {
-//         name: "",
-//         type: "address"
-//       }
-//     ],
-//   },
-//   {
-//     type: "function",
-//     name: "governance",
-//     inputs: [],
-//     outputs: [
-//       {
-//         type: "address",
-//         name: "governance",
-//       }
-//     ],
-//   },
-// ] as AbiItem[];
+  "function governance() view returns(address)",
+  "function setManagementFee(uint256 fee)",
+  "function setPerformanceFee(uint256 fee)",
+  "function setGovernance(address governor)",
+  "function acceptGovernance()",
+  "function pendingGovernance() view returns (address)",
+];
+
+export const multicallAbi = ["function aggregate(tuple(address,bytes)[] calls) returns (uint256, bytes[])"];
