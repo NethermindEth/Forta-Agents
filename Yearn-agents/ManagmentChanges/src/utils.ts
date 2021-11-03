8c7faa9f019ceb1cacee7ab3c13d5fe559462fa8
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-11-03 17:12:31 -0400

    feat: Add more utils and add protocol to findings

--- Yearn-agents/ManagmentChanges/src/utils.ts
+++ Yearn-agents/ManagmentChanges/src/utils.ts
@@ -1,6 +1,8 @@
 import { Finding, FindingType, FindingSeverity } from "forta-agent";
 import { FindingGenerator, decodeParameter } from "forta-agent-tools";
-import Web3 from "web3";
+import { ethers } from "ethers";
+import { helperAbi, yearnDataProvider } from "./yearn.metadata";
+import { TransactionEvent, EventType } from "forta-agent";
 
 export const updateManagementSignature = "UpdateManagement(address)";
 
@@ -18,6 +20,7 @@ export const createUpdateManagementFindingGenerator = (vaultAddress: string): Fi
       alertId: "Yearn-9-1",
       type: FindingType.Info,
       severity: FindingSeverity.Info,
+      protocol: "Yearn",
       metadata: {
         vaultAddress: vaultAddress,
         setManger: setManager,
@@ -36,6 +39,7 @@ export const createUpdateManagementFeeFindingGenerator = (vaultAddress: string):
       alertId: "Yearn-9-1",
       type: FindingType.Info,
       severity: FindingSeverity.Info,
+      protocol: "Yearn",
       metadata: {
         vaultAddress: vaultAddress,
         setFee: setFee.toString(),
@@ -54,6 +58,7 @@ export const createUpdatePerformanceFeeFindingGenerator = (vaultAddress: string)
       alertId: "Yearn-9-1",
       type: FindingType.Info,
       severity: FindingSeverity.Info,
+      protocol: "Yearn",
       metadata: {
         vaultAddress: vaultAddress,
         setFee: setFee.toString(),
@@ -62,6 +67,71 @@ export const createUpdatePerformanceFeeFindingGenerator = (vaultAddress: string)
   };
 };
 
-export const getYearnVaults = (web3: Web3, blockNumber: string | number): string[] => {
+export const getYearnVaults = async (
+  etherProvider: ethers.providers.JsonRpcProvider,
+  blockNumber: string | number
+): Promise<string[]> => {
+  const signer = etherProvider.getSigner(0);
+  const yearnHelper = new ethers.Contract(yearnDataProvider, helperAbi, signer);
+  return yearnHelper.assetsAddresses({ blockTag: blockNumber });
+};
 
+// This functions was copied from forta-agent cli
+// TODO check why function from forta module fails
+export const createTransactionEvent = (receipt: any, block: any, networkId: number) => {
+  const transaction = block.transactions.find((tx: any) => tx.hash === receipt.transactionHash)!;
+  const tx: any = {
+    hash: transaction.hash,
+    from: transaction.from.toLowerCase(),
+    to: transaction.to ? transaction.to.toLowerCase() : null,
+    nonce: parseInt(transaction.nonce),
+    gas: transaction.gas,
+    gasPrice: transaction.gasPrice,
+    value: transaction.value,
+    data: transaction.input,
+    r: transaction.r,
+    s: transaction.s,
+    v: transaction.v,
+  };
+  const addresses = {
+    [tx.from]: true,
+  };
+  if (tx.to) {
+    addresses[tx.to] = true;
+  }
+
+  const rcpt = {
+    blockNumber: parseInt(receipt.blockNumber),
+    blockHash: receipt.blockHash,
+    transactionIndex: parseInt(receipt.transactionIndex),
+    transactionHash: receipt.transactionHash,
+    status: receipt.status === "0x1",
+    logsBloom: receipt.logsBloom,
+    contractAddress: receipt.contractAddress ? receipt.contractAddress.toLowerCase() : null,
+    gasUsed: receipt.gasUsed,
+    cumulativeGasUsed: receipt.cumulativeGasUsed,
+    logs: receipt.logs.map((log: any) => ({
+      address: log.address.toLowerCase(),
+      topics: log.topics,
+      data: log.data,
+      logIndex: parseInt(log.logIndex),
+      blockNumber: parseInt(log.blockNumber),
+      blockHash: log.blockHash,
+      transactionIndex: parseInt(log.transactionIndex),
+      transactionHash: log.transactionHash,
+      removed: log.removed,
+    })),
+    root: receipt.root ?? "",
+  };
+  rcpt.logs.forEach((log: any) => (addresses[log.address] = true));
+
+  const blok = {
+    hash: block.hash,
+    number: parseInt(block.number),
+    timestamp: parseInt(block.timestamp),
+  };
+
+  const trcs: any[] = [];
+
+  return new TransactionEvent(EventType.BLOCK, networkId, tx, rcpt, trcs, addresses, blok);
 };
