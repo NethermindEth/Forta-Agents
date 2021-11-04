17e74809e4db43ff39e8f8fbc10e8f7f0e6b48d0
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-11-04 13:36:39 +0300

    feat: done with SF tests

--- Yearn-agents/Protocol-Specific-Agents/src/agent.spec.ts
+++ Yearn-agents/Protocol-Specific-Agents/src/agent.spec.ts
@@ -3,23 +3,21 @@ import {
   FindingSeverity,
   Finding,
   HandleTransaction,
-  TransactionEvent,
 } from 'forta-agent';
 import {
   createAddress,
   encodeFunctionSignature,
-  TestBlockEvent,
   TestTransactionEvent,
 } from 'forta-agent-tools';
-import Mock, { Args } from './mock/mock';
+import Mock, { strategies, isActive, name, Args } from './mock/mock';
 import { provideHandleTransaction } from './agent';
 import { JUG_CONTRACT, JUG_DRIP_FUNCTION_SIGNATURE } from './utils';
 import MakerFetcher from './maker.fetcher';
 
-const createMock = () => {
+const createMock = (args: Args) => {
   return {
     eth: {
-      Contract: Mock.build_Mock(),
+      Contract: Mock.build_Mock(args),
     },
   } as any;
 };
@@ -42,8 +40,14 @@ const createFindingSF = (_strategy: any, collateralType: string): Finding => {
 describe('Protocol Alerts Agent Test Suite', () => {
   let handleTransaction: HandleTransaction;
 
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should return empty findings ', async () => {
-    const mockWeb3 = createMock();
+    const args = [true, false];
+
+    const mockWeb3 = createMock(args);
     const fetcher = new MakerFetcher(mockWeb3);
 
     let findings: Finding[] = [];
@@ -57,8 +61,10 @@ describe('Protocol Alerts Agent Test Suite', () => {
     expect(findings).toStrictEqual([]);
   });
 
-  it('should return findings ', async () => {
-    const mockWeb3 = createMock();
+  it('should return finding for active maker ', async () => {
+    const args = [false, true];
+
+    const mockWeb3 = createMock(args);
     const fetcher = new MakerFetcher(mockWeb3);
 
     let findings: Finding[] = [];
@@ -79,8 +85,82 @@ describe('Protocol Alerts Agent Test Suite', () => {
 
     findings = await handleTransaction(txEvent);
 
+    expect(strategies).toBeCalledTimes(5);
+    expect(isActive).toBeCalledTimes(2);
+    expect(name).toBeCalledTimes(4);
+    expect(findings).toStrictEqual([
+      createFindingSF(createAddress('0x3'), '0x' + collateralType),
+    ]);
+  });
+
+  it('should return empty finding if both inactive maker ', async () => {
+    const args = [false, false];
+
+    const mockWeb3 = createMock(args);
+    const fetcher = new MakerFetcher(mockWeb3);
+
+    let findings: Finding[] = [];
+
+    const selector = encodeFunctionSignature(JUG_DRIP_FUNCTION_SIGNATURE);
+    const collateralType =
+      '4554482d43000000000000000000000000000000000000000000000000000000';
+    const INPUT = selector + collateralType;
+
+    handleTransaction = provideHandleTransaction(mockWeb3, fetcher);
+
+    const txEvent: any = new TestTransactionEvent()
+      .addTraces({
+        to: JUG_CONTRACT,
+        input: INPUT,
+      })
+      .setStatus(true);
+
+    findings = await handleTransaction(txEvent);
+
+    expect(strategies).toBeCalledTimes(5);
+    expect(isActive).toBeCalledTimes(2);
+    expect(name).toBeCalledTimes(4);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it('should return 2 finding for active makers ', async () => {
+    const args = [true, true];
+
+    const mockWeb3 = createMock(args);
+    const fetcher = new MakerFetcher(mockWeb3);
+
+    let findings: Finding[] = [];
+
+    const selector = encodeFunctionSignature(JUG_DRIP_FUNCTION_SIGNATURE);
+    const collateralType =
+      '4554482d43000000000000000000000000000000000000000000000000000000';
+    const INPUT1 = selector + collateralType;
+
+    const collateralType2 =
+      '3554482d43000000000000000000000000000000000000000000000000000000';
+    const INPUT2 = selector + collateralType2;
+
+    handleTransaction = provideHandleTransaction(mockWeb3, fetcher);
+
+    const txEvent: any = new TestTransactionEvent()
+      .addTraces({
+        to: JUG_CONTRACT,
+        input: INPUT1,
+      })
+      .addTraces({
+        to: JUG_CONTRACT,
+        input: INPUT2,
+      })
+      .setStatus(true);
+
+    findings = await handleTransaction(txEvent);
+
+    expect(strategies).toBeCalledTimes(5);
+    expect(isActive).toBeCalledTimes(2);
+    expect(name).toBeCalledTimes(4);
     expect(findings).toStrictEqual([
       createFindingSF(createAddress('0x2'), '0x' + collateralType),
+      createFindingSF(createAddress('0x3'), '0x' + collateralType2),
     ]);
   });
 });
--- Yearn-agents/Protocol-Specific-Agents/src/mock/mock.ts
+++ Yearn-agents/Protocol-Specific-Agents/src/mock/mock.ts
@@ -2,13 +2,14 @@ import { createAddress } from 'forta-agent-tools';
 
 const VAULTS: string[] = [createAddress('0x1')];
 
-export type Args = [name: string];
-
 export const strategies = jest.fn();
 export const isActive = jest.fn();
 export const name = jest.fn();
+export const ilk = jest.fn();
 
-const build_Mock = () =>
+export type Args = boolean[];
+
+const build_Mock = (args: Args) =>
   class MockContract {
     private addr: string;
 
@@ -26,11 +27,24 @@ const build_Mock = () =>
       strategies
         .mockReturnValueOnce(createAddress('0x2'))
         .mockReturnValueOnce(createAddress('0x3'))
+        .mockReturnValueOnce(createAddress('0x4'))
+        .mockReturnValueOnce(createAddress('0x5'))
         .mockReturnValueOnce(createAddress('0x'));
 
-      isActive.mockReturnValueOnce(true).mockReturnValueOnce(true);
+      isActive.mockReturnValueOnce(args[0]).mockReturnValueOnce(args[1]);
+      name
+        .mockReturnValueOnce('Maker')
+        .mockReturnValueOnce('Maker')
+        .mockReturnValueOnce('Curve')
+        .mockReturnValueOnce('Uniswap');
 
-      name.mockReturnValueOnce('Maker').mockReturnValueOnce('Curve');
+      ilk
+        .mockReturnValueOnce(
+          '0x4554482d43000000000000000000000000000000000000000000000000000000'
+        )
+        .mockReturnValueOnce(
+          '0x3554482d43000000000000000000000000000000000000000000000000000000'
+        );
     }
 
     private NAME() {
@@ -41,8 +55,7 @@ const build_Mock = () =>
 
     private ilk() {
       return {
-        call: () =>
-          '0x4554482d43000000000000000000000000000000000000000000000000000000',
+        call: () => ilk(),
       };
     }
 
@@ -69,4 +82,6 @@ export default {
   build_Mock,
   VAULTS,
   strategies,
+  isActive,
+  name,
 };
