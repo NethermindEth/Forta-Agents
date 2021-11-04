e00b6ae33d5e687aed5ecffda09c16da740ec47c
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-11-04 15:11:23 +0300

    feat: modify folder structure

---
+++ Yearn-agents/Protocol-Specific-Agents/StabilityFeeAgent/src/agent.ts
@@ -0,0 +1,54 @@
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  getJsonRpcUrl,
+} from 'forta-agent';
+import { provideFunctionCallsDetectorHandler } from 'forta-agent-tools';
+import Web3 from 'web3';
+import MakerFetcher from './maker.fetcher';
+import {
+  createFindingStabilityFee,
+  getCollateralType,
+  JUG_CONTRACT,
+  JUG_DRIP_FUNCTION_SIGNATURE,
+} from './utils';
+
+const web3: Web3 = new Web3(getJsonRpcUrl());
+const fetcher = new MakerFetcher(web3);
+
+export const provideHandleTransaction = (web3: Web3, fetcher: MakerFetcher) => {
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    if (!txEvent.status) return findings;
+    const makers = await fetcher.getActiveMakers(txEvent.blockNumber);
+
+    for (const strategy of makers) {
+      const collateralType = await getCollateralType(
+        web3,
+        strategy,
+        txEvent.blockNumber
+      );
+
+      const filterOnArguments = (args: { [key: string]: any }): boolean => {
+        return args[0] === collateralType;
+      };
+
+      const agentHandler = provideFunctionCallsDetectorHandler(
+        createFindingStabilityFee(strategy.toString()),
+        JUG_DRIP_FUNCTION_SIGNATURE,
+        { to: JUG_CONTRACT, filterOnArguments }
+      );
+
+      findings.push(...(await agentHandler(txEvent)));
+    }
+
+    return findings;
+  };
+};
+
+export default {
+  handleTransaction: provideHandleTransaction(web3, fetcher),
+  provideHandleTransaction,
+};
