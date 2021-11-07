89885815dc2d963d9c4370f8640f3d3038506b45
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-11-07 16:00:49 +0300

    refac: rename folder

---
+++ Yearn-agents/Protocol-Specific-Agents/SFandSpotPriceAgents/src/agent.ts
@@ -0,0 +1,148 @@
+import {
+  Finding,
+  TransactionEvent,
+  getJsonRpcUrl,
+  Log,
+  HandleTransaction,
+} from 'forta-agent';
+import {
+  decodeParameters,
+  provideEventCheckerHandler,
+  provideFunctionCallsDetectorHandler,
+} from 'forta-agent-tools';
+import Web3 from 'web3';
+import MakerFetcher from './maker.fetcher';
+import TimeTracker from './time.tracker';
+import {
+  createFindingStabilityFee,
+  createStaleSpotFinding,
+  getCollateralType,
+  JUG_CONTRACT,
+  JUG_DRIP_FUNCTION_SIGNATURE,
+  POKE_SIGNATURE,
+  SPOT_ADDRESS,
+} from './utils';
+
+const web3: Web3 = new Web3(getJsonRpcUrl());
+const fetcher = new MakerFetcher(web3);
+
+// The Handler that checks Stability Fee changes for Maker Strategies
+const provideStabilityFeeHandler = async (
+  web3: Web3,
+  makers: string[],
+  txEvent: TransactionEvent
+): Promise<Finding[]> => {
+  const findings: Finding[] = [];
+
+  if (!txEvent.status) return [];
+
+  for (const strategy of makers) {
+    const collateralType = await getCollateralType(
+      web3,
+      strategy,
+      txEvent.blockNumber
+    );
+
+    const filterOnArguments = (args: { [key: string]: any }): boolean => {
+      return args[0] === collateralType;
+    };
+
+    const agentHandler = provideFunctionCallsDetectorHandler(
+      createFindingStabilityFee(strategy.toString()),
+      JUG_DRIP_FUNCTION_SIGNATURE,
+      { to: JUG_CONTRACT, filterOnArguments }
+    );
+
+    findings.push(...(await agentHandler(txEvent)));
+  }
+
+  return findings;
+};
+
+// The Handler that checks poke() method is not called for >= 3 hours
+const provideStaleSpotPriceHandler = async (
+  web3: Web3,
+  makers: string[],
+  txEvent: TransactionEvent,
+  timeTracker: TimeTracker
+): Promise<Finding[]> => {
+  const findings: Finding[] = [];
+  const timestamp = txEvent.block.timestamp;
+
+  for (const strategy of makers) {
+    const ilk = await getCollateralType(web3, strategy, txEvent.blockNumber);
+
+    const filterEvents = (log: Log) => {
+      const parameters = decodeParameters(
+        ['bytes32', 'bytes32', 'uint256'],
+        log.data
+      );
+
+      return ilk === parameters[0];
+    };
+
+    const handler = provideEventCheckerHandler(
+      () => {
+        return {} as Finding;
+      },
+      POKE_SIGNATURE,
+      SPOT_ADDRESS,
+      filterEvents
+    );
+
+    if (timeTracker.isOutOf3Hours(timestamp)) {
+      timeTracker.updateFindingReport(false);
+      timeTracker.updateFunctionWasCalled(false);
+    }
+
+    if (
+      (await handler(txEvent)).length !== 0 &&
+      timeTracker.isIn3Hours(timestamp)
+    ) {
+      timeTracker.updateFunctionWasCalled(true);
+    }
+
+    if (
+      (!timeTracker.isIn3Hours(timestamp) &&
+        !timeTracker.isFirstHour(timestamp) &&
+        !timeTracker.functionWasCalled &&
+        !timeTracker.findingReported) ||
+      !txEvent.status
+    ) {
+      timeTracker.updateFindingReport(true);
+      findings.push(createStaleSpotFinding(SPOT_ADDRESS, strategy));
+    }
+
+    timeTracker.updateHour(timestamp);
+    return findings;
+  }
+
+  return findings;
+};
+
+export const provideHandleTransaction = (
+  web3: Web3,
+  fetcher: MakerFetcher
+): HandleTransaction => {
+  const timeTracker = new TimeTracker();
+  return async (txEvent: TransactionEvent) => {
+    const makers = await fetcher.getActiveMakers(txEvent.blockNumber);
+
+    const findings = [
+      ...(await provideStabilityFeeHandler(web3, makers, txEvent)),
+      ...(await provideStaleSpotPriceHandler(
+        web3,
+        makers,
+        txEvent,
+        timeTracker
+      )),
+    ];
+
+    return findings;
+  };
+};
+
+export default {
+  handleTransaction: provideHandleTransaction(web3, fetcher),
+  provideHandleTransaction,
+};
---
+++ Yearn-agents/Protocol-Specific-Agents/SFandSpotPriceAgents/src/mock/mock.ts
@@ -0,0 +1,87 @@
+import { createAddress } from 'forta-agent-tools';
+
+const VAULTS: string[] = [createAddress('0x1')];
+
+export const strategies = jest.fn();
+export const isActive = jest.fn();
+export const name = jest.fn();
+export const ilk = jest.fn();
+
+export type Args = boolean[];
+
+const build_Mock = (args: Args) =>
+  class MockContract {
+    private addr: string;
+
+    public methods = {
+      name: this.NAME,
+      withdrawalQueue: this.withdrawalQueue,
+      assetsAddresses: this.assetsAddresses,
+      ilk: this.ilk,
+      isActive: this.isActive,
+    };
+
+    constructor(_: any, addr: string) {
+      this.addr = addr;
+
+      strategies
+        .mockReturnValueOnce(createAddress('0x2'))
+        .mockReturnValueOnce(createAddress('0x3'))
+        .mockReturnValueOnce(createAddress('0x4'))
+        .mockReturnValueOnce(createAddress('0x5'))
+        .mockReturnValueOnce(createAddress('0x'));
+
+      isActive.mockReturnValueOnce(args[0]).mockReturnValueOnce(args[1]);
+      name
+        .mockReturnValueOnce('Maker')
+        .mockReturnValueOnce('Maker')
+        .mockReturnValueOnce('Curve')
+        .mockReturnValueOnce('Uniswap');
+
+      ilk
+        .mockReturnValueOnce(
+          '0x4554482d43000000000000000000000000000000000000000000000000000000'
+        )
+        .mockReturnValueOnce(
+          '0x3554482d43000000000000000000000000000000000000000000000000000000'
+        );
+    }
+
+    private NAME() {
+      return {
+        call: () => name(),
+      };
+    }
+
+    private ilk() {
+      return {
+        call: () => ilk(),
+      };
+    }
+
+    private withdrawalQueue() {
+      return {
+        call: () => strategies(),
+      };
+    }
+
+    private assetsAddresses() {
+      return {
+        call: () => VAULTS,
+      };
+    }
+
+    private isActive() {
+      return {
+        call: () => isActive(),
+      };
+    }
+  };
+
+export default {
+  build_Mock,
+  VAULTS,
+  strategies,
+  isActive,
+  name,
+};
