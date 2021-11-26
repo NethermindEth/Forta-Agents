791c0e59990f5210b8a10144c4399ff64a449a6c
rachit2501 <rac.sri25@gmail.com>
2021-11-26 19:24:46 +0530

    version update

--- Curve-finance-suite/src/agent.ts
+++ Curve-finance-suite/src/agent.ts
@@ -1,19 +1,19 @@
-import provideCrossAssetSwap from './agents/cross.asset.swap';
-import provideKillMeAgent from './agents/kill.me';
-import provideRemoveLiquidityImbalanceAgent from './agents/remove.imbalance.liquidity';
-import provideUnkillAgent from './agents/unkill';
-import provideAddPoolAgent from './agents/registry.add.pool';
-import provideRemovePoolAgent from './agents/registry.remove.pool';
-import provideMetaPoolDeployment from './agents/deploy.metapool';
-import providecreateLockAgent from './agents/curve.dao.create.lockevent';
-import providesetKilledAgent from './agents/curve.dao.killing.gauge';
-import providesetRewardsAgent from './agents/curve.gauge.set.rewards';
-import provideCommitNewAdminEvent from './agents/address.provider.ownership.transfer';
-import provideclaimManyAgent from './agents/curve.dao.claim.many';
-import provideMıgratePoolAgent from './agents/pool.migration';
-import provideApplyNewFeesAgent from './agents/apply.newfee';
-import provideRampAgent from './agents/ramp';
-import provideStompRampAgent from './agents/stop.ramp';
+import provideCrossAssetSwap from "./agents/cross.asset.swap";
+import provideKillMeAgent from "./agents/kill.me";
+import provideRemoveLiquidityImbalanceAgent from "./agents/remove.imbalance.liquidity";
+import provideUnkillAgent from "./agents/unkill";
+import provideAddPoolAgent from "./agents/registry.add.pool";
+import provideRemovePoolAgent from "./agents/registry.remove.pool";
+import provideMetaPoolDeployment from "./agents/deploy.metapool";
+import providecreateLockAgent from "./agents/curve.dao.create.lockevent";
+import providesetKilledAgent from "./agents/curve.dao.killing.gauge";
+import providesetRewardsAgent from "./agents/curve.gauge.set.rewards";
+import provideCommitNewAdminEvent from "./agents/address.provider.ownership.transfer";
+import provideclaimManyAgent from "./agents/curve.dao.claim.many";
+import provideMıgratePoolAgent from "./agents/pool.migration";
+import provideApplyNewFeesAgent from "./agents/apply.newfee";
+import provideRampAgent from "./agents/ramp";
+import provideStompRampAgent from "./agents/stop.ramp";
 
 export {
   provideCrossAssetSwap,
