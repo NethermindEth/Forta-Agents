f2802e2284e2b023cf694d3864b0977ff3cf0ed7
Kalzak <kalzakdev@protonmail.com>
2021-12-02 19:14:52 +0800

    registry add and remove pool agent refactor

--- Curve-finance-suite/src/agents/registry.add.pool.ts
+++ Curve-finance-suite/src/agents/registry.add.pool.ts
@@ -5,30 +5,35 @@ import {
   FindingType,
   HandleTransaction,
 } from "forta-agent";
+
 import createFinding from "../utils/create.finding";
 
 // Event signature found in Registry.vy in Curve repo
 export const ADD_POOL_SIGNATURE = "PoolAdded(address,bytes)";
 
-const provideAddPoolAgent = (
+export default function provideAddPoolAgent(
   alertID: string,
   address: string
-): HandleTransaction => {
+): HandleTransaction {
   return async (TextEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
+    
+    // If the relevnt address is not involved then exit
     if (TextEvent.addresses[address] == false) return findings;
 
+    // If an event with the signature of `ADD_POOL_SIGNATURE` is within the TX
     if (TextEvent.filterEvent(ADD_POOL_SIGNATURE, address).length > 0) {
+      // Create a finding
       findings.push(createFinding(
         "Add Pool",
         "New Pool Added",
         alertID,
         FindingSeverity.Info,
-        FindingType.Unknown
+        FindingType.Unknown,
       ));
     }
+
+    // Return any findings
     return findings;
   };
 };
-
-export default provideAddPoolAgent;
--- Curve-finance-suite/src/agents/registry.remove.pool.ts
+++ Curve-finance-suite/src/agents/registry.remove.pool.ts
@@ -5,20 +5,25 @@ import {
   FindingType,
   HandleTransaction,
 } from "forta-agent";
+
 import createFinding from "../utils/create.finding";
 
 // Event signature found in Registry.vy in Curve repo
 export const REMOVE_POOL_SIGNATURE = "PoolRemoved(address)";
 
-const provideRemovePoolAgent = (
+export default function provideRemovePoolAgent(
   alertID: string,
   address: string
-): HandleTransaction => {
+): HandleTransaction {
   return async (TextEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
+
+    // If the relevant address is not involved then exit
     if (TextEvent.addresses[address] == false) return findings;
 
+    // If an event with the signature of `ADD_POOL_SIGNATURE` is within the TX
     if (TextEvent.filterEvent(REMOVE_POOL_SIGNATURE, address).length > 0) {
+      // Create a finding
       findings.push(createFinding(
         "Remove Pool",
         "Pool Removed",
@@ -27,8 +32,8 @@ const provideRemovePoolAgent = (
         FindingType.Unknown
       ));
     }
+
+    // Return any findings
     return findings;
   };
 };
-
-export default provideRemovePoolAgent;
