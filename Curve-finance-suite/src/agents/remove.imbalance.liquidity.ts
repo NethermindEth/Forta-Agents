d770725e7203640b2897c51a5493ea29737e399c
Yasmine Bachi <gy_bachi@esi.dz>
2021-12-03 09:24:07 +0100

    remove.imbalace.liquidity agent refactoring

--- Curve-finance-suite/src/agents/remove.imbalance.liquidity.ts
+++ Curve-finance-suite/src/agents/remove.imbalance.liquidity.ts
@@ -6,6 +6,12 @@ import {
   FindingType,
 } from 'forta-agent';
 
+import {
+  decodeParameters,
+  FindingGenerator,
+  provideEventCheckerHandler,
+} from "forta-agent-tools";
+
 import abi from '../utils/stable.swap.abi';
 import createFinding from "../utils/create.finding";
 
@@ -14,31 +20,43 @@ import abiDecoder from 'abi-decoder';
 abiDecoder.addABI(abi);
 
 export const REMOVE_LIQUIDITY_IMBALANCE_SIGNATURE =
-  'RemoveLiquidiityImbalance(address, uint256[3], uint256[3],uint256, uint256)';
+  'RemoveLiquidityImbalance(address,uint256[3],uint256[3],uint256,uint256)';
+
+  const createFindingGenerator = (alertID: string): FindingGenerator => {
+    return (metadata: { [key: string]: any } | undefined) => {
+      const { 0: address, 4:token_supply } = decodeParameters(
+        ["address","uint256[3]","uint256[3]","uint256", "uint256"],
+        metadata?.data
+      );
+      return Finding.fromObject({
+        name: "RemoveLiquidityImbalance event Detected",
+        description: "RemoveLiquidityImbalance event emitted on pool",
+        alertId: alertID,
+        severity: FindingSeverity.Low,
+        type: FindingType.Suspicious,
+        metadata: {
+          provider: address,
+          token_supply: token_supply,
+        },
+      });
+    };
+  };
+
 
 export default function provideRemoveLiquidityImbalanceAgent(
   alertID: string,
   address: string
 ): HandleTransaction {
+  const agentHandler = provideEventCheckerHandler(
+    createFindingGenerator(alertID),
+    REMOVE_LIQUIDITY_IMBALANCE_SIGNATURE,
+    address
+  );
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
-    const findings: Finding[] = [];
-
-    if (txEvent.addresses[address] == false) return findings;
-
-    if (
-      txEvent.filterEvent(REMOVE_LIQUIDITY_IMBALANCE_SIGNATURE, address)
-        .length > 0
-    ) {
-      findings.push(createFinding(
-        "RemoveLiquidityImbalance funciton called",
-        "RemoveLiquidityImbalance funciton called on pool",
-        alertID,
-        FindingSeverity.Low,
-        FindingType.Suspicious,
-        { data: address }
-      ));
-    }
+    
+    const findings: Finding[] = await agentHandler(txEvent);
 
     return findings;
   };
+
 }
