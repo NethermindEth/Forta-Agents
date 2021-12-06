e16bbc499a87c010c9633ee7137063351a27f5c3
Yasmine Bachi <gy_bachi@esi.dz>
2021-12-06 22:04:39 +0100

    Added forta-agent-tools dependency and Code formatting

--- Curve-finance-suite/Remove-Imbalace-Liquidity/src/agent.spec.ts
+++ Curve-finance-suite/Remove-Imbalace-Liquidity/src/agent.spec.ts
@@ -4,73 +4,71 @@ import {
   HandleTransaction,
   TransactionEvent
 } from "forta-agent"
-import { createAddress, encodeParameters, TestTransactionEvent } from "forta-agent-tools";
-import { provideRemoveLiquidityImbalanceAgent, iface } from "./agent"
+import { 
+  createAddress, 
+  encodeParameters, 
+  TestTransactionEvent 
+} from "forta-agent-tools";
+import { 
+  provideRemoveLiquidityImbalanceAgent, 
+  iface 
+} from "./agent"
 
 const ADDRESS = createAddress('0x1');
 const ALERTID = 'remove-liquidity-imbalance';
 
-const createFinding = (pool_address: string, token_supply: string) =>  Finding.fromObject({
+const createFinding = (pool_address: string, token_supply: string) => Finding.fromObject({
   name: 'RemoveLiquidityImbalance event Detected',
   description: 'RemoveLiquidityImbalance event emitted',
   alertId: ALERTID,
   protocol: 'ethereum',
   severity: 2,
   type: FindingType.Info,
-  everestId: undefined,
   metadata: {
     pool_address: pool_address,
     token_supply: token_supply
   },
 });
 
-
-
 describe('Remove liquidity imbalance Agent tests suite', () => {
-  const handleTransaction: HandleTransaction = provideRemoveLiquidityImbalanceAgent(ALERTID, ADDRESS);;
+  const handleTransaction: HandleTransaction = provideRemoveLiquidityImbalanceAgent(ALERTID, ADDRESS);
   const data = encodeParameters(
     ["uint256[3]","uint256[3]","uint256", "uint256"],
-    [ [1,2,3],[1,2,3],3,5]
+    [[1,2,3], [1,2,3], 3, 5]
   );
 
-  beforeAll(() => {
-
-  });
-
   it('Should return an empty findings when no event is emitted', async () => {
-
     const txEvent: TransactionEvent = new TestTransactionEvent()
+    
     const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([]);
   });
 
 
   it('Should return a finding when RemoveLiquidityImbalance event is emitted', async () => {
-
     const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
       iface.getEvent('RemoveLiquidityImbalance').format('sighash'),
       ADDRESS,
       data
     );
+
     const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([createFinding(ADDRESS,'5')]);
   });
 
   it('Should return an empty findings because of wrong signature', async () => {
-
     const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
       'signature()',
       ADDRESS,
       data
     );
-    const findings = await handleTransaction(txEvent);
 
+    const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([]);
 
   });
 
   it('Should ignore events emitted by other contracts', async () => {
-
     const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
       iface.getEvent('RemoveLiquidityImbalance').format('sighash'),
       createAddress('0x2'),
