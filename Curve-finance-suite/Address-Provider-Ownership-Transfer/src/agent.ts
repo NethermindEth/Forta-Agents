21fa582e35438f77c6f5bb85fd723cca6672eeb9
Yasmine Bachi <gy_bachi@esi.dz>
2021-12-06 17:22:57 +0100

    Create Address-provider-ownership-transfert agent from template

---
+++ Curve-finance-suite/Address-Provider-Ownership-Transfer/src/agent.ts
@@ -0,0 +1,50 @@
+import {
+  Finding,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  HandleTransaction,
+  ethers,
+} from "forta-agent";
+import {
+  decodeParameters,
+  FindingGenerator,
+  provideEventCheckerHandler,
+} from "forta-agent-tools";
+
+export const ADDRESS = "0x0000000022d53366457f9d5e68ec105046fc4383"; // Registry Address Provider contract address
+const abi = ["event CommitNewAdmin(uint256,address)",]
+export const iface = new ethers.utils.Interface(abi); 
+
+const createFindingGenerator = (alertID: string): FindingGenerator => {
+  return (metadata: { [key: string]: any } | undefined) => {
+   
+    return Finding.fromObject({
+      name: "Curve Admin Event Detected",
+      description: "An ownership transfer has been initiated",
+      alertId: alertID,
+      severity: FindingSeverity.Medium,
+      type: FindingType.Info,
+      metadata: {
+        newAdmin: metadata!.topics[2],
+      },
+    });
+  };
+};
+
+export const provideCommitNewAdminEvent = (
+  alertID: string,
+  address: string
+): HandleTransaction => provideEventCheckerHandler(
+      createFindingGenerator(alertID),
+      iface.getEvent('CommitNewAdmin').format('sighash'),
+      address
+    );
+
+
+export default {
+  handleTransaction: provideCommitNewAdminEvent(
+    "CURVE-3",
+    ADDRESS
+  )
+} 
