ad6c1e662d8cf80debbaea786c083dae635f5434
Yasmine Bachi <gy_bachi@esi.dz>
2021-12-06 22:25:13 +0100

    code formatting

--- Curve-finance-suite/Address-Provider-Ownership-Transfer/src/agent.spec.ts
+++ Curve-finance-suite/Address-Provider-Ownership-Transfer/src/agent.spec.ts
@@ -5,14 +5,19 @@ import {
   FindingType,
   TransactionEvent,
 } from "forta-agent";
-import { TestTransactionEvent, createAddress } from "forta-agent-tools";
-
-import {provideCommitNewAdminEvent, iface } from "./agent";
+import { 
+  TestTransactionEvent, 
+  createAddress 
+} from "forta-agent-tools";
+import {
+  provideCommitNewAdminEvent, 
+  iface 
+} from "./agent";
 
 const ADDRESS = createAddress('0x1');
 const ALERT_ID = "address-provider-ownership-transfer-test";
 const DURATION = "1";
-const NEW_ADMINS= [createAddress('0x4'), createAddress('0x5')]
+const NEW_ADMINS= [createAddress('0x4'), createAddress('0x5')];
 
 const createFinding = (NEW_ADMIN: string) => Finding.fromObject({
   name: "Curve Admin Event Detected",
@@ -25,7 +30,6 @@ const createFinding = (NEW_ADMIN: string) => Finding.fromObject({
   },
 });
 
-
 describe("Transfer Ownership Agent for Registry Contract", () => {
   let handleTransaction: HandleTransaction = provideCommitNewAdminEvent(ALERT_ID, ADDRESS);
   let topics1: string[];
@@ -33,21 +37,17 @@ describe("Transfer Ownership Agent for Registry Contract", () => {
 
   beforeAll(() => {
     topics1= [DURATION, NEW_ADMINS[0]];
-    topics2 = [DURATION,NEW_ADMINS[1]]
-
+    topics2 = [DURATION,NEW_ADMINS[1]];
   });
 
-
   it('Should return an empty findings when no event is emitted', async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent();
 
-    const txEvent: TransactionEvent = new TestTransactionEvent()
     const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([]);
   });
 
-
   it("should return a finding for each CommitNewAdmin event emission", async () => {
-    
     const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
       iface.getEvent('CommitNewAdmin').format('sighash'),
       ADDRESS, 
@@ -59,13 +59,15 @@ describe("Transfer Ownership Agent for Registry Contract", () => {
       '',
       ...topics2 
     );
-    const findings = await handleTransaction(txEvent);
 
-    expect(findings).toStrictEqual([ createFinding(NEW_ADMINS[0]),createFinding(NEW_ADMINS[1])]);
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([ 
+      createFinding(NEW_ADMINS[0]),
+      createFinding(NEW_ADMINS[1]),
+    ]);
   });
 
   it("should return empty finding because of wrong signature", async () => {
-
     const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
       "wrongSignature()",
       ADDRESS,
@@ -74,7 +76,6 @@ describe("Transfer Ownership Agent for Registry Contract", () => {
     );
 
     const findings = await handleTransaction(txEvent);
-
     expect(findings).toStrictEqual([]);
   });
 
@@ -87,7 +88,6 @@ describe("Transfer Ownership Agent for Registry Contract", () => {
     );
 
     const findings = await handleTransaction(txEvent);
-
     expect(findings).toStrictEqual([]);
   });
 });
