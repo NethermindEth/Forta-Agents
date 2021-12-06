68aebf4bf079fd471818afceabbccf4f9fe8a080
RCantu92 <Roberto_Cantu@live.com>
2021-12-06 10:23:57 -0600

    Create directory for pool migration agent

---
+++ Curve-finance-suite/Pool-Migration/src/agent.spec.ts
@@ -0,0 +1,142 @@
+import {
+  Finding,
+  HandleTransaction,
+  FindingSeverity,
+  FindingType,
+  TransactionEvent,
+  Trace,
+} from 'forta-agent';
+import Web3 from 'web3';
+import provideMıgratePoolAgent from './agent';
+import { createAddress } from "forta-agent-tools";
+
+const abi = new Web3().eth.abi;
+const ADDRESS = '0X1111';
+const ALERT_ID = 'test';
+
+interface TraceInfo {
+  from: string;
+  to: string;
+  input: string;
+}
+
+const createTxEvent = (data: TraceInfo[]): TransactionEvent => {
+  const traces: Trace[] = data.map((traceInfo: TraceInfo) => {
+    return {
+      action: {
+        from: traceInfo.from,
+        input: traceInfo.input,
+        to: traceInfo.to,
+      },
+    } as Trace;
+  });
+
+  const txn: TransactionEvent = { traces } as TransactionEvent;
+  return txn;
+};
+
+describe('Pool Migration Agent', () => {
+  let handleTransactions: HandleTransaction;
+
+  beforeAll(() => {
+    handleTransactions = provideMıgratePoolAgent(ALERT_ID, ADDRESS);
+  });
+
+  it('should return a finding', async () => {
+    const _old_pool = createAddress('0x1');
+    const _new_pool = createAddress('0x2');
+    const _from = createAddress('0x3');
+    const _input: string = abi.encodeFunctionCall(
+      {
+        name: 'migrate_to_new_pool',
+        type: 'function',
+        inputs: [
+          {
+            type: 'address',
+            name: '_old_pool',
+          },
+          {
+            type: 'address',
+            name: '_new_pool',
+          },
+          {
+            type: 'uint256',
+            name: '_amount',
+          },
+        ],
+      },
+      [_old_pool, _new_pool, '1000']
+    );
+
+    // NOTE: Attempted to use `createTransactionEvent`
+    // from 'forta-agent', but used different keys
+    // in object. Revisit?
+    const txEvent: TransactionEvent = createTxEvent([
+      { input: _input, to: ADDRESS, from: _from },
+    ]);
+
+    const findings = await handleTransactions(txEvent);
+
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Pool Migration Finding",
+        description: "Pool migrated to new address",
+        alertId: ALERT_ID,
+        severity: FindingSeverity.Medium,
+        type: FindingType.Unknown,
+        metadata: {
+          from: _from,
+          input: _input,
+          to: ADDRESS,
+        },
+      }),
+    ]);
+  });
+
+  it('should return empty finding cause bad input', async () => {
+    const _from = createAddress('0x3');
+    const _input: string = 'bad sig';
+
+    const txEvent: TransactionEvent = createTxEvent([
+      { input: _input, to: ADDRESS, from: _from },
+    ]);
+    const findings = await handleTransactions(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it('should return empty finding cause bad function selector', async () => {
+    const _old_pool = createAddress('0x1');
+    const _new_pool = createAddress('0x2');
+    const _from = createAddress('0x3');
+
+    const _input: string = abi.encodeFunctionCall(
+      {
+        name: 'wrong function selector',
+        type: 'function',
+        inputs: [
+          {
+            type: 'address',
+            name: '_old_pool',
+          },
+          {
+            type: 'address',
+            name: '_new_pool',
+          },
+          {
+            type: 'uint256',
+            name: '_amount',
+          },
+        ],
+      },
+      [_old_pool, _new_pool, '1000']
+    );
+
+    const txEvent: TransactionEvent = createTxEvent([
+      { input: _input, to: ADDRESS, from: _from },
+    ]);
+    const findings = await handleTransactions(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+});
---
+++ Curve-finance-suite/Pool-Migration/src/agent.ts
@@ -0,0 +1,58 @@
+import {
+  Finding,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  HandleTransaction,
+} from 'forta-agent';
+import {
+  FindingGenerator,
+  // provideFunctionCallsDetectorHandler,
+} from 'forta-agent-tools';
+
+// NOTE: Tried using using updated GAM, but could not
+// get it to continue without erroring out. May need
+// documentation for `nethermindeth-general-agents-module'
+// to compare to new `forta-agent-tools`.
+import { provideFunctionCallsDetectorAgent } from 'nethermindeth-general-agents-module';
+
+const POOL_MIGRATION_ADDRESS: string = "0xd6930b7f661257DA36F93160149b031735237594";
+
+// Function signature found in PoolMigrator.vy in Curve repo
+export const MIGRATE_POOL_SIG = 'migrate_to_new_pool(address,address,uint256)';
+
+const createFindingGenerator = (alertId: string): FindingGenerator => {
+  return (metadata: { [key: string]: any } | undefined): Finding => {
+    return Finding.fromObject({
+      name: 'Pool Migration Finding',
+      description: 'Pool migrated to new address',
+      alertId: alertId,
+      severity: FindingSeverity.Medium,
+      type: FindingType.Unknown,
+      metadata,
+    });
+  };
+};
+
+// NOTE: "ı" in migrate not a standard "i". Why?
+// NOTE 02: `provideFunctionCallsDetectorHandler` is
+// what has been giving me problems (check note on L9)
+export default function provideMıgratePoolAgent(
+  alertID: string,
+  contractAddress: string
+): HandleTransaction {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    // NOTE: Attempted to create `agentHandler` outside of
+    // `provideMıgratePoolAgent` but couldn't figure out how
+    // to pass arguments `alertID` and `contractAddress` to
+    // be used by `createFindingGenerator`.
+    // Current implementation follows docs.
+    const agentHandler = provideFunctionCallsDetectorAgent(
+      createFindingGenerator(alertID),
+      MIGRATE_POOL_SIG,
+      { to: contractAddress }
+    );
+    const findings: Finding[] = await agentHandler(txEvent);
+    return findings;
+  };
+}
