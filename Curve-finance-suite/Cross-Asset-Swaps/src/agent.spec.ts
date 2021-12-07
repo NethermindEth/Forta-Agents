98f71775a07ffbfac14aa80401bbaf98c1946a6b
Lázaro Raúl <riglesiasvera@gmail.com>
2021-12-07 14:42:48 -0500

    feat: Cross Asset Swap tests suite

--- Curve-finance-suite/Cross-Asset-Swaps/src/agent.spec.ts
+++ Curve-finance-suite/Cross-Asset-Swaps/src/agent.spec.ts
@@ -1,43 +1,125 @@
-// import {
-//   Finding,
-//   HandleTransaction,
-//   FindingSeverity,
-//   FindingType,
-//   TransactionEvent,
-// } from 'forta-agent';
-// import provideCrossAssetSwap, {
-//   CROSS_CHAIN_SWAP_SIGNATURE,
-// } from '../agents/cross.asset.swap';
-// import createTxEventWithLog from '../utils/create.event.log';
+import {
+  Finding,
+  HandleTransaction,
+  FindingSeverity,
+  FindingType,
+  TransactionEvent,
+} from 'forta-agent';
+import {
+  provideCrossAssetSwap,
+  CROSS_CHAIN_SWAP_IFACE as IFACE,
+} from './agent';
+import { 
+  createAddress, 
+  encodeParameter, 
+  TestTransactionEvent, 
+} from 'forta-agent-tools';
 
-// const ADDRESS = '0x1212';
-// const ALERT_ID = 'test';
+const TARGET = createAddress('0xdead');
+const ALERT_ID = 'cross-asset-swap-test';
 
-// describe('Cross Swap Agent', () => {
-//   let handleTransaction: HandleTransaction;
+const createFinding = (
+  tokenId: string, 
+  owner: string, 
+  synth: string, 
+  balance: string
+) => Finding.fromObject({
+  name: "Cross Asset Swaps Interaction",
+  description: "TokenUpdate event emitted",
+  alertId: ALERT_ID,
+  protocol: "Curve Finance",
+  type: FindingType.Info,
+  severity: FindingSeverity.Info,
+  metadata: {
+    tokenId,
+    owner,
+    synth,
+    balance,
+  },
+});
 
-//   beforeAll(() => {
-//     handleTransaction = provideCrossAssetSwap(ALERT_ID, ADDRESS);
-//   });
+describe('Cross Swap Agent tests suite', () => {
+  const handler: HandleTransaction = provideCrossAssetSwap(
+    ALERT_ID,
+    TARGET,
+  );
 
-//   it('create and send a tx with the tx event', async () => {
-//     const txEvent: TransactionEvent = createTxEventWithLog(
-//       CROSS_CHAIN_SWAP_SIGNATURE,
-//       ADDRESS
-//     );
-//     const findings = await handleTransaction(txEvent);
+  it('should ignore empty transactions', async () => {
+    const tx: TransactionEvent = new TestTransactionEvent();
+    
+    const findings = await handler(tx);
+    expect(findings).toStrictEqual([]);
+  });
 
-//     expect(findings).toStrictEqual([
-//       Finding.fromObject({
-//         name: 'CrossChainSwap funciton called',
-//         description: 'CrossChainSwap funciton called on pool',
-//         alertId: ALERT_ID,
-//         severity: FindingSeverity.Low,
-//         type: FindingType.Suspicious,
-//         metadata: {
-//           data: ADDRESS,
-//         },
-//       }),
-//     ]);
-//   });
-// });
+  it('should ignore events emitted in other contracts', async () => {
+    const tx: TransactionEvent = new TestTransactionEvent()
+      .addEventLog(
+        IFACE.getEvent('TokenUpdate').format("sighash"),
+        createAddress('0xcafe'),
+        encodeParameter('uint256', 200),
+        encodeParameter('uint256', 20),
+        encodeParameter('address', createAddress("0xc1")),
+        encodeParameter('address', createAddress("0xc2")),
+      )
+      .addEventLog(
+        IFACE.getEvent('TokenUpdate').format("sighash"),
+        createAddress('0xffff'),
+        encodeParameter('uint256', 1),
+        encodeParameter('uint256', 1),
+        encodeParameter('address', createAddress("0xc12")),
+        encodeParameter('address', createAddress("0xc23")),
+      );
+
+    const findings = await handler(tx);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it('should ignore events other events', async () => {
+    const tx: TransactionEvent = new TestTransactionEvent()
+      .addEventLog(
+        'TokenUpdated(uint256,address,address,uint256)',
+        createAddress('0xcafe'),
+        encodeParameter('uint256', 200),
+        encodeParameter('uint256', 20),
+        encodeParameter('address', createAddress("0xc1")),
+        encodeParameter('address', createAddress("0xc2")),
+      )
+      .addEventLog(
+        'FooEvent(uint256,address,address,uint256)',
+        createAddress('0xffff'),
+        encodeParameter('uint256', 111),
+        encodeParameter('uint256', 12),
+        encodeParameter('address', createAddress("0xdead")),
+        encodeParameter('address', createAddress("0xcafe")),
+      );
+
+    const findings = await handler(tx);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it('should detect the TokenUpdate events in the target address', async () => {
+    const tx: TransactionEvent = new TestTransactionEvent()
+      .addEventLog(
+        IFACE.getEvent('TokenUpdate').format("sighash"),
+        TARGET,
+        encodeParameter('uint256', 200),
+        encodeParameter('uint256', 20),
+        encodeParameter('address', createAddress("0xc1")),
+        encodeParameter('address', createAddress("0xc2")),
+      )
+      .addEventLog(
+        IFACE.getEvent('TokenUpdate').format("sighash"),
+        TARGET,
+        encodeParameter('uint256', 111),
+        encodeParameter('uint256', 12),
+        encodeParameter('address', createAddress("0xa")),
+        encodeParameter('address', createAddress("0xb")),
+      );
+
+    const findings = await handler(tx);
+    expect(findings).toStrictEqual([
+      createFinding("20", createAddress("0xc1"), createAddress("0xc2"), "200"),
+      createFinding("12", createAddress("0xa"), createAddress("0xb"), "111"),
+    ]);
+  });
+});
--- Curve-finance-suite/Cross-Asset-Swaps/src/agent.ts
+++ Curve-finance-suite/Cross-Asset-Swaps/src/agent.ts
@@ -9,28 +9,28 @@ import { utils } from 'ethers';
 
 const SYNTH_SWAP_ADDRESS: string = "0x58A3c68e2D3aAf316239c003779F71aCb870Ee47";
 export const CROSS_CHAIN_SWAP_SIGNATURE: string = 
-  'evet TokenUpdate(indexed uint256 token_id, indexed address owner, indexed address synth, uint256 balance)';
+  'event TokenUpdate(uint256 indexed token_id, address indexed owner, address indexed synth, uint256 balance)';
 export const CROSS_CHAIN_SWAP_IFACE: utils.Interface = new utils.Interface([CROSS_CHAIN_SWAP_SIGNATURE]);
 
 export const provideCrossAssetSwap = (
   alertID: string,
-  address: string
+  address: string,
 ): HandleTransaction => async (txEvent: TransactionEvent): Promise<Finding[]> => 
   txEvent.filterLog(
     CROSS_CHAIN_SWAP_SIGNATURE,
-    address
+    address,
   ).map((log: utils.LogDescription) => Finding.fromObject({
     name: "Cross Asset Swaps Interaction",
     description: "TokenUpdate event emitted",
     alertId: alertID,
-    protocol: "Curve",
+    protocol: "Curve Finance",
     type: FindingType.Info,
     severity: FindingSeverity.Info,
     metadata: {
       tokenId: `${log.args["token_id"]}`,
-      owner: log.args["owner"],
-      synth: log.args["synth"],
-      balance: `${log.args["underlying_balance"]}`,
+      owner: log.args["owner"].toLowerCase(),
+      synth: log.args["synth"].toLowerCase(),
+      balance: `${log.args["balance"]}`,
     }
   }));
   
