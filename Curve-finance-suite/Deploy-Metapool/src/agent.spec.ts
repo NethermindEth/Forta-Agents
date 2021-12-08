74aa0dbee347750ea1ef4ab4dafef877022e68f2
RCantu92 <Roberto_Cantu@live.com>
2021-12-08 14:27:55 -0600

    Import GAM TestTransactionEvent for first unit test

--- Curve-finance-suite/Deploy-Metapool/src/agent.spec.ts
+++ Curve-finance-suite/Deploy-Metapool/src/agent.spec.ts
@@ -7,29 +7,45 @@ import {
 } from 'forta-agent';
 import {
   provideMetaPoolDeployment,
-  DEPLOY_META_POOL_SIGNATURE,
+  metaPoolAbi,
 } from './agent';
+import { 
+  createAddress,
+  TestTransactionEvent,
+  encodeParameters,
+} from "forta-agent-tools";
 
-import createTxEventWithLog from '../utils/create.event.log';
-
-const ADDRESS = '0x1212';
+const TEST_FACTORY_ADDRESS = createAddress('0x1212');
 const ALERT_ID = 'test';
 
 describe('Meta Pool Deployment Agent', () => {
   let handleTransaction: HandleTransaction;
 
+  const basePoolAddress: string = createAddress('0x1');
+  const coinAddress: string = createAddress('0x2');
+  const ampCoef: number = 10;
+  const fee: number = 100;
+  const msgSender: string = createAddress('0x3');
+
+  const data: string = encodeParameters(
+    ["address","address","uint256", "uint256", "address"],
+    [coinAddress, basePoolAddress, ampCoef, fee, msgSender]
+  );
+
   beforeAll(() => {
-    handleTransaction = provideMetaPoolDeployment(ALERT_ID, ADDRESS);
+    handleTransaction = provideMetaPoolDeployment(ALERT_ID, TEST_FACTORY_ADDRESS);
   });
 
   it('should return a finding', async () => {
-    const txEvent: TransactionEvent = createTxEventWithLog(
-      DEPLOY_META_POOL_SIGNATURE,
-      ADDRESS
-    );
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addInvolvedAddresses(TEST_FACTORY_ADDRESS, msgSender)
+      .addEventLog(metaPoolAbi,TEST_FACTORY_ADDRESS,data);
+
+    console.log("the txEvent in the first test is: " + JSON.stringify(txEvent));
 
     const findings = await handleTransaction(txEvent);
 
+    /*
     expect(findings).toStrictEqual([
       Finding.fromObject({
         name: 'Deploy Meta Pool Event',
@@ -37,12 +53,27 @@ describe('Meta Pool Deployment Agent', () => {
         alertId: ALERT_ID,
         severity: FindingSeverity.Info,
         type: FindingType.Unknown,
+        metadata: {
+          coin: coinAddress,
+          basePool: basePoolAddress,
+          a: ampCoef.toString(),
+          fee: fee.toString(),
+          deployer: msgSender
+        }
       }),
     ]);
+    */
   });
 
+  /*
+  // NOTE: Revisit this after figuring out if necessary to encode
+  // and decode.
   it('should return empty finding cause bad signature', async () => {
-    const txEvent: TransactionEvent = createTxEventWithLog('badSig', ADDRESS);
+    const badData: string = encodeParameters(['string'],['badSig']);
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addInvolvedAddresses(TEST_FACTORY_ADDRESS, msgSender)
+      .addEventLog(metaPoolAbi, TEST_FACTORY_ADDRESS, badData);
 
     const findings = await handleTransaction(txEvent);
 
@@ -50,13 +81,13 @@ describe('Meta Pool Deployment Agent', () => {
   });
 
   it('should return empty finding cause wrong Address', async () => {
-    const txEvent: TransactionEvent = createTxEventWithLog(
-      DEPLOY_META_POOL_SIGNATURE,
-      '0x1111'
-    );
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addInvolvedAddresses(TEST_FACTORY_ADDRESS, msgSender)
+      .addEventLog(metaPoolAbi,'0x1111');
 
     const findings = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
   });
+  */
 });
--- Curve-finance-suite/Deploy-Metapool/src/agent.ts
+++ Curve-finance-suite/Deploy-Metapool/src/agent.ts
@@ -5,24 +5,52 @@ import {
   FindingSeverity,
   FindingType,
 } from "forta-agent";
-// import createFinding from "../utils/create.finding";
+import { utils } from 'ethers';
+import {
+  provideEventCheckerHandler,
+  FindingGenerator,
+  decodeParameters
+} from "forta-agent-tools";
+
 
 // Event signature found in Factory.vy in Curve repo
+/*
 export const DEPLOY_META_POOL_SIGNATURE =
   "MetaPoolDeployed(address,address,uint256,uint256,address)";
+*/
 
 const CURVE_FACTORY_ADDRESS: string = "0x0959158b6040D32d04c301A72CBFD6b39E21c9AE";
 
-// NOTE FROM PR COMMENTS: Add metadata
-// to make Finding more useful
-const createFinding = (alertID: string): Finding => {
-  return Finding.fromObject({
-    name: "Deploy Meta Pool Event",
-    description: "New meta pool is deployed",
-    alertId: alertID,
-    severity: FindingSeverity.Info,
-    type: FindingType.Unknown,
-  });
+export const metaPoolAbi: string = 'event MetaPoolDeployed(address,address,uint256,uint256,address)';
+export const FACTORY_IFACE: utils.Interface = new utils.Interface([
+  metaPoolAbi,
+]);
+
+const createFindingGenerator = (alertId: string): FindingGenerator => {
+
+  console.log("Made it to the very beginning of createFindingGenerator");
+
+  return (metadata: { [key: string]: any } | undefined): Finding => {
+    const output = decodeParameters(
+      ["address","address","uint256", "uint256", "address"],
+      metadata?.data
+    );
+
+    return Finding.fromObject({
+      name: "Deploy Meta Pool Event",
+      description: "New meta pool is deployed",
+      alertId: alertId,
+      severity: FindingSeverity.Info,
+      type: FindingType.Unknown,
+      metadata:{
+        coin: output[0],
+        basePool: output[1],
+        a: output[2],
+        fee: output[3],
+        deployer: output[4]
+      },
+    });
+  };
 };
 
 export function provideMetaPoolDeployment(
@@ -30,14 +58,12 @@ export function provideMetaPoolDeployment(
   address: string
 ): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
-    const findings: Finding[] = [];
-
-    if (!txEvent.addresses[address]) return findings;
-
-    if (txEvent.filterEvent(DEPLOY_META_POOL_SIGNATURE, address).length > 0) {
-      findings.push(createFinding(alertID));
-    }
-
+    const handler = provideEventCheckerHandler(
+      createFindingGenerator(alertID),
+      FACTORY_IFACE.getEvent('MetaPoolDeployed').format('sighash'),
+      address
+    );
+    const findings: Finding[] = await handler(txEvent);
     return findings;
   };
 }
