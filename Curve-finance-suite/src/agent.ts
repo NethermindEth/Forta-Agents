310329b7320a6efac3884461cc10ab915c40d6c0
RCantu92 <Roberto_Cantu@live.com>
2021-12-08 14:35:43 -0600

    Delete pool migration agent imports into old files

--- Curve-finance-suite/src/agent.ts
+++ Curve-finance-suite/src/agent.ts
@@ -1,6 +1,6 @@
 import provideCrossAssetSwap from "./agents/cross.asset.swap";
-import provideKillMeAgent from "./agents/kill.me"; // Yasmine doing this
-import provideRemoveLiquidityImbalanceAgent from "./agents/remove.imbalance.liquidity"; // Yasmine doing this
+import provideKillMeAgent from "./agents/kill.me";
+import provideRemoveLiquidityImbalanceAgent from "./agents/remove.imbalance.liquidity";
 import provideUnkillAgent from "./agents/unkill";
 import provideAddPoolAgent from "./agents/registry.add.pool";
 import provideRemovePoolAgent from "./agents/registry.remove.pool";
@@ -10,7 +10,6 @@ import providesetKilledAgent from "./agents/curve.dao.killing.gauge";
 import providesetRewardsAgent from "./agents/curve.gauge.set.rewards";
 import provideCommitNewAdminEvent from "./agents/address.provider.ownership.transfer";
 import provideclaimManyAgent from "./agents/curve.dao.claim.many";
-// import provideMıgratePoolAgent from "./agents/pool.migration";
 import provideApplyNewFeesAgent from "./agents/apply.newfee";
 import provideRampAgent from "./agents/ramp";
 import provideStompRampAgent from "./agents/stop.ramp";
@@ -27,7 +26,6 @@ export {
   providecreateLockAgent,
   providesetRewardsAgent,
   provideCommitNewAdminEvent,
-  // provideMıgratePoolAgent,
   provideclaimManyAgent,
   provideApplyNewFeesAgent,
   provideRampAgent,
