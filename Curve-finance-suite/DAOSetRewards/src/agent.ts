d335a109165a18d0d91a80d1cbd4b4437bc720d3
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-12-07 21:34:54 -0500

    feat: Add agent logic

---
+++ Curve-finance-suite/DAOSetRewards/src/agent.ts
@@ -0,0 +1,36 @@
+import {
+  Finding,
+  HandleTransaction,
+  FindingSeverity,
+  FindingType,
+} from "forta-agent";
+import { provideFunctionCallsDetectorHandler } from "forta-agent-tools";
+import { gaugeInterface } from "./abi";
+
+export const createFinding = (callInfo: any): Finding => {
+  return Finding.fromObject({
+    name: "DAO Set Gauge Rewards",
+    description: "Curve DAO try to set Gauge Rewards Options",
+    alertId: "CURVE-11",
+    protocol: "Curve Finance",
+    type: FindingType.Info,
+    severity: FindingSeverity.Medium,
+    metadata: {
+      gauge: callInfo.to,
+      rewardContract: callInfo.arguments[0],
+      sigs: callInfo.arguments[1],
+      rewardToken: callInfo.arguments[2].toString(),
+    },
+  });
+};
+
+export const provideHandleTransaction = (daoCurve: string): HandleTransaction =>
+  provideFunctionCallsDetectorHandler(
+    createFinding,
+    gaugeInterface.getFunction("set_rewards").format("sighash"),
+    { from: daoCurve }
+  );
+
+export default {
+  handleTransaction: provideHandleTransaction(""),
+};
