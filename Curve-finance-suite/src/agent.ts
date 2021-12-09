02889fb3ab62e835d4914f970d9f6c803d8910de
RCantu92 <Roberto_Cantu@live.com>
2021-12-09 13:36:12 -0600

    Delete import and export in original agent.ts

--- Curve-finance-suite/src/agent.ts
+++ Curve-finance-suite/src/agent.ts
@@ -1,10 +1,9 @@
 import provideCrossAssetSwap from "./agents/cross.asset.swap";
-import provideKillMeAgent from "./agents/kill.me"; // Yasmine doing this
-import provideRemoveLiquidityImbalanceAgent from "./agents/remove.imbalance.liquidity"; // Yasmine doing this
+import provideKillMeAgent from "./agents/kill.me";
+import provideRemoveLiquidityImbalanceAgent from "./agents/remove.imbalance.liquidity";
 import provideUnkillAgent from "./agents/unkill";
 import provideAddPoolAgent from "./agents/registry.add.pool";
 import provideRemovePoolAgent from "./agents/registry.remove.pool";
-import provideMetaPoolDeployment from "./agents/deploy.metapool";
 import providecreateLockAgent from "./agents/curve.dao.create.lockevent";
 import providesetKilledAgent from "./agents/curve.dao.killing.gauge";
 import providesetRewardsAgent from "./agents/curve.gauge.set.rewards";
@@ -23,7 +22,6 @@ export {
   provideUnkillAgent,
   provideAddPoolAgent,
   provideRemovePoolAgent,
-  provideMetaPoolDeployment,
   providecreateLockAgent,
   providesetRewardsAgent,
   provideCommitNewAdminEvent,
