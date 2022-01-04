f97eeb80aa27d825503650f648454762618e9884
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-04 11:54:15 -0500

    chore: Initial Agent

---
+++ OlympusDAO-Agent-Suit/OwnershipTransfers/src/agent.spec.ts
@@ -0,0 +1,12 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent
+} from "forta-agent";
+import agent from "./agent";
+
+describe("Ownership Transfers Test Suite", () => {
+  it("should do something", async () => {});
+});
---
+++ OlympusDAO-Agent-Suit/OwnershipTransfers/src/agent.ts
@@ -0,0 +1,18 @@
+import { 
+  Finding, 
+  HandleTransaction, 
+  TransactionEvent, 
+  FindingSeverity, 
+  FindingType 
+} from 'forta-agent';
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = [];
+
+
+  return findings;
+};
+
+export default {
+  handleTransaction,
+};
