4a1cb92a350b7b13e1b80f316f89632946ec38d4
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-04 15:07:46 -0500

    test: Create Test Suite

--- OlympusDAO-Agent-Suit/StakingContractOnlyPolicy/src/agent.spec.ts
+++ OlympusDAO-Agent-Suit/StakingContractOnlyPolicy/src/agent.spec.ts
@@ -1,48 +1,87 @@
 import {
-  FindingType,
-  FindingSeverity,
-  Finding,
   HandleTransaction,
-  createTransactionEvent
 } from "forta-agent"
-import agent from "./agent"
+import { provideHandleTransaction, FUNCTION_ABIS } from "./agent"
+import { TestTransactionEvent, createAddress } from "forta-agent-tools";
+import { ethers } from "ethers";
+import { createFinding } from "./utils";
 
-describe("high gas agent", () => {
-  let handleTransaction: HandleTransaction
 
-  const createTxEventWithGasUsed = (gasUsed: string) => createTransactionEvent({
-    transaction: {} as any,
-    receipt: { gasUsed } as any,
-    block: {} as any,
-  })
+describe("State Contract Mangement Tests", () => {
+  let handleTransaction: HandleTransaction;
+  let stakeContractAddress: string;
+  let contractInterface: ethers.utils.Interface;
+
 
   beforeAll(() => {
-    handleTransaction = agent.handleTransaction
-  })
+    stakeContractAddress = createAddress("0x1");
+    contractInterface = new ethers.utils.Interface(FUNCTION_ABIS);
+    handleTransaction = provideHandleTransaction(FUNCTION_ABIS, stakeContractAddress);
+  });
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if gas used is below threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1")
+  it("should return empty findings if no call is done", async () => {
+    const txEvent = new TestTransactionEvent();
 
-      const findings = await handleTransaction(txEvent)
+    const findings = await handleTransaction(txEvent)
 
-      expect(findings).toStrictEqual([])
-    })
+    expect(findings).toStrictEqual([])
+  });
 
-    it("returns a finding if gas used is above threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1000001")
+  it("should return a finding if addRecipient method was called on the stake contract", async () => {
+    const txEvent = new TestTransactionEvent().addTraces({
+      to: stakeContractAddress,
+      input: contractInterface.encodeFunctionData("addRecipient", [createAddress("0x1"), 100]),
+    });
 
-      const findings = await handleTransaction(txEvent)
+    const findings = await handleTransaction(txEvent)
 
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Gas Used",
-          description: `Gas Used: ${txEvent.gasUsed}`,
-          alertId: "FORTA-1",
-          type: FindingType.Suspicious,
-          severity: FindingSeverity.Medium
-        }),
-      ])
-    })
-  })
+    expect(findings).toStrictEqual([ createFinding("addRecipient") ]);
+  });
+
+  it("should return a finding if removeRecipient method was called on the stake contract", async () => {
+    const txEvent = new TestTransactionEvent().addTraces({
+      to: stakeContractAddress,
+      input: contractInterface.encodeFunctionData("removeRecipient", [1, createAddress("0x1")]),
+    });
+
+    const findings = await handleTransaction(txEvent)
+
+    expect(findings).toStrictEqual([ createFinding("removeRecipient") ]);
+  });
+
+  it("should return a finding if setAdjustment method was called on the stake contract", async () => {
+    const txEvent = new TestTransactionEvent().addTraces({
+      to: stakeContractAddress,
+      input: contractInterface.encodeFunctionData("setAdjustment", [100, true, 1, 5]),
+    });
+
+    const findings = await handleTransaction(txEvent)
+
+    expect(findings).toStrictEqual([ createFinding("setAdjustment") ]);
+  });
+
+  it("should ignore correct function calls but in different contracts", async () => {
+    const txEvent = new TestTransactionEvent().addTraces({
+      to: createAddress("0x3"),
+      input: contractInterface.encodeFunctionData("addRecipient", [createAddress("0x1"), 100]),
+    });
+
+    const findings = await handleTransaction(txEvent)
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("should return multiple findings if multiple relevant methods are called in the correct contract", async () => {
+    const txEvent = new TestTransactionEvent().addTraces({
+      to: stakeContractAddress,
+      input: contractInterface.encodeFunctionData("addRecipient", [createAddress("0x1"), 100]),
+    },{
+      to: stakeContractAddress,
+      input: contractInterface.encodeFunctionData("removeRecipient", [2, createAddress("0x1")]),
+    });
+
+    const findings = await handleTransaction(txEvent)
+
+    expect(findings).toStrictEqual([ createFinding("addRecipient"), createFinding("removeRecipient") ]);
+  });
 })
