59100eb01cf0fd577c7e47c6a777fd22bd95f0ce
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-04 09:57:48 -0500

    chore: Initial Agent

---
+++ OlympusDAO-Agent-Suit/TotalReserves/src/agent.spec.ts
@@ -0,0 +1,20 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent
+} from "forta-agent"
+import agent from "./agent"
+
+describe("high gas agent", () => {
+  describe("handleTransaction", () => {
+    it("returns empty findings if gas used is below threshold", async () => {
+      
+    })
+
+    it("returns a finding if gas used is above threshold", async () => {
+     
+    })
+  })
+})
---
+++ OlympusDAO-Agent-Suit/TotalReserves/src/agent.ts
@@ -0,0 +1,22 @@
+import { 
+  BlockEvent, 
+  Finding, 
+  HandleBlock, 
+  HandleTransaction, 
+  TransactionEvent, 
+  FindingSeverity, 
+  FindingType 
+} from 'forta-agent';
+
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = [];
+
+
+  return findings;
+};
+
+
+export default {
+  handleTransaction
+};
