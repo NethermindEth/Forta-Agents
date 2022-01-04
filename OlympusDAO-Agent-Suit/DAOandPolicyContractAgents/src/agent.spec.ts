1d9a78dc00eb08a9acf7c3d9ff7bf350b3d8eec3
Murat Çeliktepe <celiktepemurat@gmail.com>
2022-01-04 12:51:25 +0300

    chore: clean up agent

--- OlympusDAO-Agent-Suit/DAOandPolicyContractAgents/src/agent.spec.ts
+++ OlympusDAO-Agent-Suit/DAOandPolicyContractAgents/src/agent.spec.ts
@@ -3,46 +3,14 @@ import {
   FindingSeverity,
   Finding,
   HandleTransaction,
-  createTransactionEvent
-} from "forta-agent"
-import agent from "./agent"
+  createTransactionEvent,
+} from 'forta-agent';
+import agent from './agent';
 
-describe("high gas agent", () => {
-  let handleTransaction: HandleTransaction
-
-  const createTxEventWithGasUsed = (gasUsed: string) => createTransactionEvent({
-    transaction: {} as any,
-    receipt: { gasUsed } as any,
-    block: {} as any,
-  })
+describe('high gas agent', () => {
+  let handleTransaction: HandleTransaction;
 
   beforeAll(() => {
-    handleTransaction = agent.handleTransaction
-  })
-
-  describe("handleTransaction", () => {
-    it("returns empty findings if gas used is below threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1")
-
-      const findings = await handleTransaction(txEvent)
-
-      expect(findings).toStrictEqual([])
-    })
-
-    it("returns a finding if gas used is above threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1000001")
-
-      const findings = await handleTransaction(txEvent)
-
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Gas Used",
-          description: `Gas Used: ${txEvent.gasUsed}`,
-          alertId: "FORTA-1",
-          type: FindingType.Suspicious,
-          severity: FindingSeverity.Medium
-        }),
-      ])
-    })
-  })
-})
+    handleTransaction = agent.handleTransaction;
+  });
+});
