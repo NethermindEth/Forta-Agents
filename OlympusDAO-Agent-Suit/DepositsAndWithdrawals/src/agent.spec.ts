663504459a1485a2cb3a609d7c6d174bf24e16ef
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-04 13:32:59 -0500

    chore: Initial Agent

---
+++ OlympusDAO-Agent-Suit/DepositsAndWithdrawals/src/agent.spec.ts
@@ -0,0 +1,12 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent
+} from "forta-agent"
+import agent from "./agent";
+
+describe("high gas agent", () => {
+  it("should do something", async () => {})
+})
