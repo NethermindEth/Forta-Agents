6ac55b88041f62d5b21d7dc34a9a7bbeb3cd247c
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-04 10:48:24 -0500

    feat: Agent Implementation

--- OlympusDAO-Agent-Suit/TotalReserves/src/agent.ts
+++ OlympusDAO-Agent-Suit/TotalReserves/src/agent.ts
@@ -1,22 +1,51 @@
 import { 
-  BlockEvent, 
   Finding, 
-  HandleBlock, 
-  HandleTransaction, 
   TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
+  getEthersProvider,
+  LogDescription
 } from 'forta-agent';
+import abi from './abi';
+import { BigNumber } from 'ethers';
+import ReserveFetcher from './reserve.fetcher';
+import finding from "./findings";
 
+const BIG_CHANGE_PERCENT: number = 20;
+const TREASURY_CONTRACT: string = "0x31F8Cc382c9898b273eff4e0b7626a6987C846E8";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = [];
+const provideHandleTransaction = (fetcher: ReserveFetcher) =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
 
+    const events: LogDescription[] = txEvent.filterLog(
+      abi.TREASURY_ABI,
+      fetcher.treasury,
+    );
 
-  return findings;
-};
+    let reserve: BigNumber = await fetcher.getReserve(txEvent.blockNumber);
+
+    events.forEach((log: LogDescription) => {
+      const logReserve: BigNumber = log.args["totalReserves"];
+      findings.push(
+        ...finding.checkValues(
+          reserve, 
+          logReserve,
+          BIG_CHANGE_PERCENT,
+        )
+      );
+      reserve = logReserve;
+    });
+
+    fetcher.set(txEvent.blockNumber, reserve);
+
+    return findings;
+  };
 
 
 export default {
-  handleTransaction
+  handleTransaction: provideHandleTransaction(
+    new ReserveFetcher(
+      TREASURY_CONTRACT,
+      getEthersProvider(),
+    ),
+  ),
 };
---
+++ OlympusDAO-Agent-Suit/TotalReserves/src/findings.ts
@@ -0,0 +1,59 @@
+import { 
+  Finding, 
+  FindingSeverity, 
+  FindingType,
+} from 'forta-agent';
+import { BigNumber } from 'ethers';
+
+const reserveIncreasement = (
+  initialReserve: BigNumber, 
+  curReserve: BigNumber, 
+  percentChange: number
+): Finding => Finding.fromObject({
+  name: "OlympusDAO Treasury totalReserves monitor",
+  description: "Big increasement detected",
+  alertId: "olympus-2-1",
+  severity: FindingSeverity.Info,
+  type: FindingType.Info,
+  metadata: {
+    totalReserve: initialReserve.toString(),
+    changedTo: curReserve.toString(),
+    increasedBy: `${percentChange}% or more`,
+  },
+});
+
+const reserveDecreasement = (
+  initialReserve: BigNumber, 
+  curReserve: BigNumber, 
+  percentChange: number
+): Finding => Finding.fromObject({
+  name: "OlympusDAO Treasury totalReserves monitor",
+  description: "Big decreasement detected",
+  alertId: "olympus-2-2",
+  severity: FindingSeverity.Info,
+  type: FindingType.Info,
+  metadata: {
+    totalReserve: initialReserve.toString(),
+    changedTo: curReserve.toString(),
+    decreasedBy: `${percentChange}% or more`,
+  },
+});
+
+const checkValues = (
+  initialReserve: BigNumber, 
+  curReserve: BigNumber, 
+  percentChange: number
+): Finding[] => {
+  const percent: BigNumber = initialReserve
+    .mul(BigNumber.from(percentChange))
+    .div(BigNumber.from(100));
+  if(curReserve.gte(initialReserve.add(percent)))
+    return [reserveIncreasement(initialReserve, curReserve, percentChange)];
+  if(curReserve.lte(initialReserve.sub(percent)))
+    return [reserveDecreasement(initialReserve, curReserve, percentChange)];
+  return [];
+};
+
+export default {
+  checkValues,
+};
