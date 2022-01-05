25c7930f02876acd5ee355002d53156cb027af5f
Murat Çeliktepe <celiktepemurat@gmail.com>
2022-01-05 13:27:47 +0300

    refac: update structure of agent.ts

--- OlympusDAO-Agent-Suit/Bond-OwnershipChanges/src/agent.ts
+++ OlympusDAO-Agent-Suit/Bond-OwnershipChanges/src/agent.ts
@@ -3,17 +3,12 @@ import { BONDs, ABIs, createFinding } from './utils';
 
 export const provideHandleTransaction =
   (_bonds: string[]): HandleTransaction =>
-  async (txEvent: TransactionEvent): Promise<Finding[]> => {
-    const promises = _bonds
+  async (txEvent: TransactionEvent): Promise<Finding[]> =>
+    _bonds
       .map((bond) =>
         txEvent.filterLog(ABIs, bond).map((log) => createFinding(log, bond))
       )
       .flat();
-
-    const findings = await Promise.all(promises);
-
-    return findings;
-  };
 
 export default {
   handleTransaction: provideHandleTransaction(BONDs),
