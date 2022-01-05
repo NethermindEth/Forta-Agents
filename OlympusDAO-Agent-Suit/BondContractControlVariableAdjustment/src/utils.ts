7bc28e8b9d702af2f26639c74268846b86f3a964
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-05 18:56:29 -0500

    feat: Add createFinding util function

--- OlympusDAO-Agent-Suit/BondContractControlVariableAdjustment/src/utils.ts
+++ OlympusDAO-Agent-Suit/BondContractControlVariableAdjustment/src/utils.ts
@@ -1,12 +1,25 @@
-import { Finding } from "forta-agent";
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
 import { providers, utils, BigNumber, Contract } from "ethers";
 import { redeemHelperInterface } from "./abi";
 import LRU from "lru-cache";
 
 const bondsCache: LRU<number, Promise<string[]>> = new LRU({ max: 10000 });
 
-export const createFinding = (): Finding => {
-  return undefined as any;
+export const createFinding = (initialBCV: string, newBCV: string, adjustment: string, addition: string): Finding => {
+  return Finding.fromObject({
+    name: "Bond Contract ControlVariableAdjustment Event",
+    description: "A ControlVariableAdjustment event was emitted from a bond contract",
+    alertId: "OlympusDAO-X",
+    protocol: "OlympusDAO",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    metadata: {
+      initialBCV: initialBCV,
+      newBCV: newBCV,
+      adjustment: adjustment,
+      addition: addition,
+    }
+  });
 };
 
 const getBondsLength = async (redeemHelperAddress: string, block: number, provider: providers.Provider): Promise<BigNumber>  => {
