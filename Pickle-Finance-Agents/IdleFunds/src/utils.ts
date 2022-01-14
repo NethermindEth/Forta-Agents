e7fc78b604e0beade3252a855aba18d84cd414fd
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-14 12:15:53 -0500

    feat: Add util function for getting jar balance

--- Pickle-Finance-Agents/IdleFunds/src/utils.ts
+++ Pickle-Finance-Agents/IdleFunds/src/utils.ts
@@ -1,11 +1,36 @@
 import { pickleJarInterface, pickleRegistryInterface } from "./abi";
-import { Contract, providers } from "ethers";
+import { Contract, providers, BigNumber } from "ethers";
 
-export const getPickleJars = async (pickleRegistryAddress: string, blockNumber: number, provider: providers.Provider): Promise<string[]> => {
-  const pickleRegistry = new Contract(pickleRegistryAddress, pickleRegistryInterface, provider);
+export const getPickleJars = async (
+  pickleRegistryAddress: string,
+  blockNumber: number,
+  provider: providers.Provider
+): Promise<string[]> => {
+  const pickleRegistry = new Contract(
+    pickleRegistryAddress,
+    pickleRegistryInterface,
+    provider
+  );
 
-  const developmentVaultsPromise: Promise<string[]> = pickleRegistry.developmentVaults({ blockTag: blockNumber });
-  const productionVaultsPromise: Promise<string[]>  = pickleRegistry.productionVaults({ blockTag: blockNumber });
+  const developmentVaultsPromise: Promise<string[]> =
+    pickleRegistry.developmentVaults({ blockTag: blockNumber });
+  const productionVaultsPromise: Promise<string[]> =
+    pickleRegistry.productionVaults({ blockTag: blockNumber });
 
   return (await developmentVaultsPromise).concat(await productionVaultsPromise);
-}
+};
+
+export const getPickleJarsBalance = async (
+  pickleJarAddress: string,
+  blockNumber: number,
+  provider: providers.Provider
+): Promise<BigNumber> => {
+  const pickleJar = new Contract(
+    pickleJarAddress,
+    pickleJarInterface,
+    provider
+  );
+
+  return pickleJar.balance({ blockTag: blockNumber });
+};
+
