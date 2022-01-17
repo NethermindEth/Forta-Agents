dec2eb34d05b0abe6dc6ce8b64cd35baeefdbfcf
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-17 13:48:10 -0500

    feat: Agent implementation

---
+++ Pickle-Finance-Agents/LargeDepositsWithdrawals/src/findings.ts
@@ -0,0 +1,33 @@
+import { BigNumber } from "ethers";
+import { Finding, FindingSeverity, FindingType, LogDescription } from "forta-agent";
+
+type FindingGenerator = (_: LogDescription) => Finding;
+
+const DepositFinding: FindingGenerator = (log: LogDescription): Finding => Finding.fromObject({
+  name: "Pickle Jar Deposit",
+  description: "Large deposit detected",
+  alertId: "pickle-2-1",
+  severity: FindingSeverity.Info,
+  type: FindingType.Info,
+  protocol: "Pickle Finance",
+  metadata: {
+    amount: log.args[2].toString(),
+    from: log.args[1].toLowerCase(),
+  }
+});
+
+const WhitdrawFinding: FindingGenerator = (log: LogDescription): Finding => Finding.fromObject({
+  name: "Pickle Jar Withdraw",
+  description: "Large withdraw detected",
+  alertId: "pickle-2-2",
+  severity: FindingSeverity.Info,
+  type: FindingType.Info,
+  protocol: "Pickle Finance",
+  metadata: {
+    amount: log.args[2].toString(),
+    from: log.args[0].toLowerCase(),
+  }
+});
+
+export const createFinding: FindingGenerator = (log: LogDescription): Finding =>
+  BigNumber.from(0).eq(log.args[0])? DepositFinding(log) : WhitdrawFinding(log);
