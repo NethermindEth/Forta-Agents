81179d9f2120ce4f64e56f2848e79e46092f070f
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-17 08:51:36 -0500

    feat: Initial Agent

---
+++ Pickle-Finance-Agents/LargeDepositsWithdrawals/src/agent.spec.ts
@@ -0,0 +1,14 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent
+} from "forta-agent"
+import agent from "./agent"
+
+describe("Large Withdrawals/Deposits monitor agent tests suite", () => {
+    it("should do something", async () => {
+
+    })
+})
