838080a1dd544f5e951528f79245d3b3a8887ede
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-17 15:32:14 -0500

    test: Add Agent tests suite

--- Pickle-Finance-Agents/LargeDepositsWithdrawals/src/findings.ts
+++ Pickle-Finance-Agents/LargeDepositsWithdrawals/src/findings.ts
@@ -11,12 +11,13 @@ const DepositFinding: FindingGenerator = (log: LogDescription): Finding => Findi
   type: FindingType.Info,
   protocol: "Pickle Finance",
   metadata: {
+    jar: log.address.toString(),
     amount: log.args[2].toString(),
     from: log.args[1].toLowerCase(),
   }
 });
 
-const WhitdrawFinding: FindingGenerator = (log: LogDescription): Finding => Finding.fromObject({
+const WithdrawFinding: FindingGenerator = (log: LogDescription): Finding => Finding.fromObject({
   name: "Pickle Jar Withdraw",
   description: "Large withdraw detected",
   alertId: "pickle-2-2",
@@ -24,10 +25,11 @@ const WhitdrawFinding: FindingGenerator = (log: LogDescription): Finding => Find
   type: FindingType.Info,
   protocol: "Pickle Finance",
   metadata: {
+    jar: log.address.toString(),
     amount: log.args[2].toString(),
     from: log.args[0].toLowerCase(),
   }
 });
 
 export const createFinding: FindingGenerator = (log: LogDescription): Finding =>
-  BigNumber.from(0).eq(log.args[0])? DepositFinding(log) : WhitdrawFinding(log);
+  BigNumber.from(0).eq(log.args[0])? DepositFinding(log) : WithdrawFinding(log);
