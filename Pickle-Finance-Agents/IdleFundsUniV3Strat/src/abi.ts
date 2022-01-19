1eae04bcdc6c0e351724e6e365bd1439650611cb
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-19 14:55:58 -0500

    feat: Create abi file

---
+++ Pickle-Finance-Agents/IdleFundsUniV3Strat/src/abi.ts
@@ -0,0 +1,11 @@
+import { utils } from "ethers";
+
+export const keeperInterface = new utils.Interface([
+  "function strategyAddress(uint256 index) external view returns (address)",
+]);
+
+export const strategyInterface = new utils.Interface([
+  "function liquidityOfThis() external view returns (uint256)",
+  "function liquidityOf() external view returns (uint256)",
+]);
+
