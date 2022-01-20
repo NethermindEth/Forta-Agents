debaba9429fbb1b08307d790795616804738ebd7
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-19 20:37:17 -0500

    refac: Change returns type from BigNumberish to BigNumber

--- Pickle-Finance-Agents/IdleFundsUniV3Strat/src/utils.ts
+++ Pickle-Finance-Agents/IdleFundsUniV3Strat/src/utils.ts
@@ -1,32 +1,64 @@
 import { keeperInterface, strategyInterface } from "./abi";
-import { providers, Contract, utils, BigNumberish } from "ethers";
+import { providers, Contract, utils, BigNumber } from "ethers";
 import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
-export const getStrategies = async (keeperAddress: string, blockNumber: number, provider: providers.Provider): Promise<string[]> => {
+export const getStrategies = async (
+  keeperAddress: string,
+  blockNumber: number,
+  provider: providers.Provider
+): Promise<string[]> => {
   const keeperContract = new Contract(keeperAddress, keeperInterface, provider);
   const strategiesPromises: Promise<string>[] = [];
 
-  const arrayLengthEncoded = await provider.getStorageAt(keeperAddress, 0, blockNumber);
-  const arrayLength = utils.defaultAbiCoder.decode(["uint256"], arrayLengthEncoded)[0];
+  const arrayLengthEncoded = await provider.getStorageAt(
+    keeperAddress,
+    0,
+    blockNumber
+  );
+  const arrayLength = utils.defaultAbiCoder.decode(
+    ["uint256"],
+    arrayLengthEncoded
+  )[0];
 
   for (let i = 0; i < arrayLength; i++) {
-    strategiesPromises.push(keeperContract.strategyArray(i, { blockTag: blockNumber })); 
+    strategiesPromises.push(
+      keeperContract.strategyArray(i, { blockTag: blockNumber })
+    );
   }
 
   return Promise.all(strategiesPromises);
 };
 
-export const getIdleFunds = async (strategyAddress: string, blockNumber: number, provider: providers.Provider): Promise<BigNumberish> => {
-  const strategyContract = new Contract(strategyAddress, strategyInterface, provider);
+export const getIdleFunds = async (
+  strategyAddress: string,
+  blockNumber: number,
+  provider: providers.Provider
+): Promise<BigNumber> => {
+  const strategyContract = new Contract(
+    strategyAddress,
+    strategyInterface,
+    provider
+  );
   return strategyContract.liquidityOfThis({ blockTag: blockNumber });
 };
 
-export const getTotalFunds = async (strategyAddress: string, blockNumber: number, provider: providers.Provider): Promise<BigNumberish> => {
-  const strategyContract = new Contract(strategyAddress, strategyInterface, provider);
+export const getTotalFunds = async (
+  strategyAddress: string,
+  blockNumber: number,
+  provider: providers.Provider
+): Promise<BigNumber> => {
+  const strategyContract = new Contract(
+    strategyAddress,
+    strategyInterface,
+    provider
+  );
   return strategyContract.liquidityOf({ blockTag: blockNumber });
 };
 
-export const createFinding = (strategyAddress: string, percentOfIdleFunds: string): Finding => {
+export const createFinding = (
+  strategyAddress: string,
+  percentOfIdleFunds: string
+): Finding => {
   return Finding.fromObject({
     name: "Idle Funds in UniV3 strategy",
     description: "An UniV3 strategy has too much idle funds",
@@ -37,6 +69,6 @@ export const createFinding = (strategyAddress: string, percentOfIdleFunds: strin
     metadata: {
       strategy: strategyAddress,
       idleFunds: percentOfIdleFunds,
-    }
+    },
   });
 };
