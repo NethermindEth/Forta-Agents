809351188d6b0edf5bfd7bf1840100e3e10f71d7
Lázaro Raúl <riglesiasvera@gmail.com>
2022-01-27 10:26:22 -0500

    feat: Initial Agent

---
+++ Impossible-Agent-Suite/SwapFees/src/agent.spec.ts
@@ -0,0 +1,14 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+} from "forta-agent";
+import agent from "./agent";
+
+describe("Swap Fee Monitor agent tests suite", () => {
+  it("Should do something", async () => {
+    
+  });
+});
