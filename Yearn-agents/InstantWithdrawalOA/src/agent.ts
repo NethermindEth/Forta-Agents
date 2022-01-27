e0244a5c8b8aae3601337208b815eb69e0df56ef
Mauricio Perdomo <ml.perdomo97@gmail.com>
2022-01-27 13:11:23 -0500

    feat: Add agent logic

--- Yearn-agents/InstantWithdrawalOA/src/agent.ts
+++ Yearn-agents/InstantWithdrawalOA/src/agent.ts
@@ -1,45 +1,84 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import {
+  BlockEvent,
+  Finding,
+  getEthersProvider,
+  getJsonRpcUrl,
+} from "forta-agent";
+import {
+  getBiggerInvestors,
+  getVaults,
+  createFinding,
+  getStatsForVault,
+  getForkProvider,
+} from "./utils";
+import { providers } from "ethers";
 
-let findingsCount = 0
+const VAULTS_REGISTRY = "0x437758D475F70249e03EDa6bE23684aD1FC375F0";
+const FINDINGS_PACKS: Finding[][] = [];
+const REPORT_PERIOD = 6 * 60 * 60; // 6 hours
+let lastReported = 0;
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const detectFindings = async (
+  registryAddress: string,
+  jsonRpcURL: string,
+  provider: providers.Provider,
+  outFindingsPacks: Finding[][]
+) => {
+  while (true) {
+    try {
+      const blockNumber = await provider.getBlockNumber();
+      const vaults = await getVaults(registryAddress, blockNumber, provider);
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+      const investByVaultPromises = vaults.map((vault) =>
+        getBiggerInvestors(vault)
+      );
+      let investByVault = await Promise.all(investByVaultPromises);
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-    findingsCount++
+      investByVault = investByVault.filter(
+        (investorsInfo) => investorsInfo.length > 0
+      );
+
+      const statsPromises = investByVault.map((investors) =>
+        getStatsForVault(investors, jsonRpcURL, blockNumber, getForkProvider)
+      );
+
+      const findings = (await Promise.all(statsPromises)).map(
+        ([vault, totalSupply, totalInvestorValues, ableToWithdrawn]) =>
+          createFinding(
+            vault,
+            totalSupply,
+            totalInvestorValues,
+            ableToWithdrawn
+          )
+      );
+
+      outFindingsPacks.push(findings);
+    } catch {}
   }
+};
 
-  return findings
-}
+const initialize = () => {
+  detectFindings(
+    VAULTS_REGISTRY,
+    getJsonRpcUrl(),
+    getEthersProvider(),
+    FINDINGS_PACKS
+  );
+};
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+const handleBlock = async (blockEvent: BlockEvent): Promise<Finding[]> => {
+  if (blockEvent.block.timestamp - lastReported >= REPORT_PERIOD) {
+    if (FINDINGS_PACKS.length > 0) {
+      const findings = FINDINGS_PACKS[FINDINGS_PACKS.length - 1];
+      FINDINGS_PACKS.splice(0, FINDINGS_PACKS.length);
+      lastReported = blockEvent.block.timestamp;
+      return findings;
+    }
+  }
+  return [];
+};
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleBlock,
+  initialize,
+};
