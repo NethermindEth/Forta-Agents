540ca6eca27de154891c69e22e5c8700beb5bbc9
RCantu92 <Roberto_Cantu@live.com>
2022-01-27 21:35:00 -0600

    feat: Update agent implementation to pass four unit tests

--- Alpaca-Finance-Agents/Liquidation-Alert/src/agent.spec.ts
+++ Alpaca-Finance-Agents/Liquidation-Alert/src/agent.spec.ts
@@ -12,38 +12,38 @@ import {
 } from "forta-agent-tools";
 import { provideHandleTransaction } from "./agent"
 
-const TEST_ALERT_ID: string = 'test';
 const TEST_VAULT_ADDRESS = createAddress('0x4321');
 const testMsgSender: string = createAddress('0x1234');
 
 const killEventSig: string = "Kill(uint256,address,address,uint256,uint256,uint256,uint256)";
 
 // PARAMS FOR EVENT
-// TODO: GIVE THEM VALUES
-const testId: number = 0;
-const testKiller: string = " ";
-const testOwner: string = " ";
-const testPosVal: number = 0;
-const testDebt: number = 0;
-const testPrize: number = 0;
+// TODO: GIVE THEM MORE LEGITIMATE VALUES
+const testId: number = 1;
+const testKiller: string = createAddress('0x0101');
+const testOwner: string = createAddress('0x0202');
+const testPosVal: number = 1;
+const testDebt: number = 1;
+const testPrize: number = 1;
+const testLeft: number = 1;
 
 
 const data: string = encodeParameters(
-  ["uint256","address","address", "uint256", "uint256", "uint256", "address"],
-  [testId, testKiller, testOwner, testPosVal, testDebt, testPrize]
+  ["uint256","address","address", "uint256", "uint256", "uint256", "uint256"],
+  [testId, testKiller, testOwner, testPosVal, testDebt, testPrize, testLeft]
 );
 
 describe("Liquidation Alert Agent", () => {
   let handleTransaction: HandleTransaction
 
   beforeAll(() => {
-    handleTransaction = provideHandleTransaction(TEST_ALERT_ID);
+    handleTransaction = provideHandleTransaction(TEST_VAULT_ADDRESS);
   })
 
   it('should return a Finding from Kill event emission', async () => {
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
-      .setTo(TEST_VAULT_ADDRESS) // BUSD Vault
+      .setTo(TEST_VAULT_ADDRESS)
       .addEventLog(killEventSig, TEST_VAULT_ADDRESS, data);
 
     const findings = await handleTransaction(txEvent);
@@ -52,18 +52,44 @@ describe("Liquidation Alert Agent", () => {
       Finding.fromObject({
         name: "Liquidation Event",
         description: "Liquidation Has Occurred",
-        alertId: TEST_ALERT_ID,
+        alertId: "ALPACA-3",
         severity: FindingSeverity.Info,
-        type: FindingType.Unknown,
+        type: FindingType.Info,
         metadata: {
-          id: testId.toString(),
-          killer: testKiller,
-          owner: testOwner,
-          PosVal: testPosVal.toString(),
+          positionId: testId.toString(),
+          positionkiller: testKiller,
+          positionOwner: testOwner,
+          positionValue: testPosVal.toString(),
           debt: testDebt.toString(),
-          prize: testPrize.toString()
+          prize: testPrize.toString(),
+          left: testLeft.toString(),
+          vault: TEST_VAULT_ADDRESS
         }
       }),
     ]);
+  });
+
+  it("should return no Findings due to incorrect event signature", async () => {
+    const badWorkSig: string = 'badSig';
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addInvolvedAddresses(TEST_VAULT_ADDRESS, testMsgSender)
+      .addEventLog(badWorkSig, TEST_VAULT_ADDRESS, data);
+
+    const findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("should return no Findings due to wrong contract ddress", async () => {
+    const wrongVaultAddress: string = createAddress("0x1111");
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .addInvolvedAddresses(wrongVaultAddress, testMsgSender)
+      .addEventLog(killEventSig,wrongVaultAddress);
+
+    const findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
   });
 })
--- Alpaca-Finance-Agents/Liquidation-Alert/src/agent.ts
+++ Alpaca-Finance-Agents/Liquidation-Alert/src/agent.ts
@@ -1,4 +1,3 @@
-import BigNumber from 'bignumber.js'
 import { 
   BlockEvent, 
   Finding, 
@@ -9,27 +8,70 @@ import {
   FindingType,
 } from 'forta-agent'
 
-const BUSD_VAULT_ADDRESS: string = "0x7C9e73d4C71dae564d41F78d56439bB4ba87592f";
-const killEventAbi: string = "event Kill(uint256 indexed id, address indexed killer, address owner, uint256 posVal, uint256 debt, uint256 prize, uint256 left)";
+const BUSD_VAULT_ADDRESS: string = "0x7C9e73d4C71dae564d41F78d56439bB4ba87592f"; // NOTE: JUST FOR TEST, MAY FALLBACK ON USING MAP FROM PREVIOUS AGENT
+// NOTE: HAD TO OMIT 'indexed' FROM BOTH 'id' AND 'killer' PARAMS TO GET filterLog() TO SUCCESSFULLY WORK.
+// TODO: FIGURE OUT HOW TO KEEP IT AS IT IS.
+// ORIGINAL: "event Kill(uint256 indexed id, address indexed killer, address owner, uint256 posVal, uint256 debt, uint256 prize, uint256 left)"
+const killEventAbi: string = "event Kill(uint256 id, address killer, address owner, uint256 posVal, uint256 debt, uint256 prize, uint256 left)";
+
+const createFinding = (
+  posId: number,
+  killer: string,
+  posOwner: string,
+  posVal: number,
+  debt: number,
+  prize: number,
+  left: number,
+  vaultAddress: string
+): Finding => {
+  return Finding.fromObject({
+    name: "Liquidation Event",
+    description: "Liquidation Has Occurred",
+    alertId: "ALPACA-3",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    metadata:{
+      positionId: posId.toString(),
+      positionkiller: killer, // TODO: CONFIRM toString() IS UNECESSARY
+      positionOwner: posOwner, // TODO: CONFIRM toString() IS UNECESSARY
+      positionValue: posVal.toString(),
+      debt: debt.toString(),
+      prize: prize.toString(),
+      left: left.toString(),
+      vault: vaultAddress
+    },
+  })
+}
 
 export function provideHandleTransaction(
-  alertId: string//,
-  /*addresses: string[],*/
+  address: string
 ): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
-    const killEmissions = txEvent.filterLog(killEventAbi, BUSD_VAULT_ADDRESS);
-
-    console.log("The kill emissions found in the txEvent are: " + killEmissions);
-
     const findings: Finding[] = [];
+    const killEvents = txEvent.filterLog(killEventAbi, address);
+
+    for(let i = 0; i < killEvents.length; i++) {
+      const newFinding: Finding = createFinding(
+        killEvents[i].args["id"],
+        killEvents[i].args["killer"],
+        killEvents[i].args["owner"],
+        killEvents[i].args["posVal"],
+        killEvents[i].args["debt"],
+        killEvents[i].args["prize"],
+        killEvents[i].args["left"],
+        killEvents[i].address
+      );
+      findings.push(newFinding);
+    }
 
     return findings
   }
 }
 
+/*
 export default {
   handleTransaction: provideHandleTransaction(
-    "ALPACA-3"//,
-    /*VAULT_ADDRESSES*/
+    BUSD_VAULT_ADDRESS
   )
 };
+*/
