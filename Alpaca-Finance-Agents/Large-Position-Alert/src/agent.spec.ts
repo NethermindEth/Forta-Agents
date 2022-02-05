f7c0815af82a57f71680117d3c3efbdc31618614
RCantu92 <Roberto_Cantu@live.com>
2022-02-05 13:54:01 -0600

    refac: Add ethers.BigNumber utilization in agent and tests

--- Alpaca-Finance-Agents/Large-Position-Alert/src/agent.spec.ts
+++ Alpaca-Finance-Agents/Large-Position-Alert/src/agent.spec.ts
@@ -13,35 +13,37 @@ import {
 import {
   provideHandleTransaction
 } from "./agent"
+import { BigNumber } from "ethers";
 
 const workEventSig: string = "Work(uint256,uint256)";
 
-const TEST_VAULT_ADDRESS: string = createAddress("0x3230");
-const TEST_VAULT_MAP: Map<string, bigint> = new Map([
-  [TEST_VAULT_ADDRESS, BigInt(100000000000000000000000)] // 100,000
+const TEST_VAULT_ADDRESS: string = createAddress("0x32aB30Cde");
+const TEST_VAULT_THRESHOLD_MAP: Map<string, BigNumber> = new Map([
+  [TEST_VAULT_ADDRESS.toLowerCase(), BigNumber.from("100000000000000000000000")] // 100,000
 ])
 
-const testPositionId: number = 123;
-const testBorrowAmount = BigInt(500000000000000000000000); // 500,000
-const testMsgSender: string = createAddress("0x3");
-
-const data: string = encodeParameters(
-  ["uint256", "uint256"],
-  [testPositionId, testBorrowAmount]
-);
+const testMsgSender: string = createAddress("0x3bc24EF");
 
 describe("Large Position Alert Agent", () => {
   let handleTransaction: HandleTransaction
 
   beforeAll(() => {
-    handleTransaction = provideHandleTransaction(TEST_VAULT_MAP);
+    handleTransaction = provideHandleTransaction(TEST_VAULT_THRESHOLD_MAP);
   });
 
   it("should return a Finding from Work event emission in test Vault", async () => {
+    const testPositionId: number = 123;
+    const testBorrowAmount: BigNumber = BigNumber.from("500000000000000000000000"); // 500,000
+
+    const testData: string = encodeParameters(
+      ["uint256", "uint256"],
+      [testPositionId, testBorrowAmount]
+    );
+
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
       .setTo(TEST_VAULT_ADDRESS)
-      .addEventLog(workEventSig, TEST_VAULT_ADDRESS, data);
+      .addEventLog(workEventSig, TEST_VAULT_ADDRESS, testData);
 
     const findings = await handleTransaction(txEvent);
 
@@ -62,23 +64,39 @@ describe("Large Position Alert Agent", () => {
   });
 
   it("should return no Findings due to incorrect event signature", async () => {
+    const testPositionId: number = 654;
+    const testBorrowAmount: BigNumber = BigNumber.from("200000000000000000000000"); // 200,000
+
+    const testData: string = encodeParameters(
+      ["uint256", "uint256"],
+      [testPositionId, testBorrowAmount]
+    );
+
     const badWorkSig: string = 'badSig';
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .addInvolvedAddresses(TEST_VAULT_ADDRESS, testMsgSender)
-      .addEventLog(badWorkSig, TEST_VAULT_ADDRESS, data);
+      .addEventLog(badWorkSig, TEST_VAULT_ADDRESS, testData);
 
     const findings = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
   });
 
-  it("should return no Findings due to wrong contract ddress", async () => {
-    const wrongVaultAddress: string = createAddress("0x1111");
+  it("should return no Findings due to wrong contract address", async () => {
+    const testPositionId: number = 963;
+    const testBorrowAmount: BigNumber = BigNumber.from("350000000000000000000000"); // 350,000
+
+    const testData: string = encodeParameters(
+      ["uint256", "uint256"],
+      [testPositionId, testBorrowAmount]
+    );
+
+    const wrongVaultAddress: string = createAddress("0x1aBC43Fe");
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .addInvolvedAddresses(wrongVaultAddress, testMsgSender)
-      .addEventLog(workEventSig,wrongVaultAddress);
+      .addEventLog(workEventSig, wrongVaultAddress, testData);
 
     const findings = await handleTransaction(txEvent);
 
@@ -86,16 +104,17 @@ describe("Large Position Alert Agent", () => {
   });
 
   it("should return no Findings due not passing borrow amount threshold", async () => {
-    const lowBorrowAmount = BigInt(5000000000000000000000); // 5,000
+    const testPositionId: number = 147;
+    const testBorrowAmount: BigNumber = BigNumber.from("5000000000000000000000"); // 5,000
 
-    const lowBorrowAmountData: string = encodeParameters(
+    const testData: string = encodeParameters(
       ["uint256", "uint256"],
-      [testPositionId, lowBorrowAmount]
+      [testPositionId, testBorrowAmount]
     );
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .addInvolvedAddresses(TEST_VAULT_ADDRESS, testMsgSender)
-      .addEventLog(workEventSig, TEST_VAULT_ADDRESS, lowBorrowAmountData);
+      .addEventLog(workEventSig, TEST_VAULT_ADDRESS, testData);
 
     const findings = await handleTransaction(txEvent);
 
--- Alpaca-Finance-Agents/Large-Position-Alert/src/agent.ts
+++ Alpaca-Finance-Agents/Large-Position-Alert/src/agent.ts
@@ -5,19 +5,20 @@ import {
   FindingSeverity, 
   FindingType 
 } from 'forta-agent'
+import { BigNumber } from "ethers";
 
-const VAULTS_MAP: Map<string, bigint> = new Map([
-  ["0x7C9e73d4C71dae564d41F78d56439bB4ba87592f".toLowerCase(), BigInt(100000000000000000000000)], // 100,000 BUSD
-  ["0xbfF4a34A4644a113E8200D7F1D79b3555f723AfE".toLowerCase(), BigInt(30000000000000000000)],     // 30 ETH
-  ["0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7".toLowerCase(), BigInt(3000000000000000000)],      // 3 BTCB
-  ["0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7".toLowerCase(), BigInt(100000000000000000000000)], // 100,000 USDT
-  ["0xf1bE8ecC990cBcb90e166b71E368299f0116d421".toLowerCase(), BigInt(200000000000000000000000)], // 200,000 ALPACA
-  ["0x3282d2a151ca00BfE7ed17Aa16E42880248CD3Cd".toLowerCase(), BigInt(100000000000000000000000)]  // 100,000 TUSD
+const VAULTS_THRESHOLD_MAP: Map<string, BigNumber> = new Map([
+  ["0x7C9e73d4C71dae564d41F78d56439bB4ba87592f".toLowerCase(), BigNumber.from("100000000000000000000000")], // 100,000 BUSD
+  ["0xbfF4a34A4644a113E8200D7F1D79b3555f723AfE".toLowerCase(), BigNumber.from("30000000000000000000")],     // 30 ETH
+  ["0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7".toLowerCase(), BigNumber.from("3000000000000000000")],      // 3 BTCB
+  ["0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7".toLowerCase(), BigNumber.from("100000000000000000000000")], // 100,000 USDT
+  ["0xf1bE8ecC990cBcb90e166b71E368299f0116d421".toLowerCase(), BigNumber.from("200000000000000000000000")], // 200,000 ALPACA
+  ["0x3282d2a151ca00BfE7ed17Aa16E42880248CD3Cd".toLowerCase(), BigNumber.from("100000000000000000000000")]  // 100,000 TUSD
 ])
 
 const workEventAbi: string = "event Work(uint256 id, uint256 loan)";
 
-const createFinding = (posId: number, loanAmount: bigint, vaultAddress: string): Finding => {
+const createFinding = (posId: number, loanAmount: BigNumber, vaultAddress: string): Finding => {
   return Finding.fromObject({
     name: "Large Position Event",
     description: "Large Position Has Been Taken",
@@ -27,22 +28,22 @@ const createFinding = (posId: number, loanAmount: bigint, vaultAddress: string):
     metadata:{
       positionId: posId.toString(),
       loanAmount: loanAmount.toString(),
-      vault: vaultAddress?.toString()
+      vault: vaultAddress.toString()
     },
   })
 }
 
-export function provideHandleTransaction(vaultsMap: Map<string, bigint>): HandleTransaction {
+export function provideHandleTransaction(vaultsMap: Map<string, BigNumber>): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
     const workEvents = txEvent.filterLog(workEventAbi);
 
     for(let i = 0; i < workEvents.length; i++) {
-      const vaultThreshold = vaultsMap.get(workEvents[i].address);
-      if (vaultThreshold && BigInt(parseInt(workEvents[i].args["loan"])) > vaultThreshold) {
+      const vaultThreshold = vaultsMap.get(workEvents[i].address.toLowerCase());
+      if (vaultThreshold && vaultThreshold.lt(workEvents[i].args["loan"])) {
         const newFinding: Finding = createFinding(
-          parseInt(workEvents[i].args["id"]),
-          BigInt(parseInt(workEvents[i].args["loan"])),
+          workEvents[i].args["id"],
+          workEvents[i].args["loan"],
           workEvents[i].address
         );
         findings.push(newFinding);
@@ -54,5 +55,5 @@ export function provideHandleTransaction(vaultsMap: Map<string, bigint>): Handle
 }
 
 export default {
-  handleTransaction: provideHandleTransaction(VAULTS_MAP)
+  handleTransaction: provideHandleTransaction(VAULTS_THRESHOLD_MAP)
 };
