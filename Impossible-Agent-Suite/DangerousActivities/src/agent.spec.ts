c7dcdbc65942092ca1d4e6998458f4d887a86e41
Lázaro Raúl <riglesiasvera@gmail.com>
2022-02-17 15:11:34 -0500

    feat: Initial Agent

---
+++ Impossible-Agent-Suite/DangerousActivities/src/agent.spec.ts
@@ -0,0 +1,14 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent
+} from "forta-agent";
+import agent from "./agent";
+
+describe("Dangerous Activities monitor tests suite", () => {
+  it("should do something", async () => {
+   
+  });
+});
---
+++ Impossible-Agent-Suite/DangerousActivities/src/agent.ts
@@ -0,0 +1,19 @@
+import { 
+  Finding, 
+  HandleTransaction, 
+  TransactionEvent, 
+  FindingSeverity, 
+  FindingType,
+} from 'forta-agent';
+
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = [];
+
+  return findings;
+};
+
+
+export default {
+  handleTransaction,
+};
