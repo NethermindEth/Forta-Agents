fce66df8d4ebf1f081efaaa103407df1550675d3
RCantu92 <Roberto_Cantu@live.com>
2022-02-17 20:29:46 -0600

    refac: Refactor tests for legibility

--- SafeSnap-Agents/Cooldown-Alert/src/agent.spec.ts
+++ SafeSnap-Agents/Cooldown-Alert/src/agent.spec.ts
@@ -12,12 +12,9 @@ import {
   TestBlockEvent,
   TestTransactionEvent,
   encodeParameters,
-  MockEthersProvider
 } from "forta-agent-tools";
 import {
   propQuestionCreatedSig,
-  moduleIFace,
-  oracleIFace
 } from "./abi";
 import {
   provideHandleBlock,
@@ -31,16 +28,55 @@ import {
 const testMsgSender: string = createAddress("0xda456f");
 const testOracle: string = createAddress("0x2d5ef8");
 const testModule: string = createAddress("0xac689d");
+const testBlockNumber: number = 2256000;
 
-const testQuestionIds: string[] = [];
+const testPropIds: string[] = [
+  "aa-11",
+  "ab-12",
+  "ac-23",
+  "ad-34",
+  "ae-45",
+  "af-56"
+];
+const testQuestionIds: string[] = [
+  "0x81d50202a3f5c3971f123d9ddcca9c2c91d3e863019bdc3de8fcc2480ffadf02",
+  "0x57c12963d94d1a2eec26eae9b064089728246725d507feb9e94a9703eb796a07",
+  "0x080f91de22bcdcd395d9f59ad1eb7d2bd851213f36ce88eee1b3321a275c8c2c",
+  "0x678e23012921e26c47439412244caa7bc94fed24e713c82d6cf16862911fe90e",
+  "0x2fe16d1c3df69c437f73c27c37eaec4f488af69c101429817eb381cfa7416ac7",
+  "0xc071094ab7ce9b9a69f4e4312a66f6774dd296cfae05204a3f3ba0a9ef424487"
+]
+const testFinalizeTSs: number[] = [
+  1500000,
+  2500000,
+  1600000,
+  1700000,
+  2800000,
+  1800000
+];
+const testData: string[] = [
+  "0xd34db3ef",
+  "0xf0ob4r",
+  "0x339da0",
+  "0xad32f5",
+  "0x339da0",
+  "0xad32f5"
+];
+
+function createTestTopics(questionId: string, propId: string): string[] {
+  return [
+    encodeParameters(["bytes32"], [questionId]),
+    encodeParameters(["string"], [propId])
+  ];
+}
+
+const testFilteredQuestionIds: string[] = [];
 
 const mockFetcher = {
   testModule,
   getOracle: jest.fn(),
   getFinalizeTS: jest.fn()
 }
-
-const mockProvider: MockEthersProvider = new MockEthersProvider();
 
 describe("Cooldown Monitor Agent", () => {
   let handleBlock: HandleBlock;
@@ -66,50 +102,28 @@ describe("Cooldown Monitor Agent", () => {
 
     handleTransaction = provideHandleTransaction(
       testModule,
-      testQuestionIds
+      testFilteredQuestionIds
     );
 
     handleBlock = provideHandleBlock(
       mockFetcher as any,
       testOracle,
-      testQuestionIds
+      testFilteredQuestionIds
     );
   });
 
   it("should return a Finding from detecting when a question's cooldown starts", async () => {
-    const testPropId: string = "aa-11";
-    const testQuestionId: string = "0x81d50202a3f5c3971f123d9ddcca9c2c91d3e863019bdc3de8fcc2480ffadf02";
-    const testBlockNumber: number = 2256000;
-
-    const testFinalizeTS: number = 1500000;
-
-    const testTopics: string[] = [
-      encodeParameters(["bytes32"], [testQuestionId]),
-      encodeParameters(["string"], [testPropId])
-    ];
-
-    const testData: string = "0xd34db3ef";
-
-    mockProvider.addCallTo(
-      testModule, testBlockNumber, moduleIFace,
-      "oracle",
-      { inputs:[], outputs:[testOracle] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionId], outputs:[testFinalizeTS] },
-    );
+    const testTopics: string[] = createTestTopics(testQuestionIds[0], testPropIds[0]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
       .setTo(testModule)
       .setTimestamp(testBlockNumber)
-      .addEventLog(propQuestionCreatedSig, testModule, testData, ...testTopics);
+      .addEventLog(propQuestionCreatedSig, testModule, testData[0], ...testTopics);
 
     await handleTransaction(txEvent);
 
-    prepareMockFetcher(testBlockNumber, testOracle, [testQuestionId], [testFinalizeTS]);
+    prepareMockFetcher(testBlockNumber, testOracle, [testQuestionIds[0]], [testFinalizeTSs[0]]);
 
     const blockEvent: BlockEvent = new TestBlockEvent().setNumber(testBlockNumber);
 
@@ -124,8 +138,8 @@ describe("Cooldown Monitor Agent", () => {
         severity: FindingSeverity.Info,
         protocol: "Gnosis SafeSnap",
         metadata: {
-          questionId: testQuestionId,
-          questionFinalizeTimeStamp: testFinalizeTS.toString(),
+          questionId: testQuestionIds[0],
+          questionFinalizeTimeStamp: testFinalizeTSs[0].toString(),
           blockNumber: testBlockNumber.toString()
         },
       })
@@ -133,39 +147,17 @@ describe("Cooldown Monitor Agent", () => {
   });
 
   it("should return no Findings due to finalize timestamp being more than block number", async () => {
-    const testPropId: string = "ab-12";
-    const testQuestionId: string = "0x57c12963d94d1a2eec26eae9b064089728246725d507feb9e94a9703eb796a07";
-    const testBlockNumber: number = 1000000;
-
-    const testFinalizeTS: number = 1500000;
-
-    const testTopics: string[] = [
-      encodeParameters(["bytes32"], [testQuestionId]),
-      encodeParameters(["string"], [testPropId])
-    ];
-
-    const testData: string = "0xf0ob4r";
-
-    mockProvider.addCallTo(
-      testModule, testBlockNumber, moduleIFace,
-      "oracle",
-      { inputs:[], outputs:[testOracle] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionId], outputs:[testFinalizeTS] },
-    );
+    const testTopics: string[] = createTestTopics(testQuestionIds[1], testPropIds[1]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
       .setTo(testModule)
       .setTimestamp(testBlockNumber)
-      .addEventLog(propQuestionCreatedSig, testModule, testData, ...testTopics);
+      .addEventLog(propQuestionCreatedSig, testModule, testData[1], ...testTopics);
 
     await handleTransaction(txEvent);
 
-    prepareMockFetcher(testBlockNumber, testOracle, [testQuestionId], [testFinalizeTS]);
+    prepareMockFetcher(testBlockNumber, testOracle, [testQuestionIds[1]], [testFinalizeTSs[1]]);
 
     const blockEvent: BlockEvent = new TestBlockEvent().setNumber(testBlockNumber);
 
@@ -176,65 +168,23 @@ describe("Cooldown Monitor Agent", () => {
   });
 
   it("should return multiple Findings from detecting cooldowns from different questions", async () => {
-    const testBlockNumber: number = 2000000;
-
-
-    const testPropIdOne: string = "ac-23";
-    const testQuestionIdOne: string = "0x080f91de22bcdcd395d9f59ad1eb7d2bd851213f36ce88eee1b3321a275c8c2c";
-
-    const testFinalizeTSOne: number = 1500000;
-
-    const testTopicsOne: string[] = [
-      encodeParameters(["bytes32"], [testQuestionIdOne]),
-      encodeParameters(["string"], [testPropIdOne])
-    ];
-
-    const testDataOne: string = "0x339da0";
-
-
-    const testPropIdTwo: string = "ad-34";
-    const testQuestionIdTwo: string = "0x678e23012921e26c47439412244caa7bc94fed24e713c82d6cf16862911fe90e";
-
-    const testFinalizeTSTwo: number = 1700000;
-
-    const testTopicsTwo: string[] = [
-      encodeParameters(["bytes32"], [testQuestionIdTwo]),
-      encodeParameters(["string"], [testPropIdTwo])
-    ];
-
-    const testDataTwo: string = "0xad32f5";
-
-    mockProvider.addCallTo(
-      testModule, testBlockNumber, moduleIFace,
-      "oracle",
-      { inputs:[], outputs:[testOracle] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionIdOne], outputs:[testFinalizeTSOne] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionIdTwo], outputs:[testFinalizeTSTwo] },
-    );
-
+    const testTopicsOne: string[] = createTestTopics(testQuestionIds[2], testPropIds[2]);
+    const testTopicsTwo: string[] = createTestTopics(testQuestionIds[3], testPropIds[3]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
       .setTo(testModule)
       .setTimestamp(testBlockNumber)
-      .addEventLog(propQuestionCreatedSig, testModule, testDataOne, ...testTopicsOne)
-      .addEventLog(propQuestionCreatedSig, testModule, testDataTwo, ...testTopicsTwo);
+      .addEventLog(propQuestionCreatedSig, testModule, testData[2], ...testTopicsOne)
+      .addEventLog(propQuestionCreatedSig, testModule, testData[3], ...testTopicsTwo);
 
     await handleTransaction(txEvent);
 
     prepareMockFetcher(
       testBlockNumber,
       testOracle,
-      [testQuestionIdOne, testQuestionIdTwo],
-      [testFinalizeTSOne, testFinalizeTSTwo]
+      [testQuestionIds[2], testQuestionIds[3]],
+      [testFinalizeTSs[2], testFinalizeTSs[3]]
     );
 
     const blockEvent: BlockEvent = new TestBlockEvent().setNumber(testBlockNumber);
@@ -250,8 +200,8 @@ describe("Cooldown Monitor Agent", () => {
         severity: FindingSeverity.Info,
         protocol: "Gnosis SafeSnap",
         metadata: {
-          questionId: testQuestionIdOne,
-          questionFinalizeTimeStamp: testFinalizeTSOne.toString(),
+          questionId: testQuestionIds[2],
+          questionFinalizeTimeStamp: testFinalizeTSs[2].toString(),
           blockNumber: testBlockNumber.toString()
         },
       }),
@@ -263,8 +213,8 @@ describe("Cooldown Monitor Agent", () => {
         severity: FindingSeverity.Info,
         protocol: "Gnosis SafeSnap",
         metadata: {
-          questionId: testQuestionIdTwo,
-          questionFinalizeTimeStamp: testFinalizeTSTwo.toString(),
+          questionId: testQuestionIds[3],
+          questionFinalizeTimeStamp: testFinalizeTSs[3].toString(),
           blockNumber: testBlockNumber.toString()
         },
       })
@@ -272,64 +222,23 @@ describe("Cooldown Monitor Agent", () => {
   });
 
   it("should return one Finding even if transaciton includes multiple questionIds", async () => {
-    const testBlockNumber: number = 2256000;
-
-
-    const testPropIdOne: string = "ae-45";
-    const testQuestionIdOne: string = "0x2fe16d1c3df69c437f73c27c37eaec4f488af69c101429817eb381cfa7416ac7";
-
-    const testFinalizeTSOne: number = 2500000;
-
-    const testTopicsOne: string[] = [
-      encodeParameters(["bytes32"], [testQuestionIdOne]),
-      encodeParameters(["string"], [testPropIdOne])
-    ];
-
-    const testDataOne: string = "0x339da0";
-
-
-    const testPropIdTwo: string = "af-56";
-    const testQuestionIdTwo: string = "0xc071094ab7ce9b9a69f4e4312a66f6774dd296cfae05204a3f3ba0a9ef424487";
-
-    const testFinalizeTSTwo: number = 1700000;
-
-    const testTopicsTwo: string[] = [
-      encodeParameters(["bytes32"], [testQuestionIdTwo]),
-      encodeParameters(["string"], [testPropIdTwo])
-    ];
-
-    const testDataTwo: string = "0xad32f5";
-
-    mockProvider.addCallTo(
-      testModule, testBlockNumber, moduleIFace,
-      "oracle",
-      { inputs:[], outputs:[testOracle] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionIdOne], outputs:[testFinalizeTSOne] },
-    );
-    mockProvider.addCallTo(
-      testOracle, testBlockNumber, oracleIFace,
-      "getFinalizeTS",
-      { inputs:[testQuestionIdTwo], outputs:[testFinalizeTSTwo] },
-    );
-
+    const testTopicsOne: string[] = createTestTopics(testQuestionIds[4], testPropIds[4]);
+    const testTopicsTwo: string[] = createTestTopics(testQuestionIds[5], testPropIds[5]);
+    
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setFrom(testMsgSender)
       .setTo(testModule)
       .setTimestamp(testBlockNumber)
-      .addEventLog(propQuestionCreatedSig, testModule, testDataOne, ...testTopicsOne)
-      .addEventLog(propQuestionCreatedSig, testModule, testDataTwo, ...testTopicsTwo);
+      .addEventLog(propQuestionCreatedSig, testModule, testData[4], ...testTopicsOne)
+      .addEventLog(propQuestionCreatedSig, testModule, testData[5], ...testTopicsTwo);
 
     await handleTransaction(txEvent);
 
     prepareMockFetcher(
       testBlockNumber,
       testOracle,
-      [testQuestionIdOne, testQuestionIdTwo],
-      [testFinalizeTSOne, testFinalizeTSTwo]
+      [testQuestionIds[4], testQuestionIds[5]],
+      [testFinalizeTSs[4], testFinalizeTSs[5]]
     );
 
     const blockEvent: BlockEvent = new TestBlockEvent().setNumber(testBlockNumber);
@@ -345,8 +254,8 @@ describe("Cooldown Monitor Agent", () => {
         severity: FindingSeverity.Info,
         protocol: "Gnosis SafeSnap",
         metadata: {
-          questionId: testQuestionIdTwo,
-          questionFinalizeTimeStamp: testFinalizeTSTwo.toString(),
+          questionId: testQuestionIds[5],
+          questionFinalizeTimeStamp: testFinalizeTSs[5].toString(),
           blockNumber: testBlockNumber.toString()
         },
       })
