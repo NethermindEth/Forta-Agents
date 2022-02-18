96c806e10eeb4d33bd813e2bbf6d9e7551e2671a
Yasmine Bachi <gy_bachi@esi.dz>
2022-02-18 10:53:30 +0100

    fix: removed questionsId list

--- SafeSnap-Agents/AnswersSubmission/src/agent.ts
+++ SafeSnap-Agents/AnswersSubmission/src/agent.ts
@@ -5,47 +5,32 @@ import {
   TransactionEvent,
 } from "forta-agent";
 import OracleFetcher from "./oracle.fetcher";
-import {
-  ANSWERS_EVENTS_SIGNATURES,
-  QUESTION_CREATED_SIGNATURE,
-  SAFESNAP_CONTRACT,
-  createFinding,
-} from "./utils";
+import { EVENTS_SIGNATURES, SAFESNAP_CONTRACT, createFinding } from "./utils";
 
 const FETCHER = new OracleFetcher(getEthersProvider());
-const questions: string[] = [];
 let oracle: string = "";
+let safesnap_contract = SAFESNAP_CONTRACT;
 
-export const initialize = async () => {
-  oracle = await FETCHER.getOracle("latest", SAFESNAP_CONTRACT);
-};
+export const initialize =
+  (reality_module: string, fetcher: OracleFetcher) => async () => {
+    oracle = await fetcher.getOracle("latest", reality_module);
+  };
 
 export const provideHandleTransaction =
-  (reality_module: string, _oracle: string): HandleTransaction =>
+  (_oracle: string): HandleTransaction =>
   async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
     if (!_oracle) _oracle = oracle;
 
-    txEvent
-      .filterLog([QUESTION_CREATED_SIGNATURE], reality_module)
-      .map((log) => {
-        if (!questions.includes(log.args[0])) questions.push(log.args[0]); // save questionId if it doesn't exist.
-      });
-    if (questions.length === 0) return findings;
-
-    // get answers events logs.
-    txEvent.filterLog(ANSWERS_EVENTS_SIGNATURES, _oracle).map((log) => {
-      if (
-        (log.name === "LogAnswerReveal" && questions.includes(log.args[0])) ||
-        (log.name === "LogNewAnswer" && questions.includes(log.args[1]))
-      )
-        findings.push(createFinding(log));
+    // get events logs on the oracle.
+    txEvent.filterLog(EVENTS_SIGNATURES, _oracle).map((log) => {
+      findings.push(createFinding(log));
     });
 
     return findings;
   };
 
 export default {
-  initialize,
-  handleTransaction: provideHandleTransaction(SAFESNAP_CONTRACT, oracle),
+  initialize: initialize(safesnap_contract, FETCHER),
+  handleTransaction: provideHandleTransaction(oracle),
 };
