1661e9e5c7c5de328411bc77c113961c7adbf1b0
Lázaro Raúl <riglesiasvera@gmail.com>
2022-02-25 15:29:20 -0500

    feat: Add protocol to the findings
    add consistent usage of ;
    
    change some formatting

--- SafeSnap-Agents/CallMonitoring/src/agent.spec.ts
+++ SafeSnap-Agents/CallMonitoring/src/agent.spec.ts
@@ -18,7 +18,6 @@ import {
   GNOSIS_ADDRESS
 } from "./constants";
 
-
 const iface: utils.Interface = new utils.Interface([
   ADD_PROPOSOAL,
   ADD_PROPOSAL_WITH_NONCE,
@@ -28,23 +27,22 @@ const iface: utils.Interface = new utils.Interface([
   EXECUTE_PROPOSAL,
   EXECUTE_PROPOSAL_WITH_INDEX,
 ]);
-const zeroAddress:string = "0x0000000000000000000000000000000000000000"
+const zeroAddress: string = "0x0000000000000000000000000000000000000000";
 
-const createTransaction = (functionName:string, params:any[]) => {
+const createTransaction = (functionName: string, params:any[]) => {
   return iface.encodeFunctionData(
     functionName, 
-    [...params]
+    [...params],
   );
-}
+};
 
-
-const createFinding = (functionName:string, params: any) => {
-  let metadata:any = {
-    by: zeroAddress
-  }
-  for(let key in params){
+const createFinding = (functionName: string, params: any) => {
+  const metadata:any = {
+    by: zeroAddress,
+  };
+  for(const key in params){
     metadata[key] = params[key].toString()
-  }
+  };
 
   return Finding.fromObject({
     name: "Zodiac/Gnosis SafeSnap",
@@ -52,33 +50,33 @@ const createFinding = (functionName:string, params: any) => {
     alertId: "SafeSnap-1",
     severity: FindingSeverity.Info,
     type: FindingType.Info,
-    metadata: metadata
-  })
-}
+    protocol: "Gnosis SafeSnap",
+    metadata: metadata,
+  });
+};
 
+describe("Calls monitor agent", () => {
+  it("should detect addProposal function call", async () => {
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
 
-describe("Monitor function", ()=>{
-  it("Should execute addProposal function", async () => {
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
-
-    const Transaction:string = createTransaction("addProposal",[proposalId,txHashes])
+    const Transaction: string = createTransaction("addProposal",[proposalId,txHashes]);
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
     const expectedFinding = createFinding("addProposal",{
       proposalId: proposalId,
       txHashes: txHashes[0],
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-  })
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
 
-  it("Should execute addProposalWithNonce function", async () => {
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
-    let nonce:string = "4"
+  it("should detect addProposalWithNonce function call", async () => {
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+    const nonce: string = "4";
 
-    const Transaction:string = createTransaction("addProposalWithNonce",[proposalId,txHashes,nonce])
+    const Transaction: string = createTransaction("addProposalWithNonce",[proposalId,txHashes,nonce]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
@@ -86,93 +84,92 @@ describe("Monitor function", ()=>{
       proposalId: proposalId,
       txHashes: txHashes[0],
       nonce: nonce
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-  })
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
 
-  it("Should execute markProposalAsInvalid function", async () => {
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+  it("should detect markProposalAsInvalid function call", async () => {
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
 
-    const Transaction:string = createTransaction("markProposalAsInvalid",[proposalId,txHashes])
+    const Transaction: string = createTransaction("markProposalAsInvalid", [proposalId, txHashes]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
     
-    const expectedFinding = createFinding("markProposalAsInvalid",{
+    const expectedFinding = createFinding("markProposalAsInvalid", {
       proposalId: proposalId,
       txHashes: txHashes[0],
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-  })
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
 
-  it("Should execute markProposalAsInvalidByHash function", async () => {
-    let questionHash:string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
-    const Transaction:string = createTransaction("markProposalAsInvalidByHash",[questionHash])
+  it("should detect markProposalAsInvalidByHash function call", async () => {
+    const questionHash: string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
+    const Transaction: string = createTransaction("markProposalAsInvalidByHash", [questionHash]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
     const expectedFinding = createFinding("markProposalAsInvalidByHash",{
       questionHash: questionHash,
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-  })
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
 
-  it("Should execute markProposalWithExpiredAnswerAsInvalid function", async () => {
-    let questionHash:string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
-    const Transaction:string = createTransaction("markProposalWithExpiredAnswerAsInvalid",[questionHash])
+  it("should detect markProposalWithExpiredAnswerAsInvalid function call", async () => {
+    const questionHash: string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
+    const Transaction: string = createTransaction("markProposalWithExpiredAnswerAsInvalid", [questionHash]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
-    const expectedFinding = createFinding("markProposalWithExpiredAnswerAsInvalid",{
+    const expectedFinding = createFinding("markProposalWithExpiredAnswerAsInvalid", {
       questionHash: questionHash,
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-  })
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
 
-  it("Should execute executeProposal function", async () => {
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
-    let to: string = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"
-    let value:string = "0"
-    let data:string = "0x28ed4f6cd0185bc80db4dae66d986f141f5fe4797341406a201f5189027c6e6a775522c80000000000000000000000000a147ddf0817ade664eb9cb343d96a21ed857d11"
-    let operation:string = "0"
+  it("should detect executeProposal function call", async () => {
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+    const to: string = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85";
+    const value: string = "0";
+    const data: string = "0x28ed4f6cd0185bc80db4dae66d986f141f5fe4797341406a201f5189027c6e6a775522c80000000000000000000000000a147ddf0817ade664eb9cb343d96a21ed857d11";
+    const operation: string = "0";
 
-    const Transaction:string = createTransaction("executeProposal",[proposalId,txHashes,to,value,data,operation])
+    const Transaction: string = createTransaction("executeProposal", [proposalId, txHashes, to, value, data, operation]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
-    const expectedFinding = createFinding("executeProposal",{
+    const expectedFinding = createFinding("executeProposal", {
       to: to,
       proposalId: proposalId,
       txHashes: txHashes[0],
       value: value,
       data: data,
       operation: operation,
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
-
+    expect(findings).toStrictEqual([expectedFinding]);
   });
 
-  it("Should execute executeProposalWithIndex function", async () => {
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
-    let to: string = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"
-    let value:string = "0"
-    let data:string = "0x28ed4f6cd0185bc80db4dae66d986f141f5fe4797341406a201f5189027c6e6a775522c80000000000000000000000000a147ddf0817ade664eb9cb343d96a21ed857d11"
-    let operation:string = "0"
-    let txIndex:string = "0"
+  it("should detect executeProposalWithIndex function call", async () => {
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+    const to: string = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85";
+    const value: string = "0";
+    const data: string = "0x28ed4f6cd0185bc80db4dae66d986f141f5fe4797341406a201f5189027c6e6a775522c80000000000000000000000000a147ddf0817ade664eb9cb343d96a21ed857d11";
+    const operation: string = "0";
+    const txIndex: string = "0";
 
-    const Transaction:string = createTransaction("executeProposalWithIndex",[proposalId,txHashes,to,value,data,operation,txIndex])
+    const Transaction: string = createTransaction("executeProposalWithIndex",[proposalId, txHashes, to, value, data, operation, txIndex]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(GNOSIS_ADDRESS);
     const findings = await agent.handleTransaction(txEvent);
-    const expectedFinding = createFinding("executeProposalWithIndex",{
+    const expectedFinding = createFinding("executeProposalWithIndex", {
       to: to,
       proposalId: proposalId,
       txHashes: txHashes[0],
@@ -180,46 +177,47 @@ describe("Monitor function", ()=>{
       data: data,
       operation: operation,
       txIndex: txIndex,
-    })
+    });
 
-    expect(findings).toStrictEqual([expectedFinding,]);
+    expect(findings).toStrictEqual([expectedFinding]);
   });
 
-  it("Should return no finding for other contract address", async ()=>{
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+  it("should return no finding for other contract address", async ()=>{
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
 
-    const Transaction:string = createTransaction("addProposal",[proposalId,txHashes])
+    const Transaction: string = createTransaction("addProposal", [proposalId, txHashes]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent().setData(Transaction).setTo(zeroAddress);
     const findings = await agent.handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
-  })
-  it("Should return findings for multiple function call in same Transaction", async ()=>{
-    let proposalId:string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
-    let txHashes:string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
-    let questionHash:string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
+  });
+
+  it("should return findings for multiple function calls in same Transaction", async ()=>{
+    const proposalId: string = "QmWwo3TkBtUybd7wcwix6yuGtApR427FBCxiSGJNwowDoN";
+    const txHashes: string[] = ["0x198752008246c30849803849ca78d3cdf204683715a591c62f3c9ef71cf63710"];
+    const questionHash: string = "0x6d6168616d000000000000000000000000000000000000000000000000000000";
     const txEvent: TransactionEvent = new TestTransactionEvent().setTo(GNOSIS_ADDRESS).addTraces({
         to: GNOSIS_ADDRESS,
         from: zeroAddress,
-        input: createTransaction("addProposal",[proposalId,txHashes])
-    },{
+        input: createTransaction("addProposal", [proposalId, txHashes])
+      }, {
         to: GNOSIS_ADDRESS,
         from: zeroAddress,
-        input: createTransaction("markProposalWithExpiredAnswerAsInvalid",[questionHash])
+        input: createTransaction("markProposalWithExpiredAnswerAsInvalid", [questionHash])
     });
-    const expectedFinding: Finding[] = [];
-    expectedFinding.push(createFinding("addProposal",{
+    const expectedFindings: Finding[] = [
+      createFinding("addProposal", {
         proposalId: proposalId,
         txHashes: txHashes,
-      }))
-    expectedFinding.push(createFinding("markProposalWithExpiredAnswerAsInvalid",{
+      }),
+      createFinding("markProposalWithExpiredAnswerAsInvalid", {
         questionHash: questionHash,
-    }))
+      }),
+    ];
 
     const findings = await agent.handleTransaction(txEvent);
-    expect(findings).toStrictEqual(expectedFinding);
-  })
-
-})
+    expect(findings).toStrictEqual(expectedFindings);
+  });
+});
--- SafeSnap-Agents/CallMonitoring/src/agent.ts
+++ SafeSnap-Agents/CallMonitoring/src/agent.ts
@@ -4,10 +4,7 @@ import {
   TransactionEvent, 
   FindingSeverity, 
   FindingType,
-  keccak256
-} from 'forta-agent'
-
-import { utils } from "ethers"
+} from 'forta-agent';
 import { 
   ADD_PROPOSOAL,
   ADD_PROPOSAL_WITH_NONCE,
@@ -17,57 +14,57 @@ import {
   EXECUTE_PROPOSAL,
   EXECUTE_PROPOSAL_WITH_INDEX,
   GNOSIS_ADDRESS, 
-} from "./constants"
+} from "./constants";
 
 export const provideHandleTransaction = (contractAddress: string): HandleTransaction => { 
-	return async (txEvent: TransactionEvent) => {
-	  let findings: Finding[] = []
-	  const mainFunctionInvocations = txEvent.filterFunction(
-	    [
-	      EXECUTE_PROPOSAL_WITH_INDEX,
-	      ADD_PROPOSOAL,
-	      ADD_PROPOSAL_WITH_NONCE,
-	      MARK_PROPOSAL_AS_INVALID,
-	      MARK_PROPOSAL_AS_INVALID_BY_HASH,
-	      MARK_PROPOSAL_WITH_EXPIRED_ANSWER_AS_INVALID,
-	      EXECUTE_PROPOSAL
-	      
-	    ],
-	    contractAddress
-	  );
+  return async (txEvent: TransactionEvent) => {
+    let findings: Finding[] = [];
+    const mainFunctionInvocations = txEvent.filterFunction(
+      [
+        EXECUTE_PROPOSAL_WITH_INDEX,
+        ADD_PROPOSOAL,
+        ADD_PROPOSAL_WITH_NONCE,
+        MARK_PROPOSAL_AS_INVALID,
+        MARK_PROPOSAL_AS_INVALID_BY_HASH,
+        MARK_PROPOSAL_WITH_EXPIRED_ANSWER_AS_INVALID,
+        EXECUTE_PROPOSAL
+        
+      ],
+      contractAddress,
+    );
 
-	  mainFunctionInvocations.forEach((functionInvocation) => {
-	    // Get name of the function that is executed
-	    let functionName:string = functionInvocation.name
-	    // Create a list of the params of the function
-	    let argumentsList:any[] = functionInvocation.functionFragment.inputs.map((obj)=>obj.name)
+    mainFunctionInvocations.forEach((functionInvocation) => {
+      // Get name of the function that is executed
+      let functionName:string = functionInvocation.name;
+      // Create a list of the params of the function
+      let argumentsList:any[] = functionInvocation.functionFragment.inputs.map((obj)=>obj.name);
 
-	    let metadata:any = {
-	      by: txEvent.from,
-	    }
-	    for (let key of argumentsList) {
-	        metadata[key] = functionInvocation.args[key].toString();
-	    }
+      let metadata:any = {
+        by: txEvent.from,
+      };
+      for (let key of argumentsList) {
+          metadata[key] = functionInvocation.args[key].toString();
+      }
 
-	    // Create Finding
-	    findings.push(
-	      Finding.fromObject({
-	        name: "Zodiac/Gnosis SafeSnap",
-	        description: `${functionName} execeuted`,
-	        alertId: "SafeSnap-1",
-	        severity: FindingSeverity.Info,
-	        type: FindingType.Info,
-	        metadata: metadata,
-	      })
-	    );
-	  });
+      // Create Finding
+      findings.push(
+        Finding.fromObject({
+          name: "Zodiac/Gnosis SafeSnap",
+          description: `${functionName} execeuted`,
+          alertId: "SafeSnap-1",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          protocol: "Gnosis SafeSnap",
+          metadata: metadata,
+        })
+      );
+    });
 
-	  return findings
-
-}}
+    return findings;
+}};
 
 export default {
   handleTransaction: provideHandleTransaction(
-  	GNOSIS_ADDRESS
+    GNOSIS_ADDRESS,
   ),
-}
+};
