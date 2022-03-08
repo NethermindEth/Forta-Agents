45cc3aa013badc46f7f32f5b8aded35cb0525289
RCantu92 <Roberto_Cantu@live.com>
2022-03-08 13:41:48 -0600

    chore: Update Flexa contract ABI

--- Flexa-Agents/Large-deposits/src/abi.ts
+++ Flexa-Agents/Large-deposits/src/abi.ts
@@ -1,20 +1,20 @@
 const AMP_TOKEN = [
-    `event TransferByPartition(
-      bytes32 indexed fromPartition,
-      address operator, 
-      address indexed from,
-      address indexed to,
-      uint256 value,
-      bytes data, 
-      bytes operatorData
-    )`,
-  ];
+  `event TransferByPartition(
+    bytes32 indexed fromPartition,
+    address operator, 
+    address indexed from,
+    address indexed to,
+    uint256 value,
+    bytes data, 
+    bytes operatorData
+  )`,
+];
   
-  const FLEXA_TOKEN = [
-    "function partitions(bytes32 partition) view returns (bool isPartition)",
-  ];
+const FLEXA_CONTRACT = [
+  "function partitions(bytes32) view returns (bool)",
+];
   
-  export default {
-    FLEXA_TOKEN,
-    AMP_TOKEN,
-  };
+export default {
+  FLEXA_CONTRACT,
+  AMP_TOKEN,
+};
