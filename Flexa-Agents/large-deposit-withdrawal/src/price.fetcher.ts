6f2e5be79489c21d2c36668aaba7e67e6928c79d
Yasmine Bachi <gy_bachi@esi.dz>
2022-03-08 14:00:45 +0100

    chore: fixed typo and removed unused type

--- Flexa-Agents/large-deposit-withdrawal/src/price.fetcher.ts
+++ Flexa-Agents/large-deposit-withdrawal/src/price.fetcher.ts
@@ -2,7 +2,7 @@ import { providers, Contract, BigNumber } from "ethers";
 import LRU from "lru-cache";
 import { PRICE_ABI } from "./utils";
 
-export type Reponse = [
+export type Response = [
   //response struct
   roundId: BigNumber,
   answer: BigNumber,
@@ -12,11 +12,11 @@ export type Reponse = [
 ];
 export default class PriceFetcher {
   private provider: providers.Provider;
-  private cache: LRU<string, Promise<Reponse | Set<string>>>;
+  private cache: LRU<string, Promise<Response>>;
 
   constructor(provider: providers.Provider) {
     this.provider = provider;
-    this.cache = new LRU<string, Promise<Reponse | Set<string>>>({
+    this.cache = new LRU<string, Promise<Response>>({
       max: 10000,
     });
   }
@@ -24,11 +24,11 @@ export default class PriceFetcher {
   public async getAmpPrice(
     block: number | string,
     contract: string
-  ): Promise<Reponse> {
+  ): Promise<Response> {
     const key: string = `${block} - ${contract}`;
-    if (this.cache.has(key)) return this.cache.get(key) as Promise<Reponse>;
+    if (this.cache.has(key)) return this.cache.get(key) as Promise<Response>;
     const priceFeedContract = new Contract(contract, PRICE_ABI, this.provider);
-    const ampPrice: Promise<Reponse> = priceFeedContract.latestRoundData({
+    const ampPrice: Promise<Response> = priceFeedContract.latestRoundData({
       blockTag: block,
     });
     this.cache.set(key, ampPrice);
