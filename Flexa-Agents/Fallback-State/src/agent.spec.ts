f01680d686c3f000c7ab69baf4097f619e34eec5
Tayfun Sur <tayfunsur@gmail.com>
2022-03-08 15:09:13 +0300

    fix: Correct grammar

--- Flexa-Agents/Fallback-State/src/agent.spec.ts
+++ Flexa-Agents/Fallback-State/src/agent.spec.ts
@@ -10,8 +10,8 @@ const mockFetcher = {
   getPreviousBlockTimestamp: jest.fn()
 };
 
-const testBlockNumber: number[] = [14306500, 14306501];
-const testFallbackSetDate: number[] = [1500000000, 1600000000];
+const testBlockNumbers: number[] = [14306500, 14306501];
+const testFallbackSetDates: number[] = [1500000000, 1600000000];
 const testFallbackWithdrawalDelaySeconds: number = 3900000;
 const testBlockTimestamp: number = 1600000009;
 const testPreviousBlockTimestamp: number = 1600000000;
@@ -38,13 +38,13 @@ describe("Flexa Staking Contract Fallback State Tests", () => {
 
   it("should return empty finding if contract isn't in fallback state", async () => {
     when(mockFetcher.getFallbackSetDate)
-      .calledWith(testBlockNumber[1])
-      .mockReturnValue(BigNumber.from(testFallbackSetDate[1]));
+      .calledWith(testBlockNumbers[1])
+      .mockReturnValue(BigNumber.from(testFallbackSetDates[1]));
     when(mockFetcher.getFallbackWithdrawalDelaySeconds)
-      .calledWith(testBlockNumber[1])
+      .calledWith(testBlockNumbers[1])
       .mockReturnValue(BigNumber.from(testFallbackWithdrawalDelaySeconds));
 
-    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumber[1]);
+    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumbers[1]);
     const findings = await handleBlock(blockEvent);
 
     expect(findings).toStrictEqual([]);
@@ -52,22 +52,22 @@ describe("Flexa Staking Contract Fallback State Tests", () => {
 
   it("should return empty finding if contract is in fallback state in the last two blocks", async () => {
     when(mockFetcher.getFallbackSetDate)
-      .calledWith(testBlockNumber[1])
-      .mockReturnValue(BigNumber.from(testFallbackSetDate[0]));
+      .calledWith(testBlockNumbers[1])
+      .mockReturnValue(BigNumber.from(testFallbackSetDates[0]));
     when(mockFetcher.getFallbackSetDate)
-      .calledWith(testBlockNumber[0])
-      .mockReturnValue(BigNumber.from(testFallbackSetDate[0]));
+      .calledWith(testBlockNumbers[0])
+      .mockReturnValue(BigNumber.from(testFallbackSetDates[0]));
     when(mockFetcher.getFallbackWithdrawalDelaySeconds)
-      .calledWith(testBlockNumber[1])
+      .calledWith(testBlockNumbers[1])
       .mockReturnValue(BigNumber.from(testFallbackWithdrawalDelaySeconds));
     when(mockFetcher.getFallbackWithdrawalDelaySeconds)
-      .calledWith(testBlockNumber[0])
+      .calledWith(testBlockNumbers[0])
       .mockReturnValue(BigNumber.from(testFallbackWithdrawalDelaySeconds));
     when(mockFetcher.getPreviousBlockTimestamp)
-      .calledWith(testBlockNumber[1])
+      .calledWith(testBlockNumbers[1])
       .mockReturnValue(testPreviousBlockTimestamp);
 
-    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumber[1]);
+    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumbers[1]);
     const findings = await handleBlock(blockEvent);
 
     expect(findings).toStrictEqual([]);
@@ -75,22 +75,22 @@ describe("Flexa Staking Contract Fallback State Tests", () => {
 
   it("should return a finding if contract is in fallback state only in the last block", async () => {
     when(mockFetcher.getFallbackSetDate)
-      .calledWith(testBlockNumber[1])
-      .mockReturnValue(BigNumber.from(testFallbackSetDate[0]));
+      .calledWith(testBlockNumbers[1])
+      .mockReturnValue(BigNumber.from(testFallbackSetDates[0]));
     when(mockFetcher.getFallbackSetDate)
-      .calledWith(testBlockNumber[0])
-      .mockReturnValue(BigNumber.from(testFallbackSetDate[1]));
+      .calledWith(testBlockNumbers[0])
+      .mockReturnValue(BigNumber.from(testFallbackSetDates[1]));
     when(mockFetcher.getFallbackWithdrawalDelaySeconds)
-      .calledWith(testBlockNumber[1])
+      .calledWith(testBlockNumbers[1])
       .mockReturnValue(BigNumber.from(testFallbackWithdrawalDelaySeconds));
     when(mockFetcher.getFallbackWithdrawalDelaySeconds)
-      .calledWith(testBlockNumber[0])
+      .calledWith(testBlockNumbers[0])
       .mockReturnValue(BigNumber.from(testFallbackWithdrawalDelaySeconds));
     when(mockFetcher.getPreviousBlockTimestamp)
-      .calledWith(testBlockNumber[1])
+      .calledWith(testBlockNumbers[1])
       .mockReturnValue(testPreviousBlockTimestamp);
 
-    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumber[1]);
+    const blockEvent = new TestBlockEvent().setTimestamp(testBlockTimestamp).setNumber(testBlockNumbers[1]);
     const findings = await handleBlock(blockEvent);
 
     expect(findings).toStrictEqual([createFinding()]);
