be37544056f39183bdd189b3c974cefdf5b40436
ishitarastogi <ishita3099@gmail.com>
2022-03-09 14:36:20 +0530

    Add test

--- Flexa-Agents/Large-deposits/src/agent.spec.ts
+++ Flexa-Agents/Large-deposits/src/agent.spec.ts
@@ -204,4 +204,50 @@ describe("Large stake deposits", () => {
 
     expect(findings).toStrictEqual([]);
   });
+
+  it("should return empty findings when the transfer is emitted from a different partition", async () => {
+    //When partition mapping is false
+
+    const testFromPartition: string = toBytes32("0xc571");
+    const testFrom: string = createAddress("0xabc258");
+    const testTo: string = createAddress("0xabc841");
+
+    const testOperator: string = createAddress("0xdef954");
+    const bytesOperatorData: string = toBytes32("0x0952");
+    const testValue: BigNumber = BigNumber.from(200);
+
+    const testDestinationPartition: string = toBytes32("0xd689");
+    const testData: string = encodeParameters(
+      ["bytes32", "bytes32"],
+      [testFlag, testDestinationPartition]
+    );
+
+    const { data, topics } = testAmpIFace.encodeEventLog(
+      testAmpIFace.getEvent("TransferByPartition"),
+      [
+        testFromPartition,
+        testOperator,
+        testFrom,
+        testTo,
+        testValue,
+        testData,
+        bytesOperatorData,
+      ]
+    );
+
+    // prepare the partitions call
+    mockProvider.addCallTo(testFlexa, 75, testFlexaIFace, "partitions", {
+      inputs: [testDestinationPartition],
+      outputs: [false],
+    });
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setBlock(75)
+      .addAnonymousEventLog(testAmp, data, ...topics);
+
+    const findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+
+  });
 });
