a6a521b8fd076ddea558fd79243a94b36c0007b8
Tayfun Sur <tayfunsur@gmail.com>
2022-03-11 19:58:25 +0300

    refac: Remove initialize function & different networks

--- Paraswap-Agents/Role-Changes/src/utils.ts
+++ Paraswap-Agents/Role-Changes/src/utils.ts
@@ -1,15 +1,6 @@
 import { LogDescription, Finding, FindingSeverity, FindingType } from "forta-agent";
 
-// <ChainId, AugustusSwapper contract address>
-export const AUGUSTUS_SWAPPER_CONTRACTS: { [key: number]: string } = {
-  1: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // Ethereum Mainnet
-  137: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // Polygon
-  56: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // BSC
-  43114: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // Avalanche
-  250: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // Fantom
-  3: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57", // Ropsten
-};
-
+export const CONTRACT = "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57"; //AugustusSwapper contract
 export const EVENTS_SIGNATURES = [
   "event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole)",
   "event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender)",
@@ -32,7 +23,6 @@ export const createFinding = (log: LogDescription) => {
       sender: log.args.sender,
     };
   }
-
   return Finding.fromObject({
     name: `Role Change detected on AccessControl contract`,
     description: `${log.name} event emitted`,
