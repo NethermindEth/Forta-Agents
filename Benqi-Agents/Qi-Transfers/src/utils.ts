22f6a1fa8b3a69882b4c3b8c1ce6b46efcb557cd
Tayfun Sur <tayfunsur@gmail.com>
2022-03-20 23:02:37 +0300

    feat: Create finding functions

---
+++ Benqi-Agents/Qi-Transfers/src/utils.ts
@@ -0,0 +1,39 @@
+import { Finding, FindingSeverity, FindingType, LogDescription } from "forta-agent";
+import { ethers, BigNumber } from "ethers";
+
+export const QI_TOKEN_CONTRACT = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5"; //Benqi token(qi) contract
+export const EVENT_ABI = ["event Transfer(address indexed from, address indexed to, uint256 amount)"];
+export const BALANCE_ABI = ["function balanceOf(address account) external view returns (uint)"];
+
+export const benqiInterface = new ethers.utils.Interface(BALANCE_ABI);
+
+export const createTransferFinding = (log: LogDescription) => {
+  return Finding.fromObject({
+    name: "QI Token Transfer",
+    description: "Large amount of QI token transfer is detected",
+    alertId: "BENQI-3-1",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    protocol: "Benqi",
+    metadata: {
+      from: log.args.from,
+      to: log.args.to,
+      amount: BigNumber.from(log.args.amount).toString(),
+    },
+  });
+};
+
+export const createLargeBalanceFinding = (account: string, balance: BigNumber) => {
+  return Finding.fromObject({
+    name: "Large Amount of QI Balance",
+    description: "A user with large amount of QI token is detected",
+    alertId: "BENQI-3-2",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    protocol: "Benqi",
+    metadata: {
+      account,
+      balance: balance.toString(),
+    },
+  });
+};
