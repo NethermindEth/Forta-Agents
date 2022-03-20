4458c119c7a9a5caaa69c193fd14494461ff9083
Tayfun Sur <tayfunsur@gmail.com>
2022-03-20 23:03:40 +0300

    feat: Create data fetcher for 'balanceOf' function

---
+++ Benqi-Agents/Qi-Transfers/src/data.fetcher.ts
@@ -0,0 +1,20 @@
+import { Contract, providers, BigNumber } from "ethers";
+import { benqiInterface } from "./utils";
+
+export default class DataFetcher {
+  readonly qiTokenAddress: string;
+  readonly provider: providers.Provider;
+  private qiTokenContract: Contract;
+
+  constructor(qiTokenAddress: string, provider: providers.Provider) {
+    this.qiTokenAddress = qiTokenAddress;
+    this.provider = provider;
+    this.qiTokenContract = new Contract(qiTokenAddress, benqiInterface, provider);
+  }
+
+  // return the number of tokens held by the account
+  public async getBalance(account: string): Promise<BigNumber> {
+    const balance: BigNumber = await this.qiTokenContract.balanceOf(account, { blockTag: "latest" });
+    return balance;
+  }
+}
