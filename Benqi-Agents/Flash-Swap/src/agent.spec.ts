3eb6a299a446fe1348c9422b6eaf7e5b2d2b5c02
RCantu92 <Roberto_Cantu@live.com>
2022-03-20 19:08:27 -0500

    tests: Add unit test for agent's functionality

--- Benqi-Agents/Flash-Swap/src/agent.spec.ts
+++ Benqi-Agents/Flash-Swap/src/agent.spec.ts
@@ -1,48 +1,133 @@
 import {
-  FindingType,
-  FindingSeverity,
   Finding,
   HandleTransaction,
-  createTransactionEvent
-} from "forta-agent"
-import agent from "./agent"
+  TransactionEvent
+} from "forta-agent";
+import {
+  createAddress,
+  TestTransactionEvent
+} from "forta-agent-tools/lib/tests";
+import { utils } from "ethers";
+import { provideHandleTransaction } from "./agent";
+import {
+  SWAP_ABI,
+  createFinding
+} from "./utils";
 
-describe("high gas agent", () => {
+const testPglContract: string = createAddress("0xab01");
+const testMsgSender: string = createAddress("0xac02");
+
+const testSwapIFace = new utils.Interface([SWAP_ABI]);
+
+// Format: amount0Out, amount1Out, to, data
+const testCases: any[][] = [
+  [100, 100, createAddress("0xab03"), "0x12ab"],
+  [300, 300, createAddress("0xac04"), "0x34ac"],
+  [500, 500, createAddress("0xad05"), "0x45ad"],
+  [800, 800, createAddress("0xae06"), ""]
+];
+
+const encodedSwapCalls: string[] = [
+  testSwapIFace.encodeFunctionData("swap", testCases[0]),
+  testSwapIFace.encodeFunctionData("swap", testCases[1]),
+  testSwapIFace.encodeFunctionData("swap", testCases[2]),
+  testSwapIFace.encodeFunctionData("swap", testCases[3])
+];
+
+describe("Flash Swap agent alert test suite", () => {
   let handleTransaction: HandleTransaction
 
-  const createTxEventWithGasUsed = (gasUsed: string) => createTransactionEvent({
-    transaction: {} as any,
-    receipt: { gasUsed } as any,
-    block: {} as any,
+  beforeEach(() => {
+    handleTransaction = provideHandleTransaction(testPglContract);
   })
 
-  beforeAll(() => {
-    handleTransaction = agent.handleTransaction
+  it("should ignore empty transactions", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent();
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
   })
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if gas used is below threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1")
+  it("should ignore swap function calls to the wrong contract", async () => {
+    const wrongContract: string = createAddress("0xD3ad");
 
-      const findings = await handleTransaction(txEvent)
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(testMsgSender)
+      .setTo(wrongContract)
+      .addTraces({
+        from: testMsgSender,
+        to: wrongContract,
+        input: encodedSwapCalls[0]
+      });
 
-      expect(findings).toStrictEqual([])
-    })
+    const findings: Finding[] = await handleTransaction(txEvent);
 
-    it("returns a finding if gas used is above threshold", async () => {
-      const txEvent = createTxEventWithGasUsed("1000001")
+    expect(findings).toStrictEqual([]);
+  })
 
-      const findings = await handleTransaction(txEvent)
+  it("should ignore function calls to the wrong function in the right contract", async () => {
+    const wrongAbi: string = "function burn(address to)";
+    const wrongIFace = new utils.Interface([wrongAbi]);
+    const wrongFuncCall: string = wrongIFace.encodeFunctionData("burn", [createAddress("0xde4D")]);
 
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Gas Used",
-          description: `Gas Used: ${txEvent.gasUsed}`,
-          alertId: "FORTA-1",
-          type: FindingType.Suspicious,
-          severity: FindingSeverity.Medium
-        }),
-      ])
-    })
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(testMsgSender)
+      .setTo(testPglContract)
+      .addTraces({
+        from: testMsgSender,
+        to: testPglContract,
+        input: wrongFuncCall
+      });
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  })
+
+  it("should return multiple Findings from calls to swap function when 'data.length' is more than zero", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(testMsgSender)
+      .setTo(testPglContract)
+      .addTraces({
+        from: testMsgSender,
+        to: testPglContract,
+        input: encodedSwapCalls[1]
+      })
+      .addTraces({
+        from: testMsgSender,
+        to: testPglContract,
+        input: encodedSwapCalls[2]
+      });
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([
+      createFinding(
+        testCases[1][0].toString(),
+        testCases[1][1].toString(),
+        testCases[1][2]
+      ),
+      createFinding(
+        testCases[2][0].toString(),
+        testCases[2][1].toString(),
+        testCases[2][2]
+      )
+    ]);
+  })
+
+  it("should ignore calls to swap function when 'data.length' is zero", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(testMsgSender)
+      .setTo(testPglContract)
+      .addTraces({
+        from: testMsgSender,
+        to: testPglContract,
+        input: encodedSwapCalls[3]
+      });
+
+    const findings: Finding[] = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
   })
 })
