50e05082f0cde1d236edafa9ada3a4378334a0b1
ishita rastogi <46647968+ishitarastogi@users.noreply.github.com>
2022-03-22 02:52:42 +0530

    chore: Update variable name
    Co-authored-by: Yasmine Bachi <36101876+yasmine9@users.noreply.github.com>

--- Benqi-Agents/DelegationMonitor/src/agent.ts
+++ Benqi-Agents/DelegationMonitor/src/agent.ts
@@ -11,7 +11,7 @@ import { BigNumber, providers } from "ethers";
 
 import util from "./utils";
 
-const Qi_CONTRACT: string = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5";
+const QI_CONTRACT: string = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5";
 const AMOUNT_THRESHOLD: BigNumber = BigNumber.from(10 ** 6); // 1M USD
 
 export const createFinding = (
