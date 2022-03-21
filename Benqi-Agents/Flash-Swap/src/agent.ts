f3bb8d45e3bfbbe44afea3a4a700570c47d0ca15
RCantu92 <Roberto_Cantu@live.com>
2022-03-20 19:07:47 -0500

    feat: Add agent implementation

--- Benqi-Agents/Flash-Swap/src/agent.ts
+++ Benqi-Agents/Flash-Swap/src/agent.ts
@@ -1,38 +1,32 @@
 import { 
-  BlockEvent, 
   Finding, 
-  HandleBlock, 
   HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType,
-  LogDescription
+  TransactionEvent
 } from 'forta-agent';
+import { utils } from "ethers";
+import {
+  PGL_CONTRACT,
+  SWAP_ABI,
+  createFinding
+} from "./utils";
 
-const PGL_CONTRACT: string = "0xE530dC2095Ef5653205CF5ea79F8979a7028065c";
-const SWAP_ABI: string = "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)";
 
-export const createFinding = (log: LogDescription) => {
-  return Finding.fromObject({
-    name: 'Flash Swap Alert', // Should be 'Flash Swap Alert'?
-    description: 'Flash swap detected on PGL contract.',
-    alertId: "BENQI-9",
-    severity: FindingSeverity.Info,
-    type: FindingType.Info,
-    protocol: "Benqi",
-    metadata: {},
-  });
-};
 
 export const provideHandleTransaction = (pglContract: string): HandleTransaction =>
 async (txEvent: TransactionEvent): Promise<Finding[]> => {
   const findings: Finding[] = [];
 
-  const logs: LogDescription[] = txEvent.filterLog(SWAP_ABI, pglContract);
+  const txns: utils.TransactionDescription[] = txEvent.filterFunction(SWAP_ABI, pglContract);
 
-  // if logs meet criteria for flash swap, create Findings
-  // Criteria:
-  //    1. `Swap` function call param `data` has `length > 0`.
+  txns.forEach(txn => {
+    if(txn.args["data"].length > 0) {
+      findings.push(createFinding(
+        txn.args["amount0Out"].toString(),
+        txn.args["amount1Out"].toString(),
+        txn.args["to"]
+      ));
+    }
+  });
 
   return findings;
 }
