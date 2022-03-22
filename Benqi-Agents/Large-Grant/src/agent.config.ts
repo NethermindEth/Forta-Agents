f6c793d06e801c20fa9478a172cc0107fda70087
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-03-22 17:18:53 -0300

    doc: Update agent config documentation

--- Benqi-Agents/Large-Grant/src/agent.config.ts
+++ Benqi-Agents/Large-Grant/src/agent.config.ts
@@ -2,19 +2,19 @@ import { AgentConfig, ThresholdMode } from "./utils";
 
 const CONFIG: AgentConfig = {
   /**
-   * THRESHOLD_MODE:
-   *  Can be:
-   *  - ABSOLUTE: A fixed amount in QI wei
-   *  - PERCENTAGE_TOTAL_SUPPLY: A percent of QI's totalSupply
-   *  - PERCENTAGE_COMP_BALANCE: A percent of the comptroller's QI balance
+   * config.thresholdMode:
+   *    Can be:
+   *     - ABSOLUTE: A fixed amount in QI wei
+   *     - PERCENTAGE_TOTAL_SUPPLY: A percent of QI's totalSupply
+   *     - PERCENTAGE_COMP_BALANCE: A percent of the comptroller's QI balance
    */
   thresholdMode: ThresholdMode.ABSOLUTE,
 
   /**
-   * THRESHOLD:
-   *  It can be a string indicating the fixed threshold amount, in the case of
-   *  THRESHOLD_MODE being ABSOLUTE, or a string with an integer indicating the
-   *  percentage, if THRESHOLD_MODE is PERCENTAGE based.
+   * config.threshold:
+   *    It can be a string indicating the fixed threshold amount, in the case
+   *    of config.thresholdMode being ABSOLUTE, or a string with an integer
+   *    indicating a percentage, if config.thresholdMode is PERCENTAGE based.
    */
   threshold: "1000000000000000000",
 };
