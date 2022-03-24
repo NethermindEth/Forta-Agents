2831bbf0040ee9ac35b8b2fe84294924e0eef339
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-03-24 10:31:36 -0300

    fix: Minor changes in constants.ts

--- Benqi-Agents/Large-Delegate-Votes-Change/src/constants.ts
+++ Benqi-Agents/Large-Delegate-Votes-Change/src/constants.ts
@@ -1,8 +1,8 @@
 import { BigNumber } from "ethers";
 
-export const QI_ADDRESS = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5".toLowerCase();
+export const QI_ADDRESS = "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5";
 
 export const DELEGATE_VOTES_CHANGED_ABI =
   "event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance)";
 
-export const PERCENTAGE_THRESHOLD = BigNumber.from("30");
+export const PERCENTAGE_THRESHOLD = BigNumber.from("30"); // 30%
