a90ecb3c90640711425abd8e738f74d277aab5b7
Tayfun Sur <tayfunsur@gmail.com>
2022-03-24 15:44:58 +0300

    refac: Add LRU cache

--- Benqi-Agents/Qi-Transfers/src/data.fetcher.ts
+++ Benqi-Agents/Qi-Transfers/src/data.fetcher.ts
@@ -1,20 +1,28 @@
 import { Contract, providers, BigNumber } from "ethers";
 import { benqiInterface } from "./utils";
+import LRU from "lru-cache";
 
 export default class DataFetcher {
   readonly qiTokenAddress: string;
   readonly provider: providers.Provider;
   private qiTokenContract: Contract;
+  private cache: LRU<string, BigNumber>;
 
   constructor(qiTokenAddress: string, provider: providers.Provider) {
     this.qiTokenAddress = qiTokenAddress;
     this.provider = provider;
     this.qiTokenContract = new Contract(qiTokenAddress, benqiInterface, provider);
+    this.cache = new LRU<string, BigNumber>({ max: 10000 });
   }
 
   // return the number of tokens held by the account
   public async getBalance(account: string, blockNumber: number): Promise<BigNumber> {
+    const key: string = `${account + blockNumber}`;
+
+    if (this.cache.get(key) !== undefined) return this.cache.get(key) as BigNumber;
+
     const balance: BigNumber = await this.qiTokenContract.balanceOf(account, { blockTag: blockNumber });
+    this.cache.set(key, balance);
     return balance;
   }
 }
