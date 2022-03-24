5ce504adf2208bd63f97eaed61485a0ef57ed6da
ishita rastogi <46647968+ishitarastogi@users.noreply.github.com>
2022-03-24 17:05:41 +0530

    chore: Add testnet address

--- Benqi-Agents/DelegationMonitor/src/utils.ts
+++ Benqi-Agents/DelegationMonitor/src/utils.ts
@@ -1,7 +1,8 @@
 const DELEGATE_CHANGED_EVENT = `event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate)`;
 const BALANCE_OF_FUNCTION = ["function balanceOf(address account) external view returns (uint)"];
-
+const QI_CONTRACT: string = "0xf5841E5df027ed086A749Ecc33BFE7e0Df02B06F";
 export default {
   DELEGATE_CHANGED_EVENT,
   BALANCE_OF_FUNCTION,
+  QI_CONTRACT
 };
