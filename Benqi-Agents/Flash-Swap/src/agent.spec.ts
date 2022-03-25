71b98c619538a87e04f94aefd21d132d885a4682
RCantu92 <Roberto_Cantu@live.com>
2022-03-25 10:13:19 -0500

    feat: Install prettier

--- Benqi-Agents/Flash-Swap/src/agent.spec.ts
+++ Benqi-Agents/Flash-Swap/src/agent.spec.ts
@@ -1,21 +1,8 @@
-import {
-  Finding,
-  HandleTransaction,
-  TransactionEvent
-} from "forta-agent";
-import {
-  createAddress,
-  TestTransactionEvent
-} from "forta-agent-tools/lib/tests";
-import {
-  utils,
-  BigNumber
-} from "ethers";
+import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
+import { createAddress, TestTransactionEvent } from "forta-agent-tools/lib/tests";
+import { utils, BigNumber } from "ethers";
 import { provideHandleTransaction } from "./agent";
-import {
-  SWAP_ABI,
-  createFinding
-} from "./utils";
+import { SWAP_ABI, createFinding } from "./utils";
 
 const testPglContract: string = createAddress("0xab01");
 const testMsgSender: string = createAddress("0xac02");
@@ -28,22 +15,22 @@ const testCases: any[][] = [
   [BigNumber.from(100), BigNumber.from(100), createAddress("0xab03"), "0x12ab"],
   [BigNumber.from(300), BigNumber.from(300), createAddress("0xac04"), "0x34ac"],
   [BigNumber.from(500), BigNumber.from(500), createAddress("0xad05"), "0x45ad"],
-  [BigNumber.from(800), BigNumber.from(800), createAddress("0xae06"), "0x"]
+  [BigNumber.from(800), BigNumber.from(800), createAddress("0xae06"), "0x"],
 ];
 
 const encodedSwapCalls: string[] = [
   testSwapIFace.encodeFunctionData("swap", testCases[0]),
   testSwapIFace.encodeFunctionData("swap", testCases[1]),
   testSwapIFace.encodeFunctionData("swap", testCases[2]),
-  testSwapIFace.encodeFunctionData("swap", testCases[3])
+  testSwapIFace.encodeFunctionData("swap", testCases[3]),
 ];
 
 describe("Flash Swap agent alert test suite", () => {
-  let handleTransaction: HandleTransaction
+  let handleTransaction: HandleTransaction;
 
   beforeEach(() => {
     handleTransaction = provideHandleTransaction(testPglContract);
-  })
+  });
 
   it("should ignore empty transactions", async () => {
     const txEvent: TransactionEvent = new TestTransactionEvent();
@@ -51,24 +38,21 @@ describe("Flash Swap agent alert test suite", () => {
     const findings: Finding[] = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
-  })
+  });
 
   it("should ignore swap function calls to the wrong contract", async () => {
     const wrongContract: string = createAddress("0xD3ad");
 
-    const txEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(testMsgSender)
-      .setTo(wrongContract)
-      .addTraces({
-        from: testMsgSender,
-        to: wrongContract,
-        input: encodedSwapCalls[0]
-      });
+    const txEvent: TransactionEvent = new TestTransactionEvent().setFrom(testMsgSender).setTo(wrongContract).addTraces({
+      from: testMsgSender,
+      to: wrongContract,
+      input: encodedSwapCalls[0],
+    });
 
     const findings: Finding[] = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
-  })
+  });
 
   it("should ignore function calls to the wrong function in the right contract", async () => {
     const wrongAbi: string = "function burn(address to)";
@@ -81,13 +65,13 @@ describe("Flash Swap agent alert test suite", () => {
       .addTraces({
         from: testMsgSender,
         to: testPglContract,
-        input: wrongFuncCall
+        input: wrongFuncCall,
       });
 
     const findings: Finding[] = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
-  })
+  });
 
   it("should return Findings from `swap` function calls that are flash swaps", async () => {
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -96,29 +80,21 @@ describe("Flash Swap agent alert test suite", () => {
       .addTraces({
         from: testMsgSender,
         to: testPglContract,
-        input: encodedSwapCalls[1]
+        input: encodedSwapCalls[1],
       })
       .addTraces({
         from: testMsgSender,
         to: testPglContract,
-        input: encodedSwapCalls[2]
+        input: encodedSwapCalls[2],
       });
 
     const findings: Finding[] = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([
-      createFinding(
-        testCases[1][0].toString(),
-        testCases[1][1].toString(),
-        testCases[1][2]
-      ),
-      createFinding(
-        testCases[2][0].toString(),
-        testCases[2][1].toString(),
-        testCases[2][2]
-      )
+      createFinding(testCases[1][0].toString(), testCases[1][1].toString(), testCases[1][2]),
+      createFinding(testCases[2][0].toString(), testCases[2][1].toString(), testCases[2][2]),
     ]);
-  })
+  });
 
   it("should ignore swap function calls that are regular swaps", async () => {
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -127,11 +103,11 @@ describe("Flash Swap agent alert test suite", () => {
       .addTraces({
         from: testMsgSender,
         to: testPglContract,
-        input: encodedSwapCalls[3]
+        input: encodedSwapCalls[3],
       });
 
     const findings: Finding[] = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([]);
-  })
-})
+  });
+});
--- Benqi-Agents/Flash-Swap/src/utils.ts
+++ Benqi-Agents/Flash-Swap/src/utils.ts
@@ -1,25 +1,21 @@
-import { 
-    Finding,
-    FindingSeverity, 
-    FindingType,
-} from 'forta-agent';
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
 export const PGL_CONTRACT: string = "0xE530dC2095Ef5653205CF5ea79F8979a7028065c";
 export const TESTNET_CONTRACT: string = "0x88C80C9d00D9583b252f8151D8489b5A35506e55";
 export const SWAP_ABI: string = "function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data)";
 
 export const createFinding = (amount0Out: string, amount1Out: string, to: string) => {
-    return Finding.fromObject({
-        name: 'Flash Swap Alert',
-        description: 'Flash swap detected on PGL contract.',
-        alertId: "BENQI-9",
-        severity: FindingSeverity.Info,
-        type: FindingType.Info,
-        protocol: "Benqi Finance",
-        metadata: {
-            amount0Out,
-            amount1Out,
-            to
-        },
-    });
+  return Finding.fromObject({
+    name: "Flash Swap Alert",
+    description: "Flash swap detected on PGL contract.",
+    alertId: "BENQI-9",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    protocol: "Benqi Finance",
+    metadata: {
+      amount0Out,
+      amount1Out,
+      to,
+    },
+  });
 };
