8757a8e639752cefd69fe52e7e570a6d164244fd
Lázaro Raúl <riglesiasvera@gmail.com>
2022-04-12 01:30:51 -0400

    feat: Initial agent implementation

---
+++ Ondo-Finance/Role-Changes/src/abi.ts
@@ -0,0 +1,10 @@
+const REGISTRY: string[] = [
+  "function addStrategist(address _strategist, string calldata _name) external",
+  "function grantRole(bytes32 role, address account) external",
+  "function renounceRole(bytes32 role, address account) external",
+  "function revokeRole(bytes32 role, address account) external",
+];
+
+export default {
+  REGISTRY,
+};
---
+++ Ondo-Finance/Role-Changes/src/agent.ts
@@ -0,0 +1,31 @@
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+} from "forta-agent";
+import abi from "./abi";
+import { utils } from "ethers";
+import { createFinding } from "./findings";
+
+const REGISTRY: string = "0xf69c52bf2cf76250647c0bb5390d4ba8854a1d4a";
+
+const provideHandleTransaction = (registry: string): HandleTransaction =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    const desc: utils.TransactionDescription[] = txEvent.filterFunction(abi.REGISTRY, registry);
+
+    desc.forEach((txDesc: utils.TransactionDescription) => findings.push(
+      createFinding(
+        txDesc, 
+        txEvent.from,
+      )),
+    );
+
+    return findings;
+  };
+
+export default {
+  handleTransaction: provideHandleTransaction(REGISTRY),
+  provideHandleTransaction,
+};
