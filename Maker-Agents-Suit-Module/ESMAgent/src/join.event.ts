f7513587a22ab2106c13ede7b97563c666bdd8f6
RCantu92 <Roberto_Cantu@live.com>
2022-04-13 15:10:07 -0500

    refac: Update filterLog and findingGenerator functions, add ABI

--- Maker-Agents-Suit-Module/ESMAgent/src/join.event.ts
+++ Maker-Agents-Suit-Module/ESMAgent/src/join.event.ts
@@ -9,16 +9,15 @@ import {
   TransactionEvent,
   FindingSeverity,
   FindingType,
-  Log,
+  LogDescription,
 } from 'forta-agent';
-import { decodeParam } from './utils';
 
+export const MAKER_ESM_JOIN_EVENT_ABI = 'event Join(address indexed usr, uint256 wad)';
 export const MAKER_ESM_JOIN_EVENT_SIGNATURE = 'Join(address,uint256)';
 export const MKR_DECIMALS = 18;
-export const MAKER_EVEREST_ID = '0xbabb5eed78212ab2db6705e6dfd53e7e5eaca437';
 
-const filterLog = (log: Log): boolean => {
-  const amount = decodeParam('uint256', log.data);
+const filterLog = (log: LogDescription, index?: number | undefined, array?: LogDescription[] | undefined): boolean => {
+  const amount = log.args[1];
 
   return BigInt(amount) > BigInt(2 * 10 ** MKR_DECIMALS);
 };
@@ -32,10 +31,9 @@ const createFindingGenerator = (_alertID: string): FindingGenerator => {
       protocol: 'Maker',
       severity: FindingSeverity.Medium,
       type: FindingType.Suspicious,
-      everestId: MAKER_EVEREST_ID,
       metadata: {
-        usr: decodeParam('address', metadata!.topics[1]).toLowerCase(),
-        amount: decodeParam('uint256', metadata!.data),
+        usr: metadata!.args[0],
+        amount: metadata!.args[1].toString(),
       },
     });
 };
@@ -47,7 +45,7 @@ const provideESMJoinEventAgent = (
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const agentHandler = provideEventCheckerHandler(
       createFindingGenerator(_alertID),
-      MAKER_ESM_JOIN_EVENT_SIGNATURE,
+      MAKER_ESM_JOIN_EVENT_ABI,
       _contractAddress,
       filterLog,
     );
