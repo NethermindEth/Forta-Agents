1d2263945e2e5f01b9a3ae9e514bb0989afcae41
Yasmine Bachi <gy_bachi@esi.dz>
2022-04-13 22:22:55 +0100

    fix: remove web3 from rely tests

--- Maker-Agents-Suit-Module/OSMAgent/src/rely.function.spec.ts
+++ Maker-Agents-Suit-Module/OSMAgent/src/rely.function.spec.ts
@@ -5,13 +5,13 @@ import {
   FindingType,
   TransactionEvent,
 } from "forta-agent";
-import Web3 from "web3";
-import provideRelyFunctionHandler from "./rely.function";
+import provideRelyFunctionHandler, { RELY_FUNCTION_SIG } from "./rely.function";
 import {
   createAddress,
   TestTransactionEvent,
 } from "forta-agent-tools/lib/tests";
 import { when } from "jest-when";
+import { utils } from "ethers";
 
 const CONTRACTS: string[][] = [ // index represent a timestamp
   [], // no contracts at timestamp 0
@@ -19,8 +19,9 @@ const CONTRACTS: string[][] = [ // index represent a timestamp
   [createAddress("0xb0"), createAddress("0xb1"), createAddress("0xb2")],
   [createAddress("0xc0")],
 ]
-const ADDRESS = createAddress("0x1");
-const ABI = new Web3().eth.abi;
+const ADDRESSES = [createAddress("0x1"), createAddress("0x2"), createAddress("0x3")];
+const relyIFace = new utils.Interface([RELY_FUNCTION_SIG]);
+//const ABI = new Web3().eth.abi;
 
 export const createFinding = (to: string, address: string) => {
     return Finding.fromObject({
@@ -48,25 +49,14 @@ describe("OSM Rely Function Agent", () => {
     when(mockFetcher.get).calledWith(3).mockReturnValue(CONTRACTS[3]);
   });
 
-  it("should return a finding for one of the OSM contract", async () => {
+  it("should return a finding for rely call on an OSM contract", async () => {
     const _from = createAddress("0x2");
     const _to = CONTRACTS[1][0]; 
-    const _input: string = ABI.encodeFunctionCall(
-      {
-        name: "rely",
-        type: "function",
-        inputs: [
-          {
-            type: "address",
-            name: "usr",
-          },
-        ],
-      },
-      [ADDRESS]
-    );
+    const _input: string = relyIFace.encodeFunctionData("rely",[ADDRESSES[0]]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setTimestamp(1)
+      .setTo(_to)
       .addTraces({
         to: _to,
         from: _from,
@@ -74,60 +64,38 @@ describe("OSM Rely Function Agent", () => {
       });
 
     const findings: Finding[] = await handleTransaction(txEvent);
-    expect(findings).toStrictEqual([createFinding(_to, ADDRESS)]);
+    expect(findings).toStrictEqual([createFinding(_to, ADDRESSES[0])]);
   });
 
   it("should return multiple findings", async () => {
     const _from = createAddress("0x2");
-    const _input: string = ABI.encodeFunctionCall(
-      {
-        name: "rely",
-        type: "function",
-        inputs: [
-          {
-            type: "address",
-            name: "usr",
-          },
-        ],
-      },
-      [ADDRESS]
-    );
+    const _input: string = relyIFace.encodeFunctionData("rely",[ADDRESSES[0]]);
+    const _input2: string = relyIFace.encodeFunctionData("rely",[ADDRESSES[1]]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setTimestamp(2)
+      .setTo(CONTRACTS[2][2])
       .addTraces({
         to: CONTRACTS[2][2],
         from: _from,
         input: _input,
       },{
-        to: CONTRACTS[2][1],
+        to: CONTRACTS[2][2],
         from: _from,
-        input: _input,
+        input: _input2,
       });
 
     const findings: Finding[] = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([
-      createFinding(CONTRACTS[2][1], ADDRESS),
-      createFinding(CONTRACTS[2][2], ADDRESS),
+      createFinding(CONTRACTS[2][2], ADDRESSES[0]),
+      createFinding(CONTRACTS[2][2], ADDRESSES[1]),
     ]);
   });
 
   it("should return empty finding when OSM contract address does found", async () => {
     const _from = createAddress("0x2");
     const _to = createAddress("0x1"); // BAD ADDRESS
-    const _input: string = ABI.encodeFunctionCall(
-      {
-        name: "rely",
-        type: "function",
-        inputs: [
-          {
-            type: "address",
-            name: "usr",
-          },
-        ],
-      },
-      [ADDRESS]
-    );
+    const _input: string = relyIFace.encodeFunctionData("rely",[ADDRESSES[1]]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setTimestamp(3)
