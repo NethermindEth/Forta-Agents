db119a80b7bf9e0950fe9ac6f26543dbfa6092bf
RCantu92 <Roberto_Cantu@live.com>
2022-04-14 09:34:33 -0500

    chore: Remove node_moduls and reinstall

--- Maker-Agents-Suit-Module/ESMAgent/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { encodeParameter, decodeParameter } from "forta-agent-tools";
-
-export const decodeParam = (type: string, param: string): any => decodeParameter(type, param);
-
-export const encodeParam = (ptype: string, param: string): any => encodeParameter(ptype, param);
