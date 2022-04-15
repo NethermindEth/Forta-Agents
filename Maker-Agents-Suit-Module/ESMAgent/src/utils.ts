f3fa2de67d4e405906fca910212a3a55bd6e6303
Yasmine Bachi <36101876+yasmine9@users.noreply.github.com>
2022-04-15 10:41:56 +0100

    Delete Maker-Agents-Suit-Module directory

--- Maker-Agents-Suit-Module/ESMAgent/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from 'web3';
-export const web3 = new Web3();
-
-export const decodeParam = (type: string, param: string): any =>
-  web3.eth.abi.decodeParameter(type, param);
-
-export const encodeParam = (ptype: string, param: string): any =>
-  web3.eth.abi.encodeParameter(ptype, param);
