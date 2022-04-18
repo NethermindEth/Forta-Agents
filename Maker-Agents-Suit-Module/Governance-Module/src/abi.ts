69be439cb799f7325d2387d815f1f771b01894de
Tayfun Sur <tayfunsur@gmail.com>
2022-04-18 15:54:51 +0300

    feat: Create abi & interface for chainlog contract

--- Maker-Agents-Suit-Module/Governance-Module/src/abi.ts
+++ Maker-Agents-Suit-Module/Governance-Module/src/abi.ts
@@ -7,7 +7,14 @@ const CHIEF = [
 
 const CHIEF_IFACE: Interface = new Interface(CHIEF);
 
+const CHAINLOG = ["function getAddress(bytes32 _key) public view returns (address addr)"];
+
+export const EVENT_ABI = ["event UpdateAddress(bytes32 key, address addr)"];
+
+export const CHAINLOG_IFACE: Interface = new Interface(CHAINLOG);
+
 export default {
   CHIEF,
   CHIEF_IFACE,
+  CHAINLOG_IFACE,
 };
--- Maker-Agents-Suit-Module/Governance-Module/src/config.ts
+++ Maker-Agents-Suit-Module/Governance-Module/src/config.ts
@@ -1,13 +1,13 @@
 import { BigNumber } from "ethers";
 
+const CHAINLOG_CONTRACT: string = "0xda0ab1e0017debcd72be8599041a2aa3ba7e740f";
 const SPELL_DEPLOYER: string = "0xda0c0de01d90a5933692edf03c7ce946c7c50445";
-const CHIEF_CONTRACT: string = "0x0a3f6849f78076aefaDf113F5BED87720274dDC0";
 const KNOWN_LIFTERS: string[] = ["0x5cab1e5286529370880776461c53a0e47d74fb63"];
 const MKR_THRESHOLD: BigNumber = BigNumber.from(40000);
 
 export default {
+  CHAINLOG_CONTRACT,
   SPELL_DEPLOYER,
-  CHIEF_CONTRACT,
   KNOWN_LIFTERS,
   MKR_THRESHOLD,
 };
