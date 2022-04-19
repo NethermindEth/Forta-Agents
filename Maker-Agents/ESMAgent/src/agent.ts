6fa1a7a3307f844df37b57e29c32323315f131a7
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-18 19:37:01 -0500

    feat: Add UpdateAddress logic to agent.ts

--- Maker-Agents/ESMAgent/src/agent.ts
+++ Maker-Agents/ESMAgent/src/agent.ts
@@ -1,7 +1,8 @@
 import provideESMJoinEventAgent from "./join.event";
 import provideESMFireEventAgent from "./fire.event";
 import { Finding, HandleTransaction, TransactionEvent, getEthersProvider } from "forta-agent";
-import { CHAINLOG_ADDRESS } from "./utils";
+import { utils } from "ethers";
+import { CHAINLOG_ADDRESS, UPDATE_ADDR_ABI } from "./utils";
 import AddressFetcher from "./address.fetcher";
 
 const ESM_FETCHER: AddressFetcher = new AddressFetcher(getEthersProvider(), CHAINLOG_ADDRESS);
@@ -14,13 +15,20 @@ const provideInitialize = (esmFetcher: AddressFetcher) => async () => {
   ESM_ADDRESS = await esmFetcher.getEsmAddress("latest");
 };
 
-const provideAgentHandler = (joinEvent: string, fireEvent: string, esmAddress?: string): HandleTransaction => {
+const provideAgentHandler = (joinEvent: string, fireEvent: string, esmAddress: string): HandleTransaction => {
   if (!esmAddress) esmAddress = ESM_ADDRESS;
   const joinEventHandler = provideESMJoinEventAgent(joinEvent, esmAddress);
   const fireEventHandler = provideESMFireEventAgent(fireEvent, esmAddress);
 
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     let findings: Finding[] = [];
+
+    // Listen to UpdateAddress event & update the ESM contract.
+    txEvent.filterLog(UPDATE_ADDR_ABI, CHAINLOG_ADDRESS).forEach((log) => {
+      if (log.args.key == utils.formatBytes32String("MCD_ESM")) {
+        ESM_ADDRESS = log.args.addr;
+      }
+    });
 
     findings = [...(await joinEventHandler(txEvent)), ...(await fireEventHandler(txEvent))];
 
