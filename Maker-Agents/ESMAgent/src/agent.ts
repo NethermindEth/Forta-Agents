fe461d1bd295013f514b8998b280a5f72cd8484c
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-18 20:23:37 -0500

    feat: Remove initialize, use addressFetcher as arg

--- Maker-Agents/ESMAgent/src/agent.ts
+++ Maker-Agents/ESMAgent/src/agent.ts
@@ -6,17 +6,12 @@ import { CHAINLOG_ADDRESS, UPDATE_ADDR_ABI } from "./utils";
 import AddressFetcher from "./address.fetcher";
 
 const ESM_FETCHER: AddressFetcher = new AddressFetcher(getEthersProvider(), CHAINLOG_ADDRESS);
-let ESM_ADDRESS: string = "";
 
 const JOIN_EVENT_ALERTID: string = "MakerDAO-ESM-1";
 const FIRE_EVENT_ALERTID: string = "MakerDAO-ESM-2";
 
-const provideInitialize = (esmFetcher: AddressFetcher) => async () => {
-  ESM_ADDRESS = await esmFetcher.getEsmAddress("latest");
-};
-
-const provideAgentHandler = (joinEvent: string, fireEvent: string, esmAddress: string): HandleTransaction => {
-  if (!esmAddress) esmAddress = ESM_ADDRESS;
+const provideAgentHandler = (joinEvent: string, fireEvent: string, fetcher: AddressFetcher): HandleTransaction => {
+  let esmAddress = fetcher.esmAddress;
   const joinEventHandler = provideESMJoinEventAgent(joinEvent, esmAddress);
   const fireEventHandler = provideESMFireEventAgent(fireEvent, esmAddress);
 
@@ -26,7 +21,7 @@ const provideAgentHandler = (joinEvent: string, fireEvent: string, esmAddress: s
     // Listen to UpdateAddress event & update the ESM contract.
     txEvent.filterLog(UPDATE_ADDR_ABI, CHAINLOG_ADDRESS).forEach((log) => {
       if (log.args.key == utils.formatBytes32String("MCD_ESM")) {
-        ESM_ADDRESS = log.args.addr;
+        esmAddress = log.args.addr;
       }
     });
 
@@ -37,8 +32,6 @@ const provideAgentHandler = (joinEvent: string, fireEvent: string, esmAddress: s
 };
 
 export default {
-  provideInitialize,
-  initialize: provideInitialize(ESM_FETCHER),
   provideAgentHandler,
-  handleTransaction: provideAgentHandler(JOIN_EVENT_ALERTID, FIRE_EVENT_ALERTID, ESM_ADDRESS),
+  handleTransaction: provideAgentHandler(JOIN_EVENT_ALERTID, FIRE_EVENT_ALERTID, ESM_FETCHER),
 };
