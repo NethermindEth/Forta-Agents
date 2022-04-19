69df49970889aaa95d372121bd4ea2e054bc085d
Tayfun Sur <tayfunsur@gmail.com>
2022-04-19 18:10:07 +0300

    tests: Add multiple tests into address fetcher

--- Maker-Agents/Governance-Module/src/address.fetcher.spec.ts
+++ Maker-Agents/Governance-Module/src/address.fetcher.spec.ts
@@ -6,24 +6,31 @@ import { formatBytes32String } from "ethers/lib/utils";
 
 describe("AddressFetcher test suite", () => {
   const mockProvider: MockEthersProvider = new MockEthersProvider();
-  let fetcher: AddressFetcher;
-  const CHIEF_CONTRACT: string = createAddress("0x1");
+  let fetcher: AddressFetcher = new AddressFetcher(mockProvider as any, config.CHAINLOG_CONTRACT);
 
-  const initialize = () => {
-    fetcher = new AddressFetcher(mockProvider as any, config.CHAINLOG_CONTRACT);
-    mockProvider.clear();
-    const key: string = formatBytes32String("MCD_ADM"); // chief contract's key value
+  // Format: [chiefAddress, blockNumber]
+  const TEST_CASES: [string, number][] = [
+    [createAddress("0x1"), 1],
+    [createAddress("0x2"), 2],
+    [createAddress("0x3"), 3],
+    [createAddress("0x4"), 4],
+  ];
 
-    // call to getAddress
-    mockProvider.addCallTo(config.CHAINLOG_CONTRACT, 1, CHAINLOG_IFACE, "getAddress", {
+  fetcher = new AddressFetcher(mockProvider as any, config.CHAINLOG_CONTRACT);
+  const key: string = formatBytes32String("MCD_ADM"); // chief contract's key value
+
+  function createGetAddressCall(blockNumber: number, chiefAddress: string) {
+    mockProvider.addCallTo(config.CHAINLOG_CONTRACT, blockNumber, CHAINLOG_IFACE, "getAddress", {
       inputs: [key],
-      outputs: [CHIEF_CONTRACT],
+      outputs: [chiefAddress],
     });
-  };
+  }
 
   it("should store correct MCD_ADM address", async () => {
-    initialize();
-    await fetcher.getChiefAddress(1);
-    expect(fetcher.chiefAddress).toStrictEqual(createAddress("0x1"));
+    for (let [chiefAddress, blockNumber] of TEST_CASES) {
+      createGetAddressCall(blockNumber, chiefAddress);
+      const fetchedChiefAddres: string = await fetcher.getChiefAddress(blockNumber);
+      expect(fetchedChiefAddres).toStrictEqual(chiefAddress);
+    }
   });
 });
