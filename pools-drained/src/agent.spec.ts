137997a2fdeae33293f72360fb8cd2ce6eef9869
Lázaro Raúl <riglesiasvera@gmail.com>
2022-04-20 23:19:38 -0400

    chore: Initial agent

---
+++ pools-drained/src/agent.spec.ts
@@ -0,0 +1,13 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+  ethers,
+} from "forta-agent";
+import agent from "./agent";
+
+describe("Pool drained test suite", () => {
+  it("should do something", async () => {});
+});
