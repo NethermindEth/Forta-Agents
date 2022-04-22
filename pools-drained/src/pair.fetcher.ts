a735d601c3a1593472574dd0d61af81f686185ba
Lázaro Raúl <riglesiasvera@gmail.com>
2022-04-21 22:45:33 -0400

    feat: Add cache to improve performance

--- pools-drained/src/pair.fetcher.ts
+++ pools-drained/src/pair.fetcher.ts
@@ -1,37 +1,57 @@
 import { providers, Contract, BigNumber } from "ethers";
+import LRU from "lru-cache";
 import abi from "./abi";
+
+type V2_DATA = [boolean, string, string];
+type V3_DATA = [boolean, string, string, BigNumber];
 
 export default class PairFetcher {
   private provider: providers.JsonRpcProvider;
+  private cache: LRU<string, any[]>;
 
   constructor(provider: providers.JsonRpcProvider) {
     this.provider = provider;
+    this.cache = new LRU<string, any[]>({max: 10000});
   };
 
-  public async getV2Data(pair: string, block: number): Promise<[boolean, string, string]> {
+  public async getV2Data(pair: string, block: number): Promise<V2_DATA> {
+    const key: string = `V2-${pair}-${block}`;
+    if(this.cache.has(key))
+      return this.cache.get(key) as V2_DATA;
+
     const contract: Contract = new Contract(pair, abi.COMMON, this.provider);
+    let output: V2_DATA;
     try {
       const [token0, token1] = await Promise.all([
         contract.token0({ blockTag: block }),
         contract.token1({ blockTag: block }),
       ]);
-      return [true, token0.toLowerCase(), token1.toLowerCase()];
+      output = [true, token0.toLowerCase(), token1.toLowerCase()];
     } catch {
-      return [false, "", ""];
+      output = [false, "", ""];
     }
+    this.cache.set(key, output);
+    return output;
   };
 
-  public async getV3Data(pair: string, block: number): Promise<[boolean, string, string, BigNumber]> {
+  public async getV3Data(pair: string, block: number): Promise<V3_DATA> {
+    const key: string = `V3-${pair}-${block}`;
+    if(this.cache.has(key))
+      return this.cache.get(key) as V3_DATA;
+
     const contract: Contract = new Contract(pair, abi.COMMON, this.provider);
+    let output: V3_DATA;
     try {
       const [token0, token1, fee] = await Promise.all([
         contract.token0({ blockTag: block }),
         contract.token1({ blockTag: block }),
         contract.fee({ blockTag: block }),
       ]);
-      return [true, token0.toLowerCase(), token1.toLowerCase(), fee];
+      output = [true, token0.toLowerCase(), token1.toLowerCase(), fee];
     } catch {
-      return [false, "", "", BigNumber.from(0)];
+      output = [false, "", "", BigNumber.from(0)];
     }
+    this.cache.set(key, output);
+    return output;
   };
 };
