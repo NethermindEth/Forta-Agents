b53b92352908ae433522ad7ccdc78ec21a45483e
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-24 15:37:02 -0500

    feat: Add caching to balance.fetcher.ts

--- DYDX-Bots/Large_USDC_Staking-Withdrawals/src/balance.fetcher.ts
+++ DYDX-Bots/Large_USDC_Staking-Withdrawals/src/balance.fetcher.ts
@@ -1,19 +1,28 @@
-import { providers, Contract } from "ethers";
+import { providers, Contract, BigNumber } from "ethers";
+import LRU from "lru-cache";
 import { USDC_IFACE } from "./utils";
 
 export default class BalanceFetcher {
   readonly provider: providers.Provider;
+  private cache: LRU<string, Promise<BigNumber>>;
   readonly stakeTokenAddress: string;
   private stakeTokenContract: Contract;
 
   constructor(provider: providers.Provider, tokenAddr: string) {
     this.provider = provider;
+    this.cache = new LRU<string, Promise<BigNumber>>({
+      max: 10000,
+    });
     this.stakeTokenAddress = tokenAddr;
     this.stakeTokenContract = new Contract(tokenAddr, USDC_IFACE, provider);
   }
 
-  public async getBalanceOf(address: string, block: string | number): Promise<string> {
+  public async getBalanceOf(address: string, block: string | number): Promise<BigNumber> {
+    const key: string = `${this.stakeTokenContract} - ${address} - ${block}`;
+    if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
+
     const balance = await this.stakeTokenContract.balanceOf(address, { blockTag: block });
+    this.cache.set(key, balance);
     return balance;
   }
 }
