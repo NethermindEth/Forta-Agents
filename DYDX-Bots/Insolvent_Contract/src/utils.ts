7040f4df2da81df72fbe023542740fcba488fcb5
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-27 15:58:17 -0500

    feat: Add Kovan testnet proxy address to utils.ts

--- DYDX-Bots/Insolvent_Contract/src/utils.ts
+++ DYDX-Bots/Insolvent_Contract/src/utils.ts
@@ -1,7 +1,9 @@
 import { Interface } from "@ethersproject/abi";
 import { utils } from "ethers";
 
-export const PROXY_ADDRESS: string = "0x5Aa653A076c1dbB47cec8C1B4d152444CAD91941";
+export const PROXY_ADDRESS: string = "0x5Aa653A076c1dbB47cec8C1B4d152444CAD91941"; // Mainnet proxy address
+// Uncomment line below, and comment out line above, to test the bot with the Kovan testnet
+// export const PROXY_ADDRESS: string = "0xe9511Faa2B2ccE548A5999b4bC3772e6a0f1C14A"; // Kovan testnet proxy address
 
 export const GET_TOTAL_BORROWER_DEBT_BALANCE_ABI: string =
   "function getTotalBorrowerDebtBalance() external view returns (uint256)";
