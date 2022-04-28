94500100e3e0d8b640bd9bcb455c5412a107d71f
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-28 14:45:35 -0500

    feat: Update how fallback is called

--- DYDX-Bots/Insolvent_Contract/src/balance.fetcher.ts
+++ DYDX-Bots/Insolvent_Contract/src/balance.fetcher.ts
@@ -4,36 +4,41 @@ import { IMPLEMENTATION_IFACE } from "./utils";
 
 export default class BalanceFetcher {
   readonly provider: providers.Provider;
-  private cache: LRU<string, Promise<BigNumber>>;
+  private cache: LRU<string, BigNumber>;
   readonly proxyAddress: string;
-  private proxyContract: Contract;
 
   constructor(provider: providers.Provider, proxyAddr: string) {
     this.provider = provider;
-    this.cache = new LRU<string, Promise<BigNumber>>({
+    this.cache = new LRU<string, BigNumber>({
       max: 10000,
     });
     this.proxyAddress = proxyAddr;
-    // Create proxy contract instance using the Interface of the implementation contract
-    // source: https://github.com/ethers-io/ethers.js/issues/182#issuecomment-826387240
-    // NOTE: IF THIS DOESN'T WORK, TRY await provider.call({ to: proxyAddress, data: IMPLEMENTATION_IFACE.getSigHash("/*function name*/")})/
-    this.proxyContract = new Contract(proxyAddr, IMPLEMENTATION_IFACE, provider);
   }
 
   public async getTotalBorrowerDebtBalance(block: string | number): Promise<BigNumber> {
-    const key: string = `${this.proxyContract} - totalBorrowerDebtBalance - ${block}`;
+    const key: string = `totalBorrowerDebtBalance - ${block}`;
     if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
 
-    const balance = await this.proxyContract.getTotalBorrowerDebtBalance({ blockTag: block });
+    const balance: BigNumber = BigNumber.from(
+      await this.provider.call({
+        to: this.proxyAddress,
+        data: IMPLEMENTATION_IFACE.getSighash("getTotalBorrowerDebtBalance"),
+      }, "latest")
+    );
     this.cache.set(key, balance);
     return balance;
   }
 
   public async getTotalActiveBalanceCurrentEpoch(block: string | number): Promise<BigNumber> {
-    const key: string = `${this.proxyContract} - totalActiveBalanceCurrentEpoch - ${block}`;
+    const key: string = `totalActiveBalanceCurrentEpoch - ${block}`;
     if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
 
-    const balance = await this.proxyContract.getTotalActiveBalanceCurrentEpoch({ blockTag: block });
+    const balance: BigNumber = BigNumber.from(
+      await this.provider.call({
+        to: this.proxyAddress,
+        data: IMPLEMENTATION_IFACE.getSighash("getTotalActiveBalanceCurrentEpoch"),
+      }, "latest")
+    );
     this.cache.set(key, balance);
     return balance;
   }
