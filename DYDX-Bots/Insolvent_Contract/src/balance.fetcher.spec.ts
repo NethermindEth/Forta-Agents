cc5b947a73af209b3e2feb4e7d33aecf8c7f1628
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-28 19:07:48 -0500

    feat: Add caching back to balance.fetcher.ts and test file

--- DYDX-Bots/Insolvent_Contract/src/balance.fetcher.spec.ts
+++ DYDX-Bots/Insolvent_Contract/src/balance.fetcher.spec.ts
@@ -1,17 +1,17 @@
-import { createAddress, MockEthersProvider } from "forta-agent-tools/lib/tests";
+import { createAddress } from "forta-agent-tools/lib/tests";
 import { BigNumber } from "ethers";
+import { when, resetAllWhenMocks } from "jest-when";
 import BalanceFetcher from "./balance.fetcher";
 import { IMPLEMENTATION_IFACE } from "./utils";
-import { when, resetAllWhenMocks } from "jest-when";
 
 describe("Balance Fetcher test suite", () => {
   const testProxyAddress: string = createAddress("0xab");
-  // Format: [totalBorrowerDebtBalance, totalActiveBalanceCurrentEpoch]
-  const TEST_CASES: [BigNumber, BigNumber][] = [
-    [BigNumber.from("10"), BigNumber.from("80")],
-    [BigNumber.from("20"), BigNumber.from("85")],
-    [BigNumber.from("30"), BigNumber.from("90")],
-    [BigNumber.from("40"), BigNumber.from("95")],
+  // Format: [totalBorrowerDebtBalance, totalActiveBalanceCurrentEpoch, blockNumber]
+  const TEST_CASES: [BigNumber, BigNumber, number][] = [
+    [BigNumber.from("10"), BigNumber.from("80"), 1],
+    [BigNumber.from("20"), BigNumber.from("85"), 2],
+    [BigNumber.from("30"), BigNumber.from("90"), 3],
+    [BigNumber.from("40"), BigNumber.from("95"), 4],
   ];
 
   const mockProvider: any = {
@@ -34,37 +34,37 @@ describe("Balance Fetcher test suite", () => {
   beforeEach(() => resetAllWhenMocks());
 
   it("should store correct total borrower debt balance amount", async () => {
-    for (let [totalBorrowerDebtBalance] of TEST_CASES) {
+    for (let [totalBorrowerDebtBalance, , blockNumber] of TEST_CASES) {
       createGetTotalBorrowerDebtBalance(totalBorrowerDebtBalance);
 
-      const fetchedDebtBalance: BigNumber = await fetcher.getTotalBorrowerDebtBalance();
+      const fetchedDebtBalance: BigNumber = await fetcher.getTotalBorrowerDebtBalance(blockNumber);
       expect(fetchedDebtBalance).toStrictEqual(totalBorrowerDebtBalance);
     }
 
     // clear mock to use cache
     resetAllWhenMocks();
-    for (let [totalBorrowerDebtBalance] of TEST_CASES) {
+    for (let [totalBorrowerDebtBalance, , blockNumber] of TEST_CASES) {
       createGetTotalBorrowerDebtBalance(totalBorrowerDebtBalance);
 
-      const fetchedDebtBalance: BigNumber = await fetcher.getTotalBorrowerDebtBalance();
+      const fetchedDebtBalance: BigNumber = await fetcher.getTotalBorrowerDebtBalance(blockNumber);
       expect(fetchedDebtBalance).toStrictEqual(totalBorrowerDebtBalance);
     }
   });
 
   it("should store correct total active balance current epoch amount", async () => {
-    for (let [, totalActiveBalanceCurrentEpoch] of TEST_CASES) {
+    for (let [, totalActiveBalanceCurrentEpoch, blockNumber] of TEST_CASES) {
       createGetTotalActiveBalanceCurrentEpoch(totalActiveBalanceCurrentEpoch);
 
-      const fetchedActiveBalance: BigNumber = await fetcher.getTotalActiveBalanceCurrentEpoch();
+      const fetchedActiveBalance: BigNumber = await fetcher.getTotalActiveBalanceCurrentEpoch(blockNumber);
       expect(fetchedActiveBalance).toStrictEqual(totalActiveBalanceCurrentEpoch);
     }
 
     // clear mock to use cache
     resetAllWhenMocks();
-    for (let [, totalActiveBalanceCurrentEpoch] of TEST_CASES) {
+    for (let [, totalActiveBalanceCurrentEpoch, blockNumber] of TEST_CASES) {
       createGetTotalActiveBalanceCurrentEpoch(totalActiveBalanceCurrentEpoch);
 
-      const fetchedActiveBalance: BigNumber = await fetcher.getTotalActiveBalanceCurrentEpoch();
+      const fetchedActiveBalance: BigNumber = await fetcher.getTotalActiveBalanceCurrentEpoch(blockNumber);
       expect(fetchedActiveBalance).toStrictEqual(totalActiveBalanceCurrentEpoch);
     }
   });
--- DYDX-Bots/Insolvent_Contract/src/balance.fetcher.ts
+++ DYDX-Bots/Insolvent_Contract/src/balance.fetcher.ts
@@ -1,32 +1,45 @@
-import { providers, Contract, BigNumber } from "ethers";
+import { providers, BigNumber } from "ethers";
+import LRU from "lru-cache";
 import { IMPLEMENTATION_IFACE } from "./utils";
 
 export default class BalanceFetcher {
   readonly provider: providers.Provider;
+  private cache: LRU<string, BigNumber>;
   readonly proxyAddress: string;
 
   constructor(provider: providers.Provider, proxyAddr: string) {
     this.provider = provider;
+    this.cache = new LRU<string, BigNumber>({
+      max: 10000,
+    });
     this.proxyAddress = proxyAddr;
   }
 
-  public async getTotalBorrowerDebtBalance(): Promise<BigNumber> {
+  public async getTotalBorrowerDebtBalance(block: string | number): Promise<BigNumber> {
+    const key: string = `totalBorrowerDebtBalance - ${block}`;
+    if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
+
     const balance: BigNumber = BigNumber.from(
       await this.provider.call({
         to: this.proxyAddress,
         data: IMPLEMENTATION_IFACE.getSighash("getTotalBorrowerDebtBalance"),
       }, "latest")
     );
+    this.cache.set(key, balance);
     return balance;
   }
 
-  public async getTotalActiveBalanceCurrentEpoch(): Promise<BigNumber> {
+  public async getTotalActiveBalanceCurrentEpoch(block: string | number): Promise<BigNumber> {
+    const key: string = `totalActiveBalanceCurrentEpoch - ${block}`;
+    if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
+
     const balance: BigNumber = BigNumber.from(
       await this.provider.call({
         to: this.proxyAddress,
         data: IMPLEMENTATION_IFACE.getSighash("getTotalActiveBalanceCurrentEpoch"),
       }, "latest")
     );
+    this.cache.set(key, balance);
     return balance;
   }
 }
