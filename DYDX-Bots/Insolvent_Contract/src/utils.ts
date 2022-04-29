5c077ec163d6a4c74f6dcf27c12d3d8eb71600ca
Roberto Cantu <Roberto_Cantu@live.com>
2022-04-28 20:44:50 -0500

    chore: Add comment explaining usage of proxy address on kovan testnet

--- DYDX-Bots/Insolvent_Contract/src/utils.ts
+++ DYDX-Bots/Insolvent_Contract/src/utils.ts
@@ -4,6 +4,7 @@ import { utils, BigNumber } from "ethers";
 export const PROXY_ADDRESS: string = "0x5Aa653A076c1dbB47cec8C1B4d152444CAD91941"; // Mainnet proxy address
 // Uncomment line below, and comment out line above, to test the bot with the Kovan testnet
 // export const PROXY_ADDRESS: string = "0xe9511Faa2B2ccE548A5999b4bC3772e6a0f1C14A"; // Kovan testnet proxy address
+export const PROXY_IFACE: utils.Interface = new Interface([]);
 
 export const GET_TOTAL_BORROWER_DEBT_BALANCE_ABI: string =
   "function getTotalBorrowerDebtBalance() external view returns (uint256)";
