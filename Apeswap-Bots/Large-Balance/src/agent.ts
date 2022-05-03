1adacb8326f01aab11edf6ed5eccaca89ce79bc3
ishita rastogi <46647968+ishitarastogi@users.noreply.github.com>
2022-05-03 15:52:22 +0530

    chore: update agent logic

--- Apeswap-Bots/Large-Balance/src/agent.ts
+++ Apeswap-Bots/Large-Balance/src/agent.ts
@@ -1,8 +1,10 @@
 import {
+  BlockEvent,
   Finding,
   TransactionEvent,
   getEthersProvider,
   HandleTransaction,
+  HandleBlock,
   LogDescription,
 } from "forta-agent";
 import { BigNumber } from "ethers";
@@ -17,28 +19,45 @@ const FETCHER: DataFetcher = new DataFetcher(
   GNANA_TOKEN_CONTRACT,
   getEthersProvider()
 );
+let accounts: Set<string> = new Set<string>();
 
-export const provideHandleTransaction =
-  (fetcher: DataFetcher, balanceThreshold: BigNumber): HandleTransaction =>
-  async (txEvent: TransactionEvent): Promise<Finding[]> => {
+export function provideHandleTransaction(
+  fetcher: DataFetcher,
+  accounts: Set<string>
+): HandleTransaction {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
     const logs: LogDescription[] = txEvent.filterLog(
       EVENT_ABI,
       fetcher.gnanaTokenAddress
     );
-
     for (const log of logs) {
       const toAddress = log.args.to;
-      const balance: BigNumber= await fetcher.getBalance(toAddress, txEvent.blockNumber);
-
-      if (balance.gt(balanceThreshold)) {
-        findings.push(createLargeBalanceFinding(toAddress, balance));
-      }
+      accounts.add(toAddress);
     }
-
     return findings;
   };
-
+}
+export function provideHandleBlock(
+  fetcher: DataFetcher,
+  balanceThreshold: BigNumber,
+  accounts: Set<string>
+): HandleBlock {
+  return async (blockEvent: BlockEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+    for (let addr of Array.from(accounts.values())) {
+      const balance: BigNumber = await fetcher.getBalance(
+        addr,
+        blockEvent.blockNumber
+      );
+      if (balance.gt(balanceThreshold)) {
+        findings.push(createLargeBalanceFinding(addr, balance));
+      }
+    }
+    return findings;
+  };
+}
 export default {
-  handleTransaction: provideHandleTransaction(FETCHER, BALANCE_THRESHOLD),
+  handleTransaction: provideHandleTransaction(FETCHER, accounts),
+  handleBlock: provideHandleBlock(FETCHER, BALANCE_THRESHOLD, accounts),
 };
