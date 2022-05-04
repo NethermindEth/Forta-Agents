9a6d0327ab2230ab241773c6798a966e0ae6b102
Roberto Cantu <Roberto_Cantu@live.com>
2022-05-04 13:38:09 -0500

    docs: Add test data to README, add instructions in utils.ts

--- DYDX-Bots/Parameter_Changes_Monitor/README.md
+++ DYDX-Bots/Parameter_Changes_Monitor/README.md
@@ -31,3 +31,21 @@ This agent detects parameter changes to both the Safety Module and Liquidity Mod
   - Type is always set to "Info".
   - Metadata includes:
     - `emissionPerSecond`: The new number of rewards tokens to give out each second.
+
+## Test Data
+
+> Note: To test the following, follow the comments in `utils.ts`.
+
+The bot behavior can be verified with the following contracts on the Kovan ETH testnet:
+
+[0x6dEA282B05C76cC9249513554A3C4Bf646908344](https://kovan.etherscan.io/address/0x6dea282b05c76cc9249513554a3c4bf646908344) - `TestProxy`.
+
+[0x15DEa325Cf2581f50Cdc194E688E090ED6206493](https://kovan.etherscan.io/address/0x15DEa325Cf2581f50Cdc194E688E090ED6206493) - `TestImplementation`.
+
+To test specific event emissions, use the following transactions on the Kovan ETH testnet:
+
+[0xaa0458fb2ba763b8a92fc09170e1c7f0395f79d869f5be0ad59f0abf622d91af](https://kovan.etherscan.io/tx/0xaa0458fb2ba763b8a92fc09170e1c7f0395f79d869f5be0ad59f0abf622d91af) - `BlackoutWindowChanged` event.
+
+[0xe0e21149e75f130a645e08e11eddd56175d05c61fa82bf7500455125ee36aefb](https://kovan.etherscan.io/tx/0xe0e21149e75f130a645e08e11eddd56175d05c61fa82bf7500455125ee36aefb) - `EpochParametersChanged` event.
+
+[0xc8f31c78a8382030aace15ea25f0dcf741d8b8e90df77d57196675ae7fe94e7f](https://kovan.etherscan.io/tx/0xc8f31c78a8382030aace15ea25f0dcf741d8b8e90df77d57196675ae7fe94e7f) - `RewardsPerSecondUpdated` event.
--- DYDX-Bots/Parameter_Changes_Monitor/src/utils.ts
+++ DYDX-Bots/Parameter_Changes_Monitor/src/utils.ts
@@ -1,10 +1,12 @@
 const SAFETY_MODULE_ADDRESS: string = "0x65f7BA4Ec257AF7c55fd5854E5f6356bBd0fb8EC";
 const LIQUIDITY_MODULE_ADDRESS: string = "0x5Aa653A076c1dbB47cec8C1B4d152444CAD91941";
-export const MODULE_ADDRESSES: string[] = [SAFETY_MODULE_ADDRESS, LIQUIDITY_MODULE_ADDRESS];
+export const MODULE_ADDRESSES: string[] = [SAFETY_MODULE_ADDRESS, LIQUIDITY_MODULE_ADDRESS]; // Mainnet addresses
+// To test the transactions in the README on the Kovan testnet, comment out the line above
+// and uncomment the line below
+// export const MODULE_ADDRESSES: string[] = ["0x6dEA282B05C76cC9249513554A3C4Bf646908344"]; // Proxy Kovan ETH testnet address
 
 const BLACKOUT_WINDOW_CHANGED_EVENT: string = "event BlackoutWindowChanged(uint256 blackoutWindow)";
 const EPOCH_PARAMS_CHANGED_EVENT: string = "event EpochParametersChanged((uint128, uint128) epochParameters)";
-// const EPOCH_PARAMS_CHANGED_EVENT: string = "event EpochParametersChanged(LS1Types.EpochParameters epochParameters)";
 const REWARDS_PER_SECOND_UPDATED_EVENT: string = "event RewardsPerSecondUpdated(uint256 emissionPerSecond)";
 export const EVENTS: string[] = [
   BLACKOUT_WINDOW_CHANGED_EVENT,
@@ -14,5 +16,4 @@ export const EVENTS: string[] = [
 
 export const BLACKOUT_WINDOW_CHANGED_SIG: string = "BlackoutWindowChanged(uint256)";
 export const EPOCH_PARAMS_CHANGED_SIG: string = "EpochParametersChanged((uint128,uint128))";
-// export const EPOCH_PARAMS_CHANGED_SIG: string = "EpochParametersChanged(LS1Types.EpochParameters)";
 export const REWARDS_PER_SECOND_UPDATED_SIG: string = "RewardsPerSecondUpdated(uint256)";
