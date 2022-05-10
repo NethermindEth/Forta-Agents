950599d3430ed18292d48ad872276f6ba0805407
Yasmine Bachi <gy_bachi@esi.dz>
2022-05-10 09:58:17 +0100

    tests: networkManager tests

---
+++ DYDX-Bots/Configuration-Monitor/src/network.spec.ts
@@ -0,0 +1,40 @@
+import { createAddress } from "forta-agent-tools/lib/tests";
+import NetworkManager from "./network";
+import NetworkData from "./network";
+
+const TEST_ADDRESSES: [string, string, string, string][] = [
+  [createAddress("0xb1"), createAddress("0xb2"), createAddress("0xb3"), createAddress("0xb4")],
+  [createAddress("0xc1"), createAddress("0xc2"), createAddress("0xc3"), createAddress("0xc4")],
+  [createAddress("0xd1"), createAddress("0xd2"), createAddress("0xd3"), createAddress("0xd4")],
+  [createAddress("0xe1"), createAddress("0xe2"), createAddress("0xe3"), createAddress("0xe4")],
+];
+
+const TEST_NETWORKS = [1, 22, 33, 45];
+
+const generateNetworkMap = (networks: number[], data: string[]): Record<number, NetworkData> => {
+  // generates a NetworkMap for one test case.
+  // networks and data should be same length.
+  let networkMap: Record<number, NetworkData> = {};
+  for (let i = 0; i < networks.length; i++) {
+    networkMap[networks[i]] = {
+      perpetualProxy: data[i],
+    } as NetworkData;
+  }
+  return networkMap;
+};
+
+describe("NetworkManager test suite", () => {
+  it("should return the correct contract address for each network", async () => {
+    for (let data of TEST_ADDRESSES) {
+      const networkMap: Record<number, NetworkData> = generateNetworkMap(TEST_NETWORKS, data);
+      const networkMan = new NetworkManager(networkMap);
+      for (let i = 0; i < TEST_NETWORKS.length; i++) {
+        networkMan.setNetwork(TEST_NETWORKS[i]);
+        expect(networkMan.perpetualProxy).toStrictEqual(data[i]);
+      }
+      // test with a non supported network
+      networkMan.setNetwork(99);
+      expect(networkMan.perpetualProxy).toStrictEqual("");
+    }
+  });
+});
