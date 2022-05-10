8c62dc04feaea3e5913570da9dc4103052514ec4
Roberto Cantu <Roberto_Cantu@live.com>
2022-05-10 14:04:46 -0500

    feat: Pass NETWORK_MAP to networkManager

--- DYDX-Bots/Parameter_Changes_Monitor/src/agent.ts
+++ DYDX-Bots/Parameter_Changes_Monitor/src/agent.ts
@@ -1,11 +1,11 @@
 import { Finding, HandleTransaction, TransactionEvent, getEthersProvider } from "forta-agent";
 import { providers } from "ethers";
 import NetworkData from "./network";
-import NetworkManager from "./network";
+import NetworkManager, { NETWORK_MAP } from "./network";
 import { EVENTS } from "./utils";
 import { createFinding } from "./findings";
 
-const networkManager = new NetworkManager();
+const networkManager = new NetworkManager(NETWORK_MAP);
 
 export const provideInitialize = (provider: providers.Provider) => async () => {
   const { chainId } = await provider.getNetwork();
