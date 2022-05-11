1f670e1f06f536795e5b82920261f264be1674e9
Vxatz <92011898+Vxatz@users.noreply.github.com>
2022-05-11 17:37:28 +0300

    feat: added comments on network.ts

--- Apeswap-Bots/Role-Changes/src/network.ts
+++ Apeswap-Bots/Role-Changes/src/network.ts
@@ -7,20 +7,20 @@ interface NetworkData {
 const BSC_MAINNET_DATA: NetworkData = {
   masterApe: "0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9",
   masterApeAdmin: "0x9fed2bc7f0b4f4350b52e29e0a3c2bf5ebc3cc0a",
-  miniApeV2: "0x0000000000000000000000000000000000000000",
-  miniComplexRewarderTime: "0x0000000000000000000000000000000000000000",
+  miniApeV2: "0x0000000000000000000000000000000000000000", //contract doesn't exist on BSC
+  miniComplexRewarderTime: "0x0000000000000000000000000000000000000000", //contract doesn't exist on BSC
 };
 
 const BSC_TESTNET_DATA: NetworkData = {
   masterApe: "0x062cdBba9348d65BD225e90224159d1e2d4326D8",
   masterApeAdmin: "0x22fdC4d8eEea0d219E15025d2BCDe38d948e9A13",
-  miniApeV2: "0x0000000000000000000000000000000000000000",
-  miniComplexRewarderTime: "0x0000000000000000000000000000000000000000",
+  miniApeV2: "0x0000000000000000000000000000000000000000", //contract doesn't exist on BSC
+  miniComplexRewarderTime: "0x0000000000000000000000000000000000000000", //contract doesn't exist on BSC
 };
 
 const POLYGON_MAINNET_DATA: NetworkData = {
-  masterApe: "0x0000000000000000000000000000000000000000",
-  masterApeAdmin: "0x0000000000000000000000000000000000000000",
+  masterApe: "0x0000000000000000000000000000000000000000", //contract doesn't exist on Polygon
+  masterApeAdmin: "0x0000000000000000000000000000000000000000", //contract doesn't exist on Polygon
   miniApeV2: "0x54aff400858dcac39797a81894d9920f16972d1d",
   miniComplexRewarderTime: "0x1f234b1b83e21cb5e2b99b4e498fe70ef2d6e3bf",
 };
