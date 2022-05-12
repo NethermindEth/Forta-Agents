0fe8728e7ed944b0a00a95ba9325e0bfa8fb7b51
Yasmine Bachi <gy_bachi@esi.dz>
2022-05-12 17:45:32 +0100

    tests: add PoC contracts

---
+++ DYDX-Bots/Approve_Spending_Monitor/src/PoC/testModule.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.5;
+
+contract TestModule {
+    uint256 public amount;
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function emitApproval(
+        address _owner,
+        address _spender,
+        uint256 _value
+    ) public {
+        emit Approval(_owner, _spender, _value);
+    }
+}
