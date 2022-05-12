7545992b4bada2fcc3f78b08cc576638fc023b44
Vxatz <92011898+Vxatz@users.noreply.github.com>
2022-05-12 17:43:04 +0300

    fix: folder

--- Apeswap-Bots/Tokens-Ownership-Transfers/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Interface } from "@ethersproject/abi";
-
-export const EVENTS_ABI: string[] = [
-  "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)", //BSC
-  "event LogChangeMPCOwner(address indexed oldOwner, address indexed newOwner, uint indexed effectiveHeight)", //Polygon
-];
-
-export const EVENTS_IFACE: Interface = new Interface(EVENTS_ABI);
