7a5f014f24515fa5441282745799e991d2a126bc
Roberto Cantu <Roberto_Cantu@live.com>
2022-05-12 12:47:10 -0500

    tests: Add more tests in agent.spec

--- DYDX-Bots/Roles_Monitor/src/agent.spec.ts
+++ DYDX-Bots/Roles_Monitor/src/agent.spec.ts
@@ -1,6 +1,6 @@
 import { FindingType, FindingSeverity, Finding, HandleTransaction, TransactionEvent } from "forta-agent";
 import { createAddress, TestTransactionEvent } from "forta-agent-tools/lib/tests";
-import { BigNumber, utils } from "ethers";
+import { utils } from "ethers";
 import { provideHandleTransaction } from "./agent";
 import NetworkManager from "./network";
 import { MODULE_IFACE } from "./utils";
@@ -11,11 +11,16 @@ const testSender: string = createAddress("0xad");
 const roleChangedTestCases: [string, string, string][] = [
   [utils.keccak256("0xab11"), utils.keccak256("0xab12"), utils.keccak256("0xab13")],
   [utils.keccak256("0xac21"), utils.keccak256("0xac22"), utils.keccak256("0xac23")],
+  [utils.keccak256("0xad31"), utils.keccak256("0xad32"), utils.keccak256("0xad33")]
 ];
 // Format: [role, account, sender][]
 const grantedAndRevokedTestCases: [string, string, string][] = [
   [utils.keccak256("0xfe11"), createAddress("0xab2"), createAddress("0xab3")],
   [utils.keccak256("0xfe12"), createAddress("0xac2"), createAddress("0xac3")],
+  [utils.keccak256("0xfe13"), createAddress("0xad2"), createAddress("0xad3")],
+  [utils.keccak256("0xfe14"), createAddress("0xae2"), createAddress("0xae3")],
+  [utils.keccak256("0xfe15"), createAddress("0xaf2"), createAddress("0xaf3")],
+  [utils.keccak256("0xfe15"), createAddress("0xba2"), createAddress("0xba3")]
 ];
 
 describe("Roles Changes Monitor Test Suite", () => {
@@ -43,13 +48,13 @@ describe("Roles Changes Monitor Test Suite", () => {
     const RoleAdminChangedLogOne = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleAdminChanged"), [
       roleChangedTestCases[0][0],
       roleChangedTestCases[0][1],
-      roleChangedTestCases[0][2],
+      roleChangedTestCases[0][2]
     ]);
 
     const RoleAdminChangedLogTwo = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleAdminChanged"), [
       roleChangedTestCases[1][0],
       roleChangedTestCases[1][1],
-      roleChangedTestCases[1][2],
+      roleChangedTestCases[1][2]
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -104,13 +109,13 @@ describe("Roles Changes Monitor Test Suite", () => {
     const RoleGrantedLogOne = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleGranted"), [
       grantedAndRevokedTestCases[0][0],
       grantedAndRevokedTestCases[0][1],
-      grantedAndRevokedTestCases[0][2],
+      grantedAndRevokedTestCases[0][2]
     ]);
 
     const RoleGrantedLogTwo = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleGranted"), [
       grantedAndRevokedTestCases[1][0],
       grantedAndRevokedTestCases[1][1],
-      grantedAndRevokedTestCases[1][2],
+      grantedAndRevokedTestCases[1][2]
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -153,14 +158,17 @@ describe("Roles Changes Monitor Test Suite", () => {
     ]);
   });
 
-  /*
-  it("should detect a RewardsPerSecondUpdated event emission from both the safety and liquidity modules", async () => {
-    const RewardsPerSecondLogOne = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RewardsPerSecondUpdated"), [
-      testCases[6],
+  it("should detect a RoleRevoked event emission from both the safety and liquidity modules", async () => {
+    const RoleRevokedLogOne = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleRevoked"), [
+      grantedAndRevokedTestCases[2][0],
+      grantedAndRevokedTestCases[2][1],
+      grantedAndRevokedTestCases[2][2]
     ]);
 
-    const RewardsPerSecondLogTwo = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RewardsPerSecondUpdated"), [
-      testCases[7],
+    const RoleRevokedLogTwo = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleRevoked"), [
+      grantedAndRevokedTestCases[3][0],
+      grantedAndRevokedTestCases[3][1],
+      grantedAndRevokedTestCases[3][2]
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -168,39 +176,43 @@ describe("Roles Changes Monitor Test Suite", () => {
       .setFrom(testSender)
       .addAnonymousEventLog(
         mockNetworkManager.safetyModule,
-        RewardsPerSecondLogOne.data,
-        ...RewardsPerSecondLogOne.topics
+        RoleRevokedLogOne.data,
+        ...RoleRevokedLogOne.topics
       )
       .addAnonymousEventLog(
         mockNetworkManager.liquidityModule,
-        RewardsPerSecondLogTwo.data,
-        ...RewardsPerSecondLogTwo.topics
+        RoleRevokedLogTwo.data,
+        ...RoleRevokedLogTwo.topics
       );
 
     const findings = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([
       Finding.fromObject({
-        name: "Rewards per second have been updated",
-        description: "RewardsPerSecondUpdated event was emitted",
-        alertId: "DYDX-17-3",
+        name: "Role has been revoked",
+        description: "RoleRevoked event was emitted",
+        alertId: "DYDX-18-3",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          emissionPerSecond: testCases[6].toString(),
+          role: grantedAndRevokedTestCases[2][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          account: grantedAndRevokedTestCases[2][1],
+          sender: grantedAndRevokedTestCases[2][2],
         },
         addresses: [mockNetworkManager.safetyModule],
       }),
       Finding.fromObject({
-        name: "Rewards per second have been updated",
-        description: "RewardsPerSecondUpdated event was emitted",
-        alertId: "DYDX-17-3",
+        name: "Role has been revoked",
+        description: "RoleRevoked event was emitted",
+        alertId: "DYDX-18-3",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          emissionPerSecond: testCases[7].toString(),
+          role: grantedAndRevokedTestCases[3][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          account: grantedAndRevokedTestCases[3][1],
+          sender: grantedAndRevokedTestCases[3][2],
         },
         addresses: [mockNetworkManager.liquidityModule],
       }),
@@ -210,22 +222,28 @@ describe("Roles Changes Monitor Test Suite", () => {
   it("should not detect the events emitting from the incorrect contract", async () => {
     const wrongContract: string = createAddress("0xd34d");
 
-    const BlackoutWindowLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("BlackoutWindowChanged"), [
-      testCases[8],
+    const RoleAdminChangedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleAdminChanged"), [
+      roleChangedTestCases[0][0],
+      roleChangedTestCases[0][1],
+      roleChangedTestCases[0][2]
     ]);
 
-    const EpochParamChangedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("EpochParametersChanged"), [
-      [testCases[9], testCases[10]],
+    const EpochParamChangedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleGranted"), [
+      grantedAndRevokedTestCases[0][0],
+      grantedAndRevokedTestCases[0][1],
+      grantedAndRevokedTestCases[0][2]
     ]);
 
-    const RewardsPerSecondLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RewardsPerSecondUpdated"), [
-      testCases[11],
+    const RewardsPerSecondLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleRevoked"), [
+      grantedAndRevokedTestCases[2][0],
+      grantedAndRevokedTestCases[2][1],
+      grantedAndRevokedTestCases[2][2]
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setTo(wrongContract)
       .setFrom(testSender)
-      .addAnonymousEventLog(wrongContract, BlackoutWindowLog.data, ...BlackoutWindowLog.topics)
+      .addAnonymousEventLog(wrongContract, RoleAdminChangedLog.data, ...RoleAdminChangedLog.topics)
       .addAnonymousEventLog(wrongContract, EpochParamChangedLog.data, ...EpochParamChangedLog.topics)
       .addAnonymousEventLog(wrongContract, RewardsPerSecondLog.data, ...RewardsPerSecondLog.topics);
 
@@ -250,70 +268,80 @@ describe("Roles Changes Monitor Test Suite", () => {
   });
 
   it("should detect various combinations of event emissions and module contracts", async () => {
-    const BlackoutWindowLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("BlackoutWindowChanged"), [
-      testCases[14],
+    const RoleAdminChangedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleAdminChanged"), [
+      roleChangedTestCases[2][0],
+      roleChangedTestCases[2][1],
+      roleChangedTestCases[2][2]
     ]);
 
-    const EpochParamChangedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("EpochParametersChanged"), [
-      [testCases[15], testCases[16]],
+    const RoleGrantedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleGranted"), [
+      grantedAndRevokedTestCases[4][0],
+      grantedAndRevokedTestCases[4][1],
+      grantedAndRevokedTestCases[4][2]
     ]);
 
-    const RewardsPerSecondLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RewardsPerSecondUpdated"), [
-      testCases[17],
+    const RoleRevokedLog = MODULE_IFACE.encodeEventLog(MODULE_IFACE.getEvent("RoleRevoked"), [
+      grantedAndRevokedTestCases[5][0],
+      grantedAndRevokedTestCases[5][1],
+      grantedAndRevokedTestCases[5][2]
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
       .setTo(mockNetworkManager.liquidityModule)
       .setFrom(testSender)
-      .addAnonymousEventLog(mockNetworkManager.safetyModule, BlackoutWindowLog.data, ...BlackoutWindowLog.topics)
+      .addAnonymousEventLog(mockNetworkManager.safetyModule, RoleAdminChangedLog.data, ...RoleAdminChangedLog.topics)
       .addAnonymousEventLog(
         mockNetworkManager.liquidityModule,
-        EpochParamChangedLog.data,
-        ...EpochParamChangedLog.topics
+        RoleGrantedLog.data,
+        ...RoleGrantedLog.topics
       )
-      .addAnonymousEventLog(mockNetworkManager.safetyModule, RewardsPerSecondLog.data, ...RewardsPerSecondLog.topics);
+      .addAnonymousEventLog(mockNetworkManager.safetyModule, RoleRevokedLog.data, ...RoleRevokedLog.topics);
 
     const findings = await handleTransaction(txEvent);
 
     expect(findings).toStrictEqual([
       Finding.fromObject({
-        name: "Blackout window has changed",
-        description: "BlackoutWindowChanged event was emitted",
-        alertId: "DYDX-17-1",
+        name: "Admin role has changed",
+        description: "RoleAdminChanged event was emitted",
+        alertId: "DYDX-18-1",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          blackoutWindow: testCases[14].toString(),
+          role: roleChangedTestCases[2][0],
+          previousAdminRole: roleChangedTestCases[2][1],
+          newAdminRole: roleChangedTestCases[2][2]
         },
         addresses: [mockNetworkManager.safetyModule],
       }),
       Finding.fromObject({
-        name: "Epoch parameters have changed",
-        description: "EpochParametersChanged event was emitted",
-        alertId: "DYDX-17-2",
+        name: "Role has been granted",
+        description: "RoleGranted event was emitted",
+        alertId: "DYDX-18-2",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          interval: testCases[15].toString(),
-          offset: testCases[16].toString(),
+          role: grantedAndRevokedTestCases[4][0],
+          account: grantedAndRevokedTestCases[4][1],
+          sender: grantedAndRevokedTestCases[4][2],
         },
         addresses: [mockNetworkManager.liquidityModule],
       }),
       Finding.fromObject({
-        name: "Rewards per second have been updated",
-        description: "RewardsPerSecondUpdated event was emitted",
-        alertId: "DYDX-17-3",
+        name: "Role has been revoked",
+        description: "RoleRevoked event was emitted",
+        alertId: "DYDX-18-3",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          emissionPerSecond: testCases[17].toString(),
+          role: grantedAndRevokedTestCases[5][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          account: grantedAndRevokedTestCases[5][1],
+          sender: grantedAndRevokedTestCases[5][2],
         },
         addresses: [mockNetworkManager.safetyModule],
       }),
     ]);
   });
-  */
 });
