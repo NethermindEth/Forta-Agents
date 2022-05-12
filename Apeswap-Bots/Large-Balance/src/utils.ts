00d1fac0831f84de081457ab3bf1ab0782e5ad50
ishitarastogi <ishita3099@gmail.com>
2022-05-12 19:29:04 +0530

    feat:Added PoC

--- Apeswap-Bots/Large-Balance/src/utils.ts
+++ Apeswap-Bots/Large-Balance/src/utils.ts
@@ -1,18 +1,19 @@
-import {
-  Finding,
-  FindingSeverity,
-  FindingType
-} from "forta-agent";
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
 import { ethers, BigNumber } from "ethers";
-export const EVENT_ABI = 
-  ["event Transfer(address indexed from, address indexed to, uint256 value)"];
+export const EVENT_ABI = [
+  "event Transfer(address indexed from, address indexed to, uint256 value)",
+];
 export const BALANCE_OF_ABI = [
-  "function balanceOf(address account) public view  returns (uint256)"];
+  "function balanceOf(address account) public view  returns (uint256)",
+];
 export const APESWAP_INTERFACE = new ethers.utils.Interface(BALANCE_OF_ABI);
-export const GNANA_TOKEN_CONTRACT = "0xddb3bd8645775f59496c821e4f55a7ea6a6dc299";
+export const GNANA_TOKEN_CONTRACT =
+  "0xddb3bd8645775f59496c821e4f55a7ea6a6dc299";
 export const GNANA_DECIMALS: number = 18;
 export const THRESHOLD_BALANCE_PERCENTAGE: number = 1;
-const TOTAL_SUPPLY: BigNumber = BigNumber.from("3000000000").mul(`${10 ** GNANA_DECIMALS}`);
+const TOTAL_SUPPLY: BigNumber = BigNumber.from("3000000000").mul(
+  `${10 ** GNANA_DECIMALS}`
+);
 export const BALANCE_THRESHOLD: BigNumber = TOTAL_SUPPLY.mul(
   THRESHOLD_BALANCE_PERCENTAGE
 ).div(100);
