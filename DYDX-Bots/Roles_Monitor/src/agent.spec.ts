2f1a9516a15be0f9d7f6abc2b7feebe5c90de622
Roberto Cantu <Roberto_Cantu@live.com>
2022-05-17 13:56:36 -0500

    test: Update tests to use and pass with testRolesMap

--- DYDX-Bots/Roles_Monitor/src/agent.spec.ts
+++ DYDX-Bots/Roles_Monitor/src/agent.spec.ts
@@ -7,20 +7,31 @@ import { MODULE_IFACE } from "./utils";
 
 const testSender: string = createAddress("0xad");
 
-// Format: [role, previousAdminRole, newAdminRole][]
+export const testRolesMap: Record<string, string> = {
+  "0x09659e0aa12c56a3dece97692f0f23fc8f2825f9ffaa3b75428e4726cdf2a814": "OWNER_ROLE", // "0xab11"
+  "0x5e620be0f7c67873fd132b7805b9bacd22770701962c4441433f6d572e09ac4c": "EPOCH_PARAMETERS_ROLE", // "0xab12"
+  "0xa4efcdbe0a4517e5f06e41c280c2685b29d3072f17b8ccb5ff92066a2a07f6de": "REWARDS_RATE_ROLE", // "0xab13"
+  "0x5a88a442e34c11687eb5944a8cd22b8238668156fa17d16580838a3b9e6c0c19": "BORROWER_ADMIN_ROLE", // "0xac21"
+  "0x71605e6bd15336ca6d608097000be252db45c93857c39479aaa8e66a40316928": "CLAIM_OPERATOR_ROLE", // "0xac22"
+  "0x15887ddf60e2e6b78e963b6dd4e93041dc8206450f1baa784eb0f6d704a44149": "STAKE_OPERATOR_ROLE", // "0xac23"
+  "0x514259bf2ff5d81df8bc4e02c421a05a287059ba14139e5620375140809e91e1": "DEBT_OPERATOR_ROLE", // "0xad31"
+  "0x54a8c0ab653c15bfb48b47fd011ba2b9617af01cb45cab344acd57c924d56798": "NONE", // "0x0000"
+  "0x673fbecda3bfdf1adbf9e5f658c0f5ee475166da8b4bcc9fd0b5d6dd0e82e0bd": "SLASHER_ROLE", // "0xad32"
+};
+
 const roleChangedTestCases: [string, string, string][] = [
   [utils.keccak256("0xab11"), utils.keccak256("0xab12"), utils.keccak256("0xab13")],
   [utils.keccak256("0xac21"), utils.keccak256("0xac22"), utils.keccak256("0xac23")],
-  [utils.keccak256("0xad31"), utils.keccak256("0xad32"), utils.keccak256("0xad33")],
+  [utils.keccak256("0xad31"), utils.keccak256("0x0000"), utils.keccak256("0xad32")],
 ];
 // Format: [role, account, sender][]
 const grantedAndRevokedTestCases: [string, string, string][] = [
-  [utils.keccak256("0xfe11"), createAddress("0xab2"), createAddress("0xab3")],
-  [utils.keccak256("0xfe12"), createAddress("0xac2"), createAddress("0xac3")],
-  [utils.keccak256("0xfe13"), createAddress("0xad2"), createAddress("0xad3")],
-  [utils.keccak256("0xfe14"), createAddress("0xae2"), createAddress("0xae3")],
-  [utils.keccak256("0xfe15"), createAddress("0xaf2"), createAddress("0xaf3")],
-  [utils.keccak256("0xfe15"), createAddress("0xba2"), createAddress("0xba3")],
+  [utils.keccak256("0xab11"), createAddress("0xab2"), createAddress("0xab3")],
+  [utils.keccak256("0xab12"), createAddress("0xac2"), createAddress("0xac3")],
+  [utils.keccak256("0xab13"), createAddress("0xad2"), createAddress("0xad3")],
+  [utils.keccak256("0xac21"), createAddress("0xae2"), createAddress("0xae3")],
+  [utils.keccak256("0xac22"), createAddress("0xaf2"), createAddress("0xaf3")],
+  [utils.keccak256("0xac23"), createAddress("0xba2"), createAddress("0xba3")],
 ];
 
 describe("Roles Changes Monitor Test Suite", () => {
@@ -34,7 +45,7 @@ describe("Roles Changes Monitor Test Suite", () => {
   };
 
   beforeAll(() => {
-    handleTransaction = provideHandleTransaction(mockNetworkManager);
+    handleTransaction = provideHandleTransaction(mockNetworkManager, testRolesMap);
   });
 
   it("should return 0 findings in empty transactions", async () => {
@@ -82,9 +93,9 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: roleChangedTestCases[0][0],
-          previousAdminRole: roleChangedTestCases[0][1],
-          newAdminRole: roleChangedTestCases[0][2],
+          role: testRolesMap[utils.keccak256("0xab11")],
+          previousAdminRole: testRolesMap[utils.keccak256("0xab12")],
+          newAdminRole: testRolesMap[utils.keccak256("0xab13")],
         },
         addresses: [mockNetworkManager.safetyModule],
       }),
@@ -96,9 +107,9 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: roleChangedTestCases[1][0],
-          previousAdminRole: roleChangedTestCases[1][1],
-          newAdminRole: roleChangedTestCases[1][2],
+          role: testRolesMap[utils.keccak256("0xac21")],
+          previousAdminRole: testRolesMap[utils.keccak256("0xac22")],
+          newAdminRole: testRolesMap[utils.keccak256("0xac23")],
         },
         addresses: [mockNetworkManager.liquidityModule],
       }),
@@ -135,7 +146,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[0][0],
+          role: testRolesMap[utils.keccak256("0xab11")],
           account: grantedAndRevokedTestCases[0][1],
           sender: grantedAndRevokedTestCases[0][2],
         },
@@ -149,7 +160,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[1][0],
+          role: testRolesMap[utils.keccak256("0xab12")],
           account: grantedAndRevokedTestCases[1][1],
           sender: grantedAndRevokedTestCases[1][2],
         },
@@ -188,7 +199,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[2][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          role: testRolesMap[utils.keccak256("0xab13")],
           account: grantedAndRevokedTestCases[2][1],
           sender: grantedAndRevokedTestCases[2][2],
         },
@@ -202,7 +213,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[3][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          role: testRolesMap[utils.keccak256("0xac21")],
           account: grantedAndRevokedTestCases[3][1],
           sender: grantedAndRevokedTestCases[3][2],
         },
@@ -296,9 +307,9 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: roleChangedTestCases[2][0],
-          previousAdminRole: roleChangedTestCases[2][1],
-          newAdminRole: roleChangedTestCases[2][2],
+          role: testRolesMap[utils.keccak256("0xad31")],
+          previousAdminRole: testRolesMap[utils.keccak256("0x0000")],
+          newAdminRole: testRolesMap[utils.keccak256("0xad32")],
         },
         addresses: [mockNetworkManager.safetyModule],
       }),
@@ -310,7 +321,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[4][0],
+          role: testRolesMap[utils.keccak256("0xac22")],
           account: grantedAndRevokedTestCases[4][1],
           sender: grantedAndRevokedTestCases[4][2],
         },
@@ -324,7 +335,7 @@ describe("Roles Changes Monitor Test Suite", () => {
         type: FindingType.Info,
         protocol: "dYdX",
         metadata: {
-          role: grantedAndRevokedTestCases[5][0], // TYPE bytes32, NEED TO CONVERT TO STRING
+          role: testRolesMap[utils.keccak256("0xac23")],
           account: grantedAndRevokedTestCases[5][1],
           sender: grantedAndRevokedTestCases[5][2],
         },
