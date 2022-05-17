06d6da9eec1f89d1cc5933d3785170f6d1990062
Yehia Tarek <yehia@startfi.io>
2022-05-17 10:48:38 +0200

    add opening number

--- GMX-Bots/Detect-many-opening/src/utils.ts
+++ GMX-Bots/Detect-many-opening/src/utils.ts
@@ -7,7 +7,8 @@ import {
 } from "forta-agent";
 
 const provider = getEthersProvider();
-const positionsNumber = 500; // arbitrum generate 15 block per second thus to get number of blocks in one minute 60s/15 = 4 block
+const positionsNumber = 50;
+const blockNumbers = 500; // arbitrum generate 15 block per second thus to get number of blocks in one minute 60s/15 = 4 block
 const GMX_VAULT_ADDRESS: string = "0x489ee077994B6658eAfA855C308275EAd8097C4A"; // Arbitrum address
 const INCREASE_POSITION_EVENT =
   "event IncreasePosition(bytes32 key,address account,address collateralToken,address indexToken,uint256 collateralDelta,uint256 sizeDelta,bool isLong,uint256 price,uint256 fee)";
@@ -16,16 +17,17 @@ const EVENT_ABI: string[] = [INCREASE_POSITION_EVENT];
 
 const EVENTS_IFACE: Interface = new Interface(EVENT_ABI);
 
-const createFinding = (account: string): Finding => {
+const createFinding = (account: string, numberOfOpening: string): Finding => {
   return Finding.fromObject({
     name: "Detects many position openings from an account within a time-frame",
-    description: "Certain account opened unusual positions within a time-frame",
+    description: `Account: ${account} opened ${numberOfOpening} positions`,
     alertId: "GMX-3",
     severity: FindingSeverity.Medium,
     type: FindingType.Suspicious,
     protocol: "GMX",
     metadata: {
       account,
+      numberOfOpening,
     },
   });
 };
@@ -36,6 +38,7 @@ export default {
   INCREASE_POSITION_EVENT,
   EVENT_ABI,
   EVENTS_IFACE,
+  blockNumbers,
   positionsNumber,
   createFinding,
 };
