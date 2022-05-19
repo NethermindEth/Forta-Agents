a0fa7cc2e02a1e573637546396b0ef66f1e50f7b
Yehia Tarek <yehia@startfi.io>
2022-05-19 11:46:13 +0200

    add flashload signature

--- Umee-Bots/lendingPool-reentrancy/src/utils.ts
+++ Umee-Bots/lendingPool-reentrancy/src/utils.ts
@@ -7,10 +7,12 @@ const LENDING_POOL_ADDRESS = "0xcE744a9BAf573167B2CF138114BA32ed7De274Fa";
 const REENTERANCY_FUNCTIONS_SIGNATURES: string[] = [
   "borrow(address,uint256,uint256,uint16,address)",
   "withdraw(address,uint256,address)",
+  "flashLoan(address,address[],uint256[],uint256[],address,bytes,uint16)",
 ];
 const REENTERANCY_FUNCTIONS_ABI: string[] = [
   "function borrow(address asset,uint256 amount,uint256 interestRateMode,uint16 referralCode,address onBehalfOf)",
   "function withdraw(address asset,uint256 amount,address to)",
+  "function flashLoan(address receiverAddress,address[] calldata assets,uint256[] calldata amounts,uint256[] calldata modes,address onBehalfOf,bytes calldata params,uint16 referralCode)",
 ];
 
 const FUNCTIONS_INTERFACE = new Interface(REENTERANCY_FUNCTIONS_ABI);
