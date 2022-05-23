1074523af96789dabd50eaed25233f7bdb2a61d0
Yehia Tarek <yehia@startfi.io>
2022-05-23 18:07:05 +0200

    fix: use continue instead of break and better comment

--- Umee-Bots/lendingPool-reentrancy/src/agent.ts
+++ Umee-Bots/lendingPool-reentrancy/src/agent.ts
@@ -10,15 +10,15 @@ export const handleTransaction =
     for (let i = 0; i < traces.length; i++) {
       const trace = traces[i];
       const depth = trace.traceAddress.length;
-
       if (trace.action.to === contractAddress) {
         let j;
 
         for (j = i + 1; j < traces.length; j++) {
           if (traces[j].traceAddress.length <= depth) {
             i = j - 1;
-            break; // subtree ended
+            continue; // subtree ended, non reentrant call
           }
+
           if (traces[j].action.to === contractAddress) {
             const selector = (traces[j].action.input || "").slice(0, 10); // "0x" and first 4 bytes
             if (reentrancyFunctionsSelectors.includes(selector)) {
