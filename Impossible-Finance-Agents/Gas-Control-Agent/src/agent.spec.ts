aa8fcd045321f8b22314bdd7dad1f4a096d90e89
Yasmine Bachi <gy_bachi@esi.dz>
2022-05-26 18:00:53 +0100

    run npm run format

--- Impossible-Finance-Agents/Gas-Control-Agent/src/agent.spec.ts
+++ Impossible-Finance-Agents/Gas-Control-Agent/src/agent.spec.ts
@@ -1,44 +1,52 @@
-import { BigNumber, BigNumberish } from 'ethers';
-import { Finding, FindingSeverity, FindingType, HandleTransaction } from 'forta-agent';
-import { createAddress, TestTransactionEvent } from 'forta-agent-tools/lib/tests';
-import { provideHandleTransaction } from './agent';
-import utils from './utils';
+import { BigNumber, BigNumberish } from "ethers";
+import {
+  Finding,
+  FindingSeverity,
+  FindingType,
+  HandleTransaction,
+} from "forta-agent";
+import {
+  createAddress,
+  TestTransactionEvent,
+} from "forta-agent-tools/lib/tests";
+import { provideHandleTransaction } from "./agent";
+import utils from "./utils";
 
 const TEST_ADDRESSES = [
-  createAddress('0x1abc'),
-  createAddress('0x2def'),
-  createAddress('0xdead'),
+  createAddress("0x1abc"),
+  createAddress("0x2def"),
+  createAddress("0xdead"),
 ];
 
 const createFinding = (addrs: string[], gas: string, threshold: BigNumber) => {
   const gasStr: string = BigNumber.from(gas).toString();
 
   return Finding.fromObject({
-    name: 'High Gas Usage Detection',
+    name: "High Gas Usage Detection",
     description: `High gas is used - above ${threshold.toString()} Gwei`,
-    alertId: 'IMPOSSIBLE-2',
+    alertId: "IMPOSSIBLE-2",
     severity: FindingSeverity.High,
     type: FindingType.Info,
-    protocol: 'Impossible Finance',
+    protocol: "Impossible Finance",
     metadata: {
       protocolContracts: addrs.toString(),
       gasInGwei: gasStr.slice(0, gasStr.length - 9),
       gasInWei: gasStr,
     },
   });
-}
+};
 
-describe('Gas monitor agent test suite', () => {
+describe("Gas monitor agent test suite", () => {
   const threshold: BigNumber = BigNumber.from(20);
   const handleTransaction: HandleTransaction = provideHandleTransaction(
-    utils.isOnList(TEST_ADDRESSES), 
-    threshold,
+    utils.isOnList(TEST_ADDRESSES),
+    threshold
   );
 
-  const toGas = (wei: BigNumberish) => 
+  const toGas = (wei: BigNumberish) =>
     BigNumber.from(wei).mul(BigNumber.from(10).pow(9));
 
-  it('should return empty finding', async () => {
+  it("should return empty finding", async () => {
     const tx = new TestTransactionEvent();
     tx.transaction.gasPrice = toGas(200).toHexString();
 
@@ -47,11 +55,13 @@ describe('Gas monitor agent test suite', () => {
     expect(findings).toStrictEqual([]);
   });
 
-  it('should return empty finding if gas is below threshold', async () => {
+  it("should return empty finding if gas is below threshold", async () => {
     const CASES: number[] = [12, 8, 2, 19, 11, 20];
 
-    for(let price of CASES) {
-      const tx = new TestTransactionEvent().addInvolvedAddresses(...TEST_ADDRESSES);
+    for (let price of CASES) {
+      const tx = new TestTransactionEvent().addInvolvedAddresses(
+        ...TEST_ADDRESSES
+      );
       tx.transaction.gasPrice = toGas(price).toHexString();
 
       const findings = await handleTransaction(tx);
@@ -60,22 +70,26 @@ describe('Gas monitor agent test suite', () => {
     }
   });
 
-  it('should return a finding if gas is above threshold', async () => {
+  it("should return a finding if gas is above threshold", async () => {
     const CASES: number[] = [21, 100, 234, 22, 30, 42];
 
-    for(let price of CASES) {
-      const tx = new TestTransactionEvent().addInvolvedAddresses(...TEST_ADDRESSES);
+    for (let price of CASES) {
+      const tx = new TestTransactionEvent().addInvolvedAddresses(
+        ...TEST_ADDRESSES
+      );
       tx.transaction.gasPrice = toGas(price).toHexString();
 
       const findings = await handleTransaction(tx);
 
-      expect(findings).toStrictEqual([createFinding(TEST_ADDRESSES, tx.transaction.gasPrice, threshold)]);
+      expect(findings).toStrictEqual([
+        createFinding(TEST_ADDRESSES, tx.transaction.gasPrice, threshold),
+      ]);
     }
   });
 
-  it('should return empty finding if non relevant addresses are involved', async () => {
+  it("should return empty finding if non relevant addresses are involved", async () => {
     const tx = new TestTransactionEvent().addInvolvedAddresses(
-      createAddress('0x6'), 
+      createAddress("0x6"),
       createAddress("0xfee")
     );
     tx.transaction.gasPrice = toGas(10000).toHexString();
@@ -85,10 +99,10 @@ describe('Gas monitor agent test suite', () => {
     expect(findings).toStrictEqual([]);
   });
 
-  it('should return a finding if gas used is above the threshold for one wei', async () => {
+  it("should return a finding if gas used is above the threshold for one wei", async () => {
     const tx = new TestTransactionEvent().addInvolvedAddresses(
-      TEST_ADDRESSES[2], 
-      TEST_ADDRESSES[1],
+      TEST_ADDRESSES[2],
+      TEST_ADDRESSES[1]
     );
     tx.transaction.gasPrice = toGas(threshold).add(1).toHexString(); // 11 GWei
 
@@ -96,9 +110,9 @@ describe('Gas monitor agent test suite', () => {
 
     expect(findings).toStrictEqual([
       createFinding(
-        [TEST_ADDRESSES[2], TEST_ADDRESSES[1]], 
+        [TEST_ADDRESSES[2], TEST_ADDRESSES[1]],
         tx.transaction.gasPrice,
-        threshold,
+        threshold
       ),
     ]);
   });
--- Impossible-Finance-Agents/Gas-Control-Agent/src/agent.ts
+++ Impossible-Finance-Agents/Gas-Control-Agent/src/agent.ts
@@ -1,14 +1,17 @@
-import { Finding, HandleTransaction, TransactionEvent } from 'forta-agent';
-import utils, { AddressValidator } from './utils';
-import { BigNumber, ethers } from 'ethers';
+import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
+import utils, { AddressValidator } from "./utils";
+import { BigNumber, ethers } from "ethers";
 
 const THRESHOLD: BigNumber = BigNumber.from(10); // gas price
 
 export const provideHandleTransaction = (
-  isValid: AddressValidator, 
-  _threshold: BigNumber,
+  isValid: AddressValidator,
+  _threshold: BigNumber
 ): HandleTransaction => {
-  const threshold: BigNumber = ethers.utils.parseUnits(_threshold.toString(), 'gwei');
+  const threshold: BigNumber = ethers.utils.parseUnits(
+    _threshold.toString(),
+    "gwei"
+  );
 
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
@@ -16,10 +19,12 @@ export const provideHandleTransaction = (
     const gasPrice: BigNumber = BigNumber.from(txEvent.gasPrice);
 
     const addrs: string[] = Object.keys(txEvent.addresses);
-    const valid: boolean[] = await Promise.all(addrs.map(addr => isValid(addr)));
+    const valid: boolean[] = await Promise.all(
+      addrs.map((addr) => isValid(addr))
+    );
     const involved: string[] = addrs.filter((_, idx) => valid[idx]);
 
-    if (involved.length && threshold.lt(gasPrice)) 
+    if (involved.length && threshold.lt(gasPrice))
       findings.push(utils.createFinding(involved, gasPrice, _threshold));
 
     return findings;
@@ -29,7 +34,7 @@ export const provideHandleTransaction = (
 export default {
   handleTransaction: provideHandleTransaction(
     utils.isOnContractList,
-    THRESHOLD,
+    THRESHOLD
   ),
   provideHandleTransaction,
 };
