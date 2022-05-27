13aeec246b002dd905ea0aba19fc746647b21331
Yasmine Bachi <gy_bachi@esi.dz>
2022-05-27 10:02:29 +0100

    run npm format

--- Impossible-Finance-Agents/Large-Deposit-Withdraw-On-Pairs/src/agent.spec.ts
+++ Impossible-Finance-Agents/Large-Deposit-Withdraw-On-Pairs/src/agent.spec.ts
@@ -1,33 +1,115 @@
 import { BigNumber } from "ethers";
 import { Interface } from "ethers/lib/utils";
-import {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  HandleTransaction,
-  TransactionEvent,
-} from "forta-agent";
+import { FindingType, FindingSeverity, Finding, HandleTransaction, TransactionEvent } from "forta-agent";
 import { createAddress, MockEthersProvider, TestTransactionEvent } from "forta-agent-tools/lib/tests";
 import abi from "./abi";
 import { provideHandleTransaction } from "./agent";
 import PairFetcher from "./pairs.fetcher";
 
-type TestData = [string, string,string,string, string, string[], string, string, boolean, number][];
+type TestData = [string, string, string, string, string, string[], string, string, boolean, number][];
 
 // address, token0, token1, realPair, event, params, reserves0, reserve1, emit finding, block
 const CASES: TestData = [
   // not pairs
-  [createAddress("0xace17e"), createAddress("0xa1"), createAddress("0xa2"), createAddress("0xfefe"), "Mint", [createAddress('0x1a'), "1000", "2000"], "1000", "2000", false, 1], 
-  [createAddress("0xca1d0"),createAddress("0xa3"), createAddress("0xa4"),  createAddress("0xfefe"), "Burn", [createAddress('0x1b'), "2000", "3000", createAddress('0x2b')], "100", "200", false, 2], 
+  [
+    createAddress("0xace17e"),
+    createAddress("0xa1"),
+    createAddress("0xa2"),
+    createAddress("0xfefe"),
+    "Mint",
+    [createAddress("0x1a"), "1000", "2000"],
+    "1000",
+    "2000",
+    false,
+    1,
+  ],
+  [
+    createAddress("0xca1d0"),
+    createAddress("0xa3"),
+    createAddress("0xa4"),
+    createAddress("0xfefe"),
+    "Burn",
+    [createAddress("0x1b"), "2000", "3000", createAddress("0x2b")],
+    "100",
+    "200",
+    false,
+    2,
+  ],
   // under threshold
-  [createAddress("0xf11e7e"), createAddress("0xa23"), createAddress("0xa32"),createAddress("0xf11e7e"), "Mint", [createAddress('0x1c'), "1", "1"], "200", "200", false, 3], 
-  [createAddress("0xc0c1d0"),  createAddress("0xa5"), createAddress("0xa6"), createAddress("0xc0c1d0"), "Burn", [createAddress('0x1b'), "10", "3", createAddress('0x2b')], "4000", "200", false, 4],
+  [
+    createAddress("0xf11e7e"),
+    createAddress("0xa23"),
+    createAddress("0xa32"),
+    createAddress("0xf11e7e"),
+    "Mint",
+    [createAddress("0x1c"), "1", "1"],
+    "200",
+    "200",
+    false,
+    3,
+  ],
+  [
+    createAddress("0xc0c1d0"),
+    createAddress("0xa5"),
+    createAddress("0xa6"),
+    createAddress("0xc0c1d0"),
+    "Burn",
+    [createAddress("0x1b"), "10", "3", createAddress("0x2b")],
+    "4000",
+    "200",
+    false,
+    4,
+  ],
   // over threshold
-  [createAddress("0xe570fad0"), createAddress("0xa85"), createAddress("0xa86"), createAddress("0xe570fad0"), "Mint", [createAddress('0x1e'), "100", "200"], "10", "20", true, 5], 
-  [createAddress("0xfee"),createAddress("0xa95"), createAddress("0xa96") ,createAddress("0xfee"), "Burn", [createAddress('0x1f'), "200", "40", createAddress('0x2f')], "1000", "200", true, 6], 
+  [
+    createAddress("0xe570fad0"),
+    createAddress("0xa85"),
+    createAddress("0xa86"),
+    createAddress("0xe570fad0"),
+    "Mint",
+    [createAddress("0x1e"), "100", "200"],
+    "10",
+    "20",
+    true,
+    5,
+  ],
+  [
+    createAddress("0xfee"),
+    createAddress("0xa95"),
+    createAddress("0xa96"),
+    createAddress("0xfee"),
+    "Burn",
+    [createAddress("0x1f"), "200", "40", createAddress("0x2f")],
+    "1000",
+    "200",
+    true,
+    6,
+  ],
   // other events on pairs
-  [createAddress("0x1ce"),createAddress("0xa75"), createAddress("0xa76"), createAddress("0x1ce"), "Test", ["1", "2"], "10", "20", false, 7], 
-  [createAddress("0xf00d"),createAddress("0xa65"), createAddress("0xa66"), createAddress("0xf00d"), "Test", ["3", "4"], "1000", "200", false, 8], 
+  [
+    createAddress("0x1ce"),
+    createAddress("0xa75"),
+    createAddress("0xa76"),
+    createAddress("0x1ce"),
+    "Test",
+    ["1", "2"],
+    "10",
+    "20",
+    false,
+    7,
+  ],
+  [
+    createAddress("0xf00d"),
+    createAddress("0xa65"),
+    createAddress("0xa66"),
+    createAddress("0xf00d"),
+    "Test",
+    ["3", "4"],
+    "1000",
+    "200",
+    false,
+    8,
+  ],
 ];
 
 const ABI: Interface = new Interface([
@@ -36,9 +118,7 @@ const ABI: Interface = new Interface([
   "event Test(uint a, uint b)",
 ]);
 
-const addLiquidityFinding = (
-  [pair, sender, amount0, amount1, reserve0, reserve1]: string[],
-): Finding =>
+const addLiquidityFinding = ([pair, sender, amount0, amount1, reserve0, reserve1]: string[]): Finding =>
   Finding.fromObject({
     name: "Impossible Finance Pair Liquidity Action",
     description: "Large liquidity Added",
@@ -49,9 +129,7 @@ const addLiquidityFinding = (
     metadata: { pair, amount0, amount1, sender, reserve0, reserve1 },
   });
 
-const removeLiquidityFinding = (
-  [pair, sender, amount0, amount1, to, reserve0, reserve1]: string[],
-): Finding =>
+const removeLiquidityFinding = ([pair, sender, amount0, amount1, to, reserve0, reserve1]: string[]): Finding =>
   Finding.fromObject({
     name: "Impossible Finance Pair Updated",
     description: "Large Liquidity Removed",
@@ -63,20 +141,20 @@ const removeLiquidityFinding = (
   });
 
 const router: Record<string, (_: string[]) => Finding> = {
-  "Burn": removeLiquidityFinding,
-  "Mint": addLiquidityFinding,
+  Burn: removeLiquidityFinding,
+  Mint: addLiquidityFinding,
 };
 
 describe("Large add/remove Liquidity agent tests suite", () => {
   const factory: string = createAddress("0xf00d");
-  const mockProvider =  new MockEthersProvider();
+  const mockProvider = new MockEthersProvider();
 
-  const mockPairsFetcher =new PairFetcher(factory, mockProvider as any);
+  const mockPairsFetcher = new PairFetcher(factory, mockProvider as any);
 
   const handler: HandleTransaction = provideHandleTransaction(mockPairsFetcher, 20);
 
   beforeEach(() => {
-   mockProvider.clear();
+    mockProvider.clear();
   });
 
   it("should report empty findings in txns without events", async () => {
@@ -87,28 +165,29 @@ describe("Large add/remove Liquidity agent tests suite", () => {
   });
 
   it("should detect/ignore events accordingly", async () => {
-    for(let [address, token0,token1,realPair, event, params, reserve0, reserve1, emitFinding, block] of CASES) {
+    for (let [address, token0, token1, realPair, event, params, reserve0, reserve1, emitFinding, block] of CASES) {
       // add call to token0, token1 of the pair
-      mockProvider.addCallTo(address,block,abi.PAIR,"token0",{inputs:[],outputs:[token0]});
-      mockProvider.addCallTo(address,block,abi.PAIR,"token1",{inputs:[],outputs:[token1]});
+      mockProvider.addCallTo(address, block, abi.PAIR, "token0", { inputs: [], outputs: [token0] });
+      mockProvider.addCallTo(address, block, abi.PAIR, "token1", { inputs: [], outputs: [token1] });
 
-      // add call to getPair(token0,token1) from the factory. 
-      mockProvider.addCallTo(factory,block,abi.FACTORY,"getPair",{inputs:[token0,token1],outputs:[realPair]});
+      // add call to getPair(token0,token1) from the factory.
+      mockProvider.addCallTo(factory, block, abi.FACTORY, "getPair", { inputs: [token0, token1], outputs: [realPair] });
 
-      // add call to getReserves. 
-      mockProvider.addCallTo(address,block -1,abi.PAIR,"getReserves",{inputs:[],outputs:[BigNumber.from(reserve0),BigNumber.from(reserve1),BigNumber.from(0)]});
-
+      // add call to getReserves.
+      mockProvider.addCallTo(address, block - 1, abi.PAIR, "getReserves", {
+        inputs: [],
+        outputs: [BigNumber.from(reserve0), BigNumber.from(reserve1), BigNumber.from(0)],
+      });
 
       const { data, topics } = ABI.encodeEventLog(ABI.getEvent(event), params);
-    
+
       const tx: TransactionEvent = new TestTransactionEvent()
         .setBlock(block)
         .addAnonymousEventLog(address, data, ...topics);
-      
+
       const findings: Finding[] = await handler(tx);
       const expected: Finding[] = [];
-      if(emitFinding)
-        expected.push(router[event]([address, ...params, reserve0, reserve1]));
+      if (emitFinding) expected.push(router[event]([address, ...params, reserve0, reserve1]));
 
       expect(findings).toStrictEqual(expected);
     }
@@ -119,24 +198,26 @@ describe("Large add/remove Liquidity agent tests suite", () => {
 
     const block: number = 42;
     const expected: Finding[] = [];
-    const tx: TestTransactionEvent = new TestTransactionEvent().setBlock(block)
+    const tx: TestTransactionEvent = new TestTransactionEvent().setBlock(block);
 
-    for(let [address,token0,token1,realPair, event, params, reserve0, reserve1,emitFinding,] of EVENTS) {
-       // add call to token0, token1 of the pair
-       mockProvider.addCallTo(address,block,abi.PAIR,"token0",{inputs:[],outputs:[token0]});
-       mockProvider.addCallTo(address,block,abi.PAIR,"token1",{inputs:[],outputs:[token1]});
- 
-       // add call to getPair(token0,token1) from the factory. 
-       mockProvider.addCallTo(factory,block,abi.FACTORY,"getPair",{inputs:[token0,token1],outputs:[realPair]});
- 
-       // add call to getReserves. 
-       mockProvider.addCallTo(address,block -1,abi.PAIR,"getReserves",{inputs:[],outputs:[BigNumber.from(reserve0),BigNumber.from(reserve1),0]});
+    for (let [address, token0, token1, realPair, event, params, reserve0, reserve1, emitFinding] of EVENTS) {
+      // add call to token0, token1 of the pair
+      mockProvider.addCallTo(address, block, abi.PAIR, "token0", { inputs: [], outputs: [token0] });
+      mockProvider.addCallTo(address, block, abi.PAIR, "token1", { inputs: [], outputs: [token1] });
+
+      // add call to getPair(token0,token1) from the factory.
+      mockProvider.addCallTo(factory, block, abi.FACTORY, "getPair", { inputs: [token0, token1], outputs: [realPair] });
+
+      // add call to getReserves.
+      mockProvider.addCallTo(address, block - 1, abi.PAIR, "getReserves", {
+        inputs: [],
+        outputs: [BigNumber.from(reserve0), BigNumber.from(reserve1), 0],
+      });
 
       const { data, topics } = ABI.encodeEventLog(ABI.getEvent(event), params);
-    
+
       tx.addAnonymousEventLog(address, data, ...topics);
-      if(emitFinding)
-      expected.push(router[event]([address, ...params, reserve0, reserve1]));
+      if (emitFinding) expected.push(router[event]([address, ...params, reserve0, reserve1]));
     }
 
     const findings: Finding[] = await handler(tx);
--- Impossible-Finance-Agents/Large-Deposit-Withdraw-On-Pairs/src/agent.ts
+++ Impossible-Finance-Agents/Large-Deposit-Withdraw-On-Pairs/src/agent.ts
@@ -1,10 +1,4 @@
-import {
-  Finding,
-  HandleTransaction,
-  TransactionEvent,
-  getEthersProvider,
-  LogDescription,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent, getEthersProvider, LogDescription } from "forta-agent";
 import abi from "./abi";
 import createFinding from "./findings";
 import PairFetcher from "./pairs.fetcher";
@@ -16,25 +10,25 @@ const FETCHER: PairFetcher = new PairFetcher(FACTORY, getEthersProvider());
 
 export const provideHandleTransaction =
   (fetcher: PairFetcher, percent: number): HandleTransaction =>
-    async (txEvent: TransactionEvent): Promise<Finding[]> => {
-      const findings: Finding[] = [];
+  async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
 
-      const logs: LogDescription[] = txEvent.filterLog(abi.PAIR.format('full'));
-      for (let log of logs) {
-        if (await fetcher.isImpossiblePair(txEvent.blockNumber, log.address.toLowerCase())) {
-          const { reserve0, reserve1 } = await fetcher.getReserves(
-            txEvent.blockNumber - 1, // reserves at the beginning of the current block
-            log.address.toLowerCase()
-          );
-          const high0: BigNumber = BigNumber.from(percent).mul(reserve0).div(100);
-          const high1: BigNumber = BigNumber.from(percent).mul(reserve1).div(100);
-          if (high0.lte(log.args.amount0) || high1.lte(log.args.amount1))
-            findings.push(createFinding(log, reserve0, reserve1));
-        }
+    const logs: LogDescription[] = txEvent.filterLog(abi.PAIR.format("full"));
+    for (let log of logs) {
+      if (await fetcher.isImpossiblePair(txEvent.blockNumber, log.address.toLowerCase())) {
+        const { reserve0, reserve1 } = await fetcher.getReserves(
+          txEvent.blockNumber - 1, // reserves at the beginning of the current block
+          log.address.toLowerCase()
+        );
+        const high0: BigNumber = BigNumber.from(percent).mul(reserve0).div(100);
+        const high1: BigNumber = BigNumber.from(percent).mul(reserve1).div(100);
+        if (high0.lte(log.args.amount0) || high1.lte(log.args.amount1))
+          findings.push(createFinding(log, reserve0, reserve1));
       }
+    }
 
-      return findings;
-    };
+    return findings;
+  };
 
 export default {
   handleTransaction: provideHandleTransaction(FETCHER, PERCENT),
