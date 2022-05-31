b4dbfbb2720491cccfe55ed0706fef545ecf9de9
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-05-31 15:49:13 -0300

    doc: Add mainnet and testnet tests descriptions

--- Umee-Bots/Large-Borrow/src/agent.config.ts
+++ Umee-Bots/Large-Borrow/src/agent.config.ts
@@ -8,4 +8,14 @@ const CONFIG: AgentConfig = {
   lendingPoolAddress: "0xcE744a9BAf573167B2CF138114BA32ed7De274Fa",
 };
 
+// Uncomment these lines for the mainnet test:
+
+// CONFIG.tvlPercentageThreshold = "0";
+// CONFIG.lendingPoolAddress = "0xcE744a9BAf573167B2CF138114BA32ed7De274Fa";
+
+// Uncomment these lines for the testnet test:
+
+// CONFIG.tvlPercentageThreshold = "50.5";
+// CONFIG.lendingPoolAddress = "0x6A28b9bafE44c2c5003Be84638e6317BDaB95251";
+
 export default CONFIG;
