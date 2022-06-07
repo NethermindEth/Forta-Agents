35abbe0c43228c1e91de4dcee8f8afdebabca5cb
Tayfun Sur <tayfunsur@gmail.com>
2022-06-07 11:08:30 +0300

    fix: test description and comment

--- Umee-Bots/Large-Deposit-Withdrawal/src/price.fetcher.spec.ts
+++ Umee-Bots/Large-Deposit-Withdrawal/src/price.fetcher.spec.ts
@@ -11,7 +11,7 @@ const ASSET_PRICE_TEST_CASE: [string, string, number, BigNumber][] = [
   [createAddress("0x1c"), createAddress("0x2c"), 7, BigNumber.from("5000000000000000005")],
 ];
 
-// format: [ethUsdFeedAddress, blockNumber, normalizedIncome]
+// format: [ethUsdFeedAddress, blockNumber, price]
 const ETH_PRICE_TEST_CASE: [string, number, BigNumber][] = [
   [createAddress("0x3a"), 4, BigNumber.from("2000000000000000000")],
   [createAddress("0x3b"), 5, BigNumber.from("600000000000000000")],
@@ -54,7 +54,7 @@ describe("Fetcher test suite", () => {
     }
   });
 
-  it("should fetch reserve normalized income correctly", async () => {
+  it("should fetch asset price correctly", async () => {
     for (let [umeeOracleAddress, assetAddress, blockNumber, price] of ASSET_PRICE_TEST_CASE) {
       const fetcher: Fetcher = new Fetcher(mockProvider as any);
 
