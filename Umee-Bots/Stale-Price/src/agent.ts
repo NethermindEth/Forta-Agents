3075b5582d8e90aec8470c4738187a5fc2acb2c6
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-06-08 18:59:16 -0300

    fix: Changes in agent logic and some fixes

--- Umee-Bots/Stale-Price/src/agent.ts
+++ Umee-Bots/Stale-Price/src/agent.ts
@@ -11,65 +11,83 @@ import {
 
 import CONFIG from "./agent.config";
 
-import utils, { AgentConfig, AssetData } from "./utils";
+import {
+  AgentConfig,
+  AssetData,
+  ASSET_SOURCE_UPDATED_ABI,
+  createFinding,
+  fetchLatestTimestamp,
+  getAssetData,
+} from "./utils";
 
 const assetsDataList: AssetData[] = [];
 
-export const provideInitialize =
-  (provider: ethers.providers.Provider): Initialize =>
-  async () => {
-    assetsDataList.push(...(await utils.getAssetData(CONFIG, provider)));
+export const getAssetsDataList = () => assetsDataList;
+export const resetAssetsDataList = () => (assetsDataList.length = 0);
+
+export const provideInitialize = (provider: ethers.providers.Provider, config: AgentConfig): Initialize => {
+  return async () => {
+    assetsDataList.push(...(await getAssetData(provider, config)));
   };
+};
 
 export const provideHandleTransaction = (
-  config: AgentConfig,
   provider: ethers.providers.Provider,
-  assetsDataList: AssetData[]
+  config: AgentConfig
 ): HandleTransaction => {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
-    const updateSourceLogs = txEvent.filterLog(utils.EVENT_ABI, config.umeeOracleAddress);
+    const updateSourceLogs = txEvent.filterLog([ASSET_SOURCE_UPDATED_ABI], config.umeeOracleAddress);
+
     await Promise.all(
       updateSourceLogs.map(async (logs) => {
-        const [asset, source] = logs.args;
+        let [asset, source] = logs.args.map((el) => el.toLowerCase());
+
         const assetsDataIndex = assetsDataList.findIndex((assetsData) => {
-          return assetsData.asset === asset.toLowerCase();
+          return assetsData.asset === asset;
         });
-        const lastUpdatedAt = await utils.fetchLatestTimestamp(source, provider);
+
+        const lastUpdatedAt = await fetchLatestTimestamp(source, provider, txEvent.blockNumber);
+
         if (assetsDataIndex === -1) {
           assetsDataList.push({
-            asset: asset.toLowerCase(),
+            asset,
             source,
-            referenceTimestamp: lastUpdatedAt,
+            referenceTimestamp: lastUpdatedAt || 0,
           });
         } else {
           assetsDataList[assetsDataIndex].source = source;
-          assetsDataList[assetsDataIndex].referenceTimestamp = lastUpdatedAt;
+          assetsDataList[assetsDataIndex].referenceTimestamp = lastUpdatedAt || 0;
         }
-        if (txEvent.block.timestamp - lastUpdatedAt >= config.threshold) {
-          findings.push(utils.createFinding({ asset, source, referenceTimestamp: lastUpdatedAt }));
+
+        if (lastUpdatedAt !== null && txEvent.block.timestamp - lastUpdatedAt >= config.threshold) {
+          findings.push(createFinding({ asset, source, referenceTimestamp: lastUpdatedAt }));
           const index = assetsDataIndex === -1 ? assetsDataList.length - 1 : assetsDataIndex;
-          assetsDataList[index].referenceTimestamp = lastUpdatedAt;
+          assetsDataList[index].referenceTimestamp = txEvent.block.timestamp;
         }
       })
     );
+
     return findings;
   };
 };
 
-export const provideHandleBlock = (
-  config: AgentConfig,
-  provider: ethers.providers.Provider,
-  assetsDataList: AssetData[]
-): HandleBlock => {
+export const provideHandleBlock = (provider: ethers.providers.Provider, config: AgentConfig): HandleBlock => {
   return async (blockEvent: BlockEvent) => {
     const findings: Finding[] = [];
+
     await Promise.all(
       assetsDataList.map(async (assetsData) => {
         if (blockEvent.block.timestamp - assetsData.referenceTimestamp >= config.threshold) {
-          const lastUpdatedAt = await utils.fetchLatestTimestamp(assetsData.source, provider);
+          const lastUpdatedAt = await fetchLatestTimestamp(assetsData.source, provider, blockEvent.blockNumber);
+
+          if (lastUpdatedAt === null) {
+            // if the source is invalid, it will eventually be updated and then handleTransaction will update it locally
+            return;
+          }
+
           if (blockEvent.block.timestamp - lastUpdatedAt >= config.threshold) {
-            findings.push(utils.createFinding({ ...assetsData, referenceTimestamp: lastUpdatedAt }));
+            findings.push(createFinding({ ...assetsData, referenceTimestamp: lastUpdatedAt }));
             assetsData.referenceTimestamp = blockEvent.block.timestamp;
           } else {
             assetsData.referenceTimestamp = lastUpdatedAt;
@@ -77,12 +95,13 @@ export const provideHandleBlock = (
         }
       })
     );
+
     return findings;
   };
 };
 
 export default {
-  initialize: provideInitialize(getEthersProvider()),
-  handleTransaction: provideHandleTransaction(CONFIG, getEthersProvider(), assetsDataList),
-  handleBlock: provideHandleBlock(CONFIG, getEthersProvider(), assetsDataList),
+  initialize: provideInitialize(getEthersProvider(), CONFIG),
+  handleTransaction: provideHandleTransaction(getEthersProvider(), CONFIG),
+  handleBlock: provideHandleBlock(getEthersProvider(), CONFIG),
 };
--- Umee-Bots/Stale-Price/src/utils.ts
+++ Umee-Bots/Stale-Price/src/utils.ts
@@ -1,5 +1,3 @@
-import { Interface } from "@ethersproject/abi";
-
 import { ethers, Finding, FindingSeverity, FindingType } from "forta-agent";
 
 export interface AgentConfig {
@@ -8,15 +6,15 @@ export interface AgentConfig {
   umeeOracleAddress: string;
 }
 
-const UMEE_FUNCTIONS_ABI: string[] = [
-  "function getReservesList() external view returns (address[] memory)",
-  "function getSourceOfAsset(address asset) external view returns (address)",
-  "function latestTimestamp() external view returns (uint256)",
-];
+export const GET_RESERVES_LIST_ABI = "function getReservesList() external view returns (address[] memory)";
+export const GET_SOURCE_OF_ASSET_ABI = "function getSourceOfAsset(address asset) external view returns (address)";
+export const LATEST_TIMESTAMP_ABI = "function latestTimestamp() external view returns (uint256)";
 
-const EVENT_ABI = ["event AssetSourceUpdated(address indexed asset, address indexed source)"];
+export const ASSET_SOURCE_UPDATED_ABI = "event AssetSourceUpdated(address indexed asset, address indexed source)";
 
-const FUNCTIONS_INTERFACE = new Interface([...UMEE_FUNCTIONS_ABI, ...EVENT_ABI]);
+const SOURCE_IFACE = new ethers.utils.Interface([LATEST_TIMESTAMP_ABI]);
+const LENDING_POOL_IFACE = new ethers.utils.Interface([GET_RESERVES_LIST_ABI]);
+const UMEE_ORACLE_IFACE = new ethers.utils.Interface([GET_SOURCE_OF_ASSET_ABI]);
 
 export interface AssetData {
   asset: string;
@@ -24,45 +22,52 @@ export interface AssetData {
   referenceTimestamp: number;
 }
 
-const fetchLatestTimestamp = async (source: string, provider: ethers.providers.Provider): Promise<number> => {
+export const fetchLatestTimestamp = async (
+  source: string,
+  provider: ethers.providers.Provider,
+  blockTag: number | string = "latest"
+): Promise<number | null> => {
   // use try/catch because source maybe a zero address or a erc20 token without chainLink aggregator support
   try {
-    const chainLinkAggregator = await new ethers.Contract(source, UMEE_FUNCTIONS_ABI, provider);
-    return (await chainLinkAggregator.latestTimestamp({ blockTag: "latest" })).toNumber();
+    const chainLinkAggregator = new ethers.Contract(source, SOURCE_IFACE, provider);
+    return (await chainLinkAggregator.latestTimestamp({ blockTag })).toNumber();
   } catch (error) {
-    return 0;
+    return null;
   }
 };
 
-const getAssetData = async (config: AgentConfig, provider: ethers.providers.Provider): Promise<AssetData[]> => {
-  const lendingPoolContract = new ethers.Contract(config.lendingPoolAddress, UMEE_FUNCTIONS_ABI, provider);
-  const umeeOracleContract = new ethers.Contract(config.umeeOracleAddress, UMEE_FUNCTIONS_ABI, provider);
-  const reservesList = await lendingPoolContract.getReservesList({ blockTag: "latest" });
-  const sources = await Promise.all(
-    reservesList.filter(async (asset: string) => {
-      // use try/catch because some asset may be without source
-      try {
-        return await umeeOracleContract.getSourceOfAsset(asset, { blockTag: "latest" });
-      } catch (error) {
-        return false;
-      }
-    })
-  );
+export const getAssetSource = async (
+  asset: string,
+  provider: ethers.providers.Provider,
+  config: AgentConfig
+): Promise<string> => {
+  const umeeOracleContract = new ethers.Contract(config.umeeOracleAddress, UMEE_ORACLE_IFACE, provider);
+
+  return await umeeOracleContract.getSourceOfAsset(asset, { blockTag: "latest" });
+};
+
+export const getAssetData = async (provider: ethers.providers.Provider, config: AgentConfig): Promise<AssetData[]> => {
+  const lendingPoolContract = new ethers.Contract(config.lendingPoolAddress, LENDING_POOL_IFACE, provider);
+
+  const reservesList: string[] = await lendingPoolContract.getReservesList({ blockTag: "latest" });
+
+  const sources = await Promise.all(reservesList.map((asset) => getAssetSource(asset, provider, config)));
 
   const assetData = await Promise.all(
     sources.map(async (source, index) => {
       const lastUpdatedAt = await fetchLatestTimestamp(source, provider);
       return {
         asset: reservesList[index].toLowerCase(),
-        source: sources[index],
-        referenceTimestamp: lastUpdatedAt,
+        source: sources[index].toLowerCase(),
+        referenceTimestamp: lastUpdatedAt || 0,
       };
     })
   );
+
   return assetData;
 };
 
-const createFinding = ({ asset, source, referenceTimestamp }: AssetData): Finding => {
+export const createFinding = ({ asset, source, referenceTimestamp }: AssetData): Finding => {
   return Finding.fromObject({
     name: "Detect stale price data from Chainlink aggregator",
     description: "price of a certain asset stops being updated from Chainlink aggregator",
@@ -72,13 +77,4 @@ const createFinding = ({ asset, source, referenceTimestamp }: AssetData): Findin
     severity: FindingSeverity.Medium,
     metadata: { asset, source, referenceTimestamp: referenceTimestamp.toString() },
   });
-};
-
-export default {
-  FUNCTIONS_INTERFACE,
-  EVENT_ABI,
-  UMEE_FUNCTIONS_ABI,
-  getAssetData,
-  fetchLatestTimestamp,
-  createFinding,
 };
