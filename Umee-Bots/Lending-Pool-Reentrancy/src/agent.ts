0f0ea014123a92b0741fd1ab01d8bd7318100c88
Yehia Tarek <yehiatarek67@gmail.com>
2022-06-08 17:40:26 +0200

    Update Umee-Bots/Lending-Pool-Reentrancy/src/agent.ts
    Co-authored-by: Franco Barpp Gomes <franco@nethermind.io>

--- Umee-Bots/Lending-Pool-Reentrancy/src/agent.ts
+++ Umee-Bots/Lending-Pool-Reentrancy/src/agent.ts
@@ -6,6 +6,7 @@ import utils, { AgentConfig } from "./utils";
 
 export const provideHandleTransaction = (config: AgentConfig): HandleTransaction => {
   const sigHashes = utils.getSigHashes(config.reentrancyBlacklist);
+  const lendingPoolAddress = config.lendingPoolAddress.toLowerCase();
 
   const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
@@ -13,7 +14,7 @@ export const provideHandleTransaction = (config: AgentConfig): HandleTransaction
     for (let i = 0; i < traces.length; i++) {
       const trace = traces[i];
       const depth = trace.traceAddress.length;
-      if (trace.action.to === config.lendingPoolAddress) {
+      if (trace.action.to === lendingPoolAddress) {
         let alerted = false;
         for (let j = i + 1; j < traces.length; j++) {
           if (traces[j].traceAddress.length <= depth) {
@@ -21,7 +22,7 @@ export const provideHandleTransaction = (config: AgentConfig): HandleTransaction
             break; // subtree ended, non reentrant call
           }
 
-          if (traces[j].action.to === config.lendingPoolAddress && !alerted) {
+          if (traces[j].action.to === lendingPoolAddress && !alerted) {
             const selector = (traces[j].action.input || "").slice(0, 10); // "0x" and first 4 bytes
             if (sigHashes.includes(selector)) {
               const initialCallSelector = (txEvent.transaction.data || "").slice(0, 10);
