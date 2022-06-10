7a8a2c7fa866d1e26df55c88894beff4cc1d6d7f
Tayfun Sur <tayfunsur@gmail.com>
2022-06-10 19:00:44 +0300

    feat: Implement bot

--- Umee-Bots/High-Gas-Use/src/agent.ts
+++ Umee-Bots/High-Gas-Use/src/agent.ts
@@ -1,68 +1,65 @@
+import BigNumber from "bignumber.js";
 import {
-  BlockEvent,
+  getTransactionReceipt,
+  Receipt,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
   FindingType,
 } from "forta-agent";
+import { CONFIG, AgentConfig } from "./agent.config";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+export const provideHandleTransaction = (
+  config: AgentConfig,
+  getTransactionReceipt: (txHash: string) => Promise<Receipt>
+): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+    const involvedAddresses: string[] = [];
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+    const sa = await getTransactionReceipt(txEvent.hash);
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+    config.monitoredAddresses.forEach((address) => {
+      if (address.toLowerCase() in txEvent.addresses) {
+        involvedAddresses.push(address);
+      }
+    });
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
+    if (involvedAddresses.length) {
+      const gasUsed = new BigNumber((await getTransactionReceipt(txEvent.hash)).gasUsed);
 
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+      if (gasUsed.isLessThan(config.mediumGasThreshold)) return findings;
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
       findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
+        Finding.from({
+          alertId: "UMEE-12",
+          name: "High amount of gas use",
+          description: "High amount of gas is used in transaction",
+          type: FindingType.Suspicious,
+          severity: getSeverity(gasUsed),
+          protocol: "Umee",
           metadata: {
-            to,
-            from,
+            from: txEvent.from,
+            to: txEvent.to || "",
+            monitoredAddresses: JSON.stringify(involvedAddresses),
+            gasUsed: gasUsed.toString(10),
           },
         })
       );
-      findingsCount++;
     }
-  });
-
-  return findings;
+    return findings;
+  };
 };
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+export const getSeverity = (gasUsed: BigNumber) => {
+  return gasUsed.isGreaterThan(CONFIG.criticalGasThreshold)
+    ? FindingSeverity.Critical
+    : gasUsed.isGreaterThan(CONFIG.highGasThreshold)
+    ? FindingSeverity.High
+    : FindingSeverity.Medium;
+};
 
 export default {
-  handleTransaction,
-  // handleBlock
+  handleTransaction: provideHandleTransaction(CONFIG, getTransactionReceipt),
 };
