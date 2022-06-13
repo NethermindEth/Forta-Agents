680df5d2e4ad5256ae7479044e4092738f38a7a9
Roberto Cantu <Roberto_Cantu@live.com>
2022-06-13 09:36:01 -0500

    chore: Remove 'multicall2' property from NetworkManager

--- TraderJoe-Bots/Large-Swap/src/network.spec.ts
+++ TraderJoe-Bots/Large-Swap/src/network.spec.ts
@@ -2,43 +2,37 @@ import { createAddress } from "forta-agent-tools/lib/tests";
 import NetworkManager from "./network";
 import NetworkData from "./network";
 
-// Format:[network, multicall2, factory, pairInitCodeHash][];
-const TEST_CASES: [number, string, Record<number, string>, string, string][] = [
+// Format:[network, multicall2Data, factory, pairInitCodeHash][];
+const TEST_CASES: [number, Record<number, string>, string, string][] = [
   [
     11,
-    createAddress("0xa2a"),
     { 11: createAddress("0xa2a") },
     createAddress("0xb1a"),
     "0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91",
   ],
   [
     22,
-    createAddress("0xa3b"),
     { 22: createAddress("0xa3b") },
     createAddress("0xb2a"),
     "0xea2e4d8ff7b84771dace7688751971197f2a4578c0298c78d11d93165de73773",
   ],
   [
     33,
-    createAddress("0xa4c"),
     { 33: createAddress("0xa4c") },
     createAddress("0xb3a"),
     "0x0bbca9af0511ad1a1da383135cf3a8d2af2a4578c0298c78d11d93165de73773",
   ],
 ];
 
-const generateNetworkMap = (
-  data: [number, string, Record<number, string>, string, string]
-): Record<number, NetworkData> => {
+const generateNetworkMap = (data: [number, Record<number, string>, string, string]): Record<number, NetworkData> => {
   // generates a NetworkMap for one test case.
   let networkMap: Record<number, NetworkData> = {};
 
   networkMap[data[0]] = {
     chainId: data[0],
-    multicall2: data[1],
-    multicall2Data: data[2],
-    factory: data[3],
-    pairInitCodeHash: data[4],
+    multicall2Data: data[1],
+    factory: data[2],
+    pairInitCodeHash: data[3],
   } as NetworkData;
 
   return networkMap;
@@ -49,12 +43,11 @@ describe("NetworkManager test suite", () => {
     for (let testCase of TEST_CASES) {
       const networkMap: Record<number, NetworkData> = generateNetworkMap(testCase);
 
-      const [network, multicall2, multicall2Data, factory, pairInitCodeHash] = testCase;
+      const [network, multicall2Data, factory, pairInitCodeHash] = testCase;
       const networkManager = new NetworkManager(networkMap);
       networkManager.setNetwork(network);
 
       expect(networkManager.chainId).toStrictEqual(network);
-      expect(networkManager.multicall2).toStrictEqual(multicall2);
       expect(networkManager.multicall2Data).toStrictEqual(multicall2Data);
       expect(networkManager.factory).toStrictEqual(factory);
       expect(networkManager.pairInitCodeHash).toStrictEqual(pairInitCodeHash);
--- TraderJoe-Bots/Large-Swap/src/network.ts
+++ TraderJoe-Bots/Large-Swap/src/network.ts
@@ -1,6 +1,5 @@
 interface NetworkData {
   chainId: number;
-  multicall2: string;
   multicall2Data: Record<number, string>;
   factory: string;
   pairInitCodeHash: string;
@@ -8,7 +7,6 @@ interface NetworkData {
 
 const AVAX_MAINNET_DATA: NetworkData = {
   chainId: 43114,
-  multicall2: "0xed386Fe855C1EFf2f843B910923Dd8846E45C5A4",
   multicall2Data: { 43114: "0xed386Fe855C1EFf2f843B910923Dd8846E45C5A4" },
   factory: "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10",
   pairInitCodeHash: "0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91",
@@ -16,7 +14,6 @@ const AVAX_MAINNET_DATA: NetworkData = {
 
 const KOVAN_TESTNET_DATA: NetworkData = {
   chainId: 42,
-  multicall2: "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696",
   multicall2Data: { 42: "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696" },
   factory: "0x3CAfAF7cA21ccfeB3B09CCC8a7e03109d207CDc4",
   pairInitCodeHash: "0xea2e4d8ff7b84771dace7688751971197f2a4578c0298c78d11d93165de73773",
@@ -29,7 +26,6 @@ export const NETWORK_MAP: Record<number, NetworkData> = {
 
 export default class NetworkManager implements NetworkData {
   public chainId: number;
-  public multicall2: string;
   public multicall2Data: Record<number, string>;
   public factory: string;
   public pairInitCodeHash: string;
@@ -37,18 +33,16 @@ export default class NetworkManager implements NetworkData {
 
   constructor(networkMap: Record<number, NetworkData> = NETWORK_MAP) {
     this.chainId = 0;
-    this.multicall2 = "";
     this.multicall2Data = {};
-    this.factory = "";
+    this.factory = "0x0000000000000000000000000000000000000000";
     this.pairInitCodeHash = "";
     this.networkMap = networkMap;
   }
 
   public setNetwork(networkId: number) {
     try {
-      const { chainId, multicall2, multicall2Data, factory, pairInitCodeHash } = this.networkMap[networkId];
+      const { chainId, multicall2Data, factory, pairInitCodeHash } = this.networkMap[networkId];
       this.chainId = chainId;
-      this.multicall2 = multicall2;
       this.multicall2Data = multicall2Data;
       this.factory = factory;
       this.pairInitCodeHash = pairInitCodeHash;
