32bb47c985bf22911f81f47b547f9aacecd39902
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-06-17 00:14:32 -0300

    fix: Specify address type of event parameters

--- Balancer-Bots/Large-Swap/src/constants.ts
+++ Balancer-Bots/Large-Swap/src/constants.ts
@@ -1,3 +1,3 @@
 export const SWAP_ABI =
-  "event Swap(bytes32 indexed poolId, IERC20 indexed tokenIn, IERC20 indexed tokenOut, uint256 amountIn, uint256 amountOut)";
-export const BALANCE_OF_ABI = "function balanceOf(address account) returns (uint256)";
+  "event Swap(bytes32 indexed poolId, address indexed tokenIn, address indexed tokenOut, uint256 amountIn, uint256 amountOut)";
+export const BALANCE_OF_ABI = "function balanceOf(address account) public view returns (uint256)";
