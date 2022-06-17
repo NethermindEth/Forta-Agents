6a5fb0913e0bf5afc44d3f81cd2232b243929434
Yehia Tarek <yehia@startfi.io>
2022-06-17 13:35:01 +0200

    feat: add bot logic

---
+++ GMX-Bots/Detect-many-closing/src/agent.ts
@@ -0,0 +1,57 @@
+import { NetworkData, networkData } from "./network";
+import { ethers, Finding, getEthersProvider, HandleTransaction, Log, TransactionEvent } from "forta-agent";
+import { NetworkManager } from "forta-agent-tools";
+
+import utils from "./utils";
+
+const networkManager = new NetworkManager(networkData);
+
+export const provideInitialize = () => {
+  return async () => {
+    const provider = getEthersProvider();
+    await networkManager.init(provider);
+  };
+};
+
+export const provideHandleTransaction =
+  (networkManager: NetworkManager<NetworkData>, provider: ethers.providers.JsonRpcProvider): HandleTransaction =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    const currentBlockNumber = txEvent.block.number;
+    const filter = {
+      fromBlock: currentBlockNumber - networkManager.get("blockNumber"),
+      toBlock: currentBlockNumber,
+      address: networkManager.get("address"),
+      topics: [utils.EVENTS_IFACE.getEventTopic(utils.DECREASE_POSITION_EVENT)],
+    };
+
+    const increasePositionLogs = await provider.getLogs(filter);
+
+    if (!increasePositionLogs) return findings; // getLogs return undefined if result is empty
+
+    const accountByNoOfClosing: Map<string, number> = new Map();
+
+    increasePositionLogs.forEach((log: Log) => {
+      const currentEvent = utils.EVENTS_IFACE.decodeEventLog(utils.DECREASE_POSITION_EVENT, log.data, log.topics);
+      const noOfClosing = accountByNoOfClosing.get(currentEvent[1]);
+      if (noOfClosing) {
+        accountByNoOfClosing.set(currentEvent[1], noOfClosing + 1);
+      } else {
+        accountByNoOfClosing.set(currentEvent[1], 1);
+      }
+    });
+
+    accountByNoOfClosing.forEach((noOfClosing, account) => {
+      if (noOfClosing >= networkManager.get("positionsNumber")) {
+        findings.push(utils.createFinding(account, noOfClosing.toString()));
+      }
+    });
+
+    return findings;
+  };
+
+export default {
+  initialize: provideInitialize(),
+  handleTransaction: provideHandleTransaction(networkManager, utils.provider),
+};
