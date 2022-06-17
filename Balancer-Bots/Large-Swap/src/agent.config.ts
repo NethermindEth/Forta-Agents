fd30140429152420232bc6ca00b7e644159ff2e6
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-06-17 16:51:01 -0300

    chore: Minor styling fix

--- Balancer-Bots/Large-Swap/src/agent.config.ts
+++ Balancer-Bots/Large-Swap/src/agent.config.ts
@@ -8,10 +8,12 @@ const CONFIG: AgentConfig = {
     // Minimum swapped token amount relative to the Vault TVL percentage that leads to a finding emission (in %)
     tvlPercentageThreshold: "50.5",
   },
+
   [Network.POLYGON]: {
     vaultAddress: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
     tvlPercentageThreshold: "50.5",
   },
+
   [Network.ARBITRUM]: {
     vaultAddress: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
     tvlPercentageThreshold: "50.5",
