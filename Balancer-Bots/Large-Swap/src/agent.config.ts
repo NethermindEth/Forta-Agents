2483a994e0a7e7ee1fe23fc2b952ee58875a3ba1
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-06-16 23:04:24 -0300

    feat: Bot logic

--- Balancer-Bots/Large-Swap/src/agent.config.ts
+++ Balancer-Bots/Large-Swap/src/agent.config.ts
@@ -1,5 +1,20 @@
-import { AgentConfig } from "./utils";
+import { AgentConfig, Network } from "./utils";
 
-const CONFIG: AgentConfig = {};
+const CONFIG: AgentConfig = {
+  [Network.ETHEREUM_MAINNET]: {
+    // Address of the Vault contract
+    vaultAddress: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    // Minimum swapped token amount relative to the Vault TVL percentage that leads to a finding emission (in %)
+    tvlPercentageThreshold: "50.5",
+  },
+  [Network.POLYGON]: {
+    vaultAddress: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    tvlPercentageThreshold: "50.5",
+  },
+  [Network.ARBITRUM]: {
+    vaultAddress: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    tvlPercentageThreshold: "50.5",
+  },
+};
 
 export default CONFIG;
--- Balancer-Bots/Large-Swap/src/constants.ts
+++ Balancer-Bots/Large-Swap/src/constants.ts
@@ -1,1 +1,3 @@
-
+export const SWAP_ABI =
+  "event Swap(bytes32 indexed poolId, IERC20 indexed tokenIn, IERC20 indexed tokenOut, uint256 amountIn, uint256 amountOut)";
+export const BALANCE_OF_ABI = "function balanceOf(address account) returns (uint256)";
