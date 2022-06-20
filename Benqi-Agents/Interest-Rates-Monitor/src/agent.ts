d627953dc217e89d8f357f12f265ae25694319cd
Yasmine Bachi <gy_bachi@esi.dz>
2022-06-20 10:33:59 +0100

    update bot logic to use the fetcher, removed QI_TOKENS from logic

--- Benqi-Agents/Interest-Rates-Monitor/src/agent.ts
+++ Benqi-Agents/Interest-Rates-Monitor/src/agent.ts
@@ -1,51 +1,67 @@
 import { BigNumber } from "ethers";
 import { findingCase, createFinding } from "./findings";
 import { BlockEvent, Finding, HandleBlock, getEthersProvider } from "forta-agent";
-import { QI_TOKENS, POC_QI_TOKENS, testnetMode } from "./utils";
+import { COMPTROLLER_ADDR, COMPTROLLER_IFACE, THRESHOLDS } from "./utils";
 import Fetcher from "./fetcher";
+import MarketsFetcher from "./markets.fetcher";
+
+const marketsFetcher: MarketsFetcher = new MarketsFetcher(getEthersProvider(), COMPTROLLER_ADDR);
+
+const initialize = async () => {
+  await marketsFetcher.getMarkets();
+};
 
 export const provideHandleBlock =
   (
-    fetcher: Fetcher,
-    qiTokens: [
-      name: string,
-      address: string,
-      lowerSupply: BigNumber,
-      upperSupply: BigNumber,
-      lowerBorrow: BigNumber,
-      lowerBorrow: BigNumber
-    ][]
+    thresholds: { supply: BigNumber[]; borrow: BigNumber[] },
+    marketsFetcher: MarketsFetcher,
+    fetcher: Fetcher
   ): HandleBlock =>
   async (blockEvent: BlockEvent) => {
     const findings: Finding[] = [];
     const block: number = blockEvent.blockNumber;
 
+    // Check if any new QiTokens have been added to Comptroller.
+    const filter = {
+      address: COMPTROLLER_ADDR,
+      blockHash: blockEvent.blockHash,
+      topics: [COMPTROLLER_IFACE.getEventTopic("MarketListed")],
+    };
+    (await marketsFetcher.provider.getLogs(filter)).forEach((log) => {
+      // update the markets set with the new markets.
+      marketsFetcher.updateMarkets(
+        COMPTROLLER_IFACE.decodeEventLog(COMPTROLLER_IFACE.getEvent("MarketListed"), log.data).qiToken
+      );
+    });
+
+    const markets = Array.from(marketsFetcher.markets);
+
     //Fetch and store supply and borrow interest rates for every Qi token
     const qiTokenSupplyInterestRate: BigNumber[] = await Promise.all(
-      qiTokens.map((qiToken) => fetcher.getSupplyInterestRates(block, qiToken[1]))
+      markets.map((qiToken) => fetcher.getSupplyInterestRates(block, qiToken))
     );
     const qiTokenBorrowInterestRate: BigNumber[] = await Promise.all(
-      qiTokens.map((qiToken) => fetcher.getBorrowInterestRates(block, qiToken[1]))
+      markets.map((qiToken) => fetcher.getBorrowInterestRates(block, qiToken))
     );
 
     //Check conditions to create finding
     qiTokenSupplyInterestRate.forEach((supplyRate, i) => {
       //Lower supply threshold check
-      if (supplyRate.lt(qiTokens[i][2])) {
-        findings.push(createFinding(qiTokens[i][0], qiTokens[i][1], supplyRate, qiTokens[i][2], findingCase[0]));
+      if (supplyRate.lt(thresholds.supply[0])) {
+        findings.push(createFinding(markets[i], supplyRate, thresholds.supply[0], findingCase[0]));
       } //Upper supply threshold check
-      else if (supplyRate.gt(qiTokens[i][3])) {
-        findings.push(createFinding(qiTokens[i][0], qiTokens[i][1], supplyRate, qiTokens[i][3], findingCase[1]));
+      else if (supplyRate.gt(thresholds.supply[1])) {
+        findings.push(createFinding(markets[i], supplyRate, thresholds.supply[1], findingCase[1]));
       }
     });
 
     qiTokenBorrowInterestRate.forEach((borrowRate, i) => {
       //Lower borrow threshold check
-      if (borrowRate.lt(qiTokens[i][4])) {
-        findings.push(createFinding(qiTokens[i][0], qiTokens[i][1], borrowRate, qiTokens[i][4], findingCase[2]));
+      if (borrowRate.lt(thresholds.borrow[0])) {
+        findings.push(createFinding(markets[i], borrowRate, thresholds.borrow[0], findingCase[2]));
       } //Upper borrow threshold check
-      else if (borrowRate.gt(qiTokens[i][5])) {
-        findings.push(createFinding(qiTokens[i][0], qiTokens[i][1], borrowRate, qiTokens[i][5], findingCase[3]));
+      else if (borrowRate.gt(thresholds.borrow[1])) {
+        findings.push(createFinding(markets[i], borrowRate, thresholds.borrow[1], findingCase[3]));
       }
     });
 
@@ -53,5 +69,6 @@ export const provideHandleBlock =
   };
 
 export default {
-  handleBlock: provideHandleBlock(new Fetcher(getEthersProvider()), testnetMode ? POC_QI_TOKENS : QI_TOKENS),
+  initialize,
+  handleBlock: provideHandleBlock(THRESHOLDS, marketsFetcher, new Fetcher(getEthersProvider())),
 };
--- Benqi-Agents/Interest-Rates-Monitor/src/utils.ts
+++ Benqi-Agents/Interest-Rates-Monitor/src/utils.ts
@@ -1,155 +1,22 @@
-import { BigNumber } from "ethers";
+import { BigNumber, utils } from "ethers";
 
-export const testnetMode: boolean = false;
+export const COMPTROLLER_ADDR = "0x486Af39519B4Dc9a7fCcd318217352830E8AD9b4";
+// use the address below for testnet
+//export const COMPTROLLER_ADDR = "0x986a347285dc266FB483F7eFDa68F221F3F1AF05";
 
-//QiTokens names, addresses and thresholds
-export const QI_TOKENS: [
-  name: string,
-  address: string,
-  lowerSupply: BigNumber,
-  upperSupply: BigNumber,
-  lowerBorrow: BigNumber,
-  upperBorrow: BigNumber
-][] = [
-  [
-    "qiAVAX",
-    "0x5C0401e81Bc07Ca70fAD469b451682c0d747Ef1c",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiBTC",
-    "0xe194c4c5aC32a3C9ffDb358d9Bfd523a0B6d1568",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiETH",
-    "0x334AD834Cd4481BB02d09615E7c11a00579A7909",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiUSDT",
-    "0xc9e5999b8e75C3fEB117F6f73E664b9f3C8ca65C",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiLINK",
-    "0x4e9f683A27a6BdAD3FC2764003759277e93696e6",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiUSDC",
-    "0xBEb5d47A3f720Ec0a390d04b4d41ED7d9688bC7F",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiDAI",
-    "0x835866d37AFB8CB8F8334dCCdaf66cf01832Ff5D",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiQI",
-    "0x35Bd6aedA81a7E5FC7A7832490e71F757b0cD9Ce",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(2200000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(2200000000), // upper borrow rate threshold
-  ],
-];
+const COMPTROLLER_ABI = ["function getAllMarkets() view returns (address[])", "event MarketListed(address qiToken)"];
+export const COMPTROLLER_IFACE = new utils.Interface(COMPTROLLER_ABI);
 
-export const POC_QI_TOKENS: [
-  name: string,
-  address: string,
-  lowerSupply: BigNumber,
-  upperSupply: BigNumber,
-  lowerBorrow: BigNumber,
-  upperBorrow: BigNumber
-][] = [
-  [
-    "qiAVAX",
-    "0x22fdC4d8eEea0d219E15025d2BCDe38d948e9A13",
+export const THRESHOLDS = {
+  supply: [
     BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(21000000000), // upper supply rate threshold
+    BigNumber.from(6200000000), // upper supply rate threshold
+  ],
+  borrow: [
     BigNumber.from(10000), // lower borrow rate threshold
     BigNumber.from(6200000000), // upper borrow rate threshold
   ],
-  [
-    "qiBTC",
-    "0x106958DF060b0Bb19223C2666BF57fa9f065B6A0",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiETH",
-    "0x256eFdbBa8D5d2873027D9f8A1bad4BdcE51321c",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiUSDT",
-    "0x5785aFe403E0852E8b57A753529713045b43f886",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiLINK",
-    "0x8e6088d50779B5E7e5206bc5af937C618d5B021c",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiUSDC",
-    "0x0F654B634687e4c0d8E8540c3E7458218b933aa2",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiDAI",
-    "0x4842F6f7cD05BB09F190De02Af0564b323846B59",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-  [
-    "qiQI",
-    "0xBA1d29aDebbfb8f79eCa81a44f8354539eF32ADB",
-    BigNumber.from(10000), // lower supply rate threshold
-    BigNumber.from(62000000000), // upper supply rate threshold
-    BigNumber.from(10000), // lower borrow rate threshold
-    BigNumber.from(6200000000), // upper borrow rate threshold
-  ],
-];
+};
 
 export const QI_TOKENS_ABI: string[] = [
   "function borrowRatePerTimestamp() external view returns (uint)",
