aff91f9715a31be9ad10d10c14bc45b26038e0eb
Yasmine Bachi <36101876+yasmine9@users.noreply.github.com>
2022-06-20 12:30:50 +0100

    Update TestComptroller.sol

--- Benqi-Agents/Interest-Rates-Monitor/PoC/TestComptroller.sol
+++ Benqi-Agents/Interest-Rates-Monitor/PoC/TestComptroller.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TestMarkets {
+contract TestComptroller {
     address[] public allMarkets;
 
     function getAllMarkets() public view returns (address[] memory) {
