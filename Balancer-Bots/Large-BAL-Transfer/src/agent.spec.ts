b4a0b7f152a813636e77311022d32506816ebc30
Tayfun Sur <tayfunsur@gmail.com>
2022-06-23 14:17:32 +0300

    tests: Add test suite

--- Balancer-Bots/Large-BAL-Transfer/src/agent.spec.ts
+++ Balancer-Bots/Large-BAL-Transfer/src/agent.spec.ts
@@ -1,74 +1,213 @@
 import {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  HandleTransaction,
-  createTransactionEvent,
   ethers,
+  Finding,
+  FindingSeverity,
+  FindingType,
+  HandleBlock,
+  Network,
 } from "forta-agent";
-import agent, {
-  ERC20_TRANSFER_EVENT,
-  TETHER_ADDRESS,
-  TETHER_DECIMALS,
-} from "./agent";
+import { BigNumber } from "bignumber.js";
+import { NetworkManager } from "forta-agent-tools";
+import {
+  TestBlockEvent,
+  createAddress as padAddress,
+  MockEthersProvider,
+} from "forta-agent-tools/lib/tests";
+import { provideHandleBlock } from "./agent";
+import { TOKEN_ABI, EVENT } from "./constants";
+import { AgentConfig, NetworkData, SmartCaller } from "./utils";
 
-describe("high tether transfer agent", () => {
-  let handleTransaction: HandleTransaction;
-  const mockTxEvent = createTransactionEvent({} as any);
+const createAddress = (address: string): string =>
+  ethers.utils.getAddress(padAddress(address.toLowerCase()));
 
-  beforeAll(() => {
-    handleTransaction = agent.handleTransaction;
+const VAULT_IFACE = new ethers.utils.Interface(EVENT);
+const IRRELEVANT_IFACE = new ethers.utils.Interface(["event Event()"]);
+const TOKEN_IFACE = new ethers.utils.Interface(TOKEN_ABI);
+const BAL_ADDRESS = createAddress("0x1");
+
+const getTransferLog = (
+  emitter: string,
+  block: number,
+  from: string,
+  to: string,
+  value: ethers.BigNumberish
+): ethers.providers.Log => {
+  return {
+    address: emitter,
+    blockNumber: block,
+    ...VAULT_IFACE.encodeEventLog(VAULT_IFACE.getEvent("Transfer"), [
+      to,
+      from,
+      ethers.BigNumber.from(value),
+    ]),
+  } as ethers.providers.Log;
+};
+
+const getIrrelevantEvent = (emitter: string, block: number): ethers.providers.Log => {
+  return {
+    address: emitter,
+    blockNumber: block,
+    ...IRRELEVANT_IFACE.encodeEventLog(IRRELEVANT_IFACE.getEvent("Event"), []),
+  } as ethers.providers.Log;
+};
+
+const createFinding = (
+  from: string,
+  to: string,
+  value: BigNumber,
+  percentage: BigNumber
+): Finding => {
+  return Finding.from({
+    name: "Large BAL Transfer",
+    description: "Large amount of BAL transferred",
+    alertId: "BAL-7",
+    protocol: "Balancer",
+    severity: FindingSeverity.Low,
+    type: FindingType.Info,
+    metadata: {
+      from,
+      to,
+      value: value.toString(10),
+      percentage: percentage.toString(10),
+    },
+  });
+};
+
+const DEFAULT_CONFIG: AgentConfig = {
+  [Network.MAINNET]: {
+    balToken: BAL_ADDRESS,
+    threshold: "10",
+  },
+};
+
+describe("Large Bal Token Transfer Bot Test Suite", () => {
+  let provider: ethers.providers.Provider;
+  let mockProvider: MockEthersProvider;
+  let networkManager: NetworkManager<NetworkData>;
+  let handleBlock: HandleBlock;
+
+  beforeEach(() => {
+    SmartCaller.clearCache();
+    mockProvider = new MockEthersProvider();
+    provider = mockProvider as unknown as ethers.providers.Provider;
+
+    mockProvider.addCallTo(BAL_ADDRESS, 1, TOKEN_IFACE, "totalSupply", {
+      inputs: [],
+      outputs: ["50000000000000000000000000"],
+    });
+
+    networkManager = new NetworkManager(DEFAULT_CONFIG, Network.MAINNET);
+    handleBlock = provideHandleBlock(provider, networkManager);
   });
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if there are no Tether transfers", async () => {
-      mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
+  it("should return empty findings with an empty logs list", async () => {
+    const blockEvent = new TestBlockEvent();
 
-      const findings = await handleTransaction(mockTxEvent);
+    expect(await handleBlock(blockEvent)).toStrictEqual([]);
+  });
 
-      expect(findings).toStrictEqual([]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS
-      );
-    });
+  it("should ignore target events emitted from another contract", async () => {
+    const blockEvent = new TestBlockEvent().setNumber(1);
 
-    it("returns a finding if there is a Tether transfer over 10,000", async () => {
-      const mockTetherTransferEvent = {
-        args: {
-          from: "0xabc",
-          to: "0xdef",
-          value: ethers.BigNumber.from("20000000000"), //20k with 6 decimals
-        },
-      };
-      mockTxEvent.filterLog = jest
-        .fn()
-        .mockReturnValue([mockTetherTransferEvent]);
+    mockProvider.addLogs([
+      getTransferLog(
+        createAddress("0xdead"),
+        1,
+        createAddress("0x2"),
+        createAddress("0x3"),
+        "6000000000000000000000000"
+      ),
+    ]);
 
-      const findings = await handleTransaction(mockTxEvent);
+    expect(await handleBlock(blockEvent)).toStrictEqual([]);
+    expect(mockProvider.call).toHaveBeenCalledTimes(0);
+  });
 
-      const normalizedValue = mockTetherTransferEvent.args.value.div(
-        10 ** TETHER_DECIMALS
-      );
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to: mockTetherTransferEvent.args.to,
-            from: mockTetherTransferEvent.args.from,
-          },
-        }),
-      ]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS
-      );
-    });
+  it("should ignore other events emitted from target contract", async () => {
+    const blockEvent = new TestBlockEvent().setNumber(1);
+
+    mockProvider.addLogs([getIrrelevantEvent(BAL_ADDRESS, 1)]);
+
+    expect(await handleBlock(blockEvent)).toStrictEqual([]);
+    expect(mockProvider.call).toHaveBeenCalledTimes(0);
+  });
+
+  it("should ignore events emitted in other blocks", async () => {
+    const blockEvent = new TestBlockEvent().setNumber(1);
+
+    mockProvider.addLogs([
+      getTransferLog(
+        BAL_ADDRESS,
+        0,
+        createAddress("0x2"),
+        createAddress("0x3"),
+        "5000000000000000000000000"
+      ),
+    ]);
+
+    expect(await handleBlock(blockEvent)).toStrictEqual([]);
+    expect(mockProvider.call).toHaveBeenCalledTimes(0);
+  });
+
+  it("should not return findings for transfers that are not large", async () => {
+    const blockEvent = new TestBlockEvent().setNumber(1);
+
+    mockProvider.addLogs([
+      getTransferLog(
+        BAL_ADDRESS,
+        1,
+        createAddress("0x2"),
+        createAddress("0x3"),
+        "4900000000000000000000000"
+      ),
+    ]);
+
+    expect(await handleBlock(blockEvent)).toStrictEqual([]);
+    expect(mockProvider.call).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return findings for transfers that are large", async () => {
+    const blockEvent = new TestBlockEvent().setNumber(1);
+
+    mockProvider.addLogs([
+      getTransferLog(
+        BAL_ADDRESS,
+        1,
+        createAddress("0x2"),
+        createAddress("0x3"),
+        "5000000000000000000000000"
+      ),
+      getTransferLog(
+        BAL_ADDRESS,
+        1,
+        createAddress("0x4"),
+        createAddress("0x5"),
+        "6000000000000000000000000"
+      ),
+    ]);
+
+    const percentage1 = new BigNumber("5000000000000000000000000")
+      .multipliedBy(100)
+      .dividedBy(new BigNumber("50000000000000000000000000"));
+
+    const percentage2 = new BigNumber("6000000000000000000000000")
+      .multipliedBy(100)
+      .dividedBy(new BigNumber("50000000000000000000000000"));
+
+    expect(await handleBlock(blockEvent)).toStrictEqual([
+      createFinding(
+        createAddress("0x2"),
+        createAddress("0x3"),
+        new BigNumber("5000000000000000000000000"),
+        percentage1
+      ),
+      createFinding(
+        createAddress("0x4"),
+        createAddress("0x5"),
+        new BigNumber("6000000000000000000000000"),
+        percentage2
+      ),
+    ]);
+    expect(mockProvider.call).toHaveBeenCalledTimes(1);
   });
 });
