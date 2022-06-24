cd455c1aeeca508efde267cb3e79022631ce7b23
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-24 16:29:54 +0100

    feat: add new random generator transaction handler to Promise.all

--- PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/agent.ts
+++ PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/agent.ts
@@ -8,56 +8,27 @@ import {
   FindingType,
 } from "forta-agent";
 
-import {
-  events,
-  PanCakeSwapLottery_Address
-} from "./agent.config"
+import newRandomGenerator from "./new.random.generator"
 
 let findingsCount = 0;
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
-  const findings: Finding[] = [];
 
-  console.log(txEvent.logs)
+  const findings = (
+    await Promise.all([
+      newRandomGenerator.handleTransaction(txEvent),
+    ])
+  ).flat();
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
-
-  // filter the transaction logs for Tether transfer events
-  const newRandomGeneratorEvents = txEvent.filterLog(
-    "event TicketsPurchase(address,uint256,uint256)", //TicketsPurchase event for testing only, should be NewRandomGenerator event
-    PanCakeSwapLottery_Address
-  );
-
-  //tx for testing TicketsPurchase: 0x388af12be44d7b6933be06d0ed744d9b5737e9371a24c5e8334c041e2ca8737f
-
-  console.log(newRandomGeneratorEvents)
-
-  newRandomGeneratorEvents.forEach((newRandomGeneratorEvent) => {
-    // extract transfer event arguments
-      findings.push(
-        Finding.fromObject({
-          name: "New Random Generator",
-          description: `: ${1}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            
-          },
-        })
-      );
-      findingsCount++;
-  });
+  findingsCount++;
+    
 
   return findings;
 };
 
-
-
 export default {
-  handleTransaction,
-  
+  handleTransaction
+ 
 };
