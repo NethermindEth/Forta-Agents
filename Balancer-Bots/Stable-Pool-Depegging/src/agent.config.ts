46c1083b30f689043fc009a5ae8a749d2a58e39c
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-06-25 00:45:46 -0300

    fix: Add Arbitrum to default configuration & add stable pool addresses

--- Balancer-Bots/Stable-Pool-Depegging/src/agent.config.ts
+++ Balancer-Bots/Stable-Pool-Depegging/src/agent.config.ts
@@ -21,13 +21,21 @@ const CONFIG: AgentConfig = {
   },
 
   [Network.POLYGON]: {
-    stablePoolAddresses: ["0x06df3b2bbb68adc8b0e302443692037ed9f91b42"],
+    stablePoolAddresses: [
+      "0x6641a8c1d33bd3dec8dd85e69c63cafb5bf36388",
+      "0x9f19a375709baf0e8e35c2c5c65aca676c4c7191",
+      "0xfeadd389a5c427952d8fdb8057d6c8ba1156cc56",
+      "0x06df3b2bbb68adc8b0e302443692037ed9f91b42",
+    ],
     absoluteThreshold: "10000",
     percentageThreshold: "40.5",
   },
 
-  [Network.POLYGON]: {
-    stablePoolAddresses: ["0x06df3b2bbb68adc8b0e302443692037ed9f91b42"],
+  [Network.ARBITRUM]: {
+    stablePoolAddresses: [
+      "0x0510cCF9eB3AB03C1508d3b9769E8Ee2CFd6FDcF",
+      "0x1533A3278f3F9141d5F820A184EA4B017fce2382",
+    ],
     absoluteThreshold: "10000",
     percentageThreshold: "40.5",
   },
