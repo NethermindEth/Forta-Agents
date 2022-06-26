21daae059e59f62cfe0dda3a673a9abf10386b97
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-26 22:36:12 +0100

    tests: added test if there are findings

--- PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/new.random.generator.spec.ts
+++ PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/new.random.generator.spec.ts
@@ -1,10 +1,15 @@
 import {
+    Finding,
+    FindingSeverity,
+    FindingType,
     HandleTransaction,
     createTransactionEvent,
+    ethers
   } from "forta-agent"
   
 import agent from "./new.random.generator"
 import {events, PanCakeSwapLottery_Address} from "./agent.config"
+import newRandomGenerator from "./new.random.generator"
   
   describe("PancakeSwap Lottery", () => {
     let handleTransaction: HandleTransaction
@@ -32,7 +37,44 @@ import {events, PanCakeSwapLottery_Address} from "./agent.config"
   
   
       })
-    })
+
+      it("returns findings if there are NewRandomGenerator events emmited ", async () => {
+  
+        const mockSwapEvent = {
+            args: {
+              to: "0x0123",
+              from: "0x0abc",
+              randomGenerator: "random generator"
+            }
+          };
+
+          mockTxEvent.filterLog = jest
+            .fn()
+            .mockReturnValue([mockSwapEvent]);
     
+          const findings = await handleTransaction(mockTxEvent);
+    
+          const {to, from, randomGenerator} = mockSwapEvent.args;
+    
+          expect(findings).toStrictEqual([
+            Finding.fromObject({
+                name: "New Random Generator",
+                description: `Random Number Generator changed`,
+                alertId: "FORTA-1",
+                severity: FindingSeverity.Info,
+                type: FindingType.Info,
+                metadata: {
+                  to,
+                  from,
+                  randomGenerator
+                },
+              }),
+          ]);
+          expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+          expect(mockTxEvent.filterLog).toHaveBeenCalledWith(events.NewRandomGenerator, PanCakeSwapLottery_Address);
+  
+      })
+    })
+
   })
   
