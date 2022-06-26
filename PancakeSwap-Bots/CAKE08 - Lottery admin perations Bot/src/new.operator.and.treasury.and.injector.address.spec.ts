fbea59fc7f8b534d537fa197109f99422624abfb
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-26 23:45:17 +0100

    tests: added test if there are findings

--- PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/new.operator.and.treasury.and.injector.address.spec.ts
+++ PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/new.operator.and.treasury.and.injector.address.spec.ts
@@ -1,4 +1,7 @@
 import {
+    Finding,
+    FindingType,
+    FindingSeverity,
     HandleTransaction,
     createTransactionEvent,
   } from "forta-agent"
@@ -15,7 +18,7 @@ import {events, PanCakeSwapLottery_Address} from "./agent.config"
     })
   
     describe("NewOperatorAndTreasuryAndInjectorAddresses handleTransaction", () => {
-      it("returns no findings if there are no NewRandomGenerator events emmited ", async () => {
+      it("returns no findings if there are no NewOperatorAndTreasuryAndInjectorAddresses events emmited ", async () => {
   
         mockTxEvent.filterLog = jest.fn().mockReturnValue([])
   
@@ -32,6 +35,46 @@ import {events, PanCakeSwapLottery_Address} from "./agent.config"
   
   
       })
+
+      it("returns findings if there are NewOperatorAndTreasuryAndInjectorAddresses events emmited ", async () => {
+  
+        const mockSwapEvent = {
+            args: {
+              from: "0x0abc",
+              operator: "0x0123",
+              treasury: "0x0def",
+              injector: "0x0456"
+            }
+          };
+
+          mockTxEvent.filterLog = jest
+            .fn()
+            .mockReturnValue([mockSwapEvent]);
+    
+          const findings = await handleTransaction(mockTxEvent);
+    
+          const {from, operator, treasury, injector} = mockSwapEvent.args;
+    
+          expect(findings).toStrictEqual([
+            Finding.fromObject({
+                name: "New Operator And Treasury And Injector Addresses",
+                description: `New Operator And Treasury And Injector Addresses`,
+                alertId: "FORTA-2",
+                severity: FindingSeverity.Info,
+                type: FindingType.Info,
+                metadata: {
+                  from,  
+                  operator,
+                  treasury,
+                  injector
+                },
+              }),
+          ]);
+          expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+          expect(mockTxEvent.filterLog).toHaveBeenCalledWith(events.NewOperatorAndTreasuryAndInjectorAddresses, PanCakeSwapLottery_Address);
+  
+      })
+
     })
     
   })
