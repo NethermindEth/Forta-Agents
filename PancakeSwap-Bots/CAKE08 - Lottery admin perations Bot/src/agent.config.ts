754a4698873e8fa2a34284fc045c0a66366c6838
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-27 14:43:20 +0100

    fix: change constants to Upper Case
    chore: add FUNCTION_NAMES constant

--- PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/agent.config.ts
+++ PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/agent.config.ts
@@ -1,12 +1,18 @@
-export const events = {
-  NewRandomGenerator: "event NewRandomGenerator(address indexed)",
+export const EVENTS = {
+  NewRandomGenerator: "event NewRandomGenerator(address indexed randomGenerator)",
   NewOperatorAndTreasuryAndInjectorAddresses:
-    "event NewOperatorAndTreasuryAndInjectorAddresses(address,address,address)",
+    "event NewOperatorAndTreasuryAndInjectorAddresses(address operator,address treasury,address injector)",
 };
 
 export const ABI = [
-  "function setMinAndMaxTicketPriceInCake(uint256,uint256)",
+  "function setMinAndMaxTicketPriceInCake(uint256 _minPriceTicketInCake,uint256 _minPriceTicketInCake)",
   "function setMaxNumberTicketsPerBuy(uint256)",
 ];
 
-export const PanCakeSwapLottery_Address = "0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c";
+export const FUNCTION_NAMES = [
+  "setMinAndMaxTicketPriceInCake",
+  "setMaxNumberTicketsPerBuy"
+]
+
+
+export const PANCAKE_SWAP_LOTTERY_ADDRESS = "0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c";
