e45fc7b46c2befa496a8b47a2ba0180376d8dbb7
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-27 15:18:37 +0100

    feat: added logic to create metadata depending on function called

--- PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/function.call.listener.ts
+++ PancakeSwap-Bots/CAKE08 - Lottery admin perations Bot/src/function.call.listener.ts
@@ -1,14 +1,26 @@
 import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
 
-import { ABI, PanCakeSwapLottery_Address } from "./agent.config";
+import { ABI, PANCAKE_SWAP_LOTTERY_ADDRESS, FUNCTION_NAMES } from "./agent.config";
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
 
   // filter the transaction logs for function calls
-  const functionCalls = txEvent.filterFunction(ABI, PanCakeSwapLottery_Address);
+  const functionCalls = txEvent.filterFunction(ABI, PANCAKE_SWAP_LOTTERY_ADDRESS);
 
   functionCalls.forEach((functionCall) => {
+
+    let metadata ={}
+
+    if(functionCall.name === FUNCTION_NAMES[0]){
+      let {_minPriceTicketInCake, _maxPriceTicketInCake} = functionCall.args
+      metadata = {_minPriceTicketInCake, _maxPriceTicketInCake}
+    }
+    else if(functionCall.name === FUNCTION_NAMES[1]){
+      let {_maxNumberTicketsPerBuy} = functionCall.args
+      metadata = {_maxNumberTicketsPerBuy}
+    }
+    
     findings.push(
       Finding.fromObject({
         name: "Function Call",
@@ -16,7 +28,7 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
         alertId: "PCSLottery-3",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
-        metadata: {},
+        metadata,
       })
     );
   });
