46b812c50c47b79aaf51bbc88f0cb1f5d77391a7
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-29 01:55:38 +0100

    refac: refactor code to use TestTransactionEvent and bot.test.constants

--- PancakeSwap-Bots/Lottery Admin Operations/src/new.random.generator.spec.ts
+++ PancakeSwap-Bots/Lottery Admin Operations/src/new.random.generator.spec.ts
@@ -1,5 +1,11 @@
-import { Finding, FindingSeverity, FindingType, HandleTransaction, createTransactionEvent, ethers, TransactionEvent } from "forta-agent";
-import { TestTransactionEvent, createAddress } from "forta-agent-tools/lib/tests.utils";
+import {  HandleTransaction, ethers } from "forta-agent";
+import { TestTransactionEvent } from "forta-agent-tools/lib/tests.utils";
+
+import {
+  NEW_RANDOM_GENERATOR_FINDING,
+  randomGenerator
+} 
+from "./bot.test.constants"
 
 import agent from "./new.random.generator";
 import { EVENTS, PANCAKE_SWAP_LOTTERY_ADDRESS } from "./bot.config";
@@ -24,32 +30,20 @@ describe("PancakeSwap Lottery", () => {
       
 
       let eventInterface = new ethers.utils.Interface([EVENTS.NewRandomGenerator])
-      let randomG = createAddress("0x123")
 
       const eventLog = eventInterface.encodeEventLog(
         eventInterface.getEvent('NewRandomGenerator'),
         [
-          randomG
+          randomGenerator
         ]
+        
       )
       
       mockTxEvent.addAnonymousEventLog(PANCAKE_SWAP_LOTTERY_ADDRESS, eventLog.data, ...eventLog.topics)
       
       const findings = await handleTransaction(mockTxEvent);
 
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "New Random Generator",
-          description: "PancakeSwapLottery: Random Number Generator changed",
-          alertId: "CAKE-8-1",
-          protocol: "PancakeSwap",
-          severity: FindingSeverity.Info,
-          type: FindingType.Info,
-          metadata: {
-            randomGenerator: randomG
-          },
-        }),
-      ]);
+      expect(findings).toStrictEqual([NEW_RANDOM_GENERATOR_FINDING]);
 
     });
 
