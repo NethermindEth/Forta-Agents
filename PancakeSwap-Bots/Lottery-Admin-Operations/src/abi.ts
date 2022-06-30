fff333de52331b9d7f3618891a08cbb3368d6e72
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-30 17:00:29 +0100

    chore: file renamed
    feat: added ALERTS constant

---
+++ PancakeSwap-Bots/Lottery-Admin-Operations/src/abi.ts
@@ -0,0 +1,19 @@
+
+export const EVENTS = {
+  NewRandomGenerator: "event NewRandomGenerator(address indexed randomGenerator)",
+  NewOperatorAndTreasuryAndInjectorAddresses:
+    "event NewOperatorAndTreasuryAndInjectorAddresses(address operator,address treasury,address injector)",
+};
+
+export const ABI = [
+  "function setMinAndMaxTicketPriceInCake(uint256 _minPriceTicketInCake,uint256 _maxPriceTicketInCake)",
+  "function setMaxNumberTicketsPerBuy(uint256 _maxNumberTicketsPerBuy)",
+];
+
+
+export const ALERTS = {
+  NewRandomGenerator: "CAKE-8-1",
+  NewOperatorAndTreasuryAndInjectorAddresses: "CAKE-8-2",
+  setMinAndMaxTicketPriceInCake: "CAKE-8-3",
+  setMaxNumberTicketsPerBuy: "CAKE-8-4"
+}
