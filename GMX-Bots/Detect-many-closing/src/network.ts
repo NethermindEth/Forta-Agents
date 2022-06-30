356821345e5d05fe96e9e6bf428244f854b00f2e
Yehia Tarek <yehia@startfi.io>
2022-06-30 21:09:22 +0200

    chore: put the correct address

--- GMX-Bots/Detect-many-closing/src/network.ts
+++ GMX-Bots/Detect-many-closing/src/network.ts
@@ -16,7 +16,7 @@ export const networkData: Record<number, NetworkData> = {
     blockNumber: 150,
   },
   42: {
-    address: "0x9ab2De34A33fB459b538c43f251eB825645e8595",
+    address: "0x19D7085288bD7130Fe67A6EE58ECFBe7005C5E9f",
     threshold: 50,
     blockNumber: 150,
   },
