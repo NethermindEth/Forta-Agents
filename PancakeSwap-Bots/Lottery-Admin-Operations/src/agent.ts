cfd831005a30824d4765e4788630897c50adcbad
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-30 17:01:41 +0100

    refac: move NetworkData to a configuration file

--- PancakeSwap-Bots/Lottery-Admin-Operations/src/agent.ts
+++ PancakeSwap-Bots/Lottery-Admin-Operations/src/agent.ts
@@ -3,36 +3,29 @@ import { NetworkManager } from "forta-agent-tools";
 import newGeneratorBot from "./new.random.generator";
 import newOperatorBot from "./new.operator.and.treasury.and.injector.address";
 import functionCallBot from "./function.call.listener";
+import { NetworkData, DATA } from "./config";
 
-interface NetworkData {
-  lotteryAddress: string;
-}
-const data: Record<number, NetworkData> = {
-  56: {
-    lotteryAddress: "0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c",
-  },
-  97: {
-    lotteryAddress: "0x1a79f536EB9E93570C30fd23Debf2a068Ea33d33",
-  },
-};
-const networkManager = new NetworkManager(data);
+
+const networkManager = new NetworkManager(DATA);
+
 const provideInitialize = (
   networkManager: NetworkManager<NetworkData>,
   provider: ethers.providers.Provider
 ): Initialize => {
   return async () => {
     await networkManager.init(provider);
+
   };
 };
 const provideHandleTransaction = (networkManager: NetworkManager<NetworkData>): HandleTransaction => {
 
-  const lotteryAddress = networkManager.get("lotteryAddress");
-
-  const newGeneratorBotHandleTransaction = newGeneratorBot.providerHandleTransaction(lotteryAddress);
-  const newOperatorBotHandleTransaction = newOperatorBot.providerHandleTransaction(lotteryAddress);
-  const functionCallBotHandleTransaction = functionCallBot.providerHandleTransaction(lotteryAddress);
-  
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+
+    const lotteryAddress = networkManager.get("lotteryAddress");
+
+    const newGeneratorBotHandleTransaction = newGeneratorBot.providerHandleTransaction(lotteryAddress);
+    const newOperatorBotHandleTransaction = newOperatorBot.providerHandleTransaction(lotteryAddress);
+    const functionCallBotHandleTransaction = functionCallBot.providerHandleTransaction(lotteryAddress);
 
     const findings = (
       await Promise.all([
