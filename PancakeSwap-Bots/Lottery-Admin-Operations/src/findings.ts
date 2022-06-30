7e2129aa2c1509d733a1f6aca82e536b861d6a0c
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-06-30 16:59:10 +0100

    feat: add createFunctionFinding function

--- PancakeSwap-Bots/Lottery-Admin-Operations/src/findings.ts
+++ PancakeSwap-Bots/Lottery-Admin-Operations/src/findings.ts
@@ -1,17 +1,32 @@
 import { Finding, FindingType, FindingSeverity } from "forta-agent";
+import { ALERTS } from "./abi";
 
 
 
-export const createFinding = (_name:string, _description:string, _metadata:{} ) => {
+export const createEventFinding = ( _name:string, _description:string,  _metadata:{} ) => {
 
-    Finding.fromObject({
-        name: _name,
+    return Finding.fromObject({
+        name: `Event: ${(_name === "NewRandomGenerator") ? "NewRandomGenerator" : _name }`,
         description: `PancakeSwapLottery: ${_description}`,
-        alertId: "CAKE-8-1",
+        alertId: `${(_name === "NewRandomGenerator") ? ALERTS.NewRandomGenerator : ALERTS.NewOperatorAndTreasuryAndInjectorAddresses}`,
         protocol: "PancakeSwap",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         metadata: _metadata
     })
 
-    }
+}
+
+export const createFunctionFinding = ( _name:string, _description:string,  _metadata:{} ) => {
+
+    return Finding.fromObject({
+        name: `Function: ${(_name === "setMinAndMaxTicketPriceInCake") ? "setMinAndMaxTicketPriceInCake" : _name }`,
+        description: `PancakeSwapLottery: Function ${_description} called`,
+        alertId: `${(_name === "setMinAndMaxTicketPriceInCake") ? ALERTS.setMinAndMaxTicketPriceInCake : ALERTS.setMaxNumberTicketsPerBuy}`,
+        protocol: "PancakeSwap",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: _metadata
+    })
+
+}
