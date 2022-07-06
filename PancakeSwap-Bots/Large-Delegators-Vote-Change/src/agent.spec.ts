8005aee1fef4574f0051f1a90ac200be47ffdda6
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-06 17:05:38 +0100

    tests: multiple findings test

--- PancakeSwap-Bots/Large-Delegators-Vote-Change/src/agent.spec.ts
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/agent.spec.ts
@@ -4,6 +4,7 @@ import {DELEGATE_VOTES_CHANGED_EVENT} from "./abi"
 import {NetworkManager} from "forta-agent-tools"
 import {NetworkData} from "./config"
 import bot from "./agent";
+import { createFinding } from "./findings";
 
 describe("delegate votes change bot", () => {
   const MOCK_CONTRACT_ADDRESS = createAddress("0x1234");
@@ -48,14 +49,66 @@ describe("delegate votes change bot", () => {
 
     });
 
-    it("returns a finding if there is a DelegateVotesChanged event emitted", async () => {
+    it("returns a finding if there is a DelegateVotesChanged event emitted that triggers an alert", async () => {
       let eventLog = eventInterface.encodeEventLog(eventInterface.getEvent("DelegateVotesChanged"), [
-        createAddress("0x2314"),
+        createAddress("0x2345"), 1000, 2000
       ]);
-      mockTxEvent.addAnonymousEventLog(createAddress("0x5647"), eventLog.data, ...eventLog.topics);
+      mockTxEvent.addAnonymousEventLog(MOCK_CONTRACT_ADDRESS, eventLog.data, ...eventLog.topics);
 
-     
-   
+      const findings: Finding[] = await handleTransaction(mockTxEvent);
+
+      let metadata = {
+        delegate: createAddress("0x2345"),
+        previousBalance: "1000",
+        newBalance: "2000"
+      }
+
+      expect(findings).toStrictEqual([createFinding("DelegateVotesChanged", metadata, FindingSeverity.High)]);
+      
     });
+
+    it("returns multiple finding if there are  multiple DelegateVotesChanged events emitted", async () => {
+      let eventLog_1 = eventInterface.encodeEventLog(eventInterface.getEvent("DelegateVotesChanged"), [
+        createAddress("0x0347"), 1000, 2000
+      ]);
+
+      let eventLog_2 = eventInterface.encodeEventLog(eventInterface.getEvent("DelegateVotesChanged"), [
+        createAddress("0x2045"), 1000, 1900
+      ]);
+
+      let eventLog_3 = eventInterface.encodeEventLog(eventInterface.getEvent("DelegateVotesChanged"), [
+        createAddress("0x0345"), 1000, 1600
+      ]);
+
+      mockTxEvent.addAnonymousEventLog(MOCK_CONTRACT_ADDRESS, eventLog_1.data, ...eventLog_1.topics);
+      mockTxEvent.addAnonymousEventLog(MOCK_CONTRACT_ADDRESS, eventLog_2.data, ...eventLog_2.topics);
+      mockTxEvent.addAnonymousEventLog(MOCK_CONTRACT_ADDRESS, eventLog_3.data, ...eventLog_3.topics);
+
+      const findings: Finding[] = await handleTransaction(mockTxEvent);
+
+      let metadata_1 = {
+        delegate: createAddress("0x0347"),
+        previousBalance: "1000",
+        newBalance: "2000"
+      }
+      let metadata_2 = {
+        delegate: createAddress("0x2045"),
+        previousBalance: "1000",
+        newBalance: "1900"
+      }
+      let metadata_3 = {
+        delegate: createAddress("0x0345"),
+        previousBalance: "1000",
+        newBalance: "1600"
+      }
+
+      expect(findings).toStrictEqual([
+        createFinding("DelegateVotesChanged", metadata_1, FindingSeverity.High),
+        createFinding("DelegateVotesChanged", metadata_2, FindingSeverity.Medium),
+        createFinding("DelegateVotesChanged", metadata_3, FindingSeverity.Low),     
+      ]);
+      
+    });
+
   });
 });
