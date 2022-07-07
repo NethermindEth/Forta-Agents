e94d7c4969135e84a1245f7a2691cf0acdccb769
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-07 03:44:21 +0100

    chore: change test contract address

--- PancakeSwap-Bots/Large-Delegators-Vote-Change/src/config.ts
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/config.ts
@@ -7,6 +7,6 @@ export const DATA: Record<number, NetworkData> = {
     cakeAddress: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82", //BSC Mainnet contract address
   },
   97: {
-    cakeAddress: "0x0151A9301b52CCAfA75cd501Bff874C8901260b0", //BSC Testnet test contract address
+    cakeAddress: "0x4B8432e8eEF48d4A4A978c4679d63B544fcA7b99", //BSC Testnet test contract address
   },
 };
