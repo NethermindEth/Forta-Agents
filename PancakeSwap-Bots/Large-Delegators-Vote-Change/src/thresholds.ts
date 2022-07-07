4c4bb739434d4c49957dc51a88bb18e7d5fd5928
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-07 18:19:14 +0100

    chore: thresholds changed to percentages

--- PancakeSwap-Bots/Large-Delegators-Vote-Change/src/thresholds.ts
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/thresholds.ts
@@ -2,6 +2,8 @@ import { ethers } from "forta-agent";
 export const BN = ethers.BigNumber;
 
 export const DECIMALS = BN.from(10).pow(18);
-export const HIGH_THRESHOLD = BN.from(50).mul(DECIMALS);
-export const MEDIUM_THRESHOLD = BN.from(25).mul(DECIMALS);
-export const LOW_THRESHOLD = BN.from(10).mul(DECIMALS);
+
+export const MIN_PREVIOUS_BALANCE = BN.from(1).mul(DECIMALS);
+export const HIGH_THRESHOLD = 1; //100%
+export const MEDIUM_THRESHOLD = 0.5; //50%
+export const LOW_THRESHOLD = 0.25; //25%
