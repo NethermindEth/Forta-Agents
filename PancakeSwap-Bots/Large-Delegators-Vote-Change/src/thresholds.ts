5cafdd853db2e7313e196f61453c31a4db4aa97c
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-07 03:25:10 +0100

    refac: refactor code to use BigNumber

---
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/thresholds.ts
@@ -0,0 +1,7 @@
+import {ethers} from "forta-agent"
+export const BN = ethers.BigNumber;
+
+export const DECIMALS = BN.from(10).pow(18);
+export const HIGH_THRESHOLD = BN.from(50).mul(DECIMALS)
+export const MEDIUM_THRESHOLD = BN.from(25).mul(DECIMALS);
+export const LOW_THRESHOLD = BN.from(10).mul(DECIMALS);;
