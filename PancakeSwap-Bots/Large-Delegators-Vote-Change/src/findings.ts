a91ad8c7705343ff25e617e12adf7e7d02855cf9
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-09 21:56:35 +0100

    chore: add alert

--- PancakeSwap-Bots/Large-Delegators-Vote-Change/src/findings.ts
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/findings.ts
@@ -4,12 +4,13 @@ export const ALERTS: { [key in FindingSeverity]?: string } = {
   [FindingSeverity.Low]: "CAKE-10-1",
   [FindingSeverity.Medium]: "CAKE-10-2",
   [FindingSeverity.High]: "CAKE-10-3",
+  [FindingSeverity.Info]: "CAKE-10-4",
 };
 
 export function createFinding(description: string, metadata: {}, severity: FindingSeverity) {
   return Finding.fromObject({
     name: "CakeToken Event Emitted: DelegateVotesChanged",
-    description: `Delegate Vote Balance increased by: ${description} %`,
+    description: `Delegate Vote Balance increased by: ${description}`,
     alertId: ALERTS[severity]!,
     protocol: "PancakeSwap",
     severity: severity,
