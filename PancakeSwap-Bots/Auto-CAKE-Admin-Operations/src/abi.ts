1e37892e8ddc31d836bade33cc97e6a09c457bb0
Thomas Pan <thomaspanf@gmail.com>
2022-07-12 14:30:09 -0700

    feat: added abi file

---
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/abi.ts
@@ -0,0 +1,12 @@
+const CAKE: string[] = [
+    "event Pause(uint256 indexed epoch)",
+    "event Unpause(uint256 indexed epoch)",
+    "event NewOperatorAddress(address operator)",
+    "event NewAdminAddress(address admin)",
+    "event NewOracle(address oracle)",
+    "event NewTreasuryFee(uint256 indexed epoch, uint256 treasuryFee)"
+];
+
+export default {
+    CAKE,
+};
