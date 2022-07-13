d29d5671e480ef5b5673fecec22a64cb1fe222f2
Thomas Pan <thomaspanf@gmail.com>
2022-07-12 22:19:59 -0700

    refac: installed and ran prettier

--- PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/agent.ts
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/agent.ts
@@ -1,15 +1,12 @@
-import abi from './abi';
-import findings from './findings';
-import { TransactionEvent } from 'forta-agent'
+import abi from "./abi";
+import findings from "./findings";
+import { TransactionEvent } from "forta-agent";
 
 const CAKE_ADDRESS = "0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA";
 
-console.log("hello world")
-export const provideHandleTransaction = (cakeAddress: string) =>
-  async (txEvent: TransactionEvent) => 
-    txEvent.filterLog(abi.CAKE, cakeAddress).map(findings.resolver);
-
-
+console.log("hello world");
+export const provideHandleTransaction = (cakeAddress: string) => async (txEvent: TransactionEvent) =>
+  txEvent.filterLog(abi.CAKE, cakeAddress).map(findings.resolver);
 
 export default {
   handleTransaction: provideHandleTransaction(CAKE_ADDRESS),
