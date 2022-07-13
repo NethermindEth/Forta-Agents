723929f96a1c81648ca61c112b89c160495d1ad0
sprtd <sagesdn@gmail.com>
2022-07-13 12:34:03 +0100

    chore: remove folder

--- PancakeSwap-Bots/Large-LP-Deposit-Withdrawal/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { getCreate2Address } from "@ethersproject/address";
-import { utils } from "ethers";
-import { PAIR_INIT_CODE_HASH } from "./constants";
-
-export const createPair = (token0: string, token1: string, factory: string): string => {
-  let salt: string = utils.solidityKeccak256(["address", "address"], [token0, token1]);
-  return getCreate2Address(factory, salt, PAIR_INIT_CODE_HASH).toLowerCase();
-};
