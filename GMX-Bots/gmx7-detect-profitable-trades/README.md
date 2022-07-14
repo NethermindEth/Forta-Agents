df90b663f2fa7078ed1d43ae32df5085cb471825
ericmartihaynes <ericmartihaynes@gmail.com>
2022-07-14 23:42:19 +0200

    GMX7

---
+++ GMX-Bots/gmx7-detect-profitable-trades/README.md
@@ -0,0 +1,31 @@
+# Frontrun
+
+## Description
+
+This bot detects accounts using GMX that have an unusual amount of profitable swaps. By default this is 90% but it can be changed.
+
+## Supported Chains
+
+- Arbitrum
+- Avalanche
+
+## Alerts
+
+- GMX-07
+  - Fired when an account using GMX has an unusual amount of profitable trades
+  - Severity is always set to "Suspicious"
+  - Type is always set to "Medium"
+  - Metadata included:
+    - `account`: Address of the account with the unusual amount of profitable trades
+    - `profitableTrades`: Number of profitable trades
+    - `totalTrades`: Number of total trades
+    - `totalProfit`: Total profit of the account, calculated from previous trades (in USD).
+
+## Changing Profitable Trades Ratio & Grace Period
+By default this ratio is set to trigger the bot when an account has more than 90% of profitable trades. This ratio can be changed in the `utils.ts` file. Similarly, the grace period is set by default to 5 trades, but can be changed in the `utils.ts` file.
+
+## Test Data
+
+The bot behaviour can be verified with the following transactions:
+
+- Currently no transactions have been found
---
+++ GMX-Bots/gmx7-detect-profitable-trades/src/agent.spec.ts
@@ -0,0 +1,531 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  Initialize,
+  createTransactionEvent,
+  ethers,
+  getEthersProvider,
+  TransactionEvent,
+} from "forta-agent";
+import agent from "./agent";
+import { TestTransactionEvent } from "forta-agent-tools/lib/tests";
+import { provideHandleTx, initialize } from "./agent";
+import { createAddress, MockEthersProvider } from "forta-agent-tools/lib/tests";
+import { Interface } from "@ethersproject/abi";
+
+const ABI: string[] = [
+  "event Swap(address account, address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut)",
+];
+
+const TEST_IFACE: Interface = new Interface(ABI);
+
+const MOCK_GMX_ROUTER_ADDRESS = "0xabbc5f99639c9b6bcb58544ddf04efa6802f4064";
+const MOCK_NETWORK_MANAGER = {
+  get: jest.fn().mockReturnValue(MOCK_GMX_ROUTER_ADDRESS),
+};
+const MOCK_SWAP_EVENT =
+  "event Swap(address account, address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut)";
+let mockPriceFeed = {
+  get: jest.fn().mockReturnValue({
+    latestRoundData: jest.fn().mockReturnValue({
+      roundId: 0,
+      answer: 1,
+      startedAt: 2,
+      updatedAt: 3,
+      answeredInRound: 4,
+    }),
+  }),
+};
+
+const handler = provideHandleTx(
+  MOCK_NETWORK_MANAGER,
+  MOCK_SWAP_EVENT,
+  mockPriceFeed as unknown as Map<string, ethers.Contract>
+);
+const provider = getEthersProvider();
+const aggregatorV3InterfaceABI = [
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "description",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+    name: "getRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "latestRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+const priceFeedData1 = {
+  wethPriceFeed: new ethers.Contract("0x639fe6ab55c921f74e7fac1ee960c0b6293ba612", aggregatorV3InterfaceABI, provider),
+  wbtcPriceFeed: new ethers.Contract("0x6ce185860a4963106506c203335a2910413708e9", aggregatorV3InterfaceABI, provider),
+  linkPriceFeed: new ethers.Contract("0x86e53cf1b870786351da77a57575e79cb55812cb", aggregatorV3InterfaceABI, provider),
+  uniPriceFeed: new ethers.Contract("0x9c917083fdb403ab5adbec26ee294f6ecada2720", aggregatorV3InterfaceABI, provider),
+  usdcPriceFeed: new ethers.Contract("0x50834f3163758fcc1df9973b6e91f0f0f0434ad3", aggregatorV3InterfaceABI, provider),
+  usdtPriceFeed: new ethers.Contract("0x3f3f5df88dc9f13eac63df89ec16ef6e7e25dde7", aggregatorV3InterfaceABI, provider),
+  daiPriceFeed: new ethers.Contract("0xc5c8e77b397e531b8ec06bfb0048328b30e9ecfb", aggregatorV3InterfaceABI, provider),
+  fraxPriceFeed: new ethers.Contract("0x0809e3d38d1b4214958faf06d8b1b1a2b73f2ab8", aggregatorV3InterfaceABI, provider),
+  mimPriceFeed: new ethers.Contract("0x87121f6c9a9f6e90e59591e4cf4804873f54a95b", aggregatorV3InterfaceABI, provider),
+};
+const init = initialize(getEthersProvider(), priceFeedData1);
+
+describe("sandwich attack frontrun agent", () => {
+  let handleTransaction: HandleTransaction;
+  let initialize: Initialize;
+  const mockTxEvent = createTransactionEvent({} as any);
+
+  const eventGain = TEST_IFACE.getEvent("Swap");
+  const logGain = TEST_IFACE.encodeEventLog(eventGain, [
+    createAddress("0xf0"),
+    "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8", //USDC
+    "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f", //WBTC
+    1,
+    1,
+  ]);
+
+  beforeAll(async () => {
+    handleTransaction = agent.handleTransaction;
+    await init(); //run initialize before the tests
+  });
+
+  jest.setTimeout(25000);
+  describe("handleTransaction", () => {
+    //empty findings
+
+    it("returns empty findings if there are no swap events to a non-router address", async () => {
+      const transaction: TransactionEvent = new TestTransactionEvent()
+        .setFrom(createAddress("0x0f"))
+        .setTo(createAddress("0x0f"));
+      const findings = await handler(transaction);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there are no swap events to a router address", async () => {
+      const transaction: TransactionEvent = new TestTransactionEvent()
+        .setFrom(createAddress("0x0f"))
+        .setTo(MOCK_GMX_ROUTER_ADDRESS);
+      const findings = await handler(transaction);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there are swap events to a non-router address", async () => {
+      const transaction1: TransactionEvent = new TestTransactionEvent()
+        .setFrom(createAddress("0xf0"))
+        .setTo(createAddress("0x0f"))
+        .setBlock(1)
+        .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+      const findings = await handler(transaction1);
+
+      expect(findings).toStrictEqual([]);
+    });
+  });
+
+  it("returns empty findings if there is an account with a suspicious amount of profitable trades but not above the grace period", async () => {
+    const transaction: TransactionEvent = new TestTransactionEvent()
+      .setFrom(createAddress("0xf0"))
+      .setTo(MOCK_GMX_ROUTER_ADDRESS)
+      .setBlock(1)
+      .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+
+    mockPriceFeed
+      .get()
+      .latestRoundData.mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      });
+    let findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("returns empty findings if there is an account with a amount of trades larger than the grace period but not enough are profitable to be suspicious", async () => {
+    const transaction: TransactionEvent = new TestTransactionEvent()
+      .setFrom(createAddress("0xf0"))
+      .setTo(MOCK_GMX_ROUTER_ADDRESS)
+      .setBlock(1)
+      .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+
+    mockPriceFeed
+      .get()
+      .latestRoundData.mockReturnValueOnce({
+        roundId: 0,
+        answer: 2100000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      });
+    let findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+
+    expect(findings).toStrictEqual([]);
+  });
+
+  //Transaction found
+
+  it("returns findings if there is an account with a suspicious amount of profitable trades", async () => {
+    const transaction: TransactionEvent = new TestTransactionEvent()
+      .setFrom(createAddress("0xf0"))
+      .setTo(MOCK_GMX_ROUTER_ADDRESS)
+      .setBlock(1)
+      .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+
+    mockPriceFeed
+      .get()
+      .latestRoundData.mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      });
+    let findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+    findings = await handler(transaction);
+
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Unusual amount of profitable trades",
+        description: `User ${createAddress("0xf0").toLowerCase()} has a ${(15 / 16) * 100}% profitable trade ratio`,
+        alertId: "GMX-07",
+        protocol: "GMX",
+        severity: FindingSeverity.Medium,
+        type: FindingType.Suspicious,
+        metadata: {
+          account: createAddress("0xf0").toLowerCase(),
+          profitableTrades: "15",
+          totalTrades: "16",
+          totalProfit: "298985", //in USD
+        },
+      }),
+    ]);
+  });
+
+  it("returns findings if there is still a suspicious amount of profitable trades", async () => {
+    const transaction: TransactionEvent = new TestTransactionEvent()
+      .setFrom(createAddress("0xf0"))
+      .setTo(MOCK_GMX_ROUTER_ADDRESS)
+      .setBlock(1)
+      .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+
+    mockPriceFeed
+      .get()
+      .latestRoundData.mockReturnValueOnce({
+        roundId: 0,
+        answer: 100000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      });
+    let findings = await handler(transaction);
+
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Unusual amount of profitable trades",
+        description: `User ${createAddress("0xf0").toLowerCase()} has a ${(16 / 17) * 100}% profitable trade ratio`,
+        alertId: "GMX-07",
+        protocol: "GMX",
+        severity: FindingSeverity.Medium,
+        type: FindingType.Suspicious,
+        metadata: {
+          account: createAddress("0xf0").toLowerCase(),
+          profitableTrades: "16",
+          totalTrades: "17",
+          totalProfit: "318984", //in USD
+        },
+      }),
+    ]);
+  });
+
+  //empty findings (again)
+
+  it("returns empty findings if the amount of profitable trades is no longer suspicious", async () => {
+    const transaction: TransactionEvent = new TestTransactionEvent()
+      .setFrom(createAddress("0xf0"))
+      .setTo(MOCK_GMX_ROUTER_ADDRESS)
+      .setBlock(1)
+      .addEventLog(eventGain.format("sighash"), createAddress("0x0"), logGain.data, ...logGain.topics.slice(1));
+
+    mockPriceFeed
+      .get()
+      .latestRoundData.mockReturnValueOnce({
+        roundId: 0,
+        answer: 2100000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      })
+      .mockReturnValueOnce({
+        roundId: 0,
+        answer: 2000000000000,
+        startedAt: 2,
+        updatedAt: 3,
+        answeredInRound: 4,
+      });
+    let findings = await handler(transaction);
+
+    expect(findings).toStrictEqual([]);
+  });
+});
