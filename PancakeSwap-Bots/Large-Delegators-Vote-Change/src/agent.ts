749dd5da7e108096bb6b66801ea936a0a29f5d1c
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-14 17:24:45 +0100

    feat: change findings conditions

--- PancakeSwap-Bots/Large-Delegators-Vote-Change/src/agent.ts
+++ PancakeSwap-Bots/Large-Delegators-Vote-Change/src/agent.ts
@@ -15,7 +15,6 @@ import { DATA, NetworkData } from "./config";
 import { createFinding } from "./findings";
 import {
   ABSOLUTE_THRESHOLD,
-  DECIMALS,
   HIGH_THRESHOLD,
   LOW_THRESHOLD,
   MEDIUM_THRESHOLD,
@@ -58,8 +57,10 @@ const provideHandleTransaction = (networkManager: NetworkManager<NetworkData>): 
 
       let delta: BN = new BN(newBalance.toString()).minus(previousBalance.toString()); // difference between balances
 
-      if (MIN_PREVIOUS_BALANCE.lt(previousBalance.toString())) {
-        let deltaPercentage: BN = delta.dividedBy(new BN(previousBalance.toString())); // previousBalance.div(DECIMALS).toNumber(); //percentage of vote increase
+      if (ABSOLUTE_THRESHOLD.lte(newBalance.toString())) {
+        findings.push(createFinding(delta.toString(), metadata, FindingSeverity.Critical));
+      } else if (MIN_PREVIOUS_BALANCE.lt(previousBalance.toString())) {
+        let deltaPercentage: BN = delta.dividedBy(new BN(previousBalance.toString())); //percentage of vote increase
         let description: string = deltaPercentage.multipliedBy(100).toString() + " %";
 
         if (deltaPercentage.gte(HIGH_THRESHOLD)) {
@@ -69,8 +70,6 @@ const provideHandleTransaction = (networkManager: NetworkManager<NetworkData>): 
         } else if (deltaPercentage.gte(LOW_THRESHOLD)) {
           findings.push(createFinding(description, metadata, FindingSeverity.Low));
         }
-      } else if (previousBalance.eq(0) && ABSOLUTE_THRESHOLD.lte(newBalance.toString())) {
-        findings.push(createFinding(newBalance, metadata, FindingSeverity.Info));
       }
     });
 
