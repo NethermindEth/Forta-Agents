83fa5a3180f41e49db369c9770ba41b06c69dfe1
Thomas Pan <thomaspanf@gmail.com>
2022-07-15 16:48:50 -0700

    refac: renamed bot to correct name

--- PancakeSwap-Bots/Prediction-Admin-Operatons/src/agent.ts
+++ PancakeSwap-Bots/Prediction-Admin-Operatons/src/agent.ts
@@ -4,7 +4,6 @@ import { TransactionEvent } from "forta-agent";
 
 const CAKE_ADDRESS = "0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA";
 
-console.log("hello world");
 export const provideHandleTransaction = (cakeAddress: string) => async (txEvent: TransactionEvent) =>
   txEvent.filterLog(abi.CAKE, cakeAddress).map(findings.resolver);
 
