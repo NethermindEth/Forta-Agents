64db1c04e58ff5c4fe3932fec47d826300704a46
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-18 15:52:04 +0100

    feat: add events transaction handler

---
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/agent.ts
@@ -0,0 +1,37 @@
+import { HandleTransaction, TransactionEvent, getEthersProvider, ethers, Initialize, Finding } from "forta-agent";
+import { NetworkManager } from "forta-agent-tools";
+import eventsBot from "./events.listener";
+import functionCallBot from "./function.call.listener";
+import { NetworkData, DATA } from "./config";
+
+const networkManager = new NetworkManager(DATA);
+
+const provideInitialize = (
+  networkManager: NetworkManager<NetworkData>,
+  provider: ethers.providers.Provider
+): Initialize => {
+  return async () => {
+    await networkManager.init(provider);
+  };
+};
+const provideHandleTransaction = (networkManager: NetworkManager<NetworkData>): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const cakeVaultAddress = networkManager.get("cakeVaultAddress");
+    
+    const eventsBotHandleTransaction = eventsBot.providerHandleTransaction(cakeVaultAddress);
+    const functionCallBotHandleTransaction = functionCallBot.providerHandleTransaction(cakeVaultAddress);
+
+    const findings = (
+      await Promise.all([
+        eventsBotHandleTransaction(txEvent),
+        functionCallBotHandleTransaction(txEvent),
+      ])
+    ).flat();
+    return findings;
+  };
+};
+export default {
+  provideHandleTransaction,
+  initialize: provideInitialize(networkManager, getEthersProvider()),
+  handleTransaction: provideHandleTransaction(networkManager),
+};
