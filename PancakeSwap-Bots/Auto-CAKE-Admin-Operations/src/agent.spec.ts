91e80b335f211cee232ef4bc585cc8f41e10ffc4
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-19 00:53:17 +0100

    tests: test fot multiple events and functions called

--- PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/agent.spec.ts
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/agent.spec.ts
@@ -11,8 +11,6 @@ import { createEventFinding, createFunctionFinding } from "./findings";
 describe("CakeVault", () => {
   let handleTransaction: HandleTransaction;
 
-  let eventInterface: ethers.utils.Interface;
-
   let functionInterface: ethers.utils.Interface;
 
   let mockTxEvent: TestTransactionEvent;
@@ -43,8 +41,6 @@ describe("CakeVault", () => {
 
     networkManager = new NetworkManager(mockData, 111);
     handleTransaction = bot.provideHandleTransaction(networkManager);
-
-    eventInterface = new ethers.utils.Interface(EVENTS);
 
     functionInterface = new ethers.utils.Interface(ABI);
 
@@ -79,7 +75,7 @@ describe("CakeVault", () => {
     });
 
     it("returns finding if function is called", async () => {
-      const data = functionInterface.encodeFunctionData(functionFragments[0].name, [createAddress("0x1234")]);
+      const data:string = functionInterface.encodeFunctionData(functionFragments[0].name, [createAddress("0x1234")]);
       
       const traces: TraceProps[] = [
         { to: MOCK_CONTRACT_ADDRESS, input: data }
@@ -109,31 +105,52 @@ describe("CakeVault", () => {
 
     });
 
- /*    it("returns findings with Event and Function call alerts", async () => {
+     it("returns findings with Event and Function call alerts", async () => {
       mockTxEvent.addInterfaceEventLog(eventFragments[0], MOCK_CONTRACT_ADDRESS, []);
       mockTxEvent.addInterfaceEventLog(eventFragments[1], MOCK_CONTRACT_ADDRESS, []);
 
+      const data_1:string = functionInterface.encodeFunctionData(functionFragments[0].name, [createAddress("0x1234")]);
+      const data_2:string = functionInterface.encodeFunctionData(functionFragments[1].name, [createAddress("0x9843")]);
+      const data_3:string = functionInterface.encodeFunctionData(functionFragments[2].name, [100000]);
+      const data_4:string = functionInterface.encodeFunctionData(functionFragments[3].name, [200000]);
+      const data_5:string = functionInterface.encodeFunctionData(functionFragments[4].name, [300000]);
+      
+      const traces: TraceProps[] = [
+        { to: MOCK_CONTRACT_ADDRESS, input: data_1 },
+        { to: MOCK_CONTRACT_ADDRESS, input: data_2 },
+        { to: MOCK_CONTRACT_ADDRESS, input: data_3 },
+        { to: MOCK_CONTRACT_ADDRESS, input: data_4 },
+        { to: MOCK_CONTRACT_ADDRESS, input: data_5 },
+      ];
 
-      //encode data into mockTxEvent to simulate function call
-      const data = functionInterface.encodeFunctionData(FUNCTION_NAMES[0], [100000, 200000]);
-      mockTxEvent.setData(data).setTo(MOCK_CONTRACT_ADDRESS);
+      mockTxEvent.addTraces(...traces);
 
       const findings: Finding[] = await handleTransaction(mockTxEvent);
 
-      expect(findings).toStrictEqual([
-        createEventFinding("NewRandomGenerator", "Random Generator Address changed", {
-          randomGenerator: createAddress("0x2314"),
-        }),
-        createEventFinding(
-          "NewOperatorAndTreasuryAndInjectorAddresses",
-          "Operator, Treasury and Injector Addresses changed",
-          { operator: createAddress("0x0123"), treasury: createAddress("0x0124"), injector: createAddress("0x0125") }
-        ),
-        createFunctionFinding(FUNCTION_NAMES[0], FUNCTION_NAMES[0], {
-          _minPriceTicketInCake: "100000",
-          _maxPriceTicketInCake: "200000",
-        }),
+      expect(findings).toStrictEqual
+      ([
+        createEventFinding(eventFragments[0].name, {}),
+        createEventFinding(eventFragments[1].name, {}),
+        createFunctionFinding(functionFragments[0].name, functionFragments[0].name, {_admin: createAddress("0x1234")}),
+        createFunctionFinding(functionFragments[1].name, functionFragments[1].name, {_treasury: createAddress("0x9843")}),
+        createFunctionFinding(functionFragments[2].name, functionFragments[2].name, {_performanceFee: "100000"}),
+        createFunctionFinding(functionFragments[3].name, functionFragments[3].name, {_callFee: "200000"}),
+        createFunctionFinding(functionFragments[4].name, functionFragments[4].name, {_withdrawFee: "300000"}),     
       ]);
-    }); */
+
+    });
+
+    it("returns finding only for the correct event", async() =>{
+
+      mockTxEvent.addInterfaceEventLog(eventFragments[0], MOCK_CONTRACT_ADDRESS, []);
+      mockTxEvent.addInterfaceEventLog(mockEventFragment, MOCK_CONTRACT_ADDRESS, [678]);
+
+      const findings: Finding[] = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([createEventFinding(eventFragments[0].name, {})]);
+
+
+    });
+
   });
 });
