5e8f1e32d33dc5bd9a2c76e32a0c3d2d50d8cc02
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-19 02:28:03 +0100

    chore: update description

--- PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/findings.ts
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/findings.ts
@@ -4,7 +4,7 @@ import { ALERTS } from "./abi";
 export const createEventFinding = (_name: string, _metadata: {}) => {
   return Finding.fromObject({
     name: "CakeVault Event Emitted",
-    description: `CakeVault contract is ${_name}`,
+    description: `CakeVault contract is ${_name}d`,
     alertId: ALERTS[_name],
     protocol: "PancakeSwap",
     severity: FindingSeverity.Info,
