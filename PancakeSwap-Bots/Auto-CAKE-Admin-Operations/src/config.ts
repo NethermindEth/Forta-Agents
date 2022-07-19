41c1ace9576d9d7464fefb721aa14e5cacf4b309
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-07-19 02:27:04 +0100

    chore: added test contract address

--- PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/config.ts
+++ PancakeSwap-Bots/Auto-CAKE-Admin-Operations/src/config.ts
@@ -7,6 +7,6 @@ export const DATA: Record<number, NetworkData> = {
     cakeVaultAddress: "0xa80240Eb5d7E05d3F250cF000eEc0891d00b51CC", //BSC Mainnet contract address
   },
   97: {
-    cakeVaultAddress: "", //BSC Testnet test contract address
+    cakeVaultAddress: "0x5af4cDEDe5650513d666fe1a9F57d78F84aEBEe9", //BSC Testnet test contract address
   },
 };
