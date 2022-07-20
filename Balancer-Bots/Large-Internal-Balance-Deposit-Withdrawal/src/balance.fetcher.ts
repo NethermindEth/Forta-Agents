4e9aa3b1947b546ad0ea0c6f3088eea7d7c674a3
Tayfun Sur <tayfunsur@gmail.com>
2022-07-20 17:31:21 +0300

    refac: Use single cache

--- Balancer-Bots/Large-Internal-Balance-Deposit-Withdrawal/src/balance.fetcher.ts
+++ Balancer-Bots/Large-Internal-Balance-Deposit-Withdrawal/src/balance.fetcher.ts
@@ -5,16 +5,12 @@ import { TOKEN_ABI } from "./constants";
 
 export default class BalanceFetcher {
   provider: providers.Provider;
-  private balanceCache: LRU<string, BigNumber>;
-  private symbolCache: LRU<string, string>;
+  private cache: LRU<string, BigNumber | string>;
   private tokenContract: Contract;
 
   constructor(provider: providers.Provider) {
     this.provider = provider;
-    this.balanceCache = new LRU<string, BigNumber>({
-      max: 10000,
-    });
-    this.symbolCache = new LRU<string, string>({
+    this.cache = new LRU<string, BigNumber | string>({
       max: 10000,
     });
     this.tokenContract = new Contract("", new Interface(TOKEN_ABI), this.provider);
@@ -25,13 +21,13 @@ export default class BalanceFetcher {
     const token = this.tokenContract.attach(tokenAddress);
 
     const key: string = `${tokenAddress}-${block}`;
-    if (this.balanceCache.has(key)) return this.balanceCache.get(key) as BigNumber;
+    if (this.cache.has(key)) return this.cache.get(key) as BigNumber;
 
     const balance: BigNumber = await token.balanceOf(vaultAddress, {
       blockTag: block,
     });
 
-    this.balanceCache.set(key, balance);
+    this.cache.set(key, balance);
 
     return balance;
   }
@@ -39,14 +35,14 @@ export default class BalanceFetcher {
   public async getSymbol(block: number | string, tokenAddress: string): Promise<string> {
     const token = this.tokenContract.attach(tokenAddress);
 
-    const key: string = `${tokenAddress}-${block}`;
-    if (this.symbolCache.has(key)) return this.symbolCache.get(key) as string;
+    const key: string = `symbol-${tokenAddress}-${block}`;
+    if (this.cache.has(key)) return this.cache.get(key) as string;
 
     const symbol: string = await token.symbol({
       blockTag: block,
     });
 
-    this.symbolCache.set(key, symbol);
+    this.cache.set(key, symbol);
 
     return symbol;
   }
