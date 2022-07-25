ecccd06e9f76675e941bd99b0c4ef806a1d38f37
Thomas Pan <thomaspanf@gmail.com>
2022-07-25 16:52:51 -0700

    chore: PoC deployed, findings not reported, currently troubleshooting

--- PancakeSwap-Bots/Set-Masterchef-Settings/src/agent.ts
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/agent.ts
@@ -2,7 +2,7 @@ import abi from "./abi";
 import findings from "./findings";
 import { TransactionEvent } from "forta-agent";
 
-const MasterChef_Address = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
+const MasterChef_Address = "0xE115b296916E63C74E01506d37c6CF2C9fBaa9E0";
 
 export const provideHandleTransaction = (cakeAddress: string) => async (txEvent: TransactionEvent) =>
   txEvent.filterLog(abi.CAKE, cakeAddress).map(findings.resolver);
