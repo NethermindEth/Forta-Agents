3d927630bce19da205d26cb59c2324ac4a0aaec9
Thomas Pan <thomaspanf@gmail.com>
2022-08-03 09:17:53 -0700

    fix: removed testnet from config

--- PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
@@ -4,9 +4,6 @@ const CONFIG: AgentConfig = {
   56: {
     masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
   },
-  97: {
-    masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506",
-  },
 };
 
 export interface NetworkData {
