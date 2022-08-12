bac8e6879acf975d684b32f3c38e37ab811520b6
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-12 22:52:50 +0100

    chore: update config with test contract

--- UMA-Bots/Funds-Deposited/src/config.ts
+++ UMA-Bots/Funds-Deposited/src/config.ts
@@ -6,7 +6,8 @@ export interface NetworkData {
     1: {
         spokePoolAddress: "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381", //Ethereum Mainnet contract address
     },
+
     5: {
-        spokePoolAddress: "", //Goerli Testnet test contract address
+        spokePoolAddress: "0x49bBE549F0Db98d0240c98fA170959e70B561757", //Goerli Testnet test contract address
     },
   };
