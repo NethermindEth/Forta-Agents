6378ec114f0322e5c6ea8e6f32afd6c9ff640792
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-15 10:53:08 +0100

    chore: update finding alert id

--- UMA-Bots/Funds-Deposited/src/findings.ts
+++ UMA-Bots/Funds-Deposited/src/findings.ts
@@ -9,7 +9,7 @@ export function createFinding(_metadata: {
   return Finding.fromObject({
     name: "SpokePool Funds Deposited Event Emitted",
     description: `Deposited ${_metadata.amount} ${_metadata.token}`,
-    alertId: "UMA-1-1",
+    alertId: "UMA-1",
     protocol: "UMA",
     severity: FindingSeverity.Info,
     type: FindingType.Info,
