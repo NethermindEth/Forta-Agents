521a534904a90a79f7ee22d9bcf3644c0929ea29
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-16 15:26:02 +0100

    chore: update chains

--- UMA-Bots/Funds-Deposited/src/config.ts
+++ UMA-Bots/Funds-Deposited/src/config.ts
@@ -7,6 +7,18 @@ export const DATA: Record<number, NetworkData> = {
     spokePoolAddress: "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381", //Ethereum Mainnet contract address
   },
 
+  42161: {
+    spokePoolAddress: "0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C", //Arbitrum contract address
+  },
+
+  137: {
+    spokePoolAddress: "0xD3ddAcAe5aFb00F9B9cD36EF0Ed7115d7f0b584c", //Polygon contract address
+  },
+
+  10: {
+    spokePoolAddress: "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9", //Optimism contract address
+  },
+
   5: {
     spokePoolAddress: "0xBC760257763b77aeEa256c129e09DB41bD2c1450", //Goerli Testnet test contract address
   },
