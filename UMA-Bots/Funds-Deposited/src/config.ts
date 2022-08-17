e6c867a1c25b4781f71d6e97fab08aeaecc647c0
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-17 14:45:11 +0100

    refac: use Network to gain chain Ids

--- UMA-Bots/Funds-Deposited/src/config.ts
+++ UMA-Bots/Funds-Deposited/src/config.ts
@@ -1,27 +1,27 @@
+import { Network } from "forta-agent";
+
 export interface NetworkData {
   spokePoolAddress: string;
 }
 
 export const DATA: Record<number, NetworkData> = {
-  1: {
+  [Network.MAINNET]: {
     spokePoolAddress: "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381", //Ethereum Mainnet contract address
   },
 
-  42161: {
+  [Network.ARBITRUM]: {
     spokePoolAddress: "0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C", //Arbitrum contract address
   },
 
-  137: {
+  [Network.POLYGON]: {
     spokePoolAddress: "0xD3ddAcAe5aFb00F9B9cD36EF0Ed7115d7f0b584c", //Polygon contract address
   },
 
-  10: {
+  [Network.OPTIMISM]: {
     spokePoolAddress: "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9", //Optimism contract address
   },
 
-  5: {
+  [Network.GOERLI]: {
     spokePoolAddress: "0xBC760257763b77aeEa256c129e09DB41bD2c1450", //Goerli Testnet test contract address
   },
 };
-
-export const MOCK_NETWORK_ID = 1111;
