e4f705ab217385becc2b772919fab0f15c373839
Maksim <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-19 17:50:08 +0530

    doc: update network.ts
    Co-authored-by: Vxatz <92011898+Vxatz@users.noreply.github.com>

--- UMA-Bots/Hubpool-Relayer-Reimbursement/src/network.ts
+++ UMA-Bots/Hubpool-Relayer-Reimbursement/src/network.ts
@@ -4,5 +4,5 @@ export interface NetworkDataInterface {
 
 export const NM_DATA: Record<number, NetworkDataInterface> = {
   1: { hubPoolAddr: "0xc186fa914353c44b2e33ebe05f21846f1048beda", },
-  5: { hubPoolAddr: "0x9F1CBa21257DA30A31d42EA53999CFcA179E849e" },
+  5: { hubPoolAddr: "0x9F1CBa21257DA30A31d42EA53999CFcA179E849e" }, //PoC
 };
