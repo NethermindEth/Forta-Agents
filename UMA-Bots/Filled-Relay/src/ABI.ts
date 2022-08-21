35585a1d3b0d0a7c703ade8da15b5ecf0f511600
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-21 17:54:27 +0100

    chore: add event and function abis

---
+++ UMA-Bots/Filled-Relay/src/ABI.ts
@@ -0,0 +1,6 @@
+export const FILLED_RELAY_EVENT =
+  "event FilledRelay(uint256 amount,uint256 totalFilledAmount,uint256 fillAmount,uint256 repaymentChainId,uint256 originChainId,uint256 destinationChainId,uint64 relayerFeePct,uint64 appliedRelayerFeePct,uint64 realizedLpFeePct,uint32 depositId,address destinationToken,address indexed relayer,address indexed depositor,address recipient,bool isSlowRelay)"
+export const FUNC_ABI = [
+  "function name() external view returns(string)",
+  "function decimals() external view returns(uint8)",
+];
