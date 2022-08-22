a831643c6b23a7d2d198c0f45d1c0b52bfc2c5f1
Tayfun Sur <tayfunsur@gmail.com>
2022-08-22 22:51:27 +0300

    feat: Create helper functions

---
+++ ETH-Merge-Bots/Milestones-in-TTD/src/utils.ts
@@ -0,0 +1,58 @@
+import BigNumber from "bignumber.js";
+
+export const getNumberOfDays = (avgBlockTime: number, estimatedNumberOfBlocksUntilMerge: BigNumber) => {
+  // One day in milliseconds
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  const date = new Date();
+  date.setSeconds(estimatedNumberOfBlocksUntilMerge.multipliedBy(avgBlockTime).plus(date.getSeconds()).toNumber());
+
+  const estimatedMergeDate = new Date(date);
+
+  const now = new Date(Date.now());
+
+  const diffInTime = estimatedMergeDate.getTime() - now.getTime();
+  const diffInDays = Math.ceil(diffInTime / oneDay);
+
+  return { diffInDays, estimatedMergeDate };
+};
+
+export const getAverageBlockTime = (
+  ethBlockData: any,
+  currentBlockTimestamp: number,
+  firstTimestamp: number,
+  blockCounter: number
+) => {
+  const avgTimeSpentInAWeek = ethBlockData.avgBlockTime * ethBlockData.blockNumberAWeek;
+
+  const avgBlockTime =
+    (avgTimeSpentInAWeek + currentBlockTimestamp - firstTimestamp) / (ethBlockData.blockNumberAWeek + blockCounter);
+
+  return avgBlockTime;
+};
+
+export const getEstimatedNumberOfBlocksUntilMerge = (
+  ttd: BigNumber,
+  totalDifficulty: BigNumber,
+  avgBlockDifficulty: BigNumber
+) => {
+  const estimatedNumberOfBlocksUntilMerge = ttd.minus(totalDifficulty).dividedBy(avgBlockDifficulty).decimalPlaces(0);
+
+  return estimatedNumberOfBlocksUntilMerge;
+};
+
+export const getAvgBlockDifficulty = (blockDifficulties: BigNumber[]): BigNumber => {
+  // Keep the last 50 blocks' difficulties
+  if (blockDifficulties.length === 51) {
+    blockDifficulties.shift();
+  }
+
+  const sumOfBlockDifficulties = blockDifficulties.reduce(
+    (accumulator: BigNumber, curr: BigNumber) => curr.plus(accumulator),
+    new BigNumber("0")
+  );
+
+  const avgBlockDifficulty = sumOfBlockDifficulties.dividedBy(blockDifficulties.length);
+
+  return avgBlockDifficulty;
+};
