343b15b328aa043513bdbad25384458bf53c664c
Tayfun Sur <tayfunsur@gmail.com>
2022-08-22 22:47:10 +0300

    chore: Add config file for Ethereum

---
+++ ETH-Merge-Bots/Milestones-in-TTD/src/eth.config.ts
@@ -0,0 +1,16 @@
+import BigNumber from "bignumber.js";
+
+export const TERMINAL_TOTAL_DIFFICULTY: BigNumber = new BigNumber("58750000000000000000000");
+
+// Days left for the merge
+export const MILESTONES = {
+  LOW: 20,
+  MEDIUM: 15,
+  HIGH: 10,
+  CRITICAL: 5,
+};
+
+export const ETH_BLOCK_DATA = {
+  avgBlockTime: 13.46, // average block time between Aug 13th - Aug 19th
+  blockNumberAWeek: 44236, // total number of blocks mined in between Aug 13th - Aug 19th
+};
