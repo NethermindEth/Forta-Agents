0dc93d2b855c272f6ee742d193fd54239fb0a3d5
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-24 00:14:00 +0530

    test: added test

--- UMA-Bots/Configuration-Change/src/agent.spec.ts
+++ UMA-Bots/Configuration-Change/src/agent.spec.ts
@@ -18,102 +18,193 @@ const networkManagerTest = new NetworkManager(MOCK_NM_DATA, 0);
 describe("Root Bundle Disputed bot", () => {
   let handleTransaction: HandleTransaction = provideHandleTransaction(MONITORED_EVENTS, networkManagerTest);
 
-  // it("returns empty findings if there is no dispute", async () => {
-  //   const txEvent: TransactionEvent = new TestTransactionEvent().setFrom(TEST_HUBPOOL_ADDR);
-  //   const findings = await handleTransaction(txEvent);
-  //   expect(findings).toStrictEqual([]);
-  // });
+  it("returns empty findings if there is no dispute", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent().setFrom(TEST_HUBPOOL_ADDR);
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
 
-  // it("doesn't return a finding if there a dispute is made from the wrong address", async () => {
-  //   const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(MONITORED_EVENTS[0], RANDOM_ADDRESSES[0], [
-  //     "123",
-  //   ]);
+  it("doesn't return a finding if there a dispute is made from the wrong address", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(MONITORED_EVENTS[0], RANDOM_ADDRESSES[0], [
+      "123",
+    ]);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   expect(findings).toStrictEqual([]);
-  // });
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
 
-  // it("doesn't return a finding if an irrelevant event is made from the HubPool address", async () => {
-  //   const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(TRANSFER_EVENT_ABI, TEST_HUBPOOL_ADDR, [
-  //     RANDOM_ADDRESSES[0],
-  //     "123",
-  //   ]);
+  it("doesn't return a finding if an irrelevant event is made from the HubPool address", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(TRANSFER_EVENT_ABI, TEST_HUBPOOL_ADDR, [
+      RANDOM_ADDRESSES[0],
+      "123",
+    ]);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   expect(findings).toStrictEqual([]);
-  // });
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
 
-  // it("returns a finding for emitted monitored event from HubPool : Event LivenessSet", async () => {
-  //   const txEvent: TransactionEvent = new TestTransactionEvent()
-  //     .setFrom(TEST_HUBPOOL_ADDR)
-  //     .addEventLog(MONITORED_EVENTS[0], TEST_HUBPOOL_ADDR, ["123"]);
+  it("returns a finding for emitted monitored event from HubPool : Event LivenessSet", async () => {
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[0], TEST_HUBPOOL_ADDR, ["123"]);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[0], ["123"]);
-  //   expect(findings).toStrictEqual([getFindingInstance(
-  //     thisFindingMetadata
-  //   )]);
-  // });
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[0], ["123"]);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
 
-  // it("returns a finding for emitted monitored event from HubPool : Event ProtocolFeeCaptureSet", async () => {
-  //   const passedParams = [RANDOM_ADDRESSES[0], "123"]
-  //   const txEvent: TransactionEvent = new TestTransactionEvent()
-  //     .setFrom(TEST_HUBPOOL_ADDR)
-  //     .addEventLog(MONITORED_EVENTS[1], TEST_HUBPOOL_ADDR, passedParams);
+  it("returns a finding for emitted monitored event from HubPool : Event ProtocolFeeCaptureSet", async () => {
+    const passedParams = [RANDOM_ADDRESSES[0], "123"]
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[1], TEST_HUBPOOL_ADDR, passedParams);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[1], passedParams);
-  //   expect(findings).toStrictEqual([getFindingInstance(
-  //     thisFindingMetadata
-  //   )]);
-  // });
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[1], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
 
-  // it("returns a finding for emitted monitored event from HubPool : Event ProtocolFeesCapturedClaimed", async () => {
-  //   const passedParams = [RANDOM_ADDRESSES[0], "123"]
-  //   const txEvent: TransactionEvent = new TestTransactionEvent()
-  //     .setFrom(TEST_HUBPOOL_ADDR)
-  //     .addEventLog(MONITORED_EVENTS[2], TEST_HUBPOOL_ADDR, passedParams);
+  it("returns a finding for emitted monitored event from HubPool : Event ProtocolFeesCapturedClaimed", async () => {
+    const passedParams = [RANDOM_ADDRESSES[0], "123"]
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[2], TEST_HUBPOOL_ADDR, passedParams);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[2], passedParams);
-  //   expect(findings).toStrictEqual([getFindingInstance(
-  //     thisFindingMetadata
-  //   )]);
-  // });
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[2], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
 
-  // it("returns a finding for emitted monitored event from HubPool : Event BondSet", async () => {
-  //   const passedParams = [RANDOM_ADDRESSES[0], "123"]
-  //   const txEvent: TransactionEvent = new TestTransactionEvent()
-  //     .setFrom(TEST_HUBPOOL_ADDR)
-  //     .addEventLog(MONITORED_EVENTS[3], TEST_HUBPOOL_ADDR, passedParams);
+  it("returns a finding for emitted monitored event from HubPool : Event BondSet", async () => {
+    const passedParams = [RANDOM_ADDRESSES[0], "123"]
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[3], TEST_HUBPOOL_ADDR, passedParams);
 
-  //   const findings = await handleTransaction(txEvent);
-  //   let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[3], passedParams);
-  //   expect(findings).toStrictEqual([getFindingInstance(
-  //     thisFindingMetadata
-  //   )]);
-  // });
-
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[3], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+  
   it("returns a finding for emitted monitored event from HubPool : Event IdentifierSet", async () => {
     const passedParams = [keccak256("hello world")];
     const txEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(TEST_HUBPOOL_ADDR)
-      .addEventLog(MONITORED_EVENTS[4], TEST_HUBPOOL_ADDR, passedParams);
-
+    .setFrom(TEST_HUBPOOL_ADDR)
+    .addEventLog(MONITORED_EVENTS[4], TEST_HUBPOOL_ADDR, passedParams);
+    
     const findings = await handleTransaction(txEvent);
     let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[4], [passedParams[0].toString()]);
     expect(findings).toStrictEqual([getFindingInstance(thisFindingMetadata)]);
   });
+  
 
-  // it("returns N findings for N events (N>=1)", async () => {
-  //   const txEvent: TransactionEvent = new TestTransactionEvent()
-  //     .setFrom(TEST_HUBPOOL_ADDR)
-  //     .addEventLog(DISPUTE_EVENT, TEST_HUBPOOL_ADDR, [RANDOM_ADDRESSES[0], RANDOM_HEX_ARR[0]])
-  //     .addEventLog(DISPUTE_EVENT, TEST_HUBPOOL_ADDR, [RANDOM_ADDRESSES[1], RANDOM_HEX_ARR[1]]);
-  //   const findings = await handleTransaction(txEvent);
-  //   expect(findings).toStrictEqual([
-  //     getFindingInstance(RANDOM_ADDRESSES[0], RANDOM_HEX_ARR_DECIMAL[0]),
-  //     getFindingInstance(RANDOM_ADDRESSES[1], RANDOM_HEX_ARR_DECIMAL[1]),
-  //   ]);
-  // });
+
+  it("returns a finding for emitted monitored event from HubPool : Event CrossChainContractsSet", async () => {
+    const passedParams = ["123", RANDOM_ADDRESSES[0], RANDOM_ADDRESSES[1]];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[5], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[5], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+
+  it("returns a finding for emitted monitored event from HubPool : Event L1TokenEnabledForLiquidityProvision", async () => {
+    const passedParams = [RANDOM_ADDRESSES[0], RANDOM_ADDRESSES[1]];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[6], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[6], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+  it("returns a finding for emitted monitored event from HubPool : Event L2TokenDisabledForLiquidityProvision", async () => {
+    const passedParams = [RANDOM_ADDRESSES[0], RANDOM_ADDRESSES[1]];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[6], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[6], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+
+  it("returns a finding for emitted monitored event from HubPool : Event SetPoolRebalanceRoute", async () => {
+    const passedParams = ["123", RANDOM_ADDRESSES[0], RANDOM_ADDRESSES[1]];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[8], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[8], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+
+  it("returns a finding for emitted monitored event from HubPool : Event SetEnableDepositRoute", async () => {
+    const passedParams = ["123", "456", RANDOM_ADDRESSES[1], true];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[9], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[9], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+
+  it("returns a finding for emitted monitored event from HubPool : Event SetEnableDepositRoute", async () => {
+    const passedParams = ["123", "0x12ab"];
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[10], TEST_HUBPOOL_ADDR, passedParams);
+
+    const findings = await handleTransaction(txEvent);
+    let thisFindingMetadata = getEventMetadataFromAbi(MONITORED_EVENTS[10], passedParams);
+    expect(findings).toStrictEqual([getFindingInstance(
+      thisFindingMetadata
+    )]);
+  });
+
+
+  it("returns N findings for N events (N>=1)", async () => {
+    const passedParams = ["123", "0x12ab"];
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(TEST_HUBPOOL_ADDR)
+      .addEventLog(MONITORED_EVENTS[10], TEST_HUBPOOL_ADDR, passedParams)
+      .addEventLog(MONITORED_EVENTS[0], TEST_HUBPOOL_ADDR, ["123"]);
+    
+      let thisFindingMetadataEvent1 = getEventMetadataFromAbi(MONITORED_EVENTS[10], passedParams);
+      let thisFindingMetadataEvent2 = getEventMetadataFromAbi(MONITORED_EVENTS[0], ["123"]);
+
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([
+      getFindingInstance(thisFindingMetadataEvent1),
+      getFindingInstance(thisFindingMetadataEvent2),
+    ]);
+  });
 });
