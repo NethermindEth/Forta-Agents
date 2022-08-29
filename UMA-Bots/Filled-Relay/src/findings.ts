38e8416aaaf3c1d1053f67890260178b7846e73f
Rodrigo Pina <rodrigopina22.rp@gmail.com>
2022-08-29 11:59:56 +0100

    chore: udpate findings

--- UMA-Bots/Filled-Relay/src/findings.ts
+++ UMA-Bots/Filled-Relay/src/findings.ts
@@ -1,23 +1,24 @@
 import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
-export function createFinding(_metadata: {
-  amount: string,
-  originChainId: string,
-  destinationChainId: string,
-  tokenName: string,
-  depositor: string,
-  recipient: string,
-  relayer: string
-}, isSlowRelay:boolean) {
-
+export function createFinding(
+  _metadata: {
+    amount: string;
+    originChainId: string;
+    destinationChainId: string;
+    tokenName: string;
+    depositor: string;
+    recipient: string;
+    relayer: string;
+  },
+  isSlowRelay: boolean
+) {
   return Finding.fromObject({
     name: "SpokePool Filled Relay Event Emitted",
-    description: `${isSlowRelay ? 'Slow': 'Fast'} Relay of ${_metadata.amount} ${_metadata.tokenName}`,
-    alertId: `${isSlowRelay ? 'UMA2-2': 'UMA-2-1'}`,
+    description: `${isSlowRelay ? "Slow" : "Fast"} Relay of ${_metadata.amount} ${_metadata.tokenName}`,
+    alertId: `${isSlowRelay ? "UMA2-2" : "UMA-2-1"}`,
     protocol: "UMA",
     severity: FindingSeverity.Info,
     type: FindingType.Info,
     metadata: _metadata,
   });
 }
-
