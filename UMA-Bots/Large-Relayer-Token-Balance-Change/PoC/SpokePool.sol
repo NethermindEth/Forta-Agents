9512dfec4b363c621a37578ca06211b82970342e
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-09-03 18:54:16 +0530

    fix: added correct PoC code

--- UMA-Bots/Large-Relayer-Token-Balance-Change/PoC/SpokePool.sol
+++ UMA-Bots/Large-Relayer-Token-Balance-Change/PoC/SpokePool.sol
@@ -1,22 +1,52 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    mapping(address => uint256) private _balances;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+    
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+    
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        _balances[from] = fromBalance - amount;
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+}
 
 contract USDT is ERC20{
-
     address monitoredAddress = 0x1Abf3a6C41035C1d2A3c74ec22405B54450f5e13;
-
     constructor() ERC20("Tether USD", "USDT"){
         _mint(monitoredAddress, 1000000);
     }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        address owner = _msgSender();
+    function transfer(address to, uint256 amount) public returns (bool) {
+        address owner = msg.sender;
         _transfer(owner, to, amount);
         return true;
     }
-
-    function decimals() public view virtual override returns (uint8) {
+    function decimals() public pure returns (uint8) {
         return 6;
     }
 }
