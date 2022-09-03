c3137fd567327aef56eaf7a5dc1bfd5c97c71b01
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-09-03 13:38:47 +0530

    doc: updated readme and package.json

--- UMA-Bots/Major-Relayer-Liquidity-Changes/src/agent.ts
+++ UMA-Bots/Major-Relayer-Liquidity-Changes/src/agent.ts
@@ -1,6 +1,6 @@
 import { Finding, HandleTransaction, ethers, Initialize, TransactionEvent, getEthersProvider } from "forta-agent";
 import { NetworkManager } from "forta-agent-tools";
-import { FILLED_RELAY_EVENT, getFindingInstance } from "./utils";
+import { TRANSFER_EVENT, getFindingInstance } from "./utils";
 import { NetworkDataInterface, NM_DATA } from "./network";
 import { BigNumber } from "ethers";
 
@@ -16,39 +16,39 @@ export function provideInitialize(
 }
 
 export function provideHandleTransaction(
-  filledRelayEvent: string,
+  transferEvent: string,
   networkManager: NetworkManager<NetworkDataInterface>
 ): HandleTransaction {
   return async (txEvent: TransactionEvent) => {
     const findings: Finding[] = [];
-    const filledRelayEventTxns = txEvent.filterLog(filledRelayEvent, networkManager.get("spokePoolAddr"));
+    // const filledRelayEventTxns = txEvent.filterLog(filledRelayEvent, networkManager.get("spokePoolAddr"));
 
-    filledRelayEventTxns.forEach((filledRelayEvent) => {
-      const { amount, originChainId, destinationChainId, depositor, recipient, isSlowRelay, destinationToken } =
-        filledRelayEvent.args;
+//     filledRelayEventTxns.forEach((filledRelayEvent) => {
+//       const { amount, originChainId, destinationChainId, depositor, recipient, isSlowRelay, destinationToken } =
+//         filledRelayEvent.args;
 
-      if (
-        Object.keys(networkManager.get("tokenThresholds")).includes(destinationToken) &&
-        BigNumber.from(amount.toString()).gte(BigNumber.from(networkManager.get("tokenThresholds")[destinationToken]))
-      ) {
-        findings.push(
-          getFindingInstance(
-            amount.toString(),
-            destinationToken,
-            originChainId.toString(),
-            destinationChainId.toString(),
-            depositor,
-            recipient,
-            isSlowRelay.toString()
-          )
-        );
-      }
-    });
+//       if (
+//         Object.keys(networkManager.get("tokenThresholds")).includes(destinationToken) &&
+//         BigNumber.from(amount.toString()).gte(BigNumber.from(networkManager.get("tokenThresholds")[destinationToken]))
+//       ) {
+//         findings.push(
+//           getFindingInstance(
+//             amount.toString(),
+//             destinationToken,
+//             originChainId.toString(),
+//             destinationChainId.toString(),
+//             depositor,
+//             recipient,
+//             isSlowRelay.toString()
+//           )
+//         );
+//       }
+//     });
     return findings;
   };
 }
 
 export default {
   initialize: provideInitialize(networkManager, getEthersProvider()),
-  handleTransaction: provideHandleTransaction(FILLED_RELAY_EVENT, networkManager),
+  handleTransaction: provideHandleTransaction(TRANSFER_EVENT, networkManager),
 };
--- UMA-Bots/Major-Relayer-Liquidity-Changes/src/utils.ts
+++ UMA-Bots/Major-Relayer-Liquidity-Changes/src/utils.ts
@@ -1,7 +1,6 @@
 import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
-export const FILLED_RELAY_EVENT =
-  "event FilledRelay(uint256 amount, uint256 totalFilledAmount, uint256 fillAmount, uint256 repaymentChainId, uint256 originChainId, uint256 destinationChainId, uint64 relayerFeePct, uint64 appliedRelayerFeePct, uint64 realizedLpFeePct, uint32 depositId, address destinationToken, address indexed relayer, address indexed depositor, address recipient, bool isSlowRelay)";
+export const TRANSFER_EVENT = "event Transfer(address,uint)";
 export const GOERLI_POC_SPOKEPOOL_ADDRESS = "0x36FC090AA5E827ad8AB14012804ff039A776D742";
 export const MAINNET_SPOKEPOOL = "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381";
 export const OPTIMISM_SPOKEPOOL = "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9";
