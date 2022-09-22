e4db569b8c547aff056788083a1d9c41053d86be
syahirAmali <syahiramali@gmail.com>
2022-09-22 17:29:33 +0800

    fix: findings generator changes

--- UMA-Bots/Hubpool-Liquidity/src/findings.helper.ts
+++ UMA-Bots/Hubpool-Liquidity/src/findings.helper.ts
@@ -1,45 +1,22 @@
 import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
 export function createFinding(
-  eventType: string,
   l1Token: string,
   oldAmount: number,
-  newAmount: number,
-  alert = false
+  currentAmount: number,
 ): Finding {
-  if (eventType === "LiquidityAdded") {
-    return Finding.from({
-      name: `UMA (Accross-Protocol) HubPool LiquidityAdded Event Emission`,
-      description: alert
-        ? `New value of liquidity is lower than expected!`
-        : `Liquidity has been added into the HubPool.`,
-      alertId: alert ? "UMA-6-1" : "UMA-6-2",
-      severity: alert ? FindingSeverity.High : FindingSeverity.Info,
-      type: alert ? FindingType.Exploit : FindingType.Info,
-      protocol: "UMA",
-      metadata: {
-        l1Token: l1Token,
-        oldAmount: oldAmount.toString(),
-        newAmount: newAmount.toString(),
-        changeInAmount: (newAmount - oldAmount).toString(),
-      },
-    });
-  } else {
-    return Finding.from({
-      name: `UMA (Accross-Protocol) HubPool LiquidityRemoved Event Emission`,
-      description: alert
-        ? `New value of liquidity is higher than expected`
-        : `Liquidity has been removed from the HubPool.`,
-      alertId: alert ? "UMA-6-3" : "UMA-6-4",
-      severity: alert ? FindingSeverity.High : FindingSeverity.Info,
-      type: alert ? FindingType.Exploit : FindingType.Info,
-      protocol: "UMA",
-      metadata: {
-        l1Token: l1Token,
-        oldAmount: oldAmount.toString(),
-        newAmount: newAmount.toString(),
-        changeInAmount: (newAmount - oldAmount).toString(),
-      },
-    });
-  }
+  return Finding.from({
+    name: "UMA (Accross-Protocol) HubPool LiquidityRemoved Event Emission",
+    description: "Liquidity from the HubPool Contract has dropped by more than 10% over the last 24 hours",
+    alertId: "UMA-6-1",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    protocol: "UMA",
+    metadata: {
+      l1Token: l1Token.toLocaleLowerCase(),
+      oldAmount: oldAmount.toString(),
+      currentAmount: currentAmount.toString(),
+      changeInAmount: (currentAmount - oldAmount).toString(),
+    },
+  });
 }
