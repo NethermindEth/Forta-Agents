b8456f87e1e4c00f6c3408c0e78b360273b5c244
syahirAmali <syahiramali@gmail.com>
2022-09-22 17:30:21 +0800

    fix: removed unused constants

--- UMA-Bots/Hubpool-Liquidity/src/utils.ts
+++ UMA-Bots/Hubpool-Liquidity/src/utils.ts
@@ -1,16 +1,12 @@
 import { Interface } from "ethers/lib/utils";
 
-const HUBPOOL_EVENTS = [
-  "event LiquidityRemoved(address indexed l1Token,uint256 amount,uint256 lpTokensBurnt,address indexed liquidityProvider)",
-  "event LiquidityAdded(address indexed l1Token,uint256 amount,uint256 lpTokensMinted,address indexed liquidityProvider)",
-];
+const HUBPOOL_EVENTS = "event LiquidityRemoved(address indexed l1Token,uint256 amount,uint256 lpTokensBurnt,address indexed liquidityProvider)";
 
-const LIQUIDITY_REMOVED_IFACE: Interface = new Interface([HUBPOOL_EVENTS[0]]);
-const LIQUIDITY_ADDED_IFACE: Interface = new Interface([HUBPOOL_EVENTS[1]]);
+const LIQUIDITY_REMOVED_IFACE: Interface = new Interface([HUBPOOL_EVENTS]);
 
 const HUBPOOL_ADDRESS = "0xc186fA914353c44b2E33eBE05f21846F1048bEda";
 
 const TOKEN_ABI: string = "function balanceOf(address account) external view returns (uint256)";
 const TOKEN_IFACE: Interface = new Interface([TOKEN_ABI]);
 
-export { HUBPOOL_ADDRESS, HUBPOOL_EVENTS, LIQUIDITY_ADDED_IFACE, LIQUIDITY_REMOVED_IFACE, TOKEN_IFACE };
+export { HUBPOOL_ADDRESS, HUBPOOL_EVENTS, LIQUIDITY_REMOVED_IFACE, TOKEN_IFACE };
