2350b396f7e732692173d45d28cf28a1f278d962
syahirAmali <syahiramali@gmail.com>
2022-09-22 17:29:46 +0800

    fix: changed to new network manager

--- UMA-Bots/Hubpool-Liquidity/src/network.ts
+++ UMA-Bots/Hubpool-Liquidity/src/network.ts
@@ -1,37 +1,14 @@
-interface NetworkData {
+import { Network } from "forta-agent";
+
+export interface NetworkData {
   hubPoolAddress: string;
 }
 
-const ETH_MAINNET_DATA: NetworkData = {
-  hubPoolAddress: "0xc186fA914353c44b2E33eBE05f21846F1048bEda",
-};
-
-const GOERLI_TESTNET_DATA: NetworkData = {
-  hubPoolAddress: "0xC9bea435388B76e627Ff99AD1187e520299E0656", // MockHubpool on Goerli testnet
-};
-
-export const NETWORK_MAP: Record<number, NetworkData> = {
-  1: ETH_MAINNET_DATA,
-  5: GOERLI_TESTNET_DATA,
-};
-
-export default class NetworkManager implements NetworkData {
-  public hubPoolAddress: string;
-  networkMap: Record<number, NetworkData>;
-
-  constructor(networkMap: Record<number, NetworkData>) {
-    this.hubPoolAddress = "";
-    this.networkMap = networkMap;
-  }
-
-  public setNetwork(networkId: number) {
-    try {
-      const { hubPoolAddress } = this.networkMap[networkId];
-      this.hubPoolAddress = hubPoolAddress;
-    } catch {
-      // The bot is run in a network not defined in the networkMap.
-      // There's no contract deployed in that network.
-      throw new Error("You are running the bot in a non supported network");
-    }
-  }
+export const NetworkManagerData: Record<number, NetworkData> = {
+  [Network.MAINNET]: {
+    hubPoolAddress: "0xc186fA914353c44b2E33eBE05f21846F1048bEda",
+  },
+  [Network.GOERLI]: {
+    hubPoolAddress: "0x62DF0cDE1eB67D2c3f2928A591f3ABc170FE42Ba", // MockHubpool on Goerli testnet
+  },
 }
