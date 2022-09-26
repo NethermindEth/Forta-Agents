ebe136b7aa137d25189bb6666d36d552efddd498
syahirAmali <syahiramali@gmail.com>
2022-09-26 13:07:27 +0800

    Fix: removed unsused abi

--- UMA-Bots/Hubpool-Liquidity/src/abi.ts
+++
@@ -1,9 +0,0 @@
-export const L1_TOKEN_ABI = [
-  {
-    inputs: [{ internalType: "address", name: "account", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-];
