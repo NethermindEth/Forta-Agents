9bb14b1e79792b71d59c2558f63f7dadc1934863
Tayfun Sur <tayfunsur@gmail.com>
2022-09-29 14:43:27 +0300

    tests: Add a test suite for balance fetcher

---
+++ UMA-Bots/Large-Relayer-Token-Balance-Change/src/balance.fetcher.spec.ts
@@ -0,0 +1,61 @@
+import { createAddress, MockEthersProvider } from "forta-agent-tools/lib/tests";
+import { BigNumber } from "ethers";
+import BalanceFetcher from "./balance.fetcher";
+import { ERC20_ABI } from "./utils";
+import { Interface } from "ethers/lib/utils";
+
+// [blockNumber, balance]
+const TEST_BALANCES: [number, BigNumber][] = [
+  [10, BigNumber.from(70)],
+  [20, BigNumber.from(100)],
+  [30, BigNumber.from(100)],
+  [40, BigNumber.from(120)],
+  [50, BigNumber.from(240)],
+];
+
+// [blockNumber, symbol]
+const TEST_SYMBOLS: [number, string][] = [
+  [10, "AAA"],
+  [20, "BBB"],
+  [30, "CCC"],
+  [40, "DDD"],
+  [50, "EEE"],
+];
+
+const MONITORED_ADDRESS = createAddress("0xa1");
+const tokenAddress = createAddress("0xa2");
+const BALANCE_IFACE = new Interface(ERC20_ABI);
+
+describe("BalanceFetcher tests suite", () => {
+  const mockProvider: MockEthersProvider = new MockEthersProvider();
+  let fetcher: BalanceFetcher;
+
+  beforeAll(() => {
+    fetcher = new BalanceFetcher(mockProvider as any);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should fetch balance and use cache correctly", async () => {
+    for (let [block, balance] of TEST_BALANCES) {
+      mockProvider.addCallTo(tokenAddress, block, BALANCE_IFACE, "balanceOf", {
+        inputs: [MONITORED_ADDRESS],
+        outputs: [balance],
+      });
+      const fetchedBalance = await fetcher.getBalance(block, MONITORED_ADDRESS, tokenAddress);
+      expect(fetchedBalance).toStrictEqual(balance);
+    }
+    expect(mockProvider.call).toBeCalledTimes(5);
+
+    // clear mockProvider to use cache
+    mockProvider.clear();
+    for (let [block, balance] of TEST_BALANCES) {
+      const fetchedBalance = await fetcher.getBalance(block, MONITORED_ADDRESS, tokenAddress);
+      expect(fetchedBalance).toStrictEqual(balance);
+    }
+    expect(mockProvider.call).toBeCalledTimes(5);
+    mockProvider.clear();
+  });
+});
