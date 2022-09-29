a65053fd4884938bc9e79e0d52eb59946408fe5d
Tayfun Sur <tayfunsur@gmail.com>
2022-09-29 14:44:11 +0300

    refac: Use balance fetcher to fetch balances

--- UMA-Bots/Large-Relayer-Token-Balance-Change/src/agent.ts
+++ UMA-Bots/Large-Relayer-Token-Balance-Change/src/agent.ts
@@ -1,12 +1,11 @@
 import { Finding, HandleTransaction, ethers, Initialize, TransactionEvent, getEthersProvider } from "forta-agent";
 import { NetworkManager } from "forta-agent-tools";
-import { TRANSFER_EVENT, getFindingInstance, loadDataForToken } from "./utils";
+import { TRANSFER_EVENT, getFindingInstance } from "./utils";
 import { NetworkDataInterface, NM_DATA } from "./network";
-import LRU from "lru-cache";
 import { BigNumber } from "ethers";
+import BalanceFetcher from "./balance.fetcher";
 
 const networkManager = new NetworkManager(NM_DATA);
-const thisLru = new LRU<string, Record<string, BigNumber>>({ max: 10000 }); // token address => { wallet address => balance }
 
 export function provideInitialize(
   networkManager: NetworkManager<NetworkDataInterface>,
@@ -20,54 +19,50 @@ export function provideInitialize(
 export function provideHandleTransaction(
   transferEvent: string,
   networkManager: NetworkManager<NetworkDataInterface>,
-  passedLru: LRU<string, Record<string, BigNumber>>,
-  provider: ethers.providers.Provider
+  balanceFetcher: BalanceFetcher
 ): HandleTransaction {
   return async (txEvent: TransactionEvent) => {
     let alertThreshold = networkManager.get("alertThreshold");
+
     const findings: Finding[] = [];
     const transferEventTxns = txEvent.filterLog(transferEvent, networkManager.get("monitoredTokens")); // Transfer event transactions for monitored addresses
-    transferEventTxns.forEach(async (transferEvent) => {
-      const { from, to, value } = transferEvent.args;
-      let valueBN: BigNumber = BigNumber.from(value);
-      if (!networkManager.get("monitoredTokens").includes(transferEvent.address)) {
-        return findings;
-      }
-      if (networkManager.get("monitoredAddresses").includes(from)) {
-        if (!passedLru.has(transferEvent.address)) {
-          await loadDataForToken(networkManager, provider, passedLru, transferEvent.address);
-        }
-        console.log(transferEvent.address);
-        let prevBalance = BigNumber.from(passedLru.get(transferEvent.address)![from]);
-        console.log(prevBalance);
-        console.log(valueBN);
-        if (valueBN.mul(100).gte(prevBalance.mul(alertThreshold))) {
-          findings.push(getFindingInstance(value.toString(), from, transferEvent.address, "false"));
-        }
-        let pastDict: Record<string, BigNumber> = passedLru.get(transferEvent.address)!;
-        pastDict[from] = prevBalance.sub(valueBN);
-        passedLru.set(transferEvent.address, pastDict);
-      }
 
-      if (networkManager.get("monitoredAddresses").includes(to)) {
-        if (!passedLru.has(transferEvent.address)) {
-          await loadDataForToken(networkManager, provider, passedLru, transferEvent.address);
+    await Promise.all(
+      transferEventTxns.map(async (transferEvent) => {
+        const { from, to, value } = transferEvent.args;
+        const valueBN: BigNumber = BigNumber.from(value);
+
+        if (
+          !networkManager
+            .get("monitoredTokens")
+            .map((e) => e.toLowerCase())
+            .includes(transferEvent.address)
+        ) {
+          return findings;
         }
 
-        let prevBalance = passedLru.get(transferEvent.address)![to];
-        if (valueBN.mul(100).gte(prevBalance.mul(alertThreshold))) {
-          findings.push(getFindingInstance(value.toString(), to, transferEvent.address, "true"));
+        if (networkManager.get("monitoredAddresses").includes(from)) {
+          const prevBalance = await balanceFetcher.getBalance(txEvent.blockNumber - 1, from, transferEvent.address);
+
+          if (valueBN.mul(100).gte(prevBalance.mul(alertThreshold))) {
+            findings.push(getFindingInstance(value.toString(), from, transferEvent.address, "false"));
+          }
         }
-        let pastDict = passedLru.get(transferEvent.address)!;
-        pastDict[to] = prevBalance.add(valueBN);
-        passedLru.set(transferEvent.address, pastDict);
-      }
-    });
+
+        if (networkManager.get("monitoredAddresses").includes(to)) {
+          const prevBalance = await balanceFetcher.getBalance(txEvent.blockNumber - 1, to, transferEvent.address);
+
+          if (valueBN.mul(100).gte(prevBalance.mul(alertThreshold))) {
+            findings.push(getFindingInstance(value.toString(), to, transferEvent.address, "true"));
+          }
+        }
+      })
+    );
     return findings;
   };
 }
 
 export default {
   initialize: provideInitialize(networkManager, getEthersProvider()),
-  handleTransaction: provideHandleTransaction(TRANSFER_EVENT, networkManager, thisLru, getEthersProvider()),
+  handleTransaction: provideHandleTransaction(TRANSFER_EVENT, networkManager, new BalanceFetcher(getEthersProvider())),
 };
