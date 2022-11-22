4ae633ca07f9299982752f4ccd744401f854c900
Tayfun Sur <tayfunsur@gmail.com>
2022-11-22 16:03:04 +0300

    chore: Create agent.config

---
+++ compound-comptroller-bot/src/agent.config.ts
@@ -0,0 +1,11 @@
+import { AgentConfig } from "./utils";
+
+const CONFIG: AgentConfig = {
+  // The address of the Compound Comptroller Contract
+  compoundComptrollerAddress: "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B",
+};
+
+// Uncomment this line for the testnet tests:
+// CONFIG.compoundComptrollerAddress = "0xD2C5a5ea72A7419A7AFC87697E60796a56F2763E";
+
+export default CONFIG;
