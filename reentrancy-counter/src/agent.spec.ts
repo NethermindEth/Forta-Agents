3a153caf8495e9c9fba01d47028c83889f34f7fe
areezladhani <58789478+areezladhani@users.noreply.github.com>
2022-11-30 15:35:23 -0700

    chore: update forta-agent version.

--- reentrancy-counter/src/agent.spec.ts
+++ reentrancy-counter/src/agent.spec.ts
@@ -1,72 +1,53 @@
 import {
   Finding,
   HandleTransaction,
-  createTransactionEvent,
   Trace,
-  Transaction,
-  Receipt,
-  Block,
   TransactionEvent,
-  TraceAction,
 } from "forta-agent";
+import { TestTransactionEvent } from "forta-agent-tools/lib/test";
 import agent, { thresholds } from "./agent";
 import { createFinding, reentracyLevel } from "./agent.utils";
 
-const createTrace = (to: string, stack: number[]): Trace => {
-  return {
-    traceAddress: stack,
-    action: {
-      to: to,
-    } as TraceAction,
-  } as Trace;
-};
-
 const createTxEvent = (traces: Trace[]) =>
-  createTransactionEvent({
-    transaction: {} as Transaction,
-    receipt: {} as Receipt,
-    block: {} as Block,
-    traces: traces,
-  });
-
+  new TestTransactionEvent().addTraces(...traces);
 
 describe("Reentrancy counter agent tests suit", () => {
   const handleTransaction: HandleTransaction = agent.handleTransaction;
 
   describe("handleTransaction", () => {
-    it("Should return empty findinds if no traces provided", async () => {
+    it("Should return empty findings if no traces provided", async () => {
       const tx: TransactionEvent = createTxEvent([]);
       const findings: Finding[] = await handleTransaction(tx);
       expect(findings).toStrictEqual([]);
     });
 
-    it("Should return empty findinds if no repetition detected", async () => {
-      const tx: TransactionEvent = createTxEvent([
-        createTrace("0x0", []),         // 0x0 -- Initial call
-        createTrace("0x1", [0]),        //    Calls 0x1
-        createTrace("0x2", [0, 0]),     //    Calls 0x2
-        createTrace("0x3", [0, 0, 0]),  //    Calls 0x3
-      ]);
+    it("Should return empty findings if no repetition detected", async () => {
+      const tx: TestTransactionEvent = new TestTransactionEvent().addTraces(
+        { to: "0x0", traceAddress: [] },
+        { to: "0x1", traceAddress: [0] },
+        { to: "0x2", traceAddress: [0, 0] },
+        { to: "0x3", traceAddress: [0, 0, 0] }
+      );
       const findings: Finding[] = await handleTransaction(tx);
       expect(findings).toStrictEqual([]);
     });
 
     it("Should ignore non reentrant calls", async () => {
-      const tx: TransactionEvent = createTxEvent([
-        createTrace("0x0", []),      // 0x0 -- Initial call
-        createTrace("0x1", [0]),     //    Calls 0x1 
-        createTrace("0x1", [1]),     //    Calls 0x1
-        createTrace("0x2", [1, 0]),  //       Calls 0x2
-        createTrace("0x2", [1, 1]),  //       Calls 0x2
-        createTrace("0x2", [1, 2]),  //       Calls 0x2
-        createTrace("0x2", [1, 3]),  //       Calls 0x2
-        createTrace("0x1", [2]),     //    Calls 0x1
-        createTrace("0x1", [3]),     //    Calls 0x1
-        createTrace("0x2", [4]),     //    Calls 0x2
-        createTrace("0x3", [5]),     //    Calls 0x3
-        createTrace("0x1", [6]),     //    Calls 0x1
-        createTrace("0x1", [7]),     //    Calls 0x1
-      ]);
+      const tx: TestTransactionEvent = new TestTransactionEvent().addTraces(
+        { to: "0x0", traceAddress: [] },
+        { to: "0x1", traceAddress: [0] },
+        { to: "0x1", traceAddress: [1] },
+        { to: "0x2", traceAddress: [1, 0] },
+        { to: "0x2", traceAddress: [1, 1] },
+        { to: "0x2", traceAddress: [1, 2] },
+        { to: "0x2", traceAddress: [1, 3] },
+        { to: "0x1", traceAddress: [2] },
+        { to: "0x1", traceAddress: [3] },
+        { to: "0x2", traceAddress: [4] },
+        { to: "0x2", traceAddress: [5] },
+        { to: "0x1", traceAddress: [6] },
+        { to: "0x1", traceAddress: [7] }
+      );
       const findings: Finding[] = await handleTransaction(tx);
       expect(findings).toStrictEqual([]);
     });
@@ -75,35 +56,32 @@ describe("Reentrancy counter agent tests suit", () => {
       // 0x0, 0x1, 0x3, 0x5, 0x6 called less than 3 times
       // 0x2 called 3 times
       // 0x4 called 5 times
-      const tx: TransactionEvent = createTxEvent([
-        createTrace("0x0", []),                                // 0x0 -- Initial call
-        createTrace("0x1", [0]),                               //    Calls 0x1
-        createTrace("0x2", [0, 0]),                            //      Calls 0x2
-        createTrace("0x3", [0, 0, 0]),                         //        Calls 0x3
-        createTrace("0x2", [0, 0, 0, 0]),                      //          Calls 0x2
-        createTrace("0x3", [0, 0, 0, 0, 0]),                   //            Calls 0x3
-        createTrace("0x2", [0, 0, 0, 0, 0, 0]),                //              Calls 0x2
-        createTrace("0x4", [0, 1]),                            //       Calls 0x4
-        createTrace("0x5", [0, 1, 0]),                         //         Calls 0x5
-        createTrace("0x6", [0, 1, 0, 0]),                      //           Calls 0x6
-        createTrace("0x4", [0, 1, 0, 0, 0]),                   //             Calls 0x4
-        createTrace("0x5", [0, 1, 0, 0, 0, 0]),                //               Calls 0x5
-        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0]),             //                 Calls 0x4
-        createTrace("0x6", [0, 1, 0, 0, 0, 0, 0, 0]),          //                   Calls 0x6
-        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0, 0, 0]),       //                     Calls 0x4
-        createTrace("0x1", [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),    //                       Calls 0x1
-        createTrace("0x4", [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]), //                         Calls 0x4       
-      ]);
+      const tx: TestTransactionEvent = new TestTransactionEvent().addTraces(
+        { to: "0x0", traceAddress: [] },
+        { to: "0x1", traceAddress: [0] },
+        { to: "0x2", traceAddress: [0, 0] },
+        { to: "0x3", traceAddress: [0, 0, 0] },
+        { to: "0x2", traceAddress: [0, 0, 0, 0] },
+        { to: "0x3", traceAddress: [0, 0, 0, 0, 0] },
+        { to: "0x2", traceAddress: [0, 0, 0, 0, 0, 0] },
+        { to: "0x4", traceAddress: [0, 1] },
+        { to: "0x5", traceAddress: [0, 1, 0] },
+        { to: "0x6", traceAddress: [0, 1, 0, 0] },
+        { to: "0x4", traceAddress: [0, 1, 0, 0, 0] },
+        { to: "0x5", traceAddress: [0, 1, 0, 0, 0, 0] },
+        { to: "0x4", traceAddress: [0, 1, 0, 0, 0, 0, 0] },
+        { to: "0x6", traceAddress: [0, 1, 0, 0, 0, 0, 0, 0] },
+        { to: "0x4", traceAddress: [0, 1, 0, 0, 0, 0, 0, 0, 0] },
+        { to: "0x1", traceAddress: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { to: "0x4", traceAddress: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+      );
       const [report0x1, severity0x1] = reentracyLevel(1, thresholds);
       const [report0x2, severity0x2] = reentracyLevel(3, thresholds);
       const [report0x4, severity0x4] = reentracyLevel(5, thresholds);
       const expected: Finding[] = [];
-      if(report0x1) 
-        expected.push(createFinding("0x1", 1, severity0x1))
-      if(report0x2) 
-        expected.push(createFinding("0x2", 3, severity0x2))
-      if(report0x4) 
-        expected.push(createFinding("0x4", 5, severity0x4))
+      if (report0x1) expected.push(createFinding("0x1", 1, severity0x1));
+      if (report0x2) expected.push(createFinding("0x2", 3, severity0x2));
+      if (report0x4) expected.push(createFinding("0x4", 5, severity0x4));
 
       const findings: Finding[] = await handleTransaction(tx);
       expect(findings).toEqual(expect.arrayContaining(expected));
