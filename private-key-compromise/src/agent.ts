8b3e9465f1a3a705a6ac9e6306c0cccfcc4cf712
Tayfun Sur <tayfunsur@gmail.com>
2023-03-24 08:48:22 +0300

    feat: Implement native token drain logic

--- private-key-compromise/src/agent.ts
+++ private-key-compromise/src/agent.ts
@@ -1,78 +1,150 @@
 import {
-  BlockEvent,
   Finding,
   Initialize,
-  HandleBlock,
-  HandleTransaction,
-  HandleAlert,
-  AlertEvent,
   TransactionEvent,
-  FindingSeverity,
-  FindingType,
+  ethers,
+  getEthersProvider,
 } from "forta-agent";
+import { NetworkData } from "./utils";
+import { NetworkManager } from "forta-agent-tools";
+import { createFinding } from "./findings";
+import { PersistenceHelper } from "./persistence.helper";
+import CONFIG from "./agent.config";
+
+const DATABASE_URL = "https://research.forta.network/database/bot/";
+const PK_COMP_TXNS_KEY = "nm-private-key-compromise-bot-key";
+
+let chainId: string;
+let pkCompValueRecord: {};
 
 export const ERC20_TRANSFER_EVENT =
   "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = [];
+const networkManager = new NetworkManager<NetworkData>(CONFIG);
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+export const provideInitialize = (
+  networkManager: NetworkManager<NetworkData>,
+  provider: ethers.providers.Provider,
+  persistenceHelper: PersistenceHelper,
+  pkCompValueKey: any
+): Initialize => {
+  return async () => {
+    await networkManager.init(provider);
+    chainId = networkManager.getNetwork().toString();
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(ERC20_TRANSFER_EVENT, TETHER_ADDRESS);
+    pkCompValueRecord = await persistenceHelper.load(pkCompValueKey.concat("-", chainId));
 
-  tetherTransferEvents.forEach(transferEvent => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+    console.log("init", pkCompValueRecord);
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
-      findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to,
-            from,
-          },
-        })
-      );
-      findingsCount++;
-    }
-  });
-
-  return findings;
+    // allTxnsWithValue = await persistenceHelper.load(anyValueKey.concat("-", chainId));
+  };
 };
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
+export const provideHandleTransaction =
+  (
+    provider: ethers.providers.Provider,
+    persistenceHelper: PersistenceHelper,
+    pkCompValueKey: string
+  ) =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+    const transferEvents = txEvent.filterLog(ERC20_TRANSFER_EVENT);
 
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
+    // const value = ethers.utils.formatEther(txEvent.transaction.value);
+    const {
+      hash,
+      from,
+      to,
+      transaction: { value, data },
+    } = txEvent;
+
+    // check for ERC20 transfers
+    // await Promise.all(transferEvents.map(async transfer => {}));
+
+    // check for native token transfers
+
+    if (to && value != "0x0" && data === "0x") {
+      console.log("hash", hash);
+      console.log("from", from);
+      console.log("to", to);
+      console.log("value", value);
+      console.log("data", data);
+
+      // const balance = ethers.utils.formatEther(
+      //   await provider.getBalance(txEvent.transaction.from)
+      // );
+
+      const balance = await provider.getBalance(txEvent.transaction.from);
+      console.log("balance", ethers.utils.formatEther(balance));
+
+      // if the account is drained
+      if (
+        balance.lt(
+          ethers.BigNumber.from(ethers.utils.parseEther(networkManager.get("threshold")))
+        )
+      ) {
+        const records: any = await persistenceHelper.load(
+          pkCompValueKey.concat("-", chainId)
+        );
+        console.log("records", records);
+
+        let transferObj: any = {};
+
+        // if this is the first time and there's no record in db
+        if (!Object.keys(records).length) {
+          transferObj[to] = [from];
+
+          await persistenceHelper.persist(
+            transferObj,
+            pkCompValueKey.concat("-", chainId)
+          );
+        } else {
+          const records: any = await persistenceHelper.load(
+            pkCompValueKey.concat("-", chainId)
+          );
+
+          if (records[to] && !records[to].includes(from)) {
+            records[to].push(from);
+            transferObj[to] = records[to];
+
+            await persistenceHelper.persist(
+              transferObj,
+              pkCompValueKey.concat("-", chainId)
+            );
+          } else if (!records[to]) {
+            records[to] = [from];
+            transferObj = records;
+
+            await persistenceHelper.persist(
+              transferObj,
+              pkCompValueKey.concat("-", chainId)
+            );
+          }
+        }
+
+        // findings.push(createFinding(hash, [from], to, 0.1));
+      }
+
+      console.log(
+        "zort",
+        await persistenceHelper.load(pkCompValueKey.concat("-", chainId))
+      );
+    }
+
+    return findings;
+  };
 
 export default {
-  // initialize,
-  handleTransaction,
-  // handleBlock,
-  // handleAlert
+  initialize: provideInitialize(
+    networkManager,
+    getEthersProvider(),
+    new PersistenceHelper(DATABASE_URL),
+    PK_COMP_TXNS_KEY
+  ),
+  handleTransaction: provideHandleTransaction(
+    getEthersProvider(),
+    new PersistenceHelper(DATABASE_URL),
+    PK_COMP_TXNS_KEY
+  ),
 };
