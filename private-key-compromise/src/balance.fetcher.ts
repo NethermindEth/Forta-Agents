441c7e3c0ac73a26ecf151383bd726db2aa05706
Tayfun Sur <tayfunsur@gmail.com>
2023-04-01 10:34:27 +0300

    refac: Update balance fetcher

--- private-key-compromise/src/balance.fetcher.ts
+++ private-key-compromise/src/balance.fetcher.ts
@@ -1,35 +1,33 @@
 import { providers, Contract, BigNumber } from "ethers";
+import { Interface } from "ethers/lib/utils";
 import LRU from "lru-cache";
 import { BALANCEOF_ABI } from "./utils";
 
 export default class BalanceFetcher {
   readonly provider: providers.Provider;
   private cache: LRU<string, Promise<BigNumber>>;
-  private tokenAddress: string;
   private tokenContract: Contract;
 
-  constructor(provider: providers.Provider, tokenAddress: string) {
+  constructor(provider: providers.Provider) {
     this.provider = provider;
     this.cache = new LRU<string, Promise<BigNumber>>({
       max: 10000,
     });
-    this.tokenAddress = tokenAddress;
-    this.tokenContract = new Contract(tokenAddress, BALANCEOF_ABI, provider);
+    this.tokenContract = new Contract("", new Interface(BALANCEOF_ABI), this.provider);
   }
 
   public async getBalanceOf(
     victimAddress: string,
+    tokenAddress: string,
     block: string | number
   ): Promise<BigNumber> {
-    const key: string = `${this.tokenAddress}-${victimAddress}-${block}`;
+    const token = this.tokenContract.attach(tokenAddress);
+
+    const key: string = `${tokenAddress}-${victimAddress}-${block}`;
 
     if (this.cache.has(key)) return this.cache.get(key) as Promise<BigNumber>;
 
-    if (this.tokenContract.address != this.tokenAddress) {
-      this.tokenContract = new Contract(this.tokenAddress, BALANCEOF_ABI, this.provider);
-    }
-
-    const balance = await this.tokenContract.balanceOf(victimAddress, {
+    const balance = await token.balanceOf(victimAddress, {
       blockTag: block,
     });
 
