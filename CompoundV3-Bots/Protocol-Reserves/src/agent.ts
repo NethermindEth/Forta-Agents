7ecc6a2549da5a6192b914d4b039c9975073e5ec
Hyodar <franco.b.gomes@gmail.com>
2023-05-02 12:12:18 -0300

    feat: Add bot implementation

--- CompoundV3-Bots/Protocol-Reserves/src/agent.ts
+++ CompoundV3-Bots/Protocol-Reserves/src/agent.ts
@@ -1,83 +1,46 @@
-import {
-  BlockEvent,
-  Finding,
-  Initialize,
-  HandleBlock,
-  HandleTransaction,
-  HandleAlert,
-  AlertEvent,
-  TransactionEvent,
-  FindingSeverity,
-  FindingType,
-} from "forta-agent";
+import { ethers, Finding, getEthersProvider, Initialize, HandleTransaction, TransactionEvent } from "forta-agent";
+import CONFIG from "./agent.config";
+import { createApproveFinding, createPauseActionFinding, createWithdrawReservesFinding } from "./finding";
+import { NetworkData } from "./utils";
+import { NetworkManager } from "forta-agent-tools";
+import { APPROVAL_ABI, PAUSE_ACTION_ABI, WITHDRAW_RESERVES_ABI } from "./constants";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+const networkManager = new NetworkManager(CONFIG);
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
-
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
-
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
-
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
-
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
-      findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to,
-            from,
-          },
-        })
-      );
-      findingsCount++;
-    }
-  });
-
-  return findings;
+export const provideInitialize = (
+  networkManager: NetworkManager<NetworkData>,
+  provider: ethers.providers.Provider
+): Initialize => {
+  return async () => {
+    await networkManager.init(provider);
+  };
 };
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
+export const provideHandleTransaction = (networkManager: NetworkManager<NetworkData>): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+    const chainId = networkManager.getNetwork();
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+    const pauseLogs = txEvent.filterLog(PAUSE_ACTION_ABI, networkManager.get("cometAddresses"));
+    pauseLogs.forEach((log) => findings.push(createPauseActionFinding(log, chainId)));
 
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
+    const withdrawLogs = txEvent.filterLog(WITHDRAW_RESERVES_ABI, networkManager.get("cometAddresses"));
+    withdrawLogs.forEach((log) => findings.push(createWithdrawReservesFinding(log, chainId)));
+
+    const approvalLogs = txEvent.filterLog(APPROVAL_ABI);
+    approvalLogs
+      .filter((log) =>
+        networkManager
+          .get("cometAddresses")
+          .find((cometAddress) => cometAddress.toLowerCase() === log.args.owner.toLowerCase())
+      )
+      .forEach((log) => findings.push(createApproveFinding(log, chainId)));
+
+    return findings;
+  };
+};
 
 export default {
-  // initialize,
-  handleTransaction,
-  // handleBlock,
-  // handleAlert
+  initialize: provideInitialize(networkManager, getEthersProvider()),
+  handleTransaction: provideHandleTransaction(networkManager),
 };
---
+++ CompoundV3-Bots/Protocol-Reserves/src/finding.ts
@@ -0,0 +1,56 @@
+import { ethers, Finding, FindingSeverity, FindingType, LogDescription, Network } from "forta-agent";
+
+export function createPauseActionFinding(log: LogDescription, chainId: number): Finding {
+  return Finding.from({
+    name: "Pause action on Comet contract",
+    description: "A pause action was executed in a Comet contract",
+    alertId: "COMP-2-1",
+    protocol: "Compound",
+    type: FindingType.Info,
+    severity: FindingSeverity.Medium,
+    metadata: {
+      chain: Network[chainId] || chainId.toString(),
+      comet: ethers.utils.getAddress(log.address),
+      supplyPaused: log.args.supplyPaused.toString(),
+      transferPaused: log.args.transferPaused.toString(),
+      withdrawPaused: log.args.withdrawPaused.toString(),
+      absorbPaused: log.args.absorbPaused.toString(),
+      buyPaused: log.args.buyPaused.toString(),
+    },
+  });
+}
+
+export function createWithdrawReservesFinding(log: LogDescription, chainId: number): Finding {
+  return Finding.from({
+    name: "Withdraw reserves action on Comet contract",
+    description: "A withdraw reserves action was executed in a Comet contract",
+    alertId: "COMP-2-2",
+    protocol: "Compound",
+    type: FindingType.Info,
+    severity: FindingSeverity.High,
+    metadata: {
+      chain: Network[chainId] || chainId.toString(),
+      comet: ethers.utils.getAddress(log.address),
+      to: ethers.utils.getAddress(log.args.to),
+      amount: log.args.amount.toString(),
+    },
+  });
+}
+
+export function createApproveFinding(log: LogDescription, chainId: number): Finding {
+  return Finding.from({
+    name: "Token approval from Comet contract",
+    description: "A token approval was emitted from a Comet contract",
+    alertId: "COMP-2-3",
+    protocol: "Compound",
+    type: FindingType.Info,
+    severity: FindingSeverity.High,
+    metadata: {
+      chain: Network[chainId] || chainId.toString(),
+      comet: ethers.utils.getAddress(log.args.owner),
+      token: ethers.utils.getAddress(log.address),
+      spender: ethers.utils.getAddress(log.args.spender),
+      amount: log.args.amount.toString(),
+    },
+  });
+}
