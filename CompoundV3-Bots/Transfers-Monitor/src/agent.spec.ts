f229517cff688478de385fee72301762d07d7bc1
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-11 17:36:49 -0300

    fix: Fix finding desscription on tests

--- CompoundV3-Bots/Transfers-Monitor/src/agent.spec.ts
+++ CompoundV3-Bots/Transfers-Monitor/src/agent.spec.ts
@@ -19,7 +19,7 @@ function mockCreateTransferFinding(
   return Finding.from({
     name: "Base token transfer on Comet contract",
     description:
-      "A base token transfer was done to a Comet contract, without a matching Supply, BuyCollateral action",
+      "A base token transfer was directed to a Comet contract, without a matching Supply, BuyCollateral event",
     alertId: "COMP2-3",
     protocol: "Compound",
     type: FindingType.Info,
