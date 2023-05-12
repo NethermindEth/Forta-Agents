c3e787d83507991f3f2f1022468b2f401c33581a
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-12 01:43:49 -0300

    chore: Remove extra whitespace in ABI

--- CompoundV3-Bots/Timelock-Transaction-Execution/src/constants.ts
+++ CompoundV3-Bots/Timelock-Transaction-Execution/src/constants.ts
@@ -1,6 +1,6 @@
 export const LOCAL_TIMELOCK_ABI = "function localTimelock() public view returns (address)";
 export const EXECUTE_TRANSACTION_ABI =
-  "event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta)";
+  "event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature, bytes data, uint eta)";
 export const PROPOSAL_EXECUTED_ABI = "event ProposalExecuted(uint indexed id)";
 export const PROPOSAL_CREATED_ABI =
   "event ProposalCreated(address indexed rootMessageSender, uint id, address[] targets, uint[] values, string[] signatures, bytes[] calldatas, uint eta)";
