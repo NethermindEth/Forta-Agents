f17220f20a451f1bcfe51a4584df9f56437f284f
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-12 01:45:10 -0300

    feat: Add log args typing & utility to get chunks of a range

--- CompoundV3-Bots/Timelock-Transaction-Execution/src/utils.ts
+++ CompoundV3-Bots/Timelock-Transaction-Execution/src/utils.ts
@@ -1,3 +1,5 @@
+import { ethers } from "forta-agent";
+
 export interface NetworkData {
   bridgeReceiverAddress: string;
   creationFetchingBlockRange: number;
@@ -5,3 +7,32 @@ export interface NetworkData {
 }
 
 export type AgentConfig = Record<number, NetworkData>;
+
+export interface ExecuteTransactionArgs {
+  txHash: string;
+  target: string;
+  value: ethers.BigNumber;
+  signature: string;
+  data: string;
+  eta: ethers.BigNumber;
+}
+
+export interface ProposalCreatedArgs {
+  rootMessageSender: string;
+  id: ethers.BigNumber;
+  targets: string[];
+  values: ethers.BigNumber[];
+  signatures: string[];
+  calldatas: string[];
+  eta: ethers.BigNumber;
+}
+
+export function rangeChunks(from: number, to: number, chunkSize: number): Array<[from: number, to: number]> {
+  const resp: Array<[from: number, to: number]> = [];
+
+  for (let i = from; i < to; i += chunkSize) {
+    resp.push([i, Math.min(i + chunkSize - 1, to)]);
+  }
+
+  return resp;
+}
