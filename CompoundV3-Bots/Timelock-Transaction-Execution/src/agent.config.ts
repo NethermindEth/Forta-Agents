bc34e026cdc4502179c8d5e928afb29820b81d22
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-12 17:36:34 -0300

    chore: Change Polygon block range in bot config

--- CompoundV3-Bots/Timelock-Transaction-Execution/src/agent.config.ts
+++ CompoundV3-Bots/Timelock-Transaction-Execution/src/agent.config.ts
@@ -4,7 +4,7 @@ import { AgentConfig } from "./utils";
 const CONFIG: AgentConfig = {
   [Network.POLYGON]: {
     bridgeReceiverAddress: "0x18281dfC4d00905DA1aaA6731414EABa843c468A",
-    creationFetchingBlockRange: 10_000,
+    creationFetchingBlockRange: 100_000,
     creationFetchingBlockStep: 5_000,
   },
 };
