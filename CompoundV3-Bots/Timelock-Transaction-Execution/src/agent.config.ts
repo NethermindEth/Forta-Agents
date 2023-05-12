d324bf9fbb63e923348ed0f8a1328a21afe84af3
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-12 00:32:36 -0300

    refac: Remove cometAddresses config field

--- CompoundV3-Bots/Timelock-Transaction-Execution/src/agent.config.ts
+++ CompoundV3-Bots/Timelock-Transaction-Execution/src/agent.config.ts
@@ -3,7 +3,6 @@ import { AgentConfig } from "./utils";
 
 const CONFIG: AgentConfig = {
   [Network.POLYGON]: {
-    cometAddresses: ["0xF25212E676D1F7F89Cd72fFEe66158f541246445"],
     bridgeReceiverAddress: "0x18281dfC4d00905DA1aaA6731414EABa843c468A",
   },
 };
--- CompoundV3-Bots/Timelock-Transaction-Execution/src/utils.ts
+++ CompoundV3-Bots/Timelock-Transaction-Execution/src/utils.ts
@@ -1,5 +1,4 @@
 export interface NetworkData {
-  cometAddresses: string[];
   bridgeReceiverAddress: string;
 }
 
