6241c83b30b652fd6c06eb0d59cafec61d495a97
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-16 01:26:07 -0300

    refac: General refactorings in fetcher unit tests

--- CompoundV3-Bots/Reserves-Monitor/src/dataFetcher.spec.ts
+++ CompoundV3-Bots/Reserves-Monitor/src/dataFetcher.spec.ts
@@ -1,45 +1,44 @@
 import { Network, ethers } from "forta-agent";
-import { NetworkManager, createAddress } from "forta-agent-tools";
+import { NetworkManager, createChecksumAddress } from "forta-agent-tools";
 import { MockEthersProvider } from "forta-agent-tools/lib/test";
-import { Interface } from "ethers/lib/utils";
-import { BigNumber } from "ethers";
 import Fetcher from "./dataFetcher";
 import { RESERVES_ABI, TARGET_RESERVES_ABI } from "./constants";
 import { NetworkData } from "./utils";
 
-const IFACE = new Interface([RESERVES_ABI, TARGET_RESERVES_ABI]);
-const network = Network.MAINNET;
+const bn = ethers.BigNumber.from;
+const addr = createChecksumAddress;
+
+const IFACE = new ethers.utils.Interface([RESERVES_ABI, TARGET_RESERVES_ABI]);
+const NETWORK = Network.MAINNET;
 
 describe("Fetcher test suite", () => {
   let mockProvider: MockEthersProvider;
+  let provider: ethers.providers.Provider;
   let networkManager: NetworkManager<NetworkData>;
   let fetcher: Fetcher;
 
   // Format: [targetReserves, reserves, blockNumber]
-  const TEST_CASES: [BigNumber, BigNumber, number][] = [
-    [BigNumber.from("100"), BigNumber.from("800"), 1],
-    [BigNumber.from("200"), BigNumber.from("900"), 2],
-    [BigNumber.from("300"), BigNumber.from("100"), 3],
-    [BigNumber.from("400"), BigNumber.from("400"), 4],
+  const TEST_CASES: [ethers.BigNumber, ethers.BigNumber, number][] = [
+    [bn("100"), bn("800"), 1],
+    [bn("200"), bn("900"), 2],
+    [bn("300"), bn("100"), 3],
+    [bn("400"), bn("400"), 4],
   ];
 
-  const COMET_ADDRESSES = [
-    createAddress("0x11"),
-    createAddress("0x12"),
-    createAddress("0x13"),
-  ];
+  const COMET_ADDRESSES = [addr("0x11"), addr("0x12"), addr("0x13")];
 
   const ALERT_INTERVAL = 1000;
 
   const DEFAULT_CONFIG = {
-    [network]: {
+    [NETWORK]: {
       cometAddresses: COMET_ADDRESSES,
       alertInterval: ALERT_INTERVAL,
     },
   };
+
   function createGetReservesCall(
     comet: string,
-    reserves: BigNumber,
+    reserves: ethers.BigNumber,
     blockNumber: number
   ) {
     return mockProvider.addCallTo(comet, blockNumber, IFACE, "getReserves", {
@@ -47,9 +46,10 @@ describe("Fetcher test suite", () => {
       outputs: [reserves],
     });
   }
+
   function createTargetReservesCall(
     comet: string,
-    targetReserves: BigNumber,
+    targetReserves: ethers.BigNumber,
     blockNumber: number
   ) {
     return mockProvider.addCallTo(comet, blockNumber, IFACE, "targetReserves", {
@@ -60,19 +60,14 @@ describe("Fetcher test suite", () => {
 
   beforeEach(async () => {
     mockProvider = new MockEthersProvider();
-    mockProvider.setNetwork(network);
+    mockProvider.setNetwork(NETWORK);
+    provider = mockProvider as unknown as ethers.providers.Provider;
 
     networkManager = new NetworkManager(DEFAULT_CONFIG);
-    await networkManager.init(
-      mockProvider as unknown as ethers.providers.Provider
-    );
-    networkManager.setNetwork(network);
+    await networkManager.init(provider);
 
     fetcher = new Fetcher();
-    fetcher.loadContracts(
-      networkManager,
-      mockProvider as unknown as ethers.providers.Provider
-    );
+    fetcher.loadContracts(networkManager, provider);
   });
 
   it("should fetch correct reserves amounts for each contract and block", async () => {
@@ -80,10 +75,7 @@ describe("Fetcher test suite", () => {
       for (const [, reserves, blockNumber] of TEST_CASES) {
         createGetReservesCall(comet, reserves, blockNumber);
 
-        const fetchedReserves: BigNumber = await fetcher.getReserves(
-          comet,
-          blockNumber
-        );
+        const fetchedReserves = await fetcher.getReserves(comet, blockNumber);
 
         expect(fetchedReserves).toStrictEqual(reserves);
       }
@@ -95,8 +87,10 @@ describe("Fetcher test suite", () => {
       for (const [targetReserves, , blockNumber] of TEST_CASES) {
         createTargetReservesCall(comet, targetReserves, blockNumber);
 
-        const fetchedTargetReserves: BigNumber =
-          await fetcher.getTargetReserves(comet, blockNumber);
+        const fetchedTargetReserves = await fetcher.getTargetReserves(
+          comet,
+          blockNumber
+        );
 
         expect(fetchedTargetReserves).toStrictEqual(targetReserves);
       }
