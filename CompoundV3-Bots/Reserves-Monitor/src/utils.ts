28827332d11d5abf15924b5472defad8f0d8c57b
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-15 23:25:00 -0300

    chore: Remove unused import

--- CompoundV3-Bots/Reserves-Monitor/src/utils.ts
+++ CompoundV3-Bots/Reserves-Monitor/src/utils.ts
@@ -1,5 +1,3 @@
-import { BigNumber } from "ethers";
-
 export interface NetworkData {
   cometAddresses: string[]; // List of comet contract adddresses for each network.
   alertFrequency: number; // Frequency of emitting alerts for the same contract.
