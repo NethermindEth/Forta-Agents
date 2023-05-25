c1cb9d5914ae9690a18e1d673a8cd935872c99d9
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-25 14:55:52 -0300

    refac: Slight change on finding name and description

--- CompoundV3-Bots/Transfers-Monitor/src/finding.ts
+++ CompoundV3-Bots/Transfers-Monitor/src/finding.ts
@@ -7,9 +7,8 @@ export function createTransferFinding(
   amount: ethers.BigNumberish
 ): Finding {
   return Finding.from({
-    name: "Base token transfer on Comet contract",
-    description:
-      "A base token transfer was directed to a Comet contract without a matching Supply or BuyCollateral event",
+    name: "Direct base token transfer to Comet contract",
+    description: "A base token transfer was made to a Comet contract without a matching Supply or BuyCollateral event",
     alertId: "COMP2-3-1",
     protocol: "Compound",
     type: FindingType.Info,
