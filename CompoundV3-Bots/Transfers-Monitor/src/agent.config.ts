4717af1a1483b08ae3d433b4753117889e8883d2
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-25 14:34:53 -0300

    docs: Add quick descriptions to config fields

--- CompoundV3-Bots/Transfers-Monitor/src/agent.config.ts
+++ CompoundV3-Bots/Transfers-Monitor/src/agent.config.ts
@@ -5,7 +5,10 @@ const CONFIG: AgentConfig = {
   [Network.MAINNET]: {
     cometContracts: [
       {
+        // Address of the Comet contract
         address: "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
+
+        // Address of the Comet base token
         baseToken: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       },
       {
