7f52c28446cc154cf454726c1d821cc91575d922
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-30 11:53:42 -0300

    docs: Add description to config fields

--- CompoundV3-Bots/Bridge-Proposal-Integrity/src/agent.config.ts
+++ CompoundV3-Bots/Bridge-Proposal-Integrity/src/agent.config.ts
@@ -3,10 +3,18 @@ import { AgentConfig } from "./utils";
 
 const CONFIG: AgentConfig = {
   [Network.POLYGON]: {
+    // Address of the BridgeReceiver contract
     bridgeReceiver: "0x18281dfC4d00905DA1aaA6731414EABa843c468A",
+
+    // Ethereum RPC URL
     rpcEndpoint: "https://eth.llamarpc.com",
-    blockChunk: 2 * 10 ** 3,
-    pastBlocks: 10 * 10 ** 3,
+
+    // Block chunk size for log fetching (i.e. X blocks per getLogs call)
+    blockChunk: 2_000,
+
+    // Maximum number of blocks checked from the current network block to find a
+    // matching proposal message
+    pastBlocks: 10_000,
   },
 };
 
