f0a028151bbc573974825e0dd809ee89415c64a7
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-05-30 11:57:28 -0300

    refac: Return early if there are no eligible logs

--- CompoundV3-Bots/Bridge-Proposal-Integrity/src/agent.ts
+++ CompoundV3-Bots/Bridge-Proposal-Integrity/src/agent.ts
@@ -40,75 +40,76 @@ export const provideHandleTransaction = (
     // Listen to ProposalCreated events on BaseBridgeReciever contract
     const proposalLogs = txEvent.filterLog(PROPOSAL_EVENT_ABI, networkManager.get("bridgeReceiver"));
 
-    if (proposalLogs.length > 0) {
-      const bridgeReceiver: ethers.Contract = new ethers.Contract(
-        networkManager.get("bridgeReceiver"),
-        [FX_CHILD_ABI, TIMELOCK_ABI],
-        provider
+    if (!proposalLogs.length) return findings;
+
+    const bridgeReceiver: ethers.Contract = new ethers.Contract(
+      networkManager.get("bridgeReceiver"),
+      [FX_CHILD_ABI, TIMELOCK_ABI],
+      provider
+    );
+    // fetch mainnet Timelock contract and FxRoot
+    const [fxChild, govTimelock] = await Promise.all([
+      bridgeReceiver.fxChild({ blockTag: txEvent.blockNumber }),
+      bridgeReceiver.govTimelock({ blockTag: txEvent.blockNumber }),
+    ]);
+
+    const fxChildContract = new ethers.Contract(fxChild, [FX_ROOT_ABI], provider);
+    const fxRoot = ethers.utils.getAddress(await fxChildContract.fxRoot({ blockTag: txEvent.blockNumber }));
+
+    for (let proposalLog of proposalLogs) {
+      const data = ethers.utils.defaultAbiCoder.encode(
+        ["address[]", "uint256[]", "string[]", "bytes[]"],
+        [proposalLog.args.targets, proposalLog.args[3], proposalLog.args.signatures, proposalLog.args.calldatas]
       );
-      // fetch mainnet Timelock contract and FxRoot
-      const [fxChild, govTimelock] = await Promise.all([
-        bridgeReceiver.fxChild({ blockTag: txEvent.blockNumber }),
-        bridgeReceiver.govTimelock({ blockTag: txEvent.blockNumber }),
+
+      // Encode call to sendMessageToChild
+      const calldata = SEND_MESSAGE_IFACE.encodeFunctionData("sendMessageToChild", [
+        networkManager.get("bridgeReceiver"),
+        data,
       ]);
 
-      const fxChildContract = new ethers.Contract(fxChild, [FX_ROOT_ABI], provider);
-      const fxRoot = ethers.utils.getAddress(await fxChildContract.fxRoot({ blockTag: txEvent.blockNumber }));
+      const timeLockContract = new ethers.Contract(govTimelock, EXECUTE_TX_IFACE, mainnetProvider);
 
-      for (let proposalLog of proposalLogs) {
-        const data = ethers.utils.defaultAbiCoder.encode(
-          ["address[]", "uint256[]", "string[]", "bytes[]"],
-          [proposalLog.args.targets, proposalLog.args[3], proposalLog.args.signatures, proposalLog.args.calldatas]
-        );
+      // ExecuteTransaction event filter
+      const eventFilter = timeLockContract.filters.ExecuteTransaction(null, fxRoot);
 
-        // Encode call to sendMessageToChild
-        const calldata = SEND_MESSAGE_IFACE.encodeFunctionData("sendMessageToChild", [
-          networkManager.get("bridgeReceiver"),
-          data,
-        ]);
+      // Retrieve past events that match the filter and have a `target` that is equal to the expected value
+      const lastBlock = await mainnetProvider.getBlockNumber();
+      const blockChunk = networkManager.get("blockChunk");
 
-        const timeLockContract = new ethers.Contract(govTimelock, EXECUTE_TX_IFACE, mainnetProvider);
+      const txExecutionLogs = (
+        await Promise.all(
+          Array.from({
+            length: Math.ceil(networkManager.get("pastBlocks") / blockChunk),
+          }).map(async (_, idx) => {
+            const fromBlock = Math.max(lastBlock - blockChunk * (idx + 1) + 1, 0);
+            return await mainnetProvider.getLogs({
+              ...eventFilter,
+              fromBlock,
+              // toBlock: Math.min(fromBlock + blockChunk - 1, lastBlock),
+              toBlock: lastBlock - blockChunk * idx,
+            });
+          })
+        )
+      ).flat();
 
-        // ExecuteTransaction event filter
-        const eventFilter = timeLockContract.filters.ExecuteTransaction(null, fxRoot);
+      let eventFound = false;
+      for (let log of txExecutionLogs) {
+        let decodedEvent = EXECUTE_TX_IFACE.decodeEventLog("ExecuteTransaction", log.data, log.topics);
 
-        // Retrieve past events that match the filter and have a `target` that is equal to the expected value
-        const lastBlock = await mainnetProvider.getBlockNumber();
-        const blockChunk = networkManager.get("blockChunk");
+        let callDataFromEvent =
+          decodedEvent.signature === "" ? decodedEvent.data : encodePacked(decodedEvent.signature, decodedEvent.data);
 
-        const txExecutionLogs = (
-          await Promise.all(
-            Array.from({
-              length: Math.ceil(networkManager.get("pastBlocks") / blockChunk),
-            }).map(async (_, idx) => {
-              const fromBlock = Math.max(lastBlock - blockChunk * (idx + 1) + 1, 0);
-              return await mainnetProvider.getLogs({
-                ...eventFilter,
-                fromBlock,
-                // toBlock: Math.min(fromBlock + blockChunk - 1, lastBlock),
-                toBlock: lastBlock - blockChunk * idx,
-              });
-            })
-          )
-        ).flat();
-
-        let eventFound = false;
-        for (let log of txExecutionLogs) {
-          let decodedEvent = EXECUTE_TX_IFACE.decodeEventLog("ExecuteTransaction", log.data, log.topics);
-
-          let callDataFromEvent =
-            decodedEvent.signature === "" ? decodedEvent.data : encodePacked(decodedEvent.signature, decodedEvent.data);
-
-          if (callDataFromEvent === calldata) {
-            eventFound = true;
-            findings.push(ProposalCreatedFinding(proposalLog, networkManager.getNetwork(), log.transactionHash));
-            break;
-          }
+        if (callDataFromEvent === calldata) {
+          eventFound = true;
+          findings.push(ProposalCreatedFinding(proposalLog, networkManager.getNetwork(), log.transactionHash));
+          break;
         }
-
-        if (!eventFound) findings.push(SuspiciousProposalCreatedFinding(proposalLog, networkManager.getNetwork()));
       }
+
+      if (!eventFound) findings.push(SuspiciousProposalCreatedFinding(proposalLog, networkManager.getNetwork()));
     }
+
     return findings;
   };
 };
