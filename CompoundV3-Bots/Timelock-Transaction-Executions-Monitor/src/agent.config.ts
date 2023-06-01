2d1cd1ca473726c04e1006c917c26539a0ee3dd3
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-06-01 19:55:44 -0300

    docs: Improve config field description

--- CompoundV3-Bots/Timelock-Transaction-Executions-Monitor/src/agent.config.ts
+++ CompoundV3-Bots/Timelock-Transaction-Executions-Monitor/src/agent.config.ts
@@ -6,7 +6,7 @@ const CONFIG: AgentConfig = {
     // Address of the bridge receiver contract
     bridgeReceiverAddress: "0x18281dfC4d00905DA1aaA6731414EABa843c468A",
     // Maximum block range to find the corresponding proposal creation event
-    // from a proposal execution
+    // from the block a proposal is executed
     creationFetchingBlockRange: 100_000,
     // Maximum amount of blocks per eth_getLogs call to find proposal creation
     // events. Typical provider limits should be noticed when setting this
