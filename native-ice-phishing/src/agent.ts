2aa14a85d9dc2838a6d5fb5e7842f0af9e03e371
Vxatz <vxatzistratis@gmail.com>
2023-06-16 12:04:26 +0300

    feat: Update soc eng logic on contracts

--- native-ice-phishing/src/agent.ts
+++ native-ice-phishing/src/agent.ts
@@ -56,6 +56,8 @@ let storedData: Data = {
   alertedHashes: [],
   alertedAddressesCritical: [],
 };
+let alertedSigsWithAddress: string[] = [];
+let alertedHashesWithAddress: string[] = [];
 
 let infoAlerts: { alerts: Alert[] } = {
   alerts: [],
@@ -190,6 +192,15 @@ export const provideInitialize = (
     );
 
     storedData.alertedHashes = alertedFuncSigs.map((sig) =>
+      ethers.utils.keccak256(ethers.utils.toUtf8Bytes(sig)).substring(0, 10)
+    );
+
+    alertedSigsWithAddress = alertedFuncSigs.map((sig) => {
+      const modifiedSig = sig.replace("()", "(address)");
+      return modifiedSig;
+    });
+
+    alertedHashesWithAddress = alertedSigsWithAddress.map((sig) =>
       ethers.utils.keccak256(ethers.utils.toUtf8Bytes(sig)).substring(0, 10)
     );
 
@@ -858,6 +869,44 @@ export const provideHandleTransaction =
         }
       }
     } else if (value !== "0x0") {
+      for (const alertedHash of alertedHashesWithAddress) {
+        if (data.startsWith(alertedHash) && !data.includes(from.slice(2))) {
+          const code = await dataFetcher.getCode(to);
+          if (!code) return findings;
+          const sourceCode = await dataFetcher.getSourceCode(to, chainId);
+          // Remove ")" from the end of the signature
+          const trimmedSigs = alertedSigsWithAddress.map((sig) =>
+            sig.slice(0, -1)
+          );
+          const suspiciousFuncsCounts = trimmedSigs.filter((sig) =>
+            sourceCode.includes(sig)
+          ).length;
+          if (suspiciousFuncsCounts >= 4) {
+            let sig;
+            try {
+              sig = await dataFetcher.getSignature(alertedHash);
+            } catch {
+              sig = alertedHash;
+            }
+
+            const anomalyScore = await calculateAlertRate(
+              Number(chainId),
+              BOT_ID,
+              "NIP-3",
+              isRelevantChain
+                ? ScanCountType.CustomScanCount
+                : ScanCountType.TxWithInputDataCount,
+              txWithInputDataCount
+            );
+
+            findings.push(
+              createLowSeverityFinding(hash, from, to, sig, anomalyScore)
+            );
+            break;
+          }
+        }
+      }
+
       for (const alertedHash of alertedHashes) {
         if (data.startsWith(alertedHash)) {
           const code = await dataFetcher.getCode(to);
