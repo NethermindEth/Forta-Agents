2aea7a1a84566cf8058cc43e4de50948bdd1418c
Tayfun Sur <tayfunsur@gmail.com>
2023-08-10 15:45:06 +0300

    chore: Add all remove liquidity functions

--- large-profit-bot/src/utils.ts
+++ large-profit-bot/src/utils.ts
@@ -201,6 +201,13 @@ export interface LargeProfitAddress {
 }
 
 export const FUNCTION_ABIS = [
+  "function burn(int24 tickLower, int24 tickUpper, uint128 amount)",
   "function remove_liquidity(uint256 _amount, uint256[2] min_amounts)",
   "function removeLiquidity(address _tokenA, address _tokenB)",
+  "function removeLiquidity(address tokenA, address tokenB, uint liquidity, uint amountAMin, uint amountBMin, address to, uint deadline)",
+  "function removeLiquidityETH(address token, uint liquidity, uint amountTokenMin, uint amountETHMin, address to, uint deadline)",
+  "function removeLiquidityWithPermit(address tokenA, address tokenB, uint liquidity, uint amountAMin, uint amountBMin, address to, uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s)",
+  "function removeLiquidityETHWithPermit(address token, uint liquidity, uint amountTokenMin, uint amountETHMin, address to, uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s)",
+  "function removeLiquidityETHSupportingFeeOnTransferTokens(address token, uint liquidity, uint amountTokenMin, uint amountETHMin, address to, uint deadline)",
+  "function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(address token, uint liquidity, uint amountTokenMin, uint amountETHMin, address to, uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s)",
 ];
