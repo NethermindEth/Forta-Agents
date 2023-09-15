8f87be00a93d00b105ec0bbbd4f70ee2139f7036
Tayfun Sur <tayfunsur@gmail.com>
2023-09-15 14:49:00 +0300

    refac: Refactor agent.ts completely

---
+++ ownership-transfer-agent/src/agent.ts
@@ -0,0 +1,37 @@
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType, ethers } from "forta-agent";
+import { isZeroAddress } from "ethereumjs-util";
+
+export const OWNERSHIP_TRANSFERRED_ABI: string =
+  "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)";
+
+export const provideHandleTransaction = (): HandleTransaction => {
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = [];
+
+    const logs = txEvent.filterLog(OWNERSHIP_TRANSFERRED_ABI);
+
+    logs.map(async (log) => {
+      if (!isZeroAddress(log.args.previousOwner)) {
+        findings.push(
+          Finding.fromObject({
+            name: "Ownership Transfer Detection",
+            description: "The ownership transfer is detected.",
+            alertId: "NETHFORTA-4",
+            severity: FindingSeverity.High,
+            type: FindingType.Suspicious,
+            metadata: {
+              from: log.args.previousOwner,
+              to: log.args.newOwner,
+            },
+          })
+        );
+      }
+    });
+
+    return findings;
+  };
+};
+
+export default {
+  handleTransaction: provideHandleTransaction(),
+};
