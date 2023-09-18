766aeeeb5152c94e71efa224cdaf51dc83bfefbb
Vxatz <vxatzistratis@gmail.com>
2023-09-18 17:36:46 +0300

    feat: Filter out failed txs & move airdrops to new alertID

--- multi-gas-threshold-agent/README.md
+++ multi-gas-threshold-agent/README.md
@@ -17,6 +17,7 @@ This bot detects transactions with unusual amount of gas used. It checks every t
 ## Alerts
 
 - NETHFORTA-1
+
   - Fired when a transaction uses more gas than the defined thresholds.
   - Severity:
     - For gas usage over `MEDIUM_GAS_THRESHOLD` is always set to "Medium".
@@ -26,6 +27,28 @@ This bot detects transactions with unusual amount of gas used. It checks every t
     - `gas`: The amount of gas used by the transaction.
     - `anomalyScore`: Score of how anomalous the alert is (0-1)
       - Score calculated by finding amount of either `MEDIUM_GAS_THRESHOLD` or `HIGH_GAS_THRESHOLD` alerts out of the total number of alerts emitted by this bot.
+  - Labels:
+    - Label 1:
+      - `entityType`: The type of the entity, always set to "Transaction"
+      - `entity`: The transaction's hash
+      - `label`: The type of the label, always set to "Suspicious"
+      - `confidence`: The confidence level of the transaction being suspicious (0-1). Always set to `0.7`
+      - `remove` Boolean indicating whether the label is removed. Always set to `false`
+    - Label 2:
+      - `entityType`: The type of the entity, always set to "Address"
+      - `entity`: The transaction initiator
+      - `label`: The type of the label, always set to "Attacker"
+      - `confidence`: The confidence level of the transaction being suspicious (0-1). Always set to `0.1`
+      - `remove` Boolean indicating whether the label is removed. Always set to `false`
+
+- NETHFORTA-1-INFO
+  - Fired when a potential airdrop transaction uses more gas than the defined thresholds.
+  - Severity is always set to "Info".
+  - Type is always set to "Info".
+  - Metadata includes:
+    - `gas`: The amount of gas used by the transaction.
+    - `anomalyScore`: Score of how anomalous the alert is (0-1)
+      - Score calculated by finding amount of Info alerts out of the total number of alerts emitted by this bot.
   - Labels:
     - Label 1:
       - `entityType`: The type of the entity, always set to "Transaction"
--- multi-gas-threshold-agent/src/agent.ts
+++ multi-gas-threshold-agent/src/agent.ts
@@ -19,13 +19,16 @@ let CHAIN_ID: string;
 
 export const MEDIUM_GAS_THRESHOLD = "4000000";
 export const HIGH_GAS_THRESHOLD = "6000000";
+const AIRDROPS_THRESHOLD = 20;
 
 const MEDIUM_GAS_KEY = "nm-medium-gas-use-bot-key";
 const HIGH_GAS_KEY = "nm-high-gas-use-bot-key";
+const INFO_GAS_KEY = "nm-info-gas-use-bot-key";
 const ALL_GAS_KEY = "nm-all-gas-use-bot-key";
 
 const DATABASE_URL = "https://research.forta.network/database/bot/";
 
+let infoHighGasAlerts = 0;
 let medHighGasAlerts = 0;
 let hiHighGasAlerts = 0;
 let allHighGasAlerts = 0;
@@ -33,7 +36,13 @@ let allHighGasAlerts = 0;
 let transactionsProcessed = 0;
 let lastBlock = 0;
 
-const getSeverity = (gasUsed: ethers.BigNumber): FindingSeverity => {
+const transferTopic = ethers.utils.id("Transfer(address,address,uint256)");
+
+const getSeverity = (gasUsed: ethers.BigNumber, isPotentialAirdrop: boolean): FindingSeverity => {
+  if (isPotentialAirdrop) {
+    infoHighGasAlerts += 1;
+    return FindingSeverity.Info;
+  }
   if (gasUsed.gte(HIGH_GAS_THRESHOLD)) {
     hiHighGasAlerts += 1;
     return FindingSeverity.High;
@@ -52,6 +61,9 @@ const getAnomalyScore = (gasSeverity: FindingSeverity) => {
   } else if (gasSeverity === FindingSeverity.Medium) {
     const medAnomalyScore = medHighGasAlerts / allHighGasAlerts;
     return medAnomalyScore.toFixed(2) === "0.00" ? medAnomalyScore.toString() : medAnomalyScore.toFixed(2);
+  } else if (gasSeverity === FindingSeverity.Info) {
+    const infoAnomalyScore = infoHighGasAlerts / allHighGasAlerts;
+    return infoAnomalyScore.toFixed(2) === "0.00" ? infoAnomalyScore.toString() : infoAnomalyScore.toFixed(2);
   } else {
     return 1;
   }
@@ -62,6 +74,7 @@ export function provideInitialize(
   persistenceHelper: PersistenceHelper,
   mediumGasKey: string,
   highGasKey: string,
+  infoGasKey: string,
   allGasKey: string
 ) {
   return async function initialize() {
@@ -69,6 +82,7 @@ export function provideInitialize(
     CHAIN_ID = chainId.toString();
     medHighGasAlerts = await persistenceHelper.load(mediumGasKey.concat("-", CHAIN_ID));
     hiHighGasAlerts = await persistenceHelper.load(highGasKey.concat("-", CHAIN_ID));
+    infoHighGasAlerts = await persistenceHelper.load(infoGasKey.concat("-", CHAIN_ID));
     allHighGasAlerts = await persistenceHelper.load(allGasKey.concat("-", CHAIN_ID));
   };
 }
@@ -95,11 +109,13 @@ export function provideHandleTransaction(
     const maxRetries = 2;
     let retryCount = 0;
     let gasUsed = ethers.BigNumber.from(0);
+    let txStatus = true;
 
     while (retryCount <= maxRetries) {
       try {
         const receipt = await getTransactionReceipt(txEvent.hash);
         gasUsed = ethers.BigNumber.from(receipt.gasUsed);
+        txStatus = receipt.status;
         break; // if we reach this point, the operation was successful and we can exit the loop
       } catch {
         console.log(`Attempt ${retryCount + 1} to fetch transaction receipt failed`);
@@ -112,43 +128,105 @@ export function provideHandleTransaction(
       }
     }
 
-    if (gasUsed.lt(MEDIUM_GAS_THRESHOLD)) {
+    if (gasUsed.lt(MEDIUM_GAS_THRESHOLD) || !txStatus) {
       return findings;
     }
 
+    // Move airdrops to a different alert ID  (Record of token address to from address to count)
+    const airdropCountByToken = new Map<string, Map<string, number>>();
+    // Loop through the logs and count the occurrences of transfers of the same token address from unique addresses
+    for (const log of txEvent.logs) {
+      if (log.topics[0] === transferTopic) {
+        const address = log.address;
+        const fromAddress = log.topics[1];
+
+        if (!airdropCountByToken.has(address)) {
+          airdropCountByToken.set(address, new Map<string, number>());
+        }
+
+        // Get the count map for the current token
+        const tokenCountMap = airdropCountByToken.get(address)!;
+
+        // Increment the count for transfers of the same token address from the same sender address
+        if (tokenCountMap.has(fromAddress)) {
+          tokenCountMap.set(fromAddress, tokenCountMap.get(fromAddress)! + 1);
+        } else {
+          tokenCountMap.set(fromAddress, 1);
+        }
+      }
+    }
+
+    const isPotentialAirdrop =
+      airdropCountByToken.size > 0 && // Check if there are any token addresses in the map
+      [...airdropCountByToken.values()].every((tokenCountMap) => {
+        return [...tokenCountMap.values()].every((count) => count >= AIRDROPS_THRESHOLD);
+      });
+
     allHighGasAlerts += 1;
-    const gasSeverity = getSeverity(gasUsed);
+    const gasSeverity = getSeverity(gasUsed, isPotentialAirdrop);
     const anomalyScore = getAnomalyScore(gasSeverity);
 
-    findings.push(
-      Finding.fromObject({
-        name: "High Gas Use Detection",
-        description: "Gas used by Transaction",
-        alertId: "NETHFORTA-1",
-        severity: gasSeverity,
-        type: FindingType.Suspicious,
-        metadata: {
-          gas: gasUsed.toString(),
-          anomalyScore: anomalyScore.toString(),
-        },
-        labels: [
-          Label.fromObject({
-            entityType: EntityType.Transaction,
-            entity: txEvent.hash,
-            label: "Suspicious",
-            confidence: 0.7,
-            remove: false,
-          }),
-          Label.fromObject({
-            entityType: EntityType.Address,
-            entity: txEvent.from,
-            label: "Attacker",
-            confidence: 0.1,
-            remove: false,
-          }),
-        ],
-      })
-    );
+    if (gasSeverity === FindingSeverity.Info) {
+      findings.push(
+        Finding.fromObject({
+          name: "High Gas Use Detection - Potential Airdrop",
+          description: "Gas used by Transaction",
+          alertId: "NETHFORTA-1-INFO",
+          severity: gasSeverity,
+          type: FindingType.Info,
+          metadata: {
+            gas: gasUsed.toString(),
+            anomalyScore: anomalyScore.toString(),
+          },
+          labels: [
+            Label.fromObject({
+              entityType: EntityType.Transaction,
+              entity: txEvent.hash,
+              label: "Suspicious",
+              confidence: 0.7,
+              remove: false,
+            }),
+            Label.fromObject({
+              entityType: EntityType.Address,
+              entity: txEvent.from,
+              label: "Attacker",
+              confidence: 0.1,
+              remove: false,
+            }),
+          ],
+        })
+      );
+    } else {
+      findings.push(
+        Finding.fromObject({
+          name: "High Gas Use Detection",
+          description: "Gas used by Transaction",
+          alertId: "NETHFORTA-1",
+          severity: gasSeverity,
+          type: FindingType.Suspicious,
+          metadata: {
+            gas: gasUsed.toString(),
+            anomalyScore: anomalyScore.toString(),
+          },
+          labels: [
+            Label.fromObject({
+              entityType: EntityType.Transaction,
+              entity: txEvent.hash,
+              label: "Suspicious",
+              confidence: 0.7,
+              remove: false,
+            }),
+            Label.fromObject({
+              entityType: EntityType.Address,
+              entity: txEvent.from,
+              label: "Attacker",
+              confidence: 0.1,
+              remove: false,
+            }),
+          ],
+        })
+      );
+    }
 
     return findings;
   };
@@ -158,6 +236,7 @@ export function provideHandleBlock(
   persistenceHelper: PersistenceHelper,
   mediumGasKey: string,
   highGasKey: string,
+  infoGasKey: string,
   allGasKey: string
 ) {
   return async (blockEvent: BlockEvent) => {
@@ -166,6 +245,7 @@ export function provideHandleBlock(
     if (blockEvent.blockNumber % 240 === 0) {
       await persistenceHelper.persist(medHighGasAlerts, mediumGasKey.concat("-", CHAIN_ID));
       await persistenceHelper.persist(hiHighGasAlerts, highGasKey.concat("-", CHAIN_ID));
+      await persistenceHelper.persist(infoHighGasAlerts, infoGasKey.concat("-", CHAIN_ID));
       await persistenceHelper.persist(allHighGasAlerts, allGasKey.concat("-", CHAIN_ID));
     }
 
@@ -179,8 +259,15 @@ export default {
     new PersistenceHelper(DATABASE_URL),
     MEDIUM_GAS_KEY,
     HIGH_GAS_KEY,
+    INFO_GAS_KEY,
     ALL_GAS_KEY
   ),
   handleTransaction: provideHandleTransaction(getTransactionReceipt),
-  handleBlock: provideHandleBlock(new PersistenceHelper(DATABASE_URL), MEDIUM_GAS_KEY, HIGH_GAS_KEY, ALL_GAS_KEY),
+  handleBlock: provideHandleBlock(
+    new PersistenceHelper(DATABASE_URL),
+    MEDIUM_GAS_KEY,
+    HIGH_GAS_KEY,
+    INFO_GAS_KEY,
+    ALL_GAS_KEY
+  ),
 };
