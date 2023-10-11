211a9568e1780093f16d0ebadd5fdd109e3b36af
Vxatz <vxatzistratis@gmail.com>
2023-10-11 17:33:29 +0300

    test: Update mocking files

--- victim-loss-identifier/src/mocks/mock.utils.ts
+++ victim-loss-identifier/src/mocks/mock.utils.ts
@@ -1,6 +1,12 @@
 import { createAddress } from "forta-agent-tools";
 import { utils, BigNumber } from "ethers";
-import { MockErc721Transfer, MockTxnReceipt, MockTxnResponse, MockExploitInfo } from "./mock.types";
+import {
+  MockErc721Transfer,
+  MockTxnReceipt,
+  MockTxnResponse,
+  MockExploitInfo,
+  MockIcePhishingTransfer,
+} from "./mock.types";
 import { TestAlertEvent } from "./mock.alert";
 
 export function createMockScammerAddressBatch(amountInBatch: number): string[] {
@@ -129,31 +135,75 @@ export function createMockErc721Transfer(exploitInfoEntry: MockExploitInfo): Moc
     contract_address: exploitInfoEntry.stolenTokenAddress,
     name: exploitInfoEntry.stolenTokenName,
     symbol: exploitInfoEntry.stolenTokenSymbol,
-    token_id: exploitInfoEntry.stolenTokenId,
+    token_id: exploitInfoEntry.stolenTokenId!,
     block_time: "block time",
     to_address: "to address",
   };
 }
 
-function createMockExploitInstance(instanceNumber: number): MockExploitInfo {
+export function createMockIcePhishingTransfer(exploitInfoEntry: MockExploitInfo): MockIcePhishingTransfer {
+  return {
+    transaction_hash: exploitInfoEntry.exploitTxnHash,
+    from_address: exploitInfoEntry.fromAddress,
+    contract_address: exploitInfoEntry.stolenTokenAddress,
+    name: exploitInfoEntry.stolenTokenName,
+    symbol: exploitInfoEntry.stolenTokenSymbol,
+    token_id: exploitInfoEntry.stolenTokenId ? exploitInfoEntry.stolenTokenId : null,
+    block_time: "block time",
+    block_number: exploitInfoEntry.blockNumber,
+    decimals: exploitInfoEntry.stolenTokenDecimals ? exploitInfoEntry.stolenTokenDecimals : null,
+    value: exploitInfoEntry.stolenTokenAmount ? exploitInfoEntry.stolenTokenAmount : null,
+    to_address: "to address",
+  };
+}
+
+export function createMockErc721ExploitInstance(instanceNumber: number): MockExploitInfo {
   return {
     exploitTxnHash: utils.hexZeroPad(`0x${instanceNumber}0`, 32),
-    fromAddress: createAddress(`0x${instanceNumber}10`),
+    fromAddress: createAddress(`0x${instanceNumber}11`),
     victimAddress: createAddress(`0x${instanceNumber}11`),
     stolenTokenAddress: createAddress(`0x${instanceNumber}1`),
     stolenTokenName: `MockErc721Token${instanceNumber}`,
     stolenTokenSymbol: `MOCK721-${instanceNumber}`,
     stolenTokenId: `${instanceNumber}3`,
+    stolenTokenDecimals: null,
+    stolenTokenAmount: null,
     txnValue: BigNumber.from(instanceNumber),
     blockNumber: 18000000 + instanceNumber,
   };
 }
 
-export function createMockExploitBatch(amountInBatch: number): MockExploitInfo[] {
+export function createMockErc20ExploitInstance(instanceNumber: number): MockExploitInfo {
+  return {
+    exploitTxnHash: utils.hexZeroPad(`0x${instanceNumber}110`, 32),
+    fromAddress: createAddress(`0x${instanceNumber}1110`),
+    victimAddress: createAddress(`0x${instanceNumber}1110`),
+    stolenTokenAddress: createAddress(`0x${instanceNumber}111`),
+    stolenTokenName: `MockIcePhishingToken${instanceNumber}`,
+    stolenTokenSymbol: `MOCK20-${instanceNumber}`,
+    stolenTokenId: null,
+    stolenTokenDecimals: 18,
+    stolenTokenAmount: `100000000000000${instanceNumber}`,
+    txnValue: BigNumber.from(instanceNumber),
+    blockNumber: 18000000 + instanceNumber,
+  };
+}
+
+export function createMockErc721ExploitBatch(amountInBatch: number): MockExploitInfo[] {
   const mockExploitBatch: MockExploitInfo[] = [];
 
   for (let i = 1; i <= amountInBatch; i++) {
-    mockExploitBatch.push(createMockExploitInstance(i));
+    mockExploitBatch.push(createMockErc721ExploitInstance(i));
+  }
+
+  return mockExploitBatch;
+}
+
+export function createMockErc20ExploitBatch(amountInBatch: number): MockExploitInfo[] {
+  const mockExploitBatch: MockExploitInfo[] = [];
+
+  for (let i = 1; i <= amountInBatch; i++) {
+    mockExploitBatch.push(createMockErc20ExploitInstance(i));
   }
 
   return mockExploitBatch;
@@ -162,12 +212,13 @@ export function createMockExploitBatch(amountInBatch: number): MockExploitInfo[]
 export function createMockAlertEvent(
   alertId: string,
   alertBlockNumber: number,
-  scammerAddress: string
+  scammerAddress: string,
+  sourceAlert?: any
 ): TestAlertEvent {
   return new TestAlertEvent(
     [],
     alertId,
-    "",
+    "0xhash",
     [],
     "",
     "",
@@ -187,6 +238,7 @@ export function createMockAlertEvent(
         hash: "",
         number: alertBlockNumber,
       },
+      sourceAlert,
     },
     {
       scammerAddresses: scammerAddress,
